9da424292212da266ac25433df13027234ff07f3
pvighi <patricio.vighi@guardian.co.uk>
2017-06-02 11:07:33 +0100

    fix to payment failure callout class to match the json we get from zuora

--- src/main/scala/com/gu/paymentFailure/PaymentFailureCallout.scala
+++ src/main/scala/com/gu/paymentFailure/PaymentFailureCallout.scala
@@ -3,15 +3,15 @@ package com.gu.paymentFailure
 import play.api.libs.json._
 
 case class PaymentFailureCallout(
-  PaymentMethodPaymentMethodType: String,
-  AccountId: String,
-  EventCategory: String,
-  PaymentMethodCreditCardType: String,
-  AccountCurrency: String,
-  EventTimestamp: String,
-  PaymentMethodExpirationDate: String,
-  PaymentErrorMessage: String
-)
+                                  PaymentMethodPaymentMethodType: String,
+                                  AccountID: String,
+                                  EventCategory: String,
+                                  PaymentMethodCreditCardType: String,
+                                  AccountCurrency: String,
+                                  EventTimestamp: String,
+                                  PaymentMethodExpirationDate: String,
+                                  PaymentErrorMessage: String
+                                )
 
 object PaymentFailureCallout {
   implicit val jf = Json.reads[PaymentFailureCallout]
