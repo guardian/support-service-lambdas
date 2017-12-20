85da4763b45a7490c945748c7044903b2ffcaf23
John Duffell <john.duffell@guardian.co.uk>
2017-12-20 11:41:48 +0000

    update email client local test

--- src/test/scala/manualTest/EmailClientSystemTest.scala
+++ src/test/scala/manualTest/EmailClientSystemTest.scala
@@ -1,7 +1,7 @@
 package manualTest
 
 import com.gu.effects.RawEffects
-import com.gu.util.ETConfig.ETSendId
+import com.gu.util.ETConfig.{ ETSendId, ETSendIds }
 import com.gu.util.exacttarget.EmailSendSteps.EmailSendStepsDeps
 import com.gu.util.exacttarget._
 import com.gu.util.{ Config, Stage }
@@ -15,20 +15,20 @@ object EmailClientSystemTest extends App {
   private val recipient = "john.duffell@guardian.co.uk"
   private def unique = "pi123" + Random.nextInt
 
-  def message(number: Int) = Message(
+  def message(hint: String, key: PrimaryKey, product: String) = Message(
     To = ToDef(
       Address = recipient,
       SubscriberKey = recipient,
       ContactAttributes = ContactAttributesDef(
         SubscriberAttributes = SubscriberAttributesDef(
           subscriber_id = "subIdValue",
-          product = "Supporter",
+          product = product,
           payment_method = "paymentMethodValue",
           card_type = "cardTypeValue",
           card_expiry_date = "cardExpiryValue",
-          first_name = s"firstNameValue$number",
+          first_name = s"$hint-firstNameValue",
           last_name = "lastNameValue",
-          primaryKey = PaymentId(s"paymentId$unique"), // must be unique otherwise the email won't arrive
+          primaryKey = key, // must be unique otherwise the email won't arrive
           price = "49.0 GBP",
           serviceStartDate = "31 January 2016",
           serviceEndDate = "31 January 2017"
@@ -37,27 +37,16 @@ object EmailClientSystemTest extends App {
     )
   )
 
-  def overdueMessage = Message(
-    To = ToDef(
-      Address = recipient,
-      SubscriberKey = recipient,
-      ContactAttributes = ContactAttributesDef(
-        SubscriberAttributes = SubscriberAttributesDef(
-          subscriber_id = "subIdValue",
-          product = "Supporter",
-          payment_method = "paymentMethodValue",
-          card_type = "cardTypeValue",
-          card_expiry_date = "cardExpiryValue",
-          first_name = s"firstNameValue overdue",
-          last_name = "lastNameValue",
-          primaryKey = InvoiceId(s"invoiceId$unique"), // must be unique otherwise the email won't arrive
-          price = "49.0 GBP",
-          serviceStartDate = "31 January 2016",
-          serviceEndDate = "31 January 2017"
-        )
-      )
+  def five(a: ETSendIds, product: String) = {
+    val p = product.replaceAll(" ", "-")
+    Seq(
+      a.pf1 -> message(s"${p}-pf1", PaymentId(s"paymentId$unique"), product),
+      a.pf2 -> message(s"${p}-pf2", PaymentId(s"paymentId$unique"), product),
+      a.pf3 -> message(s"${p}-pf3", PaymentId(s"paymentId$unique"), product),
+      a.pf4 -> message(s"${p}-pf4", PaymentId(s"paymentId$unique"), product),
+      a.cancelled -> message(s"${p}-overdue", InvoiceId(s"invoiceId$unique"), product)
     )
-  )
+  }
 
   for {
     configAttempt <- Try {
@@ -66,15 +55,24 @@ object EmailClientSystemTest extends App {
     config <- Config.parseConfig(configAttempt)
     deps = EmailSendStepsDeps.default(Stage("CODE"), RawEffects.createDefault.response, config.etConfig)
     a = config.etConfig.etSendIDs
-  } yield Seq(a.pf1 -> message(1), a.pf2 -> message(2), a.pf3 -> message(3), a.pf4 -> message(4), a.cancelled -> overdueMessage).map {
-    case (etSendId, index) =>
-      val emailResult = EmailSendSteps(
-        deps
-      )(EmailRequest(
-        etSendId = etSendId,
-        message = index
-      ))
-      println(s"result for $etSendId:::::: $emailResult")
-  }
+  } yield Seq(
+    "Supporter",
+    "Digital Pack",
+    "Guardian Weekly Zone A",
+    "Guardian Weekly Zone B",
+    "Guardian Weekly Zone C",
+    "Contributor",
+    "Newspaper Voucher",
+    "Newspaper Delivery"
+  ).flatMap(xxx => five(a, xxx)).foreach {
+      case (etSendId, index) =>
+        val emailResult = EmailSendSteps(
+          deps
+        )(EmailRequest(
+          etSendId = etSendId,
+          message = index
+        ))
+        println(s"result for $etSendId:::::: $emailResult")
+    }
 
 }
