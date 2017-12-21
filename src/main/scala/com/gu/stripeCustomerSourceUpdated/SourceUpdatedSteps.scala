f2d802dae0676795b64f02530dced54b59549c35
John Duffell <john.duffell@guardian.co.uk>
2017-12-21 11:30:28 +0000

    update description of steps

--- src/main/scala/com/gu/stripeCustomerSourceUpdated/SourceUpdatedSteps.scala
+++ src/main/scala/com/gu/stripeCustomerSourceUpdated/SourceUpdatedSteps.scala
@@ -12,7 +12,13 @@ object SourceUpdatedSteps extends Logging {
     for {
       paymentFailureCallout <- Json.fromJson[SourceUpdatedCallout](Json.parse(apiGatewayRequest.body)).toFailableOp
       _ = logger.info(s"received $paymentFailureCallout")
-      // TODO... write to zuora
+      // TODO... similar to AccountController.updateCard in members-data-api
+      // query zuora for account and payment method given the token id
+      // make sure the tokens relate to the default payment method
+      // check that the account payment gateway matches the stripe payment gateway
+      // (clear default payment method and autopay off - may not be needed)
+      // create payment method with accountid, cardid, customerid, last4, cardcountry, expiry, cardtype
+      // set payment method as the default - update account defaultpaymentmethodid
     } yield ()
   }
 
