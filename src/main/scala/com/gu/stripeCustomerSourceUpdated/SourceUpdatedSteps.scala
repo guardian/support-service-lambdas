cb548407a584fa3db53cd05d2401d8489e3032fc
John Duffell <john.duffell@guardian.co.uk>
2018-01-08 12:38:19 +0000

    add more logging to stripe update process

--- src/main/scala/com/gu/stripeCustomerSourceUpdated/SourceUpdatedSteps.scala
+++ src/main/scala/com/gu/stripeCustomerSourceUpdated/SourceUpdatedSteps.scala
@@ -1,13 +1,12 @@
 package com.gu.stripeCustomerSourceUpdated
 
 import com.gu.util._
-import com.gu.util.apigateway.{ ApiGatewayRequest, ApiGatewayResponse }
+import com.gu.util.apigateway.{ApiGatewayRequest, ApiGatewayResponse}
 import com.gu.util.reader.Types._
-import com.gu.util.zuora.CreatePaymentMethod.{ CreateStripePaymentMethod, CreditCardType }
-import com.gu.util.zuora.ZuoraModels.AccountSummary
-import com.gu.util.zuora.ZuoraQueryPaymentMethod.{ AccountId, PaymentMethodId }
+import com.gu.util.zuora.CreatePaymentMethod.{CreateStripePaymentMethod, CreditCardType}
+import com.gu.util.zuora.ZuoraQueryPaymentMethod.{AccountId, PaymentMethodId}
 import com.gu.util.zuora._
-import okhttp3.{ Request, Response }
+import okhttp3.{Request, Response}
 import play.api.libs.json.Json
 
 import scalaz._
@@ -18,33 +17,32 @@ object SourceUpdatedSteps extends Logging {
 
   def apply(deps: Deps)(apiGatewayRequest: ApiGatewayRequest): FailableOp[Unit] = {
     (for {
-      sourceUpdatedCallout <- Json.fromJson[SourceUpdatedCallout](Json.parse(apiGatewayRequest.body)).toFailableOp.pure[WithDeps].toEitherT
-      _ = logger.info(s"received source updated: $sourceUpdatedCallout")
+      sourceUpdatedCallout <- Json.fromJson[SourceUpdatedCallout](Json.parse(apiGatewayRequest.body)).toFailableOp.withLogging("fromJson SourceUpdatedCallout").pure[WithDeps].toEitherT
       accountId <- getAccountToUpdate(sourceUpdatedCallout.data.`object`.customer, sourceUpdatedCallout.data.`object`.id)
       _ <- updatePaymentMethod(accountId, sourceUpdatedCallout.data.`object`)
     } yield ()).run.run(deps.zuoraDeps)
   }
 
-  def updatePaymentMethod(accountId: AccountId, eventDataObject: EventDataObject) = {
+  def updatePaymentMethod(accountId: AccountId, eventDataObject: EventDataObject): WithDepsFailableOp[ZuoraDeps, Unit] = {
     for {
       // (clear default payment method and autopay off not needed because we are not changing the payment gateway)
       // would be nice to have to know which stripe called us, and check the gateway matches
       //...
       // create payment method with accountid, cardid, customerid, last4, cardcountry, expiry, cardtype
       // similar to ZuoraService.createPaymentMethod only in REST api
-      paymentMethod <- createPaymentMethod(eventDataObject, accountId)
+      paymentMethod <- createPaymentMethod(eventDataObject, accountId).withLogging("createPaymentMethod")
       // set payment method as the default - update account defaultpaymentmethodid
-      _ <- SetDefaultPaymentMethod.setDefaultPaymentMethod(accountId, paymentMethod.id)
+      _ <- SetDefaultPaymentMethod.setDefaultPaymentMethod(accountId, paymentMethod.id).withLogging("setDefaultPaymentMethod")
     } yield ()
   }
 
-  def getAccountToUpdate(customer: StripeCustomerId, source: StripeSourceId) = {
+  def getAccountToUpdate(customer: StripeCustomerId, source: StripeSourceId): WithDepsFailableOp[ZuoraDeps, AccountId] = {
     for { // similar to AccountController.updateCard in members-data-api
       // query zuora for account and payment method given the token id
-      paymentMethods <- ZuoraQueryPaymentMethod.getPaymentMethodForStripeCustomer(customer, source)
-      account <- Zuora.getAccountSummary(paymentMethods.accountId.value)
+      paymentMethods <- ZuoraQueryPaymentMethod.getPaymentMethodForStripeCustomer(customer, source).withLogging("getPaymentMethodForStripeCustomer")
+      account <- Zuora.getAccountSummary(paymentMethods.accountId.value).withLogging("getAccountSummary")
       // make sure the tokens relate to the default payment method
-      _ <- skipIfNotDefault(account.basicInfo.defaultPaymentMethod, paymentMethods.paymentMethodIds).pure[WithDeps].toEitherT
+      _ <- skipIfNotDefault(account.basicInfo.defaultPaymentMethod, paymentMethods.paymentMethodIds).withLogging("skipIfNotDefault").pure[WithDeps].toEitherT
       // check that the account payment gateway matches (which?) stripe payment gateway (do we even need to do this? if it's the default, it must be right)
       //...
     } yield account.basicInfo.id
