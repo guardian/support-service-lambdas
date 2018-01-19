a1da109516a17c989228622e6f038461c8db3099
John Duffell <john.duffell@guardian.co.uk>
2018-01-19 16:56:53 +0000

    make the config test assert that sig checking is on in prod

--- src/test/scala/manualTest/ConfigLoaderSystemTest.scala
+++ src/test/scala/manualTest/ConfigLoaderSystemTest.scala
@@ -1,11 +1,11 @@
 package manualTest
 
 import com.gu.effects.ConfigLoad
-import com.gu.util.{ Config, Stage }
-import org.scalatest.{ FlatSpec, Ignore, Matchers }
+import com.gu.util.{Config, Stage}
+import org.scalatest.{FlatSpec, Ignore, Matchers}
 
 import scala.io.Source
-import scala.util.{ Success, Try }
+import scala.util.{Success, Try}
 
 // this test checks the actual config in S3 so it needs credentials.  this means you can only run it manually
 // however it does stop you deploying a new version without updating the config first
@@ -14,15 +14,15 @@ class ConfigLoaderSystemTest extends FlatSpec with Matchers {
 
   "loader" should "be able to load the prod config successfully" in {
     val prod = ConfigLoad.load(Stage("PROD"))
-    validate(prod)
+    validate(prod, Some(true))
   }
 
   it should "be able to load the code config successfully" in {
     val code: Try[String] = ConfigLoad.load(Stage("CODE"))
-    validate(code)
+    validate(code, None)
   }
 
-  def validate(configAttemp: Try[String]) = {
+  def validate(configAttemp: Try[String], verificationStatus: Option[Boolean]) = {
     val con = for {
       a <- configAttemp
       b <- Config.parseConfig(a)
@@ -30,13 +30,16 @@ class ConfigLoaderSystemTest extends FlatSpec with Matchers {
     val loadedOK = con.map(config => ())
     withClue(configAttemp) {
       loadedOK should be(Success(()))
+      verificationStatus.foreach {
+        expected => con.get.stripeConfig.signatureChecking should be(expected)
+      }
     }
   }
 
   it should "be able to load the local test config successfully" in {
     val configAttempt = Try { Source.fromFile("/etc/gu/payment-failure-lambdas.private.json").mkString }
 
-    validate(configAttempt)
+    validate(configAttempt, None)
   }
 
 }
