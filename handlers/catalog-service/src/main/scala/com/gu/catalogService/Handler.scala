6bcb7481e0416589c11944c33e8438286d940e46
Jacob Winch <jacob.winch@guardian.co.uk>
2018-04-24 11:08:39 +0100

    Move implicit to top

--- handlers/catalog-service/src/main/scala/com/gu/catalogService/Handler.scala
+++ handlers/catalog-service/src/main/scala/com/gu/catalogService/Handler.scala
@@ -8,10 +8,12 @@ import com.gu.util.apigateway.LoadConfig
 import com.gu.util.apigateway.ApiGatewayHandler.LambdaIO
 import com.gu.util.zuora.{ZuoraReadCatalog, ZuoraRestConfig, ZuoraRestRequestMaker}
 import play.api.libs.json.{Json, Reads}
-
 import scalaz.{-\/, \/-}
 
 object Handler extends Logging {
+
+  case class StepsConfig(zuoraRestConfig: ZuoraRestConfig)
+  implicit val stepsConfigReads: Reads[ZuoraRestConfig] = Json.reads[ZuoraRestConfig]
 
   // this is the entry point
   // it's referenced by the cloudformation so make sure you keep it in step
@@ -41,8 +43,5 @@ object Handler extends Logging {
         }
     }.run((rawEffects.stage, rawEffects.s3Load(rawEffects.stage)))
   }
-
-  case class StepsConfig(zuoraRestConfig: ZuoraRestConfig)
-  implicit val stepsConfigReads: Reads[ZuoraRestConfig] = Json.reads[ZuoraRestConfig]
 
 }
