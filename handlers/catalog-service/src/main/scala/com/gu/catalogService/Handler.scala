7e144ec625254abaae51c03594d3774fe6e5a87a
John Duffell <john.duffell@guardian.co.uk>
2018-04-26 16:27:10 +0100

    some refactoring to the catalog lambda

--- handlers/catalog-service/src/main/scala/com/gu/catalogService/Handler.scala
+++ handlers/catalog-service/src/main/scala/com/gu/catalogService/Handler.scala
@@ -1,51 +1,46 @@
 package com.gu.catalogService
 
-import com.gu.util.Logging
 import com.gu.effects.RawEffects
 import com.gu.util.apigateway.LoadConfig
-import com.gu.util.zuora.RestRequestMaker.ClientFailableOp
+import com.gu.util.reader.Types._
 import com.gu.util.zuora.{ZuoraRestConfig, ZuoraRestRequestMaker}
-import play.api.libs.json.{JsValue, Json, Reads}
-import scalaz.{-\/, \/-}
+import com.gu.util.{Config, Logging, Stage}
+import okhttp3.{Request, Response}
+import play.api.libs.json.{Json, Reads}
+
+import scala.util.Try
 
 object Handler extends Logging {
-
-  // this is the entry point
-  // it's referenced by the cloudformation so make sure you keep it in step
-  def apply(): Unit = {
-    runWithEffects(RawEffects.createDefault)
-  }
-
-  def runWithEffects(rawEffects: RawEffects): Unit = {
-
-    def operation(fetchCatalogAttempt: ClientFailableOp[JsValue]): Unit = {
-      logger.info("Running catalog load operation")
-      fetchCatalogAttempt match {
-        case -\/(clientFail) => logger.error(s"Failure when loading the catalog: ${clientFail.message}")
-        case \/-(catalog) =>
-          logger.info(s"Successfully loaded the catalog")
-        // Upload to S3
-      }
-    }
-
-    ScheduledLambdaRunner(rawEffects, operation)
-
-  }
-
-}
-
-object ScheduledLambdaRunner {
 
   case class StepsConfig(zuoraRestConfig: ZuoraRestConfig)
   implicit val stepsConfigReads: Reads[StepsConfig] = Json.reads[StepsConfig]
 
-  def apply(rawEffects: RawEffects, operation: ClientFailableOp[JsValue] => Unit) = {
-    LoadConfig.default[StepsConfig].map { failableConfig =>
-      failableConfig.map { config =>
-        val zuoraRequests = ZuoraRestRequestMaker(rawEffects.response, config.stepsConfig.zuoraRestConfig)
-        operation(ZuoraReadCatalog(zuoraRequests))
-      }
-    }.run((rawEffects.stage, rawEffects.s3Load(rawEffects.stage)))
+  // this is the entry point
+  // it's referenced by the cloudformation so make sure you keep it in step
+  def apply(): Unit = {
+    runWithEffects(RawEffects.response, RawEffects.stage, RawEffects.s3Load, RawEffects.s3Write)
   }
 
+  // this does the wiring except side effects but not any decisions, so can be used for an end to end test
+  def runWithEffects(
+    response: Request => Response,
+    stage: Stage,
+    s3Load: Stage => Try[String],
+    s3Write: Stage => String => Try[Unit]
+  ): Unit =
+    for {
+      config <- LoadConfig.default[StepsConfig].run((stage, s3Load(stage))).withLogging("loaded config")
+      zuoraRequests = ZuoraRestRequestMaker(response, config.stepsConfig.zuoraRestConfig)
+      s3Uploader = s3Write(config.stage).andThen(_.toFailableOp("write to s3"))
+      fetchCatalogAttempt <- ZuoraReadCatalog(zuoraRequests).withLogging("loaded catalog")
+      _ <- ValidateCatalog(fetchCatalogAttempt).withLogging("catalog validation")
+      _ <- s3Uploader(fetchCatalogAttempt).withLogging("uploaded to s3")
+    } yield ()
+
+}
+
+object ValidateCatalog {
+  // testable code, makes a decision.  simplest signature (could even return a boolean?)
+  //TODO
+  def apply(catalog: String): FailableOp[Unit] = ???
 }
