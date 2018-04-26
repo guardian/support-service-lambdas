f5b0835f275bed279ab9075fe01712c15b5a873a
Jacob Winch <jacob.winch@guardian.co.uk>
2018-04-26 10:41:30 +0100

    Split wiring from (minimal) logic

--- handlers/catalog-service/src/main/scala/com/gu/catalogService/Handler.scala
+++ handlers/catalog-service/src/main/scala/com/gu/catalogService/Handler.scala
@@ -1,34 +1,26 @@
 package com.gu.catalogService
 
-import com.amazonaws.services.lambda.runtime.Context
-import com.gu.util.{Config, Logging}
-import java.io.{InputStream, OutputStream}
+import com.gu.util.Logging
 import com.gu.effects.RawEffects
 import com.gu.util.apigateway.LoadConfig
-import com.gu.util.apigateway.ApiGatewayHandler.LambdaIO
+import com.gu.util.zuora.RestRequestMaker.ClientFailableOp
 import com.gu.util.zuora.{ZuoraRestConfig, ZuoraRestRequestMaker}
-import play.api.libs.json.{Json, Reads}
+import play.api.libs.json.{JsValue, Json, Reads}
 import scalaz.{-\/, \/-}
 
 object Handler extends Logging {
 
-  case class StepsConfig(zuoraRestConfig: ZuoraRestConfig)
-  implicit val stepsConfigReads: Reads[StepsConfig] = Json.reads[StepsConfig]
-
   // this is the entry point
   // it's referenced by the cloudformation so make sure you keep it in step
-  def apply(inputStream: InputStream, outputStream: OutputStream, context: Context): Unit = {
-    logger.info(s"Starting point for Catalog Service lambda")
-    runWithEffects(RawEffects.createDefault, LambdaIO(inputStream, outputStream, context))
+  def apply(): Unit = {
+    runWithEffects(RawEffects.createDefault)
   }
 
-  def runWithEffects(rawEffects: RawEffects, lambdaIO: LambdaIO): Unit = {
+  def runWithEffects(rawEffects: RawEffects): Unit = {
 
-    def operation(config: Config[StepsConfig]) = {
+    def operation(fetchCatalogAttempt: ClientFailableOp[JsValue]): Unit = {
       logger.info("Running catalog load operation")
-      val zuoraRequests = ZuoraRestRequestMaker(rawEffects.response, config.stepsConfig.zuoraRestConfig)
-      val catalog = ZuoraReadCatalog(zuoraRequests)
-      catalog match {
+      fetchCatalogAttempt match {
         case -\/(clientFail) => logger.error(s"Failure when loading the catalog: ${clientFail.message}")
         case \/-(catalog) =>
           logger.info(s"Successfully loaded the catalog")
@@ -36,11 +28,23 @@ object Handler extends Logging {
       }
     }
 
-    LoadConfig.default[StepsConfig].map {
-      failableConfig =>
-        failableConfig.map {
-          config => operation(config)
-        }
+    ScheduledLambdaRunner(rawEffects, operation)
+
+  }
+
+}
+
+object ScheduledLambdaRunner {
+
+  case class StepsConfig(zuoraRestConfig: ZuoraRestConfig)
+  implicit val stepsConfigReads: Reads[StepsConfig] = Json.reads[StepsConfig]
+
+  def apply(rawEffects: RawEffects, operation: ClientFailableOp[JsValue] => Unit) = {
+    LoadConfig.default[StepsConfig].map { failableConfig =>
+      failableConfig.map { config =>
+        val zuoraRequests = ZuoraRestRequestMaker(rawEffects.response, config.stepsConfig.zuoraRestConfig)
+        operation(ZuoraReadCatalog(zuoraRequests))
+      }
     }.run((rawEffects.stage, rawEffects.s3Load(rawEffects.stage)))
   }
 
