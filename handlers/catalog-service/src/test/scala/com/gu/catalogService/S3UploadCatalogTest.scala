83514b01a229abc1bb28fdb12c70ca8876165571
Jacob Winch <jacob.winch@guardian.co.uk>
2018-04-30 17:16:37 +0100

    Add EffectsTest for S3 upload

---
+++ handlers/catalog-service/src/test/scala/com/gu/catalogService/S3UploadCatalogTest.scala
@@ -0,0 +1,15 @@
+package com.gu.catalogService
+
+import com.gu.effects.{LocalFile, UploadToS3}
+import com.gu.test.EffectsTest
+import com.gu.util.Stage
+import org.scalatest.FlatSpec
+
+class S3UploadCatalogEffectsTest extends FlatSpec {
+
+  "S3UploadCatalog" should "upload a file" taggedAs EffectsTest in {
+    val attempt = S3UploadCatalog(Stage("EffectsTest"), """{"catalog":"myProducts"}""", LocalFile.create, UploadToS3.putObject)
+    attempt.isRight
+  }
+
+}
