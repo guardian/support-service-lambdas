44927b8f58e05a9a6b2fbae60868f530615bdbaf
Jacob Winch <jacob.winch@guardian.co.uk>
2018-05-01 14:24:30 +0100

    Version bucket and improve test

--- handlers/catalog-service/src/test/scala/com/gu/catalogService/S3UploadCatalogTest.scala
+++ handlers/catalog-service/src/test/scala/com/gu/catalogService/S3UploadCatalogTest.scala
@@ -1,15 +1,29 @@
 package com.gu.catalogService
 
-import com.gu.effects.UploadToS3
+import java.util.Date
+import com.gu.effects.{AwsS3, UploadToS3}
 import com.gu.test.EffectsTest
-import com.gu.util.Stage
+import com.gu.util.{Logging, Stage}
 import org.scalatest.FlatSpec
+import scala.util.Try
 
-class S3UploadCatalogEffectsTest extends FlatSpec {
+class S3UploadCatalogEffectsTest extends FlatSpec with Logging {
+
+  def getLatestCatalogTimestamp: Try[Date] = Try(AwsS3.client.getObjectMetadata("gu-zuora-catalog/EffectsTest", "catalog.json").getLastModified)
 
   "S3UploadCatalog" should "upload a file" taggedAs EffectsTest in {
-    val attempt = S3UploadCatalog(Stage("EffectsTest"), """{"catalog":"myProducts"}""", UploadToS3.putObject)
-    assert(attempt.isRight)
+    val readBefore = getLatestCatalogTimestamp
+    val upload = S3UploadCatalog(Stage("EffectsTest"), """{"catalog":"myProducts"}""", UploadToS3.putObject)
+    val readAfter = getLatestCatalogTimestamp
+    val compareDates = for {
+      beforeUpload <- readBefore
+      afterUpload <- readAfter
+    } yield afterUpload.after(beforeUpload)
+
+    compareDates.fold(
+      ex => assert(false), timestampUpdated => assert(timestampUpdated)
+    )
+
   }
 
 }
