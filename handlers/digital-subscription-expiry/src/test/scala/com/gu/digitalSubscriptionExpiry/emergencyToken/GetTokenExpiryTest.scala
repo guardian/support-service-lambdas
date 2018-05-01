79653f1513c659da55644d38e95b51b969a15071
John Duffell <john.duffell@guardian.co.uk>
2018-05-01 11:49:28 +0100

    remove unnecessary duplication keep codacy happy

--- handlers/digital-subscription-expiry/src/test/scala/com/gu/digitalSubscriptionExpiry/emergencyToken/GetTokenExpiryTest.scala
+++ handlers/digital-subscription-expiry/src/test/scala/com/gu/digitalSubscriptionExpiry/emergencyToken/GetTokenExpiryTest.scala
@@ -21,31 +21,29 @@ class GetTokenExpiryTest extends FlatSpec with Matchers {
   }
   it should "read valid token in the second era" in {
 
-    val expiry = Expiry(
-      expiryDate = LocalDate.of(2018, 5, 23),
-      expiryType = ExpiryType.SUB,
-      subscriptionCode = Some(SevenDay),
-      provider = Some("G99")
-    )
-
-    val responseBody = Json.prettyPrint(Json.toJson(SuccessResponse(expiry)))
-    val expectedResponse = -\/(ApiResponse("200", new Headers, responseBody))
+    val expectedResponse: -\/[ApiResponse] =
+      expectedExpiryForDate(LocalDate.of(2018, 5, 23))
 
     getTokenExpiry("G99HXJLJHOCN").shouldBe(expectedResponse)
   }
   it should "read valid token overlapping the eras" in {
 
+    val expectedResponse: -\/[ApiResponse] =
+      expectedExpiryForDate(LocalDate.of(2018, 5, 21))
+
+    getTokenExpiry("G99DPZBLIVIIAP").shouldBe(expectedResponse)
+  }
+
+  private def expectedExpiryForDate(expiryDate: LocalDate): -\/[ApiResponse] = {
     val expiry = Expiry(
-      expiryDate = LocalDate.of(2018, 5, 21),
+      expiryDate = expiryDate,
       expiryType = ExpiryType.SUB,
       subscriptionCode = Some(SevenDay),
       provider = Some("G99")
     )
 
     val responseBody = Json.prettyPrint(Json.toJson(SuccessResponse(expiry)))
-    val expectedResponse = -\/(ApiResponse("200", new Headers, responseBody))
-
-    getTokenExpiry("G99DPZBLIVIIAP").shouldBe(expectedResponse)
+    -\/(ApiResponse("200", new Headers, responseBody))
   }
 }
 
