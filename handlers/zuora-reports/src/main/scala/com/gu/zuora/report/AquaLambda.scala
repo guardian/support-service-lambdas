23dec3e2c079bd1f93e4d43bda15d898bb101d07
pvighi <patricio.vighi@guardian.co.uk>
2018-05-29 14:04:12 +0100

    fixed error response in aqua lambda

--- handlers/zuora-reports/src/main/scala/com/gu/zuora/report/AquaLambda.scala
+++ handlers/zuora-reports/src/main/scala/com/gu/zuora/report/AquaLambda.scala
@@ -3,8 +3,7 @@ package com.gu.zuora.retention
 import java.io.{InputStream, OutputStream, OutputStreamWriter}
 
 import com.gu.util.Logging
-import com.gu.util.apigateway.ApiGatewayHandler.{LambdaIO, inputFromApiGateway}
-import com.gu.util.apigateway.ApiGatewayRequest
+import com.gu.util.apigateway.ApiGatewayHandler.LambdaIO
 import com.gu.util.config.ConfigReads.ConfigFailure
 import com.gu.util.config.{LoadConfig, Stage}
 import com.gu.util.reader.Types._
@@ -13,7 +12,8 @@ import com.gu.util.zuora.ZuoraRestConfig
 import com.gu.zuora.report.aqua.{QuerierResponse, ZuoraAquaRequestMaker}
 import okhttp3.{Request, Response}
 import play.api.libs.json.{Json, Reads, Writes}
-import scalaz.\/
+import scalaz.Scalaz._
+import scalaz._
 
 import scala.io.Source
 import scala.util.Try
@@ -24,21 +24,27 @@ object AquaLambda extends Logging {
 
   implicit val stepsConfigReads: Reads[StepsConfig] = Json.reads[StepsConfig]
 
-  def lambdaJsonResponse[RESPONSE](outputStream: OutputStream, response: RESPONSE)(implicit w: Writes[RESPONSE]): Unit = {
+  trait LambdaException extends Exception
+
+  object LambdaException {
+    def apply(message: String, root: Throwable) = new Exception(message, root) with LambdaException
+
+    def apply(message: String) = new Exception(message) with LambdaException
+  }
+
+  def parseRequest[REQUEST](inputStream: InputStream)(implicit r: Reads[REQUEST]): Try[REQUEST] = {
+    for {
+      jsonString <- Try(Source.fromInputStream(inputStream).mkString)
+      request <- Try(Json.parse(jsonString).as[REQUEST])
+    } yield request
+  }
+
+  def serializeResponse[RESPONSE](outputStream: OutputStream, response: RESPONSE)(implicit w: Writes[RESPONSE]): Unit = {
     val writer = new OutputStreamWriter(outputStream, "UTF-8")
     val jsonResponse = Json.toJson(response)
     logger.info(s"Response will be: \n ${jsonResponse.toString}")
     writer.write(Json.stringify(jsonResponse))
     writer.close()
-  }
-
-  def parseRequest[REQUEST](inputStream: InputStream)(implicit r: Reads[REQUEST]): FailableOp[REQUEST] = {
-    for {
-      jsonString <- Try(Source.fromInputStream(inputStream).mkString).toFailableOp("get data from inpu ").withLogging("lambda request")
-      request <- Json.parse(jsonString).validate[REQUEST]
-        .toFailableOp().withLogging("parsed lambda request")
-
-    } yield request
   }
 
   def apply[REQUEST](
@@ -49,14 +55,21 @@ object AquaLambda extends Logging {
     aquaCall: (Requests, REQUEST) => ClientFailableOp[QuerierResponse]
   )(implicit r: Reads[REQUEST]): Unit = {
 
-    for {
-      request <- parseRequest[REQUEST](lambdaIO.inputStream)
-      config <- LoadConfig.default[StepsConfig](implicitly)(stage, s3Load(stage))
-        .withLogging("loaded config")
-        .leftMap(_.error)
+    val lambdaResponse = for {
+      request <- parseRequest[REQUEST](lambdaIO.inputStream).toEither.disjunction
+      config <- LoadConfig.default[StepsConfig](implicitly)(stage, s3Load(stage)).withLogging("loaded config").leftMap(configError => LambdaException(configError.error))
       zuoraRequests = ZuoraAquaRequestMaker(response, config.stepsConfig.zuoraRestConfig)
-      callResponse <- aquaCall(zuoraRequests, request)
-    } yield lambdaJsonResponse(lambdaIO.outputStream, callResponse)
+      callResponse <- aquaCall(zuoraRequests, request).leftMap(error => LambdaException(error.message))
+    } yield callResponse
+
+    lambdaResponse match {
+      case -\/(exception) => {
+        logger.error("terminating lambda with error ", exception)
+        throw exception
+      }
+      case \/-(successResponse) => serializeResponse(lambdaIO.outputStream, successResponse)
+    }
+
   }
 
 }
