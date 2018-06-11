0d267a17791e6a8b2d2605c8cdab5ece3efc688a
pvighi <patricio.vighi@guardian.co.uk>
2018-06-11 12:40:38 +0100

    added proper queries to zuora retention querier

--- handlers/zuora-retention/src/main/scala/com/gu/zuora/retention/query/RetentionQueryRequest.scala
+++ handlers/zuora-retention/src/main/scala/com/gu/zuora/retention/query/RetentionQueryRequest.scala
@@ -11,30 +11,50 @@ case class RetentionQueryRequest(cutOffDate: LocalDate) //todo check if this sho
 object RetentionQueryRequest {
   implicit val reads = Json.reads[RetentionQueryRequest]
 }
+
 //TODO THIS HAS TO MOVE SOMEWHERE ELSE
 object ToAquaRequest {
   def apply(request: RetentionQueryRequest): AquaQueryRequest = {
     val dateStr = request.cutOffDate.format(DateTimeFormatter.ISO_LOCAL_DATE)
-    val statements =
-      s"""
-      SELECT
-       Account.Id, Account.CrmId, BillToContact.Id, SoldToContact.Id
-      FROM
-       Subscription
-      WHERE
-       Account.Status != 'Canceled' AND
-       (Account.ProcessingAdvice__c != 'DoNotProcess' OR Account.ProcessingAdvice__c IS NULL) AND
-       Subscription.Status = 'Cancelled'
-       AND SubscriptionEndDate <= '$dateStr'
-    """.stripMargin
 
-    val query1 = AquaQuery(
-      name = "someName",
-      query = statements
+    val exclusionQuery = AquaQuery(
+      name = "exclusionQuery",
+      query =
+        s"""
+           |SELECT
+           | Account.CrmId
+           |FROM
+           | Subscription
+           |WHERE
+           | Account.CrmId != '' AND
+           | Status != 'Expired' AND
+           | Status != 'Draft'
+           |GROUP BY
+           | Account.CrmId
+           |HAVING
+           |  MAX(Status) = 'Cancelled' AND
+           |  (MIN(Status) = 'Active' OR
+           |  SubscriptionEndDate >= '$dateStr')
+    """.stripMargin
+    )
+    val candidatesQuery = AquaQuery(
+      name = "candidatesQuery",
+      query =
+        s"""
+           |SELECT
+           |  Account.Id, Account.CrmId, BillToContact.Id, SoldToContact.Id
+           |FROM
+           |  Subscription
+           |WHERE
+           |  Account.Status != 'Canceled' AND
+           |  (Account.ProcessingAdvice__c != 'DoNotProcess' OR Account.ProcessingAdvice__c IS NULL) AND
+           |  Subscription.Status = 'Cancelled' AND
+           |  SubscriptionEndDate <= '$dateStr'
+    """.stripMargin
     )
     AquaQueryRequest(
       name = "zuora-retention",
-      queries = List(query1)
+      queries = List(candidatesQuery, exclusionQuery)
     )
   }
 }
--- handlers/zuora-retention/src/test/scala/com/gu/zuora/retention/query/RetentionQueryRequestTest.scala
+++ handlers/zuora-retention/src/test/scala/com/gu/zuora/retention/query/RetentionQueryRequestTest.scala
@@ -27,26 +27,46 @@ class RetentionQueryRequestTest extends AsyncFlatSpec {
 
   it should "convert to Aqua request " in {
     val retentionQuery = RetentionQueryRequest(LocalDate.of(2012, 12, 31))
-    val statements =
-      s"""
-      SELECT
-       Account.Id, Account.CrmId, BillToContact.Id, SoldToContact.Id
-      FROM
-       Subscription
-      WHERE
-       Account.Status != 'Canceled' AND
-       (Account.ProcessingAdvice__c != 'DoNotProcess' OR Account.ProcessingAdvice__c IS NULL) AND
-       Subscription.Status = 'Cancelled'
-       AND SubscriptionEndDate <= '2012-12-31'
-    """.stripMargin
 
-    val query1 = AquaQuery(
-      name = "someName",
-      query = statements
+    val expectedExclusionQuery = AquaQuery(
+      name = "exclusionQuery",
+      query =  s"""
+                  |SELECT
+                  | Account.CrmId
+                  |FROM
+                  | Subscription
+                  |WHERE
+                  | Account.CrmId != '' AND
+                  | Status != 'Expired' AND
+                  | Status != 'Draft'
+                  |GROUP BY
+                  | Account.CrmId
+                  |HAVING
+                  |  MAX(Status) = 'Cancelled' AND
+                  |  (MIN(Status) = 'Active' OR
+                  |  SubscriptionEndDate >= '2012-12-31')
+    """.stripMargin
     )
+
+    val expectedCandidatesQuery = AquaQuery(
+      name = "candidatesQuery",
+      query =
+        s"""
+           |SELECT
+           |  Account.Id, Account.CrmId, BillToContact.Id, SoldToContact.Id
+           |FROM
+           |  Subscription
+           |WHERE
+           |  Account.Status != 'Canceled' AND
+           |  (Account.ProcessingAdvice__c != 'DoNotProcess' OR Account.ProcessingAdvice__c IS NULL) AND
+           |  Subscription.Status = 'Cancelled' AND
+           |  SubscriptionEndDate <= '2012-12-31'
+    """.stripMargin
+    )
+
     val expected = AquaQueryRequest(
       name = "zuora-retention",
-      queries = List(query1)
+      queries = List(expectedCandidatesQuery, expectedExclusionQuery)
     )
 
     ToAquaRequest(retentionQuery) shouldBe expected
