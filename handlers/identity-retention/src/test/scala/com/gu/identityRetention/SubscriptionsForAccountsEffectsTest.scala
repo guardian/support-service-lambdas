2dedacffaa10729f9bb605afea2b3a47681fcb25
John Duffell <john.duffell@guardian.co.uk>
2018-06-13 12:54:43 +0100

    more test cleanup

--- handlers/identity-retention/src/test/scala/com/gu/identityRetention/SubscriptionsForAccountsEffectsTest.scala
+++ handlers/identity-retention/src/test/scala/com/gu/identityRetention/SubscriptionsForAccountsEffectsTest.scala
@@ -16,23 +16,25 @@ class SubscriptionsForAccountsEffectsTest extends FlatSpec with Matchers {
 
   it should "successfull query multiple accounts" taggedAs EffectsTest in {
 
-    val actual = for {
-      configAttempt <- S3ConfigLoad.load(Stage("DEV")).toEither.disjunction
-      config <- LoadConfig.parseConfig[StepsConfig](configAttempt)
-      zuoraQuerier = ZuoraQuery(ZuoraRestRequestMaker(RawEffects.response, config.stepsConfig.zuoraRestConfig))
-      subsForAccounts = SubscriptionsForAccounts(zuoraQuerier)
-      subs <- subsForAccounts(List(
-        AccountId("2c92c0f86371efdc0163871a9ad72274"),
-        AccountId("2c92c0f86371f0360163871d94eb0e68")
-      )).toDisjunction
-    } yield {
-      subs
-    }
+    val testAccountIds = List(
+      AccountId("2c92c0f86371efdc0163871a9ad72274"),
+      AccountId("2c92c0f86371f0360163871d94eb0e68")
+    )
 
     val expectedEndDates = List(
       LocalDate.of(2019, 5, 21),
       LocalDate.of(2018, 4, 4)
     )
+
+    val actual = for {
+      configAttempt <- S3ConfigLoad.load(Stage("DEV")).toEither.disjunction
+      config <- LoadConfig.parseConfig[StepsConfig](configAttempt)
+      zuoraQuerier = ZuoraQuery(ZuoraRestRequestMaker(RawEffects.response, config.stepsConfig.zuoraRestConfig))
+      subsForAccounts = SubscriptionsForAccounts(zuoraQuerier)
+      subs <- subsForAccounts(testAccountIds).toDisjunction
+    } yield {
+      subs
+    }
     actual.map(_.map(_.TermEndDate)) should be(\/-(expectedEndDates))
 
   }
