4249a7955a83ed0ab8c05cc7fffeba9cb29ceebb
pvighi <patricio.vighi@guardian.co.uk>
2018-06-19 15:39:42 +0100

    added filter effects test

---
+++ handlers/zuora-retention/src/test/scala/com/gu/zuora/retention/FilterCandidatesEffectsTest.scala
@@ -0,0 +1,36 @@
+package com.gu.zuora.retention
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+
+import com.gu.test.EffectsTest
+import org.scalatest.{FlatSpec, Matchers}
+
+class FilterCandidatesEffectsTest extends FlatSpec with Matchers {
+  it should "filter candidates with exclusion results" taggedAs EffectsTest in {
+    val input =
+      """
+    |{
+    |"jobId" : "testJobId",
+    |"fetched": [
+    |    {
+    |      "fileId": "someId",
+    |      "uri": "s3://zuora-retention-dev/effectsTest/exclusionQuery.csv",
+    |      "name": "exclusionQuery"
+    |    },
+    |    {
+    |      "fileId": "someId",
+    |      "uri": "s3://zuora-retention-dev/effectsTest/candidatesQuery.csv",
+    |      "name" : "candidatesQuery"
+    |    }
+    |  ],
+    |"dryRun" : false
+    |  }""".stripMargin
+
+    val testInputStream = new ByteArrayInputStream(input.getBytes)
+    val testOutputStream = new ByteArrayOutputStream()
+
+    FilterCandidates(testInputStream, testOutputStream, null)
+
+    testOutputStream.toString shouldBe "{\"jobId\":\"testJobId\",\"uri\":\"s3://zuora-retention-dev/testJobId/doNoProcess.csv\",\"dryRun\":false}"
+  }
+}
