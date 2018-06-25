d83007e4253d72796134e0107907989c346ddd2a
Tom Richards <tom.richards@guardian.co.uk>
2018-06-25 19:03:35 +0100

    added identity id extraction from identity cookie

--- handlers/cancellation-sf-cases/src/main/scala/com/gu/identity/IdentityCookieToIdentityId.scala
+++ handlers/cancellation-sf-cases/src/main/scala/com/gu/identity/IdentityCookieToIdentityId.scala
@@ -1,30 +1,45 @@
 package com.gu.identity
 
+import com.gu.identity.cookie.{IdentityCookieDecoder, PreProductionKeys}
 import com.gu.util.Logging
 import com.gu.util.apigateway.ApiGatewayResponse
-import com.gu.util.reader.Types.ApiGatewayOp
 import com.gu.util.reader.Types.ApiGatewayOp.{ContinueProcessing, ReturnWithResponse}
+import com.gu.util.reader.Types._
 
 object IdentityCookieToIdentityId extends Logging {
 
-  lazy val unauthorized = ReturnWithResponse(ApiGatewayResponse.unauthorized)
+  lazy val badRequest = ReturnWithResponse(ApiGatewayResponse.badRequest)
 
   def apply(headersOption: Option[Map[String, String]]): ApiGatewayOp[String] = headersOption match {
     case Some(headers) => processHeaders(headers)
     case None => {
       logger.debug("no headers")
-      unauthorized
+      badRequest
     }
   }
 
-  def processHeaders(headers: Map[String, String]): ApiGatewayOp[String] = headers get "Cookie" match {
-    case Some(cookie) => processCookie(cookie)
-    case None => {
-      logger.debug("no cookie")
-      unauthorized
+  private def processHeaders(headers: Map[String, String]): ApiGatewayOp[String] =
+    headers get "Cookie" match {
+      case Some(cookie) => processCookie(cookie, new IdentityCookieDecoder(new PreProductionKeys)) // TODO review keys selection
+      case None => {
+        logger.debug("no cookie")
+        badRequest
+      }
     }
-  }
 
-  def processCookie(cookie: String): ApiGatewayOp[String] = ContinueProcessing("100496341")
+  private def processCookie(cookieHeaderValue: String, cookieDecoder: IdentityCookieDecoder) =
+    for {
+      scGuU <- extractCookieHeaderValue(cookieHeaderValue, "SC_GU_U")
+      userFromScGuU <- cookieDecoder.getUserDataForScGuU(scGuU).toApiGatewayContinueProcessing(ApiGatewayResponse.unauthorized)
+    } yield userFromScGuU.getId
+
+  private def extractCookieHeaderValue(cookieHeaderValue: String, cookieName: String): ApiGatewayOp[String] =
+    cookieHeaderValue split ";" find (keyValue => keyValue.trim startsWith (cookieName + '=')) match {
+      case Some(keyValue) => ContinueProcessing(keyValue.substring(keyValue.indexOf('=') + 1))
+      case None => {
+        logger.debug(cookieName + " cookie is missing")
+        badRequest
+      }
+    }
 
 }
