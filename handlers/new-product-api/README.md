98c50400e99b0c3f7992656b2c745bfd6573dbe6
pvighi <patricio.vighi@guardian.co.uk>
2018-07-10 17:36:28 +0100

    [WIP] add new product api

---
+++ handlers/new-product-api/README.md
@@ -0,0 +1,1 @@
+# add-product-api
---
+++ handlers/new-product-api/src/main/scala/com/gu/newProductApi/addSubscription/Handler.scala
@@ -0,0 +1,39 @@
+package com.gu.newProductApi.addSubscription
+
+import java.io.{InputStream, OutputStream}
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.gu.effects.{GetFromS3, RawEffects}
+import com.gu.util.apigateway.{ApiGatewayHandler, ApiGatewayRequest, ApiGatewayResponse}
+import com.gu.util.apigateway.ApiGatewayHandler.{LambdaIO, Operation}
+import com.gu.util.config.{LoadConfigModule, Stage, TrustedApiConfig}
+import com.gu.util.config.LoadConfigModule.StringFromS3
+import com.gu.util.zuora.ZuoraRestConfig
+import okhttp3.{Request, Response}
+import com.gu.util.reader.Types._
+
+object Handler {
+
+  // Referenced in Cloudformation
+  def apply(inputStream: InputStream, outputStream: OutputStream, context: Context): Unit = {
+    runWithEffects(RawEffects.response, RawEffects.stage, GetFromS3.fetchString, LambdaIO(inputStream, outputStream, context))
+  }
+
+  def runWithEffects(response: Request => Response, stage: Stage, fetchString: StringFromS3, lambdaIO: LambdaIO) = {
+
+    def operation: ZuoraRestConfig => Operation = zuoraRestConfig => {
+      //  val zuoraRequests = ZuoraRestRequestMaker(response, zuoraRestConfig)
+      // val zuoraQuerier = ZuoraQuery(zuoraRequests)
+      Operation.noHealthcheck({ apiGatewayRequest: ApiGatewayRequest => ApiGatewayResponse.successfulExecution })
+    }
+
+    val loadConfig = LoadConfigModule(stage, fetchString)
+    ApiGatewayHandler(lambdaIO)(for {
+      zuoraConfig <- loadConfig[ZuoraRestConfig].toApiGatewayOp("load zuora config")
+      trustedApiConfig <- loadConfig[TrustedApiConfig].toApiGatewayOp("load trusted api config")
+      configuredOp = operation(zuoraConfig)
+    } yield (trustedApiConfig, configuredOp))
+
+  }
+
+}
