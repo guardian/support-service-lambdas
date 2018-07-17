3c0c75b873fe0b018def43e81522125c08399375
John Duffell <john.duffell@guardian.co.uk>
2018-07-17 11:30:39 +0100

    add data types and ids for contributions plan ids

--- handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/Handler.scala
+++ handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/Handler.scala
@@ -46,4 +46,37 @@ object Handler extends Logging {
 
   }
 
+  case class ProductRatePlanId(value: String) extends AnyVal
+  case class ProductRatePlanChargeId(value: String) extends AnyVal
+  case class PlanAndCharge(productRatePlanId: ProductRatePlanId, productRatePlanChargeId: ProductRatePlanChargeId)
+  case class ContributionsZuoraIds(monthly: PlanAndCharge, annual: PlanAndCharge)
+
+  def zuoraIdsForStage(stage: Stage): ApiGatewayOp[ContributionsZuoraIds] = {
+    val mappings = Map(
+      // todo ideally we should add an id to the fields in zuora so we don't have to hard code
+      Stage("PROD") -> ContributionsZuoraIds(
+        monthly = PlanAndCharge(
+          productRatePlanId = ProductRatePlanId("2c92c0f85ab269be015acd9d014549b7"),
+          productRatePlanChargeId = ProductRatePlanChargeId("2c92c0f85ab2696b015acd9eeb6150ab")
+        ),
+        annual = PlanAndCharge(
+          productRatePlanId = ProductRatePlanId("2c92c0f95e1d5c9c015e38f8c87d19a1"),
+          productRatePlanChargeId = ProductRatePlanChargeId("2c92c0f95e1d5c9c015e38f8c8ac19a3")
+        )
+      ),
+      Stage("CODE") -> ContributionsZuoraIds(
+        monthly = PlanAndCharge(
+          productRatePlanId = ProductRatePlanId("2c92c0f85ab269be015acd9d014549b7"),
+          productRatePlanChargeId = ProductRatePlanChargeId("2c92c0f85ab2696b015acd9eeb6150ab")
+        ),
+        annual = PlanAndCharge(
+          productRatePlanId = ProductRatePlanId("2c92c0f95e1d5c9c015e38f8c87d19a1"),
+          productRatePlanChargeId = ProductRatePlanChargeId("2c92c0f95e1d5c9c015e38f8c8ac19a3")
+        )
+      )
+      // probably don't need dev as we'd just pass in the actual object in the test
+    )
+    mappings.get(stage).toApiGatewayContinueProcessing(ApiGatewayResponse.internalServerError(s"missing zuora ids for stage $stage"))
+  }
+
 }
