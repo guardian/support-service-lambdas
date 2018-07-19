3306ccc88fa87c72a603a79dac35fca4531b9ebe
John Duffell <john.duffell@guardian.co.uk>
2018-07-19 09:50:02 +0100

    merge master - things that git missed, plus fix warning in test

--- handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/Handler.scala
+++ handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/Handler.scala
@@ -4,24 +4,25 @@ import java.io.{InputStream, OutputStream}
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.gu.effects.{GetFromS3, RawEffects}
-import com.gu.newproduct.api.addsubscription.zuora.{CreateSubscription, GetAccount, GetAccountSubscriptions, GetPaymentMethodStatus}
-import com.gu.newproduct.api.addsubscription.zuora.CreateSubscription.{CreateReq, SubscriptionName}
-import com.gu.util.Logging
-import com.gu.util.apigateway.{ApiGatewayHandler, ApiGatewayRequest, ApiGatewayResponse}
-import com.gu.util.apigateway.ApiGatewayHandler.{LambdaIO, Operation}
-import com.gu.util.apigateway.ResponseModels.ApiResponse
-import com.gu.util.config.{LoadConfigModule, Stage, TrustedApiConfig}
-import com.gu.util.config.LoadConfigModule.StringFromS3
-import com.gu.util.zuora.{ZuoraRestConfig, ZuoraRestRequestMaker}
-import okhttp3.{Request, Response}
-import com.gu.util.reader.Types._
-import com.gu.util.resthttp.Types.ClientFailableOp
-import TypeConvert._
+import com.gu.newproduct.api.addsubscription.TypeConvert._
+import com.gu.newproduct.api.addsubscription.validation.{PrerequisiteCheck, ValidateAccount, ValidatePaymentMethod, ValidateSubscriptions}
 import com.gu.newproduct.api.addsubscription.zuora.CreateSubscription.WireModel.{WireCreateRequest, WireSubscription}
+import com.gu.newproduct.api.addsubscription.zuora.CreateSubscription.{CreateReq, SubscriptionName}
 import com.gu.newproduct.api.addsubscription.zuora.GetAccount.WireModel.ZuoraAccount
 import com.gu.newproduct.api.addsubscription.zuora.GetAccountSubscriptions.WireModel.ZuoraSubscriptionsResponse
 import com.gu.newproduct.api.addsubscription.zuora.GetPaymentMethodStatus.PaymentMethodWire
+import com.gu.newproduct.api.addsubscription.zuora.{CreateSubscription, GetAccount, GetAccountSubscriptions, GetPaymentMethodStatus}
+import com.gu.util.Logging
+import com.gu.util.apigateway.ApiGatewayHandler.{LambdaIO, Operation}
+import com.gu.util.apigateway.ResponseModels.ApiResponse
+import com.gu.util.apigateway.{ApiGatewayHandler, ApiGatewayRequest, ApiGatewayResponse}
+import com.gu.util.config.LoadConfigModule.StringFromS3
+import com.gu.util.config.{LoadConfigModule, Stage, TrustedApiConfig}
+import com.gu.util.reader.Types._
 import com.gu.util.resthttp.RestRequestMaker
+import com.gu.util.resthttp.Types.ClientFailableOp
+import com.gu.util.zuora.{ZuoraRestConfig, ZuoraRestRequestMaker}
+import okhttp3.{Request, Response}
 
 object Handler extends Logging {
 
@@ -66,11 +67,13 @@ object Steps {
     zuoraIds: ZuoraIds.ContributionsZuoraIds,
     zuoraClient: RestRequestMaker.Requests
   ): ZuoraAccountId => ApiGatewayOp[Unit] =
-    PrerequesiteCheck(
-      GetAccount(zuoraClient.get[ZuoraAccount]),
-      GetPaymentMethodStatus(zuoraClient.get[PaymentMethodWire]),
-      GetAccountSubscriptions(zuoraClient.get[ZuoraSubscriptionsResponse]),
-      List(zuoraIds.monthly.productRatePlanId, zuoraIds.annual.productRatePlanId)
+    PrerequisiteCheck(
+      ValidateAccount(GetAccount(zuoraClient.get[ZuoraAccount])),
+      ValidatePaymentMethod(GetPaymentMethodStatus(zuoraClient.get[PaymentMethodWire])),
+      ValidateSubscriptions(
+        GetAccountSubscriptions(zuoraClient.get[ZuoraSubscriptionsResponse]),
+        List(zuoraIds.monthly.productRatePlanId, zuoraIds.annual.productRatePlanId)
+      )
     )
 
 }
