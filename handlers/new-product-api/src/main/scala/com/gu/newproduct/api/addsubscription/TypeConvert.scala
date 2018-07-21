e8e1aa67c0b92a143d181d310e4f451569ba2d0c
Patricio Vighi <patricio.vighi@guardian.co.uk>
2018-07-21 18:56:52 +0100

    added some test for type conversions

--- handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/TypeConvert.scala
+++ handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/TypeConvert.scala
@@ -1,12 +1,34 @@
 package com.gu.newproduct.api.addsubscription
 
+import com.gu.newproduct.api.addsubscription.validation.{Failed, Passed, ValidationResult}
+import com.gu.util.apigateway.ApiGatewayResponse
+import com.gu.util.apigateway.ResponseModels.ApiResponse
+import com.gu.util.reader.Types.ApiGatewayOp.{ContinueProcessing, ReturnWithResponse}
 import com.gu.util.reader.Types._
-import com.gu.util.resthttp.Types.ClientFailableOp
+import com.gu.util.resthttp.Types.{ClientFailableOp, NotFound}
 
 object TypeConvert {
 
   implicit class TypeConvertClientOp[A](theEither: ClientFailableOp[A]) {
     def toApiGatewayOp = theEither.toDisjunction.toApiGatewayOp(_)
+  }
+
+  implicit class ValidationToApiGatewayOp[A](validationResult: ValidationResult[A]) {
+    def toApiGatewayOp: ApiGatewayOp[A] = validationResult match {
+      case Passed(value) => ContinueProcessing(value)
+      case Failed(message) => ReturnWithResponse(ApiValidationErrorResponse(message))
+    }
+  }
+
+  private def ApiValidationErrorResponse[A](message: String): ApiResponse = {
+    ApiGatewayResponse.messageResponse("422", message)
+  }
+
+  implicit class ClientFailableOpToApiResponse[A](clientFailableOp: ClientFailableOp[A]) {
+    def toApiResponseCheckingNotFound(action: String, ifNotFoundReturn: String): ApiGatewayOp[A] = clientFailableOp match {
+      case NotFound(_) => ReturnWithResponse(ApiValidationErrorResponse(ifNotFoundReturn))
+      case x => x.toDisjunction.toApiGatewayOp(action)
+    }
   }
 
 }
--- handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/validation/Validation.scala
+++ handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/validation/Validation.scala
@@ -1,11 +1,5 @@
 package com.gu.newproduct.api.addsubscription.validation
 
-import com.gu.util.apigateway.ApiGatewayResponse
-import com.gu.util.apigateway.ResponseModels.ApiResponse
-import com.gu.util.reader.Types.ApiGatewayOp.{ContinueProcessing, ReturnWithResponse}
-import com.gu.util.reader.Types.{ApiGatewayOp, _}
-import com.gu.util.resthttp.Types.{ClientFailableOp, NotFound}
-
 object Validation {
 
   implicit class BooleanValidation(isValid: Boolean) {
@@ -17,24 +11,6 @@ object Validation {
     def getOrReturn(errorMsg: String): ValidationResult[A] = option match {
       case Some(value) => Passed(value)
       case None => Failed(errorMsg)
-    }
-  }
-
-  implicit class ValidationToApiGatewayOp[A](validationResult: ValidationResult[A]) {
-    def toApiGatewayOp: ApiGatewayOp[A] = validationResult match {
-      case Passed(value) => ContinueProcessing(value)
-      case Failed(message) => ReturnWithResponse(ApiValidationErrorResponse(message))
-    }
-  }
-
-  def ApiValidationErrorResponse[A](message: String): ApiResponse = {
-    ApiGatewayResponse.messageResponse("422", message)
-  }
-
-  implicit class ClientFailableOpToApiResponse[A](clientFailableOp: ClientFailableOp[A]) {
-    def toApiResponseCheckingNotFound(action: String, ifNotFoundReturn: String): ApiGatewayOp[A] = clientFailableOp match {
-      case NotFound(_) => ReturnWithResponse(ApiValidationErrorResponse(ifNotFoundReturn))
-      case x => x.toDisjunction.toApiGatewayOp(action)
     }
   }
 
