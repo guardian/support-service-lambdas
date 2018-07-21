c5cb7ffc04aa20abd36bc88816c0cee170f827a2
Patricio Vighi <patricio.vighi@guardian.co.uk>
2018-07-21 18:59:57 +0100

    sbt code reformat

--- handlers/new-product-api/src/test/scala/com/gu/newproduct/api/addsubscription/TypeConvertTest.scala
+++ handlers/new-product-api/src/test/scala/com/gu/newproduct/api/addsubscription/TypeConvertTest.scala
@@ -8,7 +8,7 @@ import com.gu.util.reader.Types.ApiGatewayOp.{ContinueProcessing, ReturnWithResp
 import com.gu.util.resthttp.Types.{ClientSuccess, GenericError, NotFound}
 class TypeConvertTest extends FlatSpec with Matchers {
 
-  def api422Response(message:String) = ReturnWithResponse(ApiGatewayResponse.messageResponse(statusCode = "422", message = message))
+  def api422Response(message: String) = ReturnWithResponse(ApiGatewayResponse.messageResponse(statusCode = "422", message = message))
   "ValidationToApiGatewayOp" should "convert failed validation to status 422 api response" in {
     val failure = Failed("validation error")
     ValidationToApiGatewayOp(failure).toApiGatewayOp shouldBe api422Response("validation error")
@@ -21,19 +21,19 @@ class TypeConvertTest extends FlatSpec with Matchers {
 
   "toApiResponseCheckingNotFound" should "convert not found client error to status 422 api response" in {
     val notFound = NotFound("server specific error")
-    val actual = ClientFailableOpToApiResponse(notFound).toApiResponseCheckingNotFound(action ="some action", ifNotFoundReturn = "validation message")
+    val actual = ClientFailableOpToApiResponse(notFound).toApiResponseCheckingNotFound(action = "some action", ifNotFoundReturn = "validation message")
     actual shouldBe api422Response("validation message")
   }
 
   it should "convert generic errors to 500 status response" in {
     val genericError = GenericError("fatal error")
-    val actual = ClientFailableOpToApiResponse(genericError).toApiResponseCheckingNotFound(action ="some action", ifNotFoundReturn = "validation message")
+    val actual = ClientFailableOpToApiResponse(genericError).toApiResponseCheckingNotFound(action = "some action", ifNotFoundReturn = "validation message")
     actual shouldBe ReturnWithResponse(ApiGatewayResponse.internalServerError(error = "ignored log message"))
   }
 
   it should "convert client success to continue processing" in {
     val success = ClientSuccess("something")
-    val actual = ClientFailableOpToApiResponse(success).toApiResponseCheckingNotFound(action ="some action", ifNotFoundReturn = "validation message")
+    val actual = ClientFailableOpToApiResponse(success).toApiResponseCheckingNotFound(action = "some action", ifNotFoundReturn = "validation message")
     actual shouldBe ContinueProcessing("something")
   }
 }
