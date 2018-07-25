78e3ea4be49adcb7bb8ef4c93bcf7892d3c56f86
pvighi <patricio.vighi@guardian.co.uk>
2018-07-25 17:35:29 +0100

    Added additional fields to payment method needed for thank you email

--- handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/zuora/GetPaymentMethod.scala
+++ handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/zuora/GetPaymentMethod.scala
@@ -8,7 +8,15 @@ import com.gu.util.resthttp.Types._
 import play.api.libs.json._
 
 object GetPaymentMethod {
-  case class PaymentMethodWire(PaymentMethodStatus: String, Type: String) {
+
+  case class PaymentMethodWire(
+    PaymentMethodStatus: String,
+    Type: String,
+    MandateID: Option[String] = None,
+    BankTransferAccountName: Option[String] = None,
+    BankTransferAccountNumberMask: Option[String] = None,
+    BankCode: Option[String] = None
+  ) {
 
     val stringToType = Map(
       "PayPal" -> PayPal,
@@ -17,19 +25,65 @@ object GetPaymentMethod {
       "BankTransfer" -> BankTransfer
     )
 
-    def toPaymentMethod: PaymentMethod =
-      PaymentMethod(
-        status = if (PaymentMethodStatus == "Active") ActivePaymentMethod else NotActivePaymentMethod,
-        paymentMethodType = stringToType.getOrElse(Type, Other)
-      )
+    private def toStatus(statusString: String) = if (statusString == "Active") ActivePaymentMethod else NotActivePaymentMethod
+
+    private def toDirectDebit(paymentMethodWire: PaymentMethodWire): ClientFailableOp[DirectDebit] = for {
+      mandateId <- paymentMethodWire.MandateID.toClientFailable("no MandateID in zuora direct debit")
+      accountName <- paymentMethodWire.BankTransferAccountName.toClientFailable("no account name in zuora direct debit")
+      accountNumberMask <- paymentMethodWire.BankTransferAccountNumberMask.toClientFailable("no account number mask in zuora direct debit")
+      sortCode <- paymentMethodWire.BankCode.toClientFailable("no bank code in zuora direct debit")
+    } yield DirectDebit(
+      toStatus(paymentMethodWire.PaymentMethodStatus),
+      BankAccountName(accountName),
+      BankAccountNumberMask(accountNumberMask),
+      SortCode(sortCode),
+      MandateId(mandateId)
+    )
+
+    def toPaymentMethod: ClientFailableOp[PaymentMethod] = {
+      val methodType = stringToType.getOrElse(Type, Other)
+      if (methodType == BankTransfer) toDirectDebit(this)
+      else ClientSuccess(NonDirectDebitMethod(toStatus(PaymentMethodStatus), methodType))
+    }
+  }
+
+  implicit class OptionToClientFailableOp[A](option: Option[A]) {
+    def toClientFailable(errorMessage: String) = option match {
+      case None => GenericError(errorMessage)
+      case Some(value) => ClientSuccess(value)
+    }
   }
 
   implicit val wireReads = Json.reads[PaymentMethodWire]
 
-  case class PaymentMethod(status: PaymentMethodStatus, paymentMethodType: PaymentMethodType)
+  sealed trait PaymentMethod {
+    def status: PaymentMethodStatus
+
+    def paymentMethodType: PaymentMethodType
+  }
+
+  case class BankAccountName(value: String) extends AnyVal
+
+  case class BankAccountNumberMask(value: String) extends AnyVal
+
+  case class SortCode(value: String) extends AnyVal
+
+  case class MandateId(value: String) extends AnyVal
+
+  case class DirectDebit(
+    status: PaymentMethodStatus,
+    accountName: BankAccountName,
+    number: BankAccountNumberMask,
+    sortCode: SortCode,
+    mandateId: MandateId
+  ) extends PaymentMethod {
+    val paymentMethodType = BankTransfer
+  }
+
+  case class NonDirectDebitMethod(status: PaymentMethodStatus, paymentMethodType: PaymentMethodType) extends PaymentMethod
 
   def apply(get: RequestsGet[PaymentMethodWire])(paymentMethodId: PaymentMethodId): ClientFailableOp[PaymentMethod] =
-    get(s"object/payment-method/${paymentMethodId.value}", WithoutCheck).map(_.toPaymentMethod)
+    get(s"object/payment-method/${paymentMethodId.value}", WithoutCheck).flatMap(_.toPaymentMethod)
 }
 
 sealed trait PaymentMethodType
--- handlers/new-product-api/src/test/scala/com/gu/newproduct/api/addsubscription/validation/ValidatePaymentMethodTest.scala
+++ handlers/new-product-api/src/test/scala/com/gu/newproduct/api/addsubscription/validation/ValidatePaymentMethodTest.scala
@@ -1,7 +1,7 @@
 package com.gu.newproduct.api.addsubscription.validation
 
 import com.gu.newproduct.api.addsubscription.zuora.GetAccount._
-import com.gu.newproduct.api.addsubscription.zuora.GetPaymentMethod.PaymentMethod
+import com.gu.newproduct.api.addsubscription.zuora.GetPaymentMethod.{NonDirectDebitMethod, PaymentMethod}
 import com.gu.newproduct.api.addsubscription.zuora.PaymentMethodStatus.{ActivePaymentMethod, NotActivePaymentMethod}
 import com.gu.newproduct.api.addsubscription.zuora.PaymentMethodType
 import com.gu.newproduct.api.addsubscription.zuora.PaymentMethodType.{CreditCard, Other}
@@ -22,14 +22,14 @@ class ValidatePaymentMethodTest extends FlatSpec with Matchers {
 
   val paymentMethodId = PaymentMethodId("paymentMethodId")
   it should "fail if payment method is not active" in {
-    val paymentMethod = PaymentMethod(NotActivePaymentMethod, CreditCard)
+    val paymentMethod = NonDirectDebitMethod(NotActivePaymentMethod, CreditCard)
     def getPaymentMethodStatus = fakeGetPaymentMethodStatus(ClientSuccess(paymentMethod)) _
     val expectedMessage = "Default payment method status in Zuora account is not active"
     ValidatePaymentMethod(getPaymentMethodStatus)(paymentMethodId).shouldBe(validationError(expectedMessage))
   }
 
   it should "succeed if payment method is active" in {
-    val paymentMethod = PaymentMethod(ActivePaymentMethod, CreditCard)
+    val paymentMethod = NonDirectDebitMethod(ActivePaymentMethod, CreditCard)
 
     def getPaymentMethodStatus = fakeGetPaymentMethodStatus(ClientSuccess(paymentMethod)) _
     ValidatePaymentMethod(getPaymentMethodStatus)(paymentMethodId) shouldBe ContinueProcessing(())
@@ -42,7 +42,7 @@ class ValidatePaymentMethodTest extends FlatSpec with Matchers {
   }
 
   it should "fail if payment method is of an unknown type" in {
-    val paymentMethod = PaymentMethod(ActivePaymentMethod, Other)
+    val paymentMethod = NonDirectDebitMethod(ActivePaymentMethod, Other)
     def getPaymentMethodStatus = fakeGetPaymentMethodStatus(ClientSuccess(paymentMethod)) _
     val validPaymentTypes = PaymentMethodType.all.filterNot(_ == Other)
     val expectedMessage = s"Invalid payment method type in Zuora account, must be one of ${validPaymentTypes.mkString(",")}"
