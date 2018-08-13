536ea66d2c07a3d90b055b4b9a105fbd01373e67
pvighi <patricio.vighi@guardian.co.uk>
2018-08-13 11:42:38 +0100

    reverted test commited by mistake

--- handlers/new-product-api/src/test/scala/com/gu/newproduct/api/addsubscription/zuora/GetAccountEffectsTest.scala
+++ handlers/new-product-api/src/test/scala/com/gu/newproduct/api/addsubscription/zuora/GetAccountEffectsTest.scala
@@ -3,7 +3,6 @@ package com.gu.newproduct.api.addsubscription.zuora
 import com.gu.effects.{GetFromS3, RawEffects}
 import com.gu.i18n.Currency.GBP
 import com.gu.newproduct.api.addsubscription.ZuoraAccountId
-import com.gu.newproduct.api.addsubscription.validation.{ValidateAccount1, ValidatedAccount}
 import com.gu.newproduct.api.addsubscription.zuora.GetAccount.WireModel._
 import com.gu.newproduct.api.addsubscription.zuora.GetAccount._
 import com.gu.test.EffectsTest
@@ -11,16 +10,14 @@ import com.gu.util.config.{LoadConfigModule, Stage}
 import com.gu.util.zuora.{ZuoraRestConfig, ZuoraRestRequestMaker}
 import org.scalatest.{FlatSpec, Matchers}
 import scalaz.\/-
-import com.gu.newproduct.api.addsubscription.validation.Validation._
+
 class GetAccountEffectsTest extends FlatSpec with Matchers {
 
   it should "get account from Zuora" taggedAs EffectsTest in {
-
-
     val actual = for {
       zuoraRestConfig <- LoadConfigModule(Stage("DEV"), GetFromS3.fetchString)[ZuoraRestConfig]
       zuoraDeps = ZuoraRestRequestMaker(RawEffects.response, zuoraRestConfig)
-      res <- GetAccount(zuoraDeps.get[ZuoraAccount])(ZuoraAccountId("2c92c0f860017cd501600893130317a7")).andValidateWith(ValidateAccount1.apply).toDisjunction
+      res <- GetAccount(zuoraDeps.get[ZuoraAccount])(ZuoraAccountId("2c92c0f860017cd501600893130317a7")).toDisjunction
     } yield res
     val expected = Account(
       identityId = Some(IdentityId("30000549")),
