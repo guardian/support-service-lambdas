3a8dafd30da86b71685c82d296e97cffaa382bd1
pvighi <patricio.vighi@guardian.co.uk>
2018-08-20 16:09:03 +0100

    comment out manual just for test

--- handlers/new-product-api/src/test/scala/com/gu/newproduct/api/productcatalog/NewProductApiCatalogManualTest.scala
+++ handlers/new-product-api/src/test/scala/com/gu/newproduct/api/productcatalog/NewProductApiCatalogManualTest.scala
@@ -3,21 +3,19 @@ package com.gu.newproduct.api.productcatalog
 import com.gu.effects.GetFromS3
 import com.gu.util.config.Stage
 
-import scala.util.Try
 import scalaz.Scalaz._
-import scalaz._
 
 object NewProductApiCatalogManualTest extends App {
 
-  val res = for {
-    zuoraIds <- ZuoraIds.zuoraIdsForStage(Stage("DEV")).toDisjunction
-    zuoraToPlanId = zuoraIds.voucherZuoraIds.zuoraIdToPlanid.get _
-    stage = Stage("DEV")
-    response <- {
-      def pricesFromZuora(stage: Stage) = PricesFromZuoraCatalog(stage, GetFromS3.fetchString, zuoraToPlanId)
-      NewProductApiCatalog.catalog1( pricesFromZuora, stage).toDisjunction
-    }
-  } yield response
+  //  val res = for {
+  //    zuoraIds <- ZuoraIds.zuoraIdsForStage(Stage("DEV")).toDisjunction
+  //    zuoraToPlanId = zuoraIds.voucherZuoraIds.zuoraIdToPlanid.get _
+  //    stage = Stage("DEV")
+  //    response <- {
+  //      def pricesFromZuora(stage: Stage) = PricesFromZuoraCatalog(stage, GetFromS3.fetchString, zuoraToPlanId)
 
-  println(res)
+  //    }
+  //  } yield response
+  //
+  //  println(res)
 }
