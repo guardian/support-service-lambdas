6bf5936c2955cf9fc73732036581d624c2ab8a21
John Duffell <john.duffell@guardian.co.uk>
2018-09-20 13:00:15 +0100

    simplify get SF override test where things are now covered by the other tests

--- handlers/sf-contact-merge/src/test/scala/com/gu/sf_contact_merge/getsfcontacts/GetSfAddressOverrideTest.scala
+++ handlers/sf-contact-merge/src/test/scala/com/gu/sf_contact_merge/getsfcontacts/GetSfAddressOverrideTest.scala
@@ -1,10 +1,10 @@
 package com.gu.sf_contact_merge.getsfcontacts
 
 import com.gu.sf_contact_merge.getsfcontacts.GetSfAddress.SFAddressFields._
-import com.gu.sf_contact_merge.getsfcontacts.GetSfAddress.{SFAddress, SFMaybeAddress, UnusableContactAddress, UsableContactAddress}
+import com.gu.sf_contact_merge.getsfcontacts.GetSfAddress.{SFAddress, UnusableContactAddress, UsableContactAddress}
 import com.gu.sf_contact_merge.getsfcontacts.GetSfAddressOverride.{DontOverrideAddress, OverrideAddressWith}
 import com.gu.util.resthttp.LazyClientFailableOp
-import com.gu.util.resthttp.Types.{ClientSuccess, GenericError}
+import com.gu.util.resthttp.Types.ClientSuccess
 import org.scalatest.{FlatSpec, Matchers}
 
 class GetSfAddressOverrideTest extends FlatSpec with Matchers {
@@ -18,75 +18,51 @@ class GetSfAddressOverrideTest extends FlatSpec with Matchers {
     Some(SFPhone("phone1"))
   )
 
-  final class MockGetSfAddress() {
-
-    var invocationLog = List[String]() // we want to check ordering of side effects...
-
-    def apply(label: String): LazyClientFailableOp[SFMaybeAddress] = {
-      val (log, result) = label match {
-        case name if name.startsWith("noaddress") =>
-          (s"get $label", ClientSuccess(UnusableContactAddress))
-        case label if label.startsWith("a") =>
-          (s"get $label", ClientSuccess(UsableContactAddress(testAddress(label))))
-        case other =>
-          (s"other: <$other>", GenericError("whoops"))
-      }
-      LazyClientFailableOp { () =>
-        invocationLog = invocationLog ++ List(log)
-        result
-      }
-    }
-
-  }
-
   "GetSfAddressOverride" should "not give an override if it's already set" in {
-
-    val invocationLog = new MockGetSfAddress()
 
     val getSfAddressOverride = GetSfAddressOverride.apply
 
-    val actual = getSfAddressOverride.apply(invocationLog("a1"), List())
-
-    val expectedOrder = List("get a1")
-    invocationLog.invocationLog should be(expectedOrder)
+    val actual = getSfAddressOverride.apply(
+      LazyClientFailableOp(() => ClientSuccess(UsableContactAddress(testAddress("a1")))),
+      List()
+    )
 
     actual should be(ClientSuccess(DontOverrideAddress))
   }
 
   "GetSfAddressOverride" should "not give an override if it only has no addresses" in {
 
-    val invocationLog = new MockGetSfAddress()
-
     val getSfAddressOverride = GetSfAddressOverride.apply
 
-    val actual = getSfAddressOverride.apply(invocationLog("noaddress"), List())
-
-    val expectedOrder = List("get noaddress")
-    invocationLog.invocationLog should be(expectedOrder)
+    val actual = getSfAddressOverride.apply(
+      LazyClientFailableOp(() => ClientSuccess(UnusableContactAddress)),
+      List()
+    )
 
     actual should be(ClientSuccess(DontOverrideAddress))
   }
 
   "GetSfAddressOverride" should "give an override if the main contact doesn't have but the other does" in {
 
-    val invocationLog = new MockGetSfAddress()
-
-    val actual = GetSfAddressOverride.apply(invocationLog("noaddress"), List(invocationLog("a1")))
-
-    val expectedOrder = List("get noaddress", "get a1")
-    invocationLog.invocationLog should be(expectedOrder)
+    val actual = GetSfAddressOverride.apply(
+      LazyClientFailableOp(() => ClientSuccess(UnusableContactAddress)),
+      List(
+        LazyClientFailableOp(() => ClientSuccess(UsableContactAddress(testAddress("a1"))))
+      )
+    )
 
     actual should be(ClientSuccess(OverrideAddressWith(testAddress("a1"))))
   }
 
   "GetSfAddressOverride" should "not carry on checking contacts once one with an address is found" in {
 
-    val invocationLog = new MockGetSfAddress()
-
-    val actual = GetSfAddressOverride.apply(invocationLog("noaddress"), List(invocationLog("a1"), invocationLog("noaddress1")))
-
-    val expectedOrder = List("get noaddress", "get a1")
-    invocationLog.invocationLog should be(expectedOrder)
+    val actual = GetSfAddressOverride.apply(
+      LazyClientFailableOp(() => ClientSuccess(UnusableContactAddress)),
+      List(
+        LazyClientFailableOp(() => ClientSuccess(UsableContactAddress(testAddress("a1")))),
+        LazyClientFailableOp(() => fail("whoops"))
+      )
+    )
 
     actual should be(ClientSuccess(OverrideAddressWith(testAddress("a1"))))
   }
