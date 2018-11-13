b40080d820ad1c5eea412514bb2fbd6f28819cfa
Tom Richards <tom.richards@guardian.co.uk>
2018-11-13 13:44:04 +0000

    finished GoCardlessDDMandateUpdateEffectsTest

--- handlers/sf-gocardless-sync/src/test/scala/com/gu/sf_gocardless_sync/gocardless/GoCardlessDDMandateUpdateEffectsTest.scala
+++ handlers/sf-gocardless-sync/src/test/scala/com/gu/sf_gocardless_sync/gocardless/GoCardlessDDMandateUpdateEffectsTest.scala
@@ -1,29 +1,71 @@
 package com.gu.sf_gocardless_sync.gocardless
 
 import com.gu.effects.{GetFromS3, RawEffects}
-import com.gu.sf_gocardless_sync.SyncSharedObjects.GoCardlessMandateUpdateID
-import com.gu.sf_gocardless_sync.gocardless.GoCardlessDDMandateUpdate.{GetBankDetail, GetEventsSince, GoCardlessCustomerBankAccountID}
+import com.gu.sf_gocardless_sync.SyncSharedObjects.{BankAccountNumberEnding, BankName, Cause, Description, GoCardlessMandateID, GoCardlessMandateUpdateID, MandateCreatedAt, Reference, Status}
+import com.gu.sf_gocardless_sync.gocardless.GoCardlessDDMandateUpdate.GetBankDetail.GoCardlessCustomerBankDetail
+import com.gu.sf_gocardless_sync.gocardless.GoCardlessDDMandateUpdate.GetEventsSince.{GoCardlessEventDetails, GoCardlessEventLinks, GoCardlessMandateUpdate, MandateUpdateWithMandateDetail}
+import com.gu.sf_gocardless_sync.gocardless.GoCardlessDDMandateUpdate.{GetBankDetail, GetEventsSince, GoCardlessCustomerBankAccountID, GoCardlessMandateDetail, GoCardlessMandateLinks}
 import com.gu.test.EffectsTest
 import com.gu.util.config.{LoadConfigModule, Stage}
 import com.gu.util.resthttp.JsonHttp
+import com.gu.util.resthttp.Types.ClientSuccess
 import org.scalatest.{FlatSpec, Matchers}
+import scalaz.\/-
 
 class GoCardlessDDMandateUpdateEffectsTest extends FlatSpec with Matchers {
 
-  it should "fetch a set of mandate events from GoCardless, with accompanying basic mandate detail" taggedAs EffectsTest in {
+  it should "fetch a set of mandate events from GoCardless, with accompanying mandate detail" taggedAs EffectsTest in {
 
     val actual = for {
       goCardlessConfig <- LoadConfigModule(Stage("DEV"), GetFromS3.fetchString)[GoCardlessConfig]
-      response = RawEffects.response
-      goCardlessClient = GoCardlessClient(response, goCardlessConfig)
-      wiredOp = GetEventsSince(goCardlessClient.wrapWith(JsonHttp.get), goCardlessConfig.batchSize)
-      result = wiredOp(GoCardlessMandateUpdateID("EV002140EW1YFZ"))
-    } yield result
+      goCardlessClient = GoCardlessClient(RawEffects.response, goCardlessConfig)
+      wiredOp = GetEventsSince(goCardlessClient.wrapWith(JsonHttp.get), 2)
+    } yield wiredOp(GoCardlessMandateUpdateID("EV002140EW1YFZ"))
 
-    System.err.println()
-    System.err.println(actual)
-    System.err.println()
-
+    actual shouldBe \/-(ClientSuccess(
+      List(
+        MandateUpdateWithMandateDetail(
+          GoCardlessMandateUpdate(
+            GoCardlessMandateUpdateID("EV00214371W8Q5"),
+            "2018-11-02T11:03:50.652Z",
+            Status("active"),
+            GoCardlessEventLinks(GoCardlessMandateID("MD0004EMSRVH12")),
+            GoCardlessEventDetails(
+              Cause("mandate_activated"),
+              Description("The time window after submission for the banks to refuse a mandate has " +
+                "ended without any errors being received, so this mandate is now active."),
+              None
+            )
+          ),
+          GoCardlessMandateDetail(
+            GoCardlessMandateID("MD0004EMSRVH12"),
+            MandateCreatedAt("2018-10-31T08:44:33.136Z"),
+            Reference("B46YFKK"),
+            GoCardlessMandateLinks(GoCardlessCustomerBankAccountID("BA0004CDWM71MX"))
+          )
+        ),
+        MandateUpdateWithMandateDetail(
+          GoCardlessMandateUpdate(
+            GoCardlessMandateUpdateID("EV002142VDRGB1"),
+            "2018-11-02T11:03:43.908Z",
+            Status("active"),
+            GoCardlessEventLinks(GoCardlessMandateID("MD0004ENP8MFV9")),
+            GoCardlessEventDetails(
+              Cause("mandate_activated"),
+              Description("The time window after submission for the banks to refuse a mandate has " +
+                "ended without any errors being received, so this mandate is now active."),
+              None
+            )
+          ),
+          GoCardlessMandateDetail(
+            GoCardlessMandateID("MD0004ENP8MFV9"),
+            MandateCreatedAt("2018-10-31T11:22:00.308Z"),
+            Reference("KBSM8BQ"),
+            GoCardlessMandateLinks(GoCardlessCustomerBankAccountID("BA0004CETVHRSB"))
+          )
+        )
+      )
+    ))
   }
 
   it should "fetch bank details given a customer_account_id" taggedAs EffectsTest in {
@@ -33,12 +75,14 @@ class GoCardlessDDMandateUpdateEffectsTest extends FlatSpec with Matchers {
       response = RawEffects.response
       goCardlessClient = GoCardlessClient(response, goCardlessConfig)
       wiredOp = GetBankDetail(goCardlessClient.wrapWith(JsonHttp.get))
-      result = wiredOp(GoCardlessCustomerBankAccountID("MD0004D28YHWH2")) //TODO this needs to change
-    } yield result
+    } yield wiredOp(GoCardlessCustomerBankAccountID("BA0004D82Z5S0H"))
 
-    System.err.println()
-    System.err.println(actual)
-    System.err.println()
+    actual shouldBe \/-(ClientSuccess(
+      GoCardlessCustomerBankDetail(
+        BankAccountNumberEnding("11"),
+        BankName("BARCLAYS BANK PLC")
+      )
+    ))
 
   }
 
