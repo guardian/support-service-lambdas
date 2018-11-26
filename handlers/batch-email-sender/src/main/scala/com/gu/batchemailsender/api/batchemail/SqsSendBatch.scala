899d5fb9201bda9b079a3501a87d1615828a5be1
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2018-11-26 16:57:00 +0000

    make sure failed ids are collected

--- handlers/batch-email-sender/src/main/scala/com/gu/batchemailsender/api/batchemail/SqsSendBatch.scala
+++ handlers/batch-email-sender/src/main/scala/com/gu/batchemailsender/api/batchemail/SqsSendBatch.scala
@@ -10,20 +10,21 @@ import scala.util.{Failure, Success, Try}
 object SqsSendBatch extends Logging {
 
   def sendBatchSync(sqsSend: Payload => Try[Unit])(emailBatchItems: List[EmailBatchItem]): List[EmailBatchItemId] = {
-    def sqsWrapper(payload: Payload, emailBatchItemId: EmailBatchItemId): Option[EmailBatchItemId] = {
-      sqsSend(payload) match {
+    def sendAndRetainItemIdOnFailure(payload: Payload, emailBatchItemId: EmailBatchItemId): Option[EmailBatchItemId] = {
+      Try(sqsSend(payload)) match {
         case Success(_) => None
-        case Failure(_) => Some(emailBatchItemId)
+        case Failure(_) =>
+          Some(emailBatchItemId)
       }
     }
 
     emailBatchItems flatMap { emailBatchItem: EmailBatchItem =>
       val brazeCampaignId: Option[String] = DataExtensionMap.getDataExtension(emailBatchItem.object_name) //TODO use this rather than expired-card every time
-      logger.info(s"found campaign id ${brazeCampaignId}")
+
       val emailToSend = EmailToSend.fromEmailBatchItem(emailBatchItem, "expired-card")
       val payloadString = Json.prettyPrint(Json.toJson(emailToSend))
-      logger.info(s"email to send: ${payloadString}")
-      sqsWrapper(Payload(payloadString), emailBatchItem.payload.record_id)
+
+      sendAndRetainItemIdOnFailure(Payload(payloadString), emailBatchItem.payload.record_id)
     }
 
   }
