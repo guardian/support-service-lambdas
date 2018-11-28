e9fb983d3e2a94bc412b978a777a8a7ef7b25c83
Peter Colley <zvvvvt@gmail.com>
2018-11-28 16:30:00 +0000

    use 500 for identity backfull and output error

--- handlers/identity-backfill/src/main/scala/com/gu/identityBackfill/TypeConvert.scala
+++ handlers/identity-backfill/src/main/scala/com/gu/identityBackfill/TypeConvert.scala
@@ -1,6 +1,6 @@
 package com.gu.identityBackfill
 
-import com.gu.util.apigateway.ApiGatewayResponse.badRequest
+import com.gu.util.apigateway.ApiGatewayResponse.messageResponse
 import com.gu.util.resthttp.Types.ClientFailableOp
 import com.gu.util.reader.Types._
 
@@ -9,7 +9,10 @@ object TypeConvert {
   implicit class TypeConvertClientOp[A](clientOp: ClientFailableOp[A]) {
     def toApiGatewayOp(action: String): ApiGatewayOp[A] = clientOp.toDisjunction.toApiGatewayOp { error =>
       logger.error(s"Failed to $action: $error")
-      badRequest(s"Failed to execute lambda - unable to $action")
+      messageResponse(
+        s"Failed to execute lambda - unable to $action, $error",
+        action
+      )
     }
   }
 
