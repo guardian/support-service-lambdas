112ebc4fe65dec1a46d2a92a256fa83a324a8a03
Peter Colley <zvvvvt@gmail.com>
2018-11-28 16:19:40 +0000

    return 400 errors on backfill client failures

--- handlers/identity-backfill/src/main/scala/com/gu/identityBackfill/TypeConvert.scala
+++ handlers/identity-backfill/src/main/scala/com/gu/identityBackfill/TypeConvert.scala
@@ -1,12 +1,16 @@
 package com.gu.identityBackfill
 
+import com.gu.util.apigateway.ApiGatewayResponse.badRequest
 import com.gu.util.resthttp.Types.ClientFailableOp
 import com.gu.util.reader.Types._
 
 object TypeConvert {
 
   implicit class TypeConvertClientOp[A](clientOp: ClientFailableOp[A]) {
-    def toApiGatewayOp(action: String): ApiGatewayOp[A] = clientOp.toDisjunction.toApiGatewayOp(action)
+    def toApiGatewayOp(action: String): ApiGatewayOp[A] = clientOp.toDisjunction.toApiGatewayOp { error =>
+      logger.error(s"Failed to $action: $error")
+      badRequest(s"Failed to execute lambda - unable to $action")
+    }
   }
 
 }
