5b342376695a4eaece5bb893981ca6635fbd09b3
pvighi <patricio.vighi@guardian.co.uk>
2019-03-07 13:38:04 +0000

    make plan matching strict now that we are sending all emails from the same place

--- handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/email/SendConfirmationEmail.scala
+++ handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/email/SendConfirmationEmail.scala
@@ -1,7 +1,7 @@
 package com.gu.newproduct.api.addsubscription.email
 
 import com.gu.newproduct.api.addsubscription.zuora.GetAccount.SfContactId
-import com.gu.newproduct.api.productcatalog.{ContributionPlanId, DigipackPlanId, Plan, VoucherPlanId}
+import com.gu.newproduct.api.productcatalog._
 import com.gu.util.Logging
 import com.gu.util.apigateway.ApiGatewayResponse
 import com.gu.util.reader.AsyncTypes.{AsyncApiGatewayOp, _}
@@ -44,7 +44,7 @@ object SendConfirmationEmail extends Logging {
       case _: VoucherPlanId => "paper-voucher"
       case _: DigipackPlanId => "digipack"
       case _: ContributionPlanId => "regular-contribution-thank-you"
-      case _ => "paper-delivery"
+      case _: HomeDeliveryPlanId => "paper-delivery"
     }
   )
 }
