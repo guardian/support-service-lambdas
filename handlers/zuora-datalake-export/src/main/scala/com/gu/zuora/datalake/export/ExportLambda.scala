c5722aa4f29f345838f1c2016a0912e53f028fd4
Mario Galic <mario.galic@guardian.co.uk>
2019-03-19 12:55:32 +0000

    Link to Zuora support request for partner ID

--- handlers/zuora-datalake-export/src/main/scala/com/gu/zuora/datalake/export/ExportLambda.scala
+++ handlers/zuora-datalake-export/src/main/scala/com/gu/zuora/datalake/export/ExportLambda.scala
@@ -98,8 +98,8 @@ case class ZuoraAquaStatefulApi(
 )
 object ZuoraAquaStatefulApi {
   def apply(): ZuoraAquaStatefulApi = System.getenv("Stage") match {
-    case "CODE" => ZuoraAquaStatefulApi(partner = "guardian-12357")
-    case "PROD" => ZuoraAquaStatefulApi(partner = "GuardianNews4398")
+    case "CODE" => ZuoraAquaStatefulApi(partner = "guardian-12357") // https://support.zuora.com/hc/en-us/requests/175239
+    case "PROD" => ZuoraAquaStatefulApi(partner = "GuardianNews4398") // https://support.zuora.com/hc/en-us/requests/177970
   }
 }
 
