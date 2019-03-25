ca945cf86a6ed8f1c29f0afe59e4fa1f8f85b67e
Mario Galic <mario.galic@guardian.co.uk>
2019-03-25 10:12:52 +0000

    Remove comment

--- handlers/braze-to-salesforce-file-upload/src/main/scala/com/gu/salesforce/braze/upload/UploadFileToSalesforceLambda.scala
+++ handlers/braze-to-salesforce-file-upload/src/main/scala/com/gu/salesforce/braze/upload/UploadFileToSalesforceLambda.scala
@@ -10,47 +10,6 @@ import com.typesafe.scalalogging.LazyLogging
 import scalaj.http.Http
 import scala.io.Source
 
-/*
-{
-    "Records": [
-        {
-            "eventVersion": "2.1",
-            "eventSource": "aws:s3",
-            "awsRegion": "eu-west-1",
-            "eventTime": "2019-03-21T14:40:00.210Z",
-            "eventName": "ObjectCreated:Put",
-            "userIdentity": {
-                "principalId": "AWS:AROAJ2472JG7IDEUTKPNU:mario.galic"
-            },
-            "requestParameters": {
-                "sourceIPAddress": "77.91.250.236"
-            },
-            "responseElements": {
-                "x-amz-request-id": "C53A0297488F6BAB",
-                "x-amz-id-2": "DS4x4B/R3dixDAcQJF2/08EUaUhiowK3hiW9V56cYeesG2HPXFwbjz1yewUGXde2QnzJ5jyIRE8="
-            },
-            "s3": {
-                "s3SchemaVersion": "1.0",
-                "configurationId": "9553147c-84da-4616-b446-7084781908b0",
-                "bucket": {
-                    "name": "braze-to-salesforce-upload-code",
-                    "ownerIdentity": {
-                        "principalId": "A3FCSJUA4O7GBA"
-                    },
-                    "arn": "arn:aws:s3:::braze-to-salesforce-upload-code"
-                },
-                "object": {
-                    "key": "Account.csv",
-                    "size": 1043872,
-                    "eTag": "aa47941dff62732c82effe6962e87451",
-                    "sequencer": "005C93A2401DBEE37E"
-                }
-            }
-        }
-    ]
-}
- */
-
 case class S3EventObject(key: String)
 case class S3Event(`object`: S3EventObject)
 case class Record(s3: S3Event)
