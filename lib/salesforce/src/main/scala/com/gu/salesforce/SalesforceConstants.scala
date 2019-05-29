a190dbe1b0f05a810424aacdbc97e7cee1a3fcf0
Tom Richards <thomas.w.richards@gmail.com>
2019-05-29 13:19:40 +0100

    fixed a recent bad refactor which was string concatenating a RelativePath (https://github.com/guardian/support-service-lambdas/pull/294/commits/5761a77d8d79e282495be630c24a9e5fd9b28413)

--- lib/salesforce/src/main/scala/com/gu/salesforce/SalesforceConstants.scala
+++ lib/salesforce/src/main/scala/com/gu/salesforce/SalesforceConstants.scala
@@ -1,12 +1,10 @@
 package com.gu.salesforce
 
-import com.gu.util.resthttp.RestRequestMaker.RelativePath
-
 object SalesforceConstants {
 
   val sfApiBaseUrl = "/services/data/v29.0"
 
-  val soqlQueryBaseUrl = RelativePath(sfApiBaseUrl + "/query/")
+  val soqlQueryBaseUrl = sfApiBaseUrl + "/query/"
 
   val sfObjectsBaseUrl = sfApiBaseUrl + "/sobjects/"
 
