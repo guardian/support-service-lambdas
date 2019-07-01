70d94c3a1d0c0d518d1c3179168943e167ba0a89
kostas_kougios <kostas.kougios.contractor@guardian.co.uk>
2019-07-01 16:26:09 +0100

    writing a script to test end to end

--- handlers/sf-datalake-export/src/test/scala/com/manual_test/EndToEndManualTest.scala
+++ handlers/sf-datalake-export/src/test/scala/com/manual_test/EndToEndManualTest.scala
@@ -2,28 +2,48 @@ package com.manual_test
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
-import com.gu.sf_datalake_export.handlers.GetBatchesHandler
+import com.gu.sf_datalake_export.handlers.{GetBatchesHandler, StartJobHandler}
+import com.gu.sf_datalake_export.salesforce_bulk_api.BulkApiParams
+import com.gu.sf_datalake_export.salesforce_bulk_api.BulkApiParams.SfQueryInfo
 import play.api.libs.json._
 
 object EndToEndManualTest extends App {
+
+  case class Job(jobId: String, jobName: String, objectName: String, uploadToDataLake: Boolean)
 
   case class Batch(batchId: String, state: String)
 
   case class Batches(jobId: String, jobName: String, objectName: String, jobStatus: String, batches: Seq[Batch])
 
+  implicit val jobFormat = Json.format[Job]
   implicit val batchFormat = Json.format[Batch]
   implicit val batchesWrites = Json.format[Batches]
 
+  val job = startJob(BulkApiParams.contact)
+  msg(job)
+  val batches = getBatches(job)
+  msg(batches)
 
-  def getBatches = {
+  def startJob(sfObject: SfQueryInfo): Job = {
+
     val request =
-      """{
-        |"jobId" : "7503E0000063jW3QAI",
-        |"jobName" : "Contact_2019-06-30",
-        |"objectName" : "Contact",
-        |"uploadToDataLake" : false
-        |}
-      """.stripMargin
+      s"""{
+         |"objectName" : "${sfObject.objectName.value}"
+         |}
+    """.stripMargin
+
+    println(s"sending request..")
+    println(request)
+
+    val testInputStream = new ByteArrayInputStream(request.getBytes)
+    val testOutput = new ByteArrayOutputStream()
+    StartJobHandler(testInputStream, testOutput, null)
+
+    Json.parse(testOutput.toByteArray).as[Job]
+  }
+
+  def getBatches(job: Job) = {
+    val request = Json.prettyPrint(Json.toJson(job))
 
     println(s"sending request..")
     println(request)
@@ -33,5 +53,14 @@ object EndToEndManualTest extends App {
     GetBatchesHandler(testInputStream, testOutput, null)
 
     Json.parse(testOutput.toByteArray).as[Batches]
+  }
+
+  def msg(s: Any): Unit = {
+    println(
+      s"""
+         |----------------------------------------------------------------------
+         |$s
+         |----------------------------------------------------------------------
+      """.stripMargin)
   }
 }
