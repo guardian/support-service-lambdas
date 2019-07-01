f2ab6d31a19c55c834a801188ff4c86cee4b15a9
kostas_kougios <kostas.kougios.contractor@guardian.co.uk>
2019-07-01 16:34:29 +0100

    writing a script to test end to end

--- handlers/sf-datalake-export/src/test/scala/com/manual_test/EndToEndManualTest.scala
+++ handlers/sf-datalake-export/src/test/scala/com/manual_test/EndToEndManualTest.scala
@@ -13,7 +13,9 @@ object EndToEndManualTest extends App {
 
   case class Batch(batchId: String, state: String)
 
-  case class Batches(jobId: String, jobName: String, objectName: String, jobStatus: String, batches: Seq[Batch])
+  case class Batches(jobId: String, jobName: String, objectName: String, jobStatus: String, batches: Seq[Batch]) {
+    def isCompleted = jobStatus == "Completed"
+  }
 
   implicit val jobFormat = Json.format[Job]
   implicit val batchFormat = Json.format[Batch]
@@ -21,7 +23,7 @@ object EndToEndManualTest extends App {
 
   val job = startJob(BulkApiParams.contact)
   msg(job)
-  val batches = getBatches(job)
+  val batches = waitBatchesToComplete(job)
   msg(batches)
 
   def startJob(sfObject: SfQueryInfo): Job = {
@@ -32,24 +34,29 @@ object EndToEndManualTest extends App {
          |}
     """.stripMargin
 
-    println(s"sending request..")
-    println(request)
-
     val testInputStream = new ByteArrayInputStream(request.getBytes)
-    val testOutput = new ByteArrayOutputStream()
+    val testOutput = new ByteArrayOutputStream
     StartJobHandler(testInputStream, testOutput, null)
 
     Json.parse(testOutput.toByteArray).as[Job]
   }
 
+  def waitBatchesToComplete(job: Job): Batches = {
+    val batches = getBatches(job)
+
+    if (batches.isCompleted) batches else {
+      println("Batches not complete: " + batches)
+
+      Thread.sleep(2000)
+      waitBatchesToComplete(job)
+    }
+  }
+
   def getBatches(job: Job) = {
     val request = Json.prettyPrint(Json.toJson(job))
 
-    println(s"sending request..")
-    println(request)
-
     val testInputStream = new ByteArrayInputStream(request.getBytes)
-    val testOutput = new ByteArrayOutputStream()
+    val testOutput = new ByteArrayOutputStream
     GetBatchesHandler(testInputStream, testOutput, null)
 
     Json.parse(testOutput.toByteArray).as[Batches]
