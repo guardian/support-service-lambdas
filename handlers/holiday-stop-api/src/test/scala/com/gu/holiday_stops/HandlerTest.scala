a4ad737255aa17136b65ff00ba9ae98d473e7e94
Francis Rhys-Jones <francis@rhys-jones.com>
2019-08-29 16:55:08 +0100

    adding basic test for the get potential stop dates endpoint

--- handlers/holiday-stop-api/src/test/scala/com/gu/holiday_stops/HandlerTest.scala
+++ handlers/holiday-stop-api/src/test/scala/com/gu/holiday_stops/HandlerTest.scala
@@ -1,9 +1,14 @@
 package com.gu.holiday_stops
 
-import org.scalatest.{FlatSpec, Matchers}
+import com.gu.effects.{FakeFetchString, SFTestEffects, TestingRawEffects}
 import com.gu.holiday_stops.Handler._
 import com.gu.salesforce.holiday_stops.SalesforceSFSubscription.SubscriptionForSubscriptionNameAndContact._
+import com.gu.util.apigateway.ApiGatewayRequest
+import com.gu.util.config.Stage
 import com.gu.util.reader.Types.ApiGatewayOp.{ContinueProcessing, ReturnWithResponse}
+import org.scalatest.Inside.inside
+import org.scalatest.{FlatSpec, Matchers}
+import play.api.libs.json.{JsSuccess, Json}
 
 class HandlerTest extends FlatSpec with Matchers {
 
@@ -21,7 +26,42 @@ class HandlerTest extends FlatSpec with Matchers {
     Handler.extractContactFromHeaders(Some(Map(
       HEADER_SALESFORCE_CONTACT_ID -> expectedSfContactIdCoreValue
     ))) shouldBe ContinueProcessing(Right(SalesforceContactId(expectedSfContactIdCoreValue)))
-
+  }
+  it should "calculate potential holiday stop dates" in {
+    inside(
+      Handler.operationForEffects(
+        testEffects.response,
+        Stage("DEV"),
+        FakeFetchString.fetchString
+      ).map { operation =>
+        operation
+          .steps(potentialIssueDateRequests("Guardian Weekly xxx", "2019-01-01", "2019-02-01"))
+      }
+    ) {
+      case ContinueProcessing(response) =>
+        response.statusCode should equal("200")
+        inside(Json.fromJson[Array[String]](Json.parse(response.body))) {
+          case JsSuccess(dates, _) =>
+            dates should contain inOrderOnly(
+              "2019-01-04", "2019-01-11", "2019-01-18", "2019-01-25", "2019-02-01"
+            )
+        }
+    }
   }
 
+  private def potentialIssueDateRequests(productPrefix: String, startDate: String, endDate: String) = {
+    ApiGatewayRequest(
+      Some("GET"),
+      Some(Map("startDate" -> startDate, "endDate" -> endDate)),
+      None,
+      Some(Map("x-product-name-prefix" -> productPrefix)),
+      None,
+      Some("/potential"))
+  }
+
+  val testEffects = new TestingRawEffects(
+    postResponses = Map(
+      SFTestEffects.authSuccess
+    )
+  )
 }
