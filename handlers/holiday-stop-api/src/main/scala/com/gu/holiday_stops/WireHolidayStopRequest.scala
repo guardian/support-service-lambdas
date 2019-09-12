71a1ff610ccf4ce358e8c7e914c25be20795cee6
Tom Richards <tom.richards@guardian.co.uk>
2019-09-12 19:43:43 +0100

    expose existing estimate and actual prices that are already stored in SalesForce

--- handlers/holiday-stop-api/src/main/scala/com/gu/holiday_stops/WireHolidayStopRequest.scala
+++ handlers/holiday-stop-api/src/main/scala/com/gu/holiday_stops/WireHolidayStopRequest.scala
@@ -19,7 +19,9 @@ object WireHolidayStopRequest {
       sfHolidayStopRequest.End_Date__c.value
     ),
     publicationsImpacted = sfHolidayStopRequest.Holiday_Stop_Request_Detail__r.map(_.records.map(detail => HolidayStopRequestsDetail(
-      publicationDate = detail.Stopped_Publication_Date__c.value
+      publicationDate = detail.Stopped_Publication_Date__c.value,
+      estimatedPrice = detail.Estimated_Price__c.map(_.value),
+      actualPrice = detail.Actual_Price__c.map(_.value)
     ))).getOrElse(List())
   )
 
@@ -47,7 +49,8 @@ object MutabilityFlags {
 
 case class HolidayStopRequestsDetail(
   publicationDate: LocalDate,
-  // TODO add other fields
+  estimatedPrice: Option[Double],
+  actualPrice: Option[Double]
 )
 object HolidayStopRequestsDetail{
   implicit val writes: OWrites[HolidayStopRequestsDetail] = Json.writes[HolidayStopRequestsDetail]
