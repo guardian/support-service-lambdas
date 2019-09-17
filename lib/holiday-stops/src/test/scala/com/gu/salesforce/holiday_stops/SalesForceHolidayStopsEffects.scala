8d02a5800299f8694a8e2dca0949fe541acce2bc
Francis Rhys-Jones <francis@rhys-jones.com>
2019-09-17 14:23:18 +0100

    improving readability of SalesForce stub

--- lib/holiday-stops/src/test/scala/com/gu/salesforce/holiday_stops/SalesForceHolidayStopsEffects.scala
+++ lib/holiday-stops/src/test/scala/com/gu/salesforce/holiday_stops/SalesForceHolidayStopsEffects.scala
@@ -1,5 +1,7 @@
 package com.gu.salesforce.holiday_stops
 
+import java.net.URLEncoder
+
 import com.gu.effects.TestingRawEffects.HTTPResponse
 import com.gu.salesforce.RecordsWrapperCaseClass
 import com.gu.salesforce.holiday_stops.SalesforceHolidayStopRequest.HolidayStopRequest
@@ -12,18 +14,36 @@ object SalesForceHolidayStopsEffects {
       | "records": []
       |}""".stripMargin
 
-  def listHolidayRequestQuery(contactId: String, subscriptionName: String) =
-    s"/services/data/v29.0/query/?q=%0A%20SELECT%20Id,%20Start_Date__c,%20End_Date__c,%20Subscription_Name__c," +
-      s"%20Product_Name__c,%0A%20Actioned_Count__c,%20Pending_Count__c," +
-      s"%20Total_Issues_Publications_Impacted_Count__c,%20(" +
-      s"%0A%20%20%20%0A%20SELECT%20Id,%20Subscription_Name__c,%20Product_Name__c,%20Stopped_Publication_Date__c," +
-      s"%0A%20Estimated_Price__c,%20Charge_Code__c,%20Actual_Price__c%0A%0A%20%20%20FROM" +
-      s"%20Holiday_Stop_Request_Detail__r%0A%20%20%20ORDER%20BY%20Stopped_Publication_Date__c%20ASC%0A%20)" +
-      s"%0A%20FROM%20Holiday_Stop_Request__c%0A%20%0AWHERE%20SF_Subscription__r.Buyer__r.Id%20%3D%20%27$contactId" +
-      s"%27%20AND%20Subscription_Name__c%20%3D%20%27$subscriptionName%27"
+  def listHolidayQuery(contactId: String, subscriptionName: String) =
+    "\n SELECT Id, Start_Date__c, End_Date__c, Subscription_Name__c, Product_Name__c," +
+      "\n Actioned_Count__c, Pending_Count__c, Total_Issues_Publications_Impacted_Count__c, (\n" +
+      "   \n" +
+      " SELECT Id, Subscription_Name__c, " +
+      "Product_Name__c, Stopped_Publication_Date__c,\n" +
+      " Estimated_Price__c, Charge_Code__c, Actual_Price__c\n" +
+      "\n   " +
+      "FROM Holiday_Stop_Request_Detail__r\n   " +
+      "ORDER BY Stopped_Publication_Date__c ASC\n" +
+      " )\n " +
+      "FROM Holiday_Stop_Request__c\n" +
+      " \n" +
+      s"WHERE SF_Subscription__r.Buyer__r.Id = '$contactId' AND Subscription_Name__c = '$subscriptionName'"
+
+  def listHolidayRequestUrl(contactId: String, subscriptionName: String) = {
+    s"/services/data/v29.0/query/?q=${escapeQueryStringValue(listHolidayQuery(contactId, subscriptionName))}"
+  }
+
+  def escapeQueryStringValue(value: String) = {
+    URLEncoder
+      .encode(value, "UTF-8")
+      .replace("+", "%20")
+      .replace("%2C", ",")
+      .replace("%28", "(")
+      .replace("%29", ")")
+  }
 
   def listHolidayStops(contactId: String, subscriptionName: String, holidayStops: List[HolidayStopRequest]): (String, HTTPResponse) =
-    listHolidayRequestQuery(
+    listHolidayRequestUrl(
       contactId,
       subscriptionName
     ) -> HTTPResponse(200, Json.toJson(RecordsWrapperCaseClass(holidayStops)).toString())
