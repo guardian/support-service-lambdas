5a771a0b64b03f3dd8c484820bc270f4f5b4117b
Tom Richards <tom.richards@guardian.co.uk>
2019-09-17 11:12:02 +0100

    holiday-stops: log credit calculation errors

--- lib/holiday-stops/src/main/scala/com/gu/holiday_stops/CreditCalculator.scala
+++ lib/holiday-stops/src/main/scala/com/gu/holiday_stops/CreditCalculator.scala
@@ -3,11 +3,21 @@ package com.gu.holiday_stops
 import java.time.LocalDate
 
 import com.gu.salesforce.holiday_stops.SalesforceHolidayStopRequestsDetail.SubscriptionName
+import com.gu.util.Logging
 import com.softwaremill.sttp.{Id, SttpBackend}
 
 object CreditCalculator {
 
   type PartiallyWiredCreditCalculator = (SubscriptionName, LocalDate) => Either[HolidayError, Double]
+
+  implicit class HolidayErrorImplicits[T](theEither: Either[HolidayError, T]) extends Logging {
+
+    def logError: Either[HolidayError, T] = {
+      theEither.left.map(err => logger.error(s"Failed to calculate holiday stop credits because : " + err.reason))
+      theEither
+    }
+
+  }
 
   def apply(
     config: Config,
@@ -17,14 +27,14 @@ object CreditCalculator {
     stoppedPublicationDate: LocalDate
   ): Either[HolidayError, Double] =
     for {
-      accessToken <- Zuora.accessTokenGetResponse(config.zuoraConfig, backend)
-      subscription <- Zuora.subscriptionGetResponse(config, accessToken, backend)(subscriptionName)
+      accessToken <- Zuora.accessTokenGetResponse(config.zuoraConfig, backend).logError
+      subscription <- Zuora.subscriptionGetResponse(config, accessToken, backend)(subscriptionName).logError
       credit <- guardianWeeklyCredit(
         config.guardianWeeklyConfig.productRatePlanIds,
         config.guardianWeeklyConfig.nForNProductRatePlanIds,
         stoppedPublicationDate
-      )(subscription)
-    } yield credit //TODO log failures
+      )(subscription).logError
+    } yield credit
 
   def guardianWeeklyCredit(guardianWeeklyProductRatePlanIds: List[String], gwNforNProductRatePlanIds: List[String], stoppedPublicationDate: LocalDate)(subscription: Subscription): Either[ZuoraHolidayWriteError, Double] =
     CurrentGuardianWeeklySubscription(subscription, guardianWeeklyProductRatePlanIds, gwNforNProductRatePlanIds)
