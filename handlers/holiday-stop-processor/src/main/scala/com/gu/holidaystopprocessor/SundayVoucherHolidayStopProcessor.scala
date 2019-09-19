e6e97a4a996a56eca5aec95e599df7eac710ce5f
Mario Galic <mario.galic@guardian.co.uk>
2019-09-19 13:21:18 +0100

    Fetch Sunday Voucher holiday stops from Salesforce

--- handlers/holiday-stop-processor/src/main/scala/com/gu/holidaystopprocessor/SundayVoucherHolidayStopProcessor.scala
+++ handlers/holiday-stop-processor/src/main/scala/com/gu/holidaystopprocessor/SundayVoucherHolidayStopProcessor.scala
@@ -2,10 +2,12 @@ package com.gu.holidaystopprocessor
 
 import java.time.LocalDate
 
+import com.gu.holiday_stops.ActionCalculator.SundayVoucherIssueSuspensionConstants
 import com.gu.holiday_stops._
 import com.gu.salesforce.holiday_stops.SalesforceHolidayStopRequestsDetail.{HolidayStopRequestsDetail, ProductName, SubscriptionName}
 
 object SundayVoucherHolidayStopProcessor {
+
   def processHolidayStops(
     config: SundayVoucherHolidayStopConfig,
     getHolidayStopRequestsFromSalesforce: (ProductName, LocalDate) => Either[OverallFailure, List[HolidayStopRequestsDetail]],
@@ -14,6 +16,22 @@ object SundayVoucherHolidayStopProcessor {
     writeHolidayStopsToSalesforce: List[HolidayStopResponse] => Either[SalesforceHolidayWriteError, Unit],
     processDateOverride: Option[LocalDate]
   ): ProcessResult = {
-    ProcessResult(Nil, Nil, Nil, None)
+    //  case ProductRatePlanKey(ProductType("Newspaper - Voucher Book"), ProductRatePlanName("Sunday")) =>
+    val processOverrideDate = LocalDate.of(2019, 9, 22)
+    getHolidayStopRequestsFromSalesforce(ProductName("Newspaper Voucher"), calculateProcessDate(Some(processOverrideDate))) match {
+      case Left(overallFailure) =>
+        ProcessResult(overallFailure)
+
+      case Right(holidayStopRequestsFromSalesforce) =>
+        val holidayStops = holidayStopRequestsFromSalesforce.distinct.map(HolidayStop(_))
+        val alreadyActionedHolidayStops = holidayStopRequestsFromSalesforce.flatMap(_.Charge_Code__c).distinct
+        // val allZuoraHolidayStopResponses = holidayStops.map(writeHolidayStopToZuora( ))
+        ProcessResult(Nil, Nil, Nil, None)
+    }
   }
+  private def calculateProcessDate(processDateOverride: Option[LocalDate]) = {
+    processDateOverride.getOrElse(LocalDate.now.plusDays(SundayVoucherIssueSuspensionConstants.processorRunLeadTimeDays))
+  }
+
+  private def writeHolidayStopToZuora() = ??? // TODO
 }
