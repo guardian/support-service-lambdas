c9b07e69f6984c3efdb3687557793b7df898fb69
Mario Galic <mario.galic@guardian.co.uk>
2019-09-19 10:57:39 +0100

    Fix test

--- lib/holiday-stops/src/test/scala/com/gu/holiday_stops/CurrentSundayVoucherSubscriptionSpec.scala
+++ lib/holiday-stops/src/test/scala/com/gu/holiday_stops/CurrentSundayVoucherSubscriptionSpec.scala
@@ -8,14 +8,14 @@ import io.circe.generic.auto._
 class CurrentSundayVoucherSubscriptionSpec extends FlatSpec with Matchers with EitherValues {
   "CurrentSundayVoucherSubscription" should "satisfy all the predicates" in {
     val subscriptionRaw = Source.fromURL(getClass.getResource("/SundayVoucherSubscription.json")).mkString
-    val subscription = decode[Subscription](subscripitonRaw).getOrElse(fail("Could not decode CurrentSundayVoucherSubscription"))
+    val subscription = decode[Subscription](subscriptionRaw).getOrElse(fail("Could not decode CurrentSundayVoucherSubscription"))
     val currentSundayVoucherSubscription = CurrentSundayVoucherSubscription(subscription, SundayVoucherHolidayStopConfig.Dev.productRatePlanChargeId)
     currentSundayVoucherSubscription.right.value.productRatePlanChargeId should be("2c92c0f95aff3b56015b1045fba832d4")
   }
 
   it should "fail on missing invoice" in {
     val subscriptionRaw = Source.fromURL(getClass.getResource("/SundayVoucherSubscriptionMissingInvoice.json")).mkString
-    val subscription = decode[Subscription](subscripitonRaw).getOrElse(fail("Could not decode CurrentSundayVoucherSubscription"))
+    val subscription = decode[Subscription](subscriptionRaw).getOrElse(fail("Could not decode CurrentSundayVoucherSubscription"))
     val currentSundayVoucherSubscription = CurrentSundayVoucherSubscription(subscription, SundayVoucherHolidayStopConfig.Dev.productRatePlanChargeId)
     currentSundayVoucherSubscription.isLeft should be(true)
   }
