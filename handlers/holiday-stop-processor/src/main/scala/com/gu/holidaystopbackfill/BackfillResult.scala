96d0785b8c058e61bd4c509e03e253913dabe318
Kelvin Chappell <kelvin.chappell@theguardian.com>
2019-09-26 11:23:35 +0100

    Remove holiday-stop backfiller

--- handlers/holiday-stop-processor/src/main/scala/com/gu/holidaystopbackfill/BackfillResult.scala
+++
@@ -1,9 +0,0 @@
-package com.gu.holidaystopbackfill
-
-import com.gu.salesforce.holiday_stops.SalesforceHolidayStopRequest.NewHolidayStopRequest
-import com.gu.salesforce.holiday_stops.SalesforceHolidayStopRequestsDetail.ActionedHolidayStopRequestsDetailToBackfill
-
-case class BackfillResult(
-  requests: List[NewHolidayStopRequest],
-  zuoraRefs: List[ActionedHolidayStopRequestsDetailToBackfill]
-)
