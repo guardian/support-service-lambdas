3fcafd34464a5060a4160c34a91225195729e82a
Mario Galic <mario.galic@guardian.co.uk>
2019-09-26 14:17:13 +0100

    Same Discount holiday stop product is reused for all products

--- lib/holiday-stops/src/main/scala/com/gu/holiday_stops/SundayVoucherHolidayStopConfig.scala
+++ lib/holiday-stops/src/main/scala/com/gu/holiday_stops/SundayVoucherHolidayStopConfig.scala
@@ -1,6 +1,5 @@
 package com.gu.holiday_stops
 
-// DO NOT USE MANUALLY: Holiday Credit - automated
 case class SundayVoucherHolidayStopConfig(
   holidayCreditProduct: HolidayCreditProduct,
   productRatePlanChargeId: String
@@ -8,24 +7,15 @@ case class SundayVoucherHolidayStopConfig(
 
 object SundayVoucherHolidayStopConfig {
   val Prod = SundayVoucherHolidayStopConfig(
-    holidayCreditProduct = HolidayCreditProduct(
-      productRatePlanId = "2c92a0076ae9189c016b080c930a6186",
-      productRatePlanChargeId = "2c92a0086ae928d7016b080f638477a6"
-    ),
+    HolidayCreditProduct.Prod,
     productRatePlanChargeId = "2c92a0fe5af9a6b9015b0fe1ed121177"
   )
   val Code = SundayVoucherHolidayStopConfig(
-    holidayCreditProduct = HolidayCreditProduct(
-      productRatePlanId = "2c92c0f86b0378b0016b08112e870d0a",
-      productRatePlanChargeId = "2c92c0f86b0378b0016b08112ec70d14"
-    ),
+    HolidayCreditProduct.Code,
     productRatePlanChargeId = "2c92c0f95aff3b54015b0ee0eb620b30"
   )
   val Dev = SundayVoucherHolidayStopConfig(
-    holidayCreditProduct = HolidayCreditProduct(
-      productRatePlanId = "2c92c0f96b03800b016b081fc04f1ba2",
-      productRatePlanChargeId = "2c92c0f96b03800b016b081fc0f41bb4"
-    ),
+    HolidayCreditProduct.Dev,
     productRatePlanChargeId = "2c92c0f95aff3b56015b1045fba832d4"
   )
 }
