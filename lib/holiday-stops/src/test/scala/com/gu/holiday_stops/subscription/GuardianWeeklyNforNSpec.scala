4b916eabf8f0dfe82bbb1a643fffbfea14af8f52
Mario Galic <mario.galic@guardian.co.uk>
2019-10-02 13:06:13 +0100

    StoppedPublicationDates should be after current invoice start date

--- lib/holiday-stops/src/test/scala/com/gu/holiday_stops/subscription/GuardianWeeklyNforNSpec.scala
+++ lib/holiday-stops/src/test/scala/com/gu/holiday_stops/subscription/GuardianWeeklyNforNSpec.scala
@@ -6,10 +6,10 @@ import com.gu.salesforce.holiday_stops.SalesforceHolidayStopRequestsDetail.Stopp
 import io.circe.generic.auto._
 import io.circe.parser.decode
 import org.scalatest._
-
 import scala.io.Source
 
 class GuardianWeeklyNforNSpec extends FlatSpec with Matchers with EitherValues {
+  private val nForNChargedThroughDate = LocalDate.parse("2019-11-15")
   "GuardianWeeklySubscription" should "represent N-for-N when stopped publication date falls within N-for-N" in {
     val subscriptionRaw = Source.fromResource("GuardianWeeklyWith6For6.json").mkString
     val subscription = decode[Subscription](subscriptionRaw).getOrElse(fail("Could not decode GuardianWeeklySubscription"))
@@ -20,14 +20,14 @@ class GuardianWeeklyNforNSpec extends FlatSpec with Matchers with EitherValues {
   it should "represent regular GW when stopped publication date falls after N-for-N" in {
     val subscriptionRaw = Source.fromResource("GuardianWeeklyWith6For6.json").mkString
     val subscription = decode[Subscription](subscriptionRaw).getOrElse(fail("Could not decode GuardianWeeklySubscription"))
-    val currentSundayVoucherSubscription = GuardianWeeklySubscription(subscription, StoppedPublicationDate(LocalDate.parse("2019-11-15")))
+    val currentSundayVoucherSubscription = GuardianWeeklySubscription(subscription, StoppedPublicationDate(nForNChargedThroughDate))
     currentSundayVoucherSubscription.right.value.price should be(37.5)
   }
 
-  it should "should fail if stopped publication date falls beyond N-for-N or predicted next invoiced period for regular GW " in {
+  it should "should fail if stopped publication date is before current invoiced period start date " in {
     val subscriptionRaw = Source.fromResource("GuardianWeeklyWith6For6.json").mkString
     val subscription = decode[Subscription](subscriptionRaw).getOrElse(fail("Could not decode GuardianWeeklySubscription"))
-    val currentSundayVoucherSubscription = GuardianWeeklySubscription(subscription, StoppedPublicationDate(LocalDate.parse("2019-11-15").plusMonths(3).plusDays(1)))
+    val currentSundayVoucherSubscription = GuardianWeeklySubscription(subscription, StoppedPublicationDate(LocalDate.parse("2019-05-04").minusDays(1)))
     currentSundayVoucherSubscription.isLeft should be(true)
   }
 }
