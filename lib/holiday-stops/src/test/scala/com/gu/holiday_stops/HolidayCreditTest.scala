0cb24f6c6b385585ce1cbc45406baa90f2611cc1
Kelvin Chappell <kelvin.chappell@theguardian.com>
2019-10-10 16:02:04 +0100

    Reinstate old test fixtures

--- lib/holiday-stops/src/test/scala/com/gu/holiday_stops/HolidayCreditTest.scala
+++ lib/holiday-stops/src/test/scala/com/gu/holiday_stops/HolidayCreditTest.scala
@@ -10,7 +10,7 @@ class HolidayCreditTest extends FlatSpec with Matchers with EitherValues {
 
   MutableCalendar.fakeToday = Some(LocalDate.parse("2019-08-01"))
 
-  val stoppedPublicationDate = StoppedPublicationDate(LocalDate.parse("2020-09-01").minusDays(1))
+  val stoppedPublicationDate = StoppedPublicationDate(LocalDate.parse("2019-09-01").minusDays(1))
 
   "HolidayCredit" should "be correct for a quarterly billing period" in {
     val charge = Fixtures.mkRatePlanCharge(price = 30, billingPeriod = "Quarter")
@@ -18,7 +18,7 @@ class HolidayCreditTest extends FlatSpec with Matchers with EitherValues {
     val subscription = Fixtures.mkSubscription().copy(ratePlans = ratePlans)
     val currentGuardianWeeklySubscription = GuardianWeeklySubscription(subscription, stoppedPublicationDate)
     val credit = currentGuardianWeeklySubscription.right.value.credit
-    credit shouldBe HolidayStopCredit(-2.31, LocalDate.parse("2020-09-02"))
+    credit shouldBe HolidayStopCredit(-2.31, LocalDate.parse("2019-09-02"))
   }
 
   it should "be correct for another quarterly billing period" in {
@@ -27,7 +27,7 @@ class HolidayCreditTest extends FlatSpec with Matchers with EitherValues {
     val subscription = Fixtures.mkSubscription().copy(ratePlans = ratePlans)
     val currentGuardianWeeklySubscription = GuardianWeeklySubscription(subscription, stoppedPublicationDate)
     val credit = currentGuardianWeeklySubscription.right.value.credit
-    credit shouldBe HolidayStopCredit(-2.89, LocalDate.parse("2020-09-02"))
+    credit shouldBe HolidayStopCredit(-2.89, LocalDate.parse("2019-09-02"))
   }
 
   it should "be correct for an annual billing period" in {
@@ -36,6 +36,6 @@ class HolidayCreditTest extends FlatSpec with Matchers with EitherValues {
     val subscription = Fixtures.mkSubscription().copy(ratePlans = ratePlans)
     val currentGuardianWeeklySubscription = GuardianWeeklySubscription(subscription, stoppedPublicationDate)
     val credit = currentGuardianWeeklySubscription.right.value.credit
-    credit shouldBe HolidayStopCredit(-2.31, LocalDate.parse("2020-09-02"))
+    credit shouldBe HolidayStopCredit(-2.31, LocalDate.parse("2019-09-02"))
   }
 }
