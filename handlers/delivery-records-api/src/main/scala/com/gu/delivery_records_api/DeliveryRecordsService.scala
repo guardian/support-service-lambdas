a1ae139e826fbf93d6b74c66e4664b4ab25e604f
Francis Rhys-Jones <francis@rhys-jones.com>
2019-11-26 10:39:15 +0000

    fixing result format for query

--- handlers/delivery-records-api/src/main/scala/com/gu/delivery_records_api/DeliveryRecordsService.scala
+++ handlers/delivery-records-api/src/main/scala/com/gu/delivery_records_api/DeliveryRecordsService.scala
@@ -4,6 +4,7 @@ import java.time.LocalDate
 
 import cats.data.EitherT
 import cats.effect.IO
+import com.gu.salesforce.RecordsWrapperCaseClass
 import com.gu.salesforce.sttp.SalesforceClient
 import io.circe.generic.auto._
 
@@ -20,6 +21,11 @@ trait DeliveryRecordsService {
 }
 
 object DeliveryRecordsService {
+
+  private case class SubscriptionRecordQueryResult(
+    Delivery_Records__r: Option[RecordsWrapperCaseClass[DeliveryRecordQueryResult]]
+  )
+
   private case class DeliveryRecordQueryResult(
     Composite_Key__c: Option[String],
     Delivery_Instructions__c: Option[String],
@@ -28,22 +34,25 @@ object DeliveryRecordsService {
 
   def apply(salesforceClient: SalesforceClient[IO]): DeliveryRecordsService = new DeliveryRecordsService {
     override def getDeliveryRecordsForSubscription(subscriptionId: String): EitherT[IO, DeliveryRecordServiceError, List[DeliveryRecord]] =
-      salesforceClient.query[DeliveryRecord](
+      salesforceClient.query[SubscriptionRecordQueryResult](
         s"SELECT ( SELECT Composite_Key__c, Delivery_Instructions__c, Delivery_Address__c FROM Delivery_Records__r ) " +
           "FROM SF_Subscription__c " +
           s"WHERE Name = '$subscriptionId'"
       ).bimap(
-        error => DeliveryRecordServiceError(error.toString),
-        { queryResult =>
-          queryResult.records.map( queryRecord =>
-            DeliveryRecord(
-              key = queryRecord.key,
-              deliveryInstruction = queryRecord.deliveryInstruction,
-              deliveryAddress = queryRecord.deliveryAddress
-            )
-          )
-        }
-      )
+          error => DeliveryRecordServiceError(error.toString),
+          { queryResult =>
+            queryResult
+              .records
+              .flatMap(_.Delivery_Records__r)
+              .flatMap(_.records)
+              .map(queryRecord =>
+                DeliveryRecord(
+                  key = queryRecord.Composite_Key__c,
+                  deliveryInstruction = queryRecord.Delivery_Instructions__c,
+                  deliveryAddress = queryRecord.Delivery_Address__c
+                ))
+          }
+        )
   }
 }
 
