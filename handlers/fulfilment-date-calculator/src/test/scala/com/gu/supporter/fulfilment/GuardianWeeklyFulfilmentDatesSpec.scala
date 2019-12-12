3aa20b7bab3becdb88e3f1f3e82a1dd5d8659c1a
Tom Richards <tom.richards@guardian.co.uk>
2019-12-12 23:40:38 +0000

    fulfilment-date-calculator: make some dates Option

--- handlers/fulfilment-date-calculator/src/test/scala/com/gu/supporter/fulfilment/GuardianWeeklyFulfilmentDatesSpec.scala
+++ handlers/fulfilment-date-calculator/src/test/scala/com/gu/supporter/fulfilment/GuardianWeeklyFulfilmentDatesSpec.scala
@@ -5,18 +5,18 @@ import org.scalatest.{FlatSpec, Matchers}
 class GuardianWeeklyFulfilmentDatesSpec extends FlatSpec with Matchers with DateSupport {
 
   "GuardianWeeklyFulfilmentDates" should "calculate deliveryAddressChangeEffectiveDate" in {
-    GuardianWeeklyFulfilmentDates("2019-12-02")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-13")
-    GuardianWeeklyFulfilmentDates("2019-12-03")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-13")
-    GuardianWeeklyFulfilmentDates("2019-12-04")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-13")
-    GuardianWeeklyFulfilmentDates("2019-12-05")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-20")
-    GuardianWeeklyFulfilmentDates("2019-12-06")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-20")
-    GuardianWeeklyFulfilmentDates("2019-12-07")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-20")
-    GuardianWeeklyFulfilmentDates("2019-12-08")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-20")
-    GuardianWeeklyFulfilmentDates("2019-12-09")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-20")
-    GuardianWeeklyFulfilmentDates("2019-12-10")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-20")
-    GuardianWeeklyFulfilmentDates("2019-12-11")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-20")
-    GuardianWeeklyFulfilmentDates("2019-12-12")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-27")
-    GuardianWeeklyFulfilmentDates("2019-12-13")("Friday").deliveryAddressChangeEffectiveDate should equalDate("2019-12-27")
+    GuardianWeeklyFulfilmentDates("2019-12-02")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-13")
+    GuardianWeeklyFulfilmentDates("2019-12-03")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-13")
+    GuardianWeeklyFulfilmentDates("2019-12-04")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-13")
+    GuardianWeeklyFulfilmentDates("2019-12-05")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-20")
+    GuardianWeeklyFulfilmentDates("2019-12-06")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-20")
+    GuardianWeeklyFulfilmentDates("2019-12-07")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-20")
+    GuardianWeeklyFulfilmentDates("2019-12-08")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-20")
+    GuardianWeeklyFulfilmentDates("2019-12-09")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-20")
+    GuardianWeeklyFulfilmentDates("2019-12-10")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-20")
+    GuardianWeeklyFulfilmentDates("2019-12-11")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-20")
+    GuardianWeeklyFulfilmentDates("2019-12-12")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-27")
+    GuardianWeeklyFulfilmentDates("2019-12-13")("Friday").deliveryAddressChangeEffectiveDate.get should equalDate("2019-12-27")
   }
 
   it should "calculate holidayStopFirstAvailableDate" in {
@@ -35,18 +35,18 @@ class GuardianWeeklyFulfilmentDatesSpec extends FlatSpec with Matchers with Date
   }
 
   it should "calculate finalFulfilmentFileGenerationDate" in {
-    GuardianWeeklyFulfilmentDates("2019-12-02")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-05")
-    GuardianWeeklyFulfilmentDates("2019-12-03")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-05")
-    GuardianWeeklyFulfilmentDates("2019-12-04")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-05")
-    GuardianWeeklyFulfilmentDates("2019-12-05")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-12")
-    GuardianWeeklyFulfilmentDates("2019-12-06")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-12")
-    GuardianWeeklyFulfilmentDates("2019-12-07")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-12")
-    GuardianWeeklyFulfilmentDates("2019-12-08")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-12")
-    GuardianWeeklyFulfilmentDates("2019-12-09")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-12")
-    GuardianWeeklyFulfilmentDates("2019-12-10")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-12")
-    GuardianWeeklyFulfilmentDates("2019-12-11")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-12")
-    GuardianWeeklyFulfilmentDates("2019-12-12")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-19")
-    GuardianWeeklyFulfilmentDates("2019-12-13")("Friday").finalFulfilmentFileGenerationDate should equalDate("2019-12-19")
+    GuardianWeeklyFulfilmentDates("2019-12-02")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-05")
+    GuardianWeeklyFulfilmentDates("2019-12-03")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-05")
+    GuardianWeeklyFulfilmentDates("2019-12-04")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-05")
+    GuardianWeeklyFulfilmentDates("2019-12-05")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-12")
+    GuardianWeeklyFulfilmentDates("2019-12-06")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-12")
+    GuardianWeeklyFulfilmentDates("2019-12-07")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-12")
+    GuardianWeeklyFulfilmentDates("2019-12-08")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-12")
+    GuardianWeeklyFulfilmentDates("2019-12-09")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-12")
+    GuardianWeeklyFulfilmentDates("2019-12-10")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-12")
+    GuardianWeeklyFulfilmentDates("2019-12-11")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-12")
+    GuardianWeeklyFulfilmentDates("2019-12-12")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-19")
+    GuardianWeeklyFulfilmentDates("2019-12-13")("Friday").finalFulfilmentFileGenerationDate.get should equalDate("2019-12-19")
   }
 
 }
