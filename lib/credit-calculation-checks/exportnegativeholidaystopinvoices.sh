51cd7678503fab608f92cddca89a2d8c9201ab75
Francis Rhys-Jones <francis@rhys-jones.com>
2020-01-24 11:25:41 +0000

    changing query name

--- lib/credit-calculation-checks/exportnegativeholidaystopinvoices.sh
+++ lib/credit-calculation-checks/exportnegativeholidaystopinvoices.sh
@@ -9,7 +9,7 @@ curl --location --request POST 'https://rest.zuora.com/v1/batch-query/' \
 "useQueryLabels" : "true",
 "dateTimeUtc" : "true",
 "queries"  : [ {
-    "name" : "ExportInvoiceItemsTool",
+    "name" : "ExportInvoiceItemsTool-Negative-Holiday-Stop-Invoices",
     "query" : "SELECT Invoice.InvoiceDate, Invoice.InvoiceNumber, Subscription.Name, avg(Invoice.Amount) AS InvoiceAmount, sum(InvoiceItem.ChargeAmount) AS InvoiceItemTotalAmount FROM InvoiceItem WHERE Invoice.InvoiceDate > '\''2019-06-01'\'' AND Invoice.Amount <= 0 AND RatePlan.Name = '\''DO NOT USE MANUALLY: Holiday Credit - automated'\'' AND RatePlanCharge.Name = '\''Holiday Credit'\'' GROUP BY Invoice.InvoiceDate, Invoice.InvoiceNumber, Subscription.Name",
     "type" : "zoqlexport"
   }]
