fe455b814ee99b15bb2216678a79accabdbad0db
Mario Galic <mario.galic@guardian.co.uk>
2020-02-13 15:49:17 +0000

    Add multiple discounts test

--- lib/zuora-core/src/test/scala/com/gu/zuora/subscription/DiscountTest.scala
+++ lib/zuora-core/src/test/scala/com/gu/zuora/subscription/DiscountTest.scala
@@ -1,52 +1,17 @@
 package com.gu.zuora.subscription
 
-import java.time.DayOfWeek.{SATURDAY, SUNDAY}
-import java.time.temporal.TemporalAdjusters
-import java.time.{DayOfWeek, LocalDate}
+import java.time.LocalDate
+import org.scalactic.TypeCheckedTripleEquals
+import org.scalatest.EitherValues
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-import com.gu.zuora.ZuoraProductTypes
-import com.gu.zuora.subscription.SubscriptionDataIntegrationTest.testSubscriptonDataIssueGeneration
-import org.scalatest.FlatSpec
-
-class DiscountTest extends FlatSpec {
+class DiscountTest extends AnyFlatSpec with Matchers with EitherValues with TypeCheckedTripleEquals {
   "Credit calculation" should "take into account discounts" in {
-    val expectedDiscount = 0.5
-    MutableCalendar.setFakeToday(Some(LocalDate.of(2019, 3, 16)))
-    val startDate = LocalDate.parse("2019-03-16") //Sunday
-    val firstSaturday = startDate.`with`(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY))
-    val firstSunday = startDate.`with`(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY))
-
-    val billDates1 = BillDates(
-      startDate,
-      startDate.plusMonths(1).minusDays(1)
-    )
-    val billDates2 = BillDates(
-      startDate.plusMonths(1),
-      startDate.plusMonths(2).minusDays(1)
-    )
-
-    val expectedIssueData = List(
-      IssueData(firstSaturday, billDates1, -1.28),
-      IssueData(firstSunday, billDates1, -1.32),
-      IssueData(firstSaturday.plusWeeks(1), billDates1, -1.28),
-      IssueData(firstSunday.plusWeeks(1), billDates1, -1.32),
-      IssueData(firstSaturday.plusWeeks(2), billDates1, -1.28),
-      IssueData(firstSunday.plusWeeks(2), billDates1, -1.32),
-      IssueData(firstSaturday.plusWeeks(3), billDates1, -1.28),
-      IssueData(firstSunday.plusWeeks(3), billDates1, -1.32),
-      IssueData(firstSaturday.plusWeeks(4), billDates1, -1.28),
-      IssueData(firstSunday.plusWeeks(4), billDates1, -1.32),
-      IssueData(firstSaturday.plusWeeks(5), billDates2, -1.28),
-      IssueData(firstSunday.plusWeeks(5), billDates2, -1.32),
-    )
-
-    testSubscriptonDataIssueGeneration(
-      subscriptionFile = "Discounts.json",
-      startDate = startDate,
-      expectedIssueData = expectedIssueData,
-      expectedTotalAnnualIssueLimitPerSubscription = 8,
-      expectedProductType = ZuoraProductTypes.NewspaperVoucherBook,
-      expectedEditionDaysOfWeek = List(SATURDAY, SUNDAY)
-    )
+    val subscription = Fixtures.subscriptionFromJson("Discounts.json")
+    val account = Fixtures.mkAccount()
+    val subscriptionData = SubscriptionData(subscription, account).right.value
+    subscriptionData.issueDataForDate(LocalDate.parse("2020-02-28")).right.value.credit should ===(-1.74)
+    subscriptionData.issueDataForDate(LocalDate.parse("2020-03-06")).right.value.credit should ===(-1.39)
   }
 }
