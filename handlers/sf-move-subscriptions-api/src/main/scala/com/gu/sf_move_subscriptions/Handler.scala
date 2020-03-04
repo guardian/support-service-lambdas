25eb3febebf6b0f3c4c908992a9b8760541ee7f6
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-03-04 15:22:30 +0000

    lambda handler boilerplate wip

---
+++ handlers/sf-move-subscriptions-api/src/main/scala/com/gu/sf_move_subscriptions/Handler.scala
@@ -0,0 +1,10 @@
+package com.gu.sf_move_subscriptions
+import com.gu.util.Logging
+
+object Handler extends Logging {
+
+  def apply(): Unit = {
+    logger.info("Starting sf_move_subscriptions lambda")
+  }
+
+}
