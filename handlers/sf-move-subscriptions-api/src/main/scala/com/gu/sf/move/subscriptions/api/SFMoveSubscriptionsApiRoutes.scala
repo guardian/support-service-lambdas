d534e05da58515644ffecc24f07a184178eeee2a
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-03-06 16:06:23 +0000

    fp boilerplate

--- handlers/sf-move-subscriptions-api/src/main/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsApiRoutes.scala
+++ handlers/sf-move-subscriptions-api/src/main/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsApiRoutes.scala
@@ -1,36 +1,41 @@
 package com.gu.sf.move.subscriptions.api
 
-import cats.effect.IO
+import cats.data.EitherT
+import cats.effect.Effect
+import cats.implicits._
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
 import org.http4s.circe.CirceEntityDecoder._
 import org.http4s.circe.CirceEntityEncoder._
-import org.http4s.dsl.io._
-import org.http4s.{DecodeFailure, HttpRoutes, Request}
+import org.http4s.dsl.Http4sDsl
+import org.http4s.{DecodeFailure, HttpRoutes, Request, Response}
 
 object SFMoveSubscriptionsApiRoutes extends LazyLogging {
 
-  def apply(): HttpRoutes[IO] = {
+  def apply[F[_] : Effect](): HttpRoutes[F] = {
+    type RouteResult[A] = EitherT[F, F[Response[F]], A]
+    object http4sDsl extends Http4sDsl[F]
+    import http4sDsl._
 
     val rootInfo = MoveSubscriptionApiRoot("This is the sf-move-subscriptions-api")
 
-    def handleMoveRequest(request: Request[IO]) = {
+    def handleMoveRequest(request: Request[F]): F[Response[F]] = {
       (for {
         reqBody <- request.attemptAs[MoveSubscriptionReqBody]
           .leftMap { decodingFailure: DecodeFailure =>
             BadRequest(MoveSubscriptionApiError(s"Failed to decoded request body: $decodingFailure"))
           }
-        resp <- SFMoveSubscriptionsService.moveSubscription(reqBody)
+        resp <- SFMoveSubscriptionsService().moveSubscription(reqBody)
           .leftMap(
             error =>
               InternalServerError(MoveSubscriptionApiError(s"Failed create voucher: $error"))
           )
-      } yield Ok(resp)).merge.flatMap(x => x)
+      } yield Ok(resp)).merge.flatten
     }
 
-    HttpRoutes.of[IO] {
+    HttpRoutes.of[F] {
       case GET -> Root => Ok(rootInfo)
-      case request @ POST -> Root / "subscription" / "move" =>
+      case request@POST -> Root / "subscription" / "move" =>
         handleMoveRequest(request)
     }
 
--- handlers/sf-move-subscriptions-api/src/main/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsService.scala
+++ handlers/sf-move-subscriptions-api/src/main/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsService.scala
@@ -1,16 +1,21 @@
 package com.gu.sf.move.subscriptions.api
 
+import cats.Monad
 import cats.data.EitherT
-import cats.effect.IO
-import cats.implicits._
 import com.typesafe.scalalogging.LazyLogging
+
+trait SFMoveSubscriptionsService[F[_]] {
+  def moveSubscription(req: MoveSubscriptionReqBody): EitherT[F, MoveSubscriptionApiError, MoveSubscriptionRes]
+}
 
 object SFMoveSubscriptionsService extends LazyLogging {
 
-  def moveSubscription(req: MoveSubscriptionReqBody): EitherT[IO, MoveSubscriptionApiError, MoveSubscriptionRes] = {
-    import req._
-    logger.info(s"attempt to move $zuoraSubscriptionId subscription to $sfAccountId SalesForce Contact")
-    Either.right(MoveSubscriptionRes("subscription moved successfully")).toEitherT
+  def apply[F[_] : Monad](): SFMoveSubscriptionsService[F] = new SFMoveSubscriptionsService[F] {
+    override def moveSubscription(req: MoveSubscriptionReqBody): EitherT[F, MoveSubscriptionApiError, MoveSubscriptionRes] = {
+      import req._
+      logger.info(s"attempt to move $zuoraSubscriptionId subscription to $sfAccountId , $sfFullContactId SalesForce Contact")
+      EitherT.rightT(MoveSubscriptionRes("subscription moved successfully"))
+    }
   }
 
 }
