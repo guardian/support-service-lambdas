75652633147f7d3f57fab2c8ee39ea725c45e7be
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-03-10 16:43:37 +0000

    adding unit test boilerplate

---
+++ handlers/sf-move-subscriptions-api/src/test/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsApiTest.scala
@@ -0,0 +1,11 @@
+package com.gu.sf.move.subscriptions.api
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SFMoveSubscriptionsApiTest extends FlatSpec with Matchers {
+
+  it should "test" in {
+    1 shouldEqual 1
+  }
+
+}
