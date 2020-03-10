973091586bbd664b1025f8b4c8f439569062dad5
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-03-10 16:57:25 +0000

    unit test wip

--- handlers/sf-move-subscriptions-api/src/test/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsApiTest.scala
+++ handlers/sf-move-subscriptions-api/src/test/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsApiTest.scala
@@ -1,11 +1,40 @@
 package com.gu.sf.move.subscriptions.api
 
+
+import cats.effect.IO
+import com.gu.DevIdentity
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.{Method, Request, Uri}
 import org.scalatest.{FlatSpec, Matchers}
+
 
 class SFMoveSubscriptionsApiTest extends FlatSpec with Matchers {
 
+  private val api = createApp()
+
   it should "test" in {
+
+    val response = api.run(
+      Request[IO](
+        method = Method.POST,
+        uri = Uri(path = "/subscription/move")
+      ).withEntity[String](
+        MoveSubscriptionData(
+          zuoraSubscriptionId = "A-1111",
+          sfAccountId = "2222",
+          sfFullContactId = "3333",
+        ).asJson.spaces2)
+    ).value.unsafeRunSync().get
+
+    println(response)
+
     1 shouldEqual 1
+  }
+
+  private def createApp() = {
+    SFMoveSubscriptionsApiApp(DevIdentity("sf-move-subscriptions-api")).value.unsafeRunSync()
+      .right.get
   }
 
 }
