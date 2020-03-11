7fada027b008dc0ae31adf1a7ab11a6aac7abfe9
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-03-11 15:56:24 +0000

    excluding SFMoveSubscriptionsService logic

--- handlers/sf-move-subscriptions-api/src/main/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsService.scala
+++ handlers/sf-move-subscriptions-api/src/main/scala/com/gu/sf/move/subscriptions/api/SFMoveSubscriptionsService.scala
@@ -2,47 +2,16 @@ package com.gu.sf.move.subscriptions.api
 
 import cats.Monad
 import cats.data.EitherT
-import cats.implicits._
-import com.gu.zuora.Zuora.ZuoraAccountMoveSubscriptionCommand
-import com.gu.zuora._
-import com.gu.zuora.subscription._
-import com.softwaremill.sttp._
+import com.softwaremill.sttp.{Id, SttpBackend}
 import com.typesafe.scalalogging.LazyLogging
 
 final case class MoveSubscriptionServiceSuccess(message: String)
 
-final case class MoveSubscriptionServiceError(error: String)
-
 class SFMoveSubscriptionsService[F[_] : Monad](apiCfg: MoveSubscriptionApiConfig, backend: SttpBackend[Id, Nothing]) extends LazyLogging {
-
-  private val ZuoraConfig = ZuoraRestOauthConfig(
-    baseUrl = apiCfg.zuoraBaseUrl,
-    oauth = Oauth(
-      clientId = apiCfg.zuoraClientId,
-      clientSecret = apiCfg.zuoraSecret
-    )
-  )
-
-  def moveSubscription(moveSubscriptionData: MoveSubscriptionReqBody): EitherT[F, MoveSubscriptionServiceError,
-    MoveSubscriptionServiceSuccess] = {
-    import moveSubscriptionData._
-    logger.info(s"attempt to move $zuoraSubscriptionId subscription to " +
-      s"(Account Id=$sfAccountId ,Full contact Id=$sfFullContactId) SalesForce Contact")
-
-    val moveSubCommand = ZuoraAccountMoveSubscriptionCommand(
-      crmId = sfAccountId,
-      sfContactId__c = sfFullContactId
-    )
-    import Zuora.{accessTokenGetResponseV2, subscriptionGetResponse, updateAccountByMovingSubscription}
-    val updateResponse = for {
-      token <- accessTokenGetResponseV2(ZuoraConfig, backend)
-      subscription <- subscriptionGetResponse(ZuoraConfig, token, backend)(SubscriptionName(zuoraSubscriptionId))
-      updateRes <- updateAccountByMovingSubscription(ZuoraConfig, token, backend)(subscription, moveSubCommand)
-    } yield updateRes
-
-    updateResponse.toEitherT[F]
-      .leftMap(err => MoveSubscriptionServiceError(err.toString))
-      .map(status => MoveSubscriptionServiceSuccess(status))
+  def moveSubscription(req: MoveSubscriptionReqBody): EitherT[F, MoveSubscriptionApiError, MoveSubscriptionServiceSuccess] = {
+    import req._
+    logger.info(s"attempt to move $zuoraSubscriptionId subscription to $sfAccountId , $sfFullContactId SalesForce Contact")
+    EitherT.rightT(MoveSubscriptionServiceSuccess("subscription moved successfully"))
   }
 }
 
@@ -50,4 +19,3 @@ object SFMoveSubscriptionsService {
   def apply[F[_] : Monad](apiCfg: MoveSubscriptionApiConfig, backend: SttpBackend[Id, Nothing]): SFMoveSubscriptionsService[F] =
     new SFMoveSubscriptionsService(apiCfg, backend)
 }
-
