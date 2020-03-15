bb5dff941561d86d98071b2bc6b20098596df1f8
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-03-15 21:02:13 +0000

    renames

--- generate_cfn_templates.sh
+++ generate_cfn_templates.sh
@@ -3,14 +3,14 @@
 set -e
 
 build_cfn_template() {
-    project_name_param=$1
-    project_name=$project_name_param npm run synth
+    app_name_param=$1
+    app_name=$app_name_param npm run synth
 
-    if [[ -s ../handlers/$project_name_param/cfn.yaml ]]
+    if [[ -s ../handlers/$app_name_param/cfn.yaml ]]
     then
-        printf "SUCCESS: synthesized of $project_name_param stack to $project_name_param/cfn.yaml file was successful"
+        printf "SUCCESS: synthesized of $app_name_param stack to $app_name_param/cfn.yaml file was successful"
     else
-        printf "ERROR: synthesized of $project_name_param stack to $project_name_param/cfn.yaml file failed"
+        printf "ERROR: synthesized of $app_name_param stack to $app_name_param/cfn.yaml file failed"
         printf 1
     fi
 }
