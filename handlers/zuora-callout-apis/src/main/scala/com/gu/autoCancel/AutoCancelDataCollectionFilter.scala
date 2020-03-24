2ff5bd419bf4a14c958b549a0688e8891b24f1ba
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-03-24 19:18:23 +0000

    logic for auto cancellation for accounts with multi active subscriptions

--- handlers/zuora-callout-apis/src/main/scala/com/gu/autoCancel/AutoCancelDataCollectionFilter.scala
+++ handlers/zuora-callout-apis/src/main/scala/com/gu/autoCancel/AutoCancelDataCollectionFilter.scala
@@ -3,65 +3,61 @@ package com.gu.autoCancel
 import java.time.LocalDate
 
 import com.gu.autoCancel.AutoCancel.AutoCancelRequest
+import com.gu.stripeCustomerSourceUpdated.TypeConvert._
 import com.gu.util.Logging
 import com.gu.util.apigateway.ApiGatewayResponse.noActionRequired
 import com.gu.util.reader.Types.ApiGatewayOp._
 import com.gu.util.reader.Types._
 import com.gu.util.resthttp.Types.ClientFailableOp
-import com.gu.util.zuora.ZuoraGetAccountSummary.{AccountSummary, Invoice, SubscriptionId}
-import com.gu.stripeCustomerSourceUpdated.TypeConvert._
+import com.gu.util.zuora.SubscriptionNumber
+import com.gu.util.zuora.ZuoraGetAccountSummary.{AccountSummary, Invoice}
 
 object AutoCancelDataCollectionFilter extends Logging {
 
   def apply(
     now: LocalDate,
-    getAccountSummary: String => ClientFailableOp[AccountSummary]
-  )(autoCancelCallout: AutoCancelCallout): ApiGatewayOp[AutoCancelRequest] = {
-    val accountId = autoCancelCallout.accountId
+    getAccountSummary: String => ClientFailableOp[AccountSummary],
+    getSubsNamesOnInvoice: String => ClientFailableOp[List[SubscriptionNumber]]
+  )(autoCancelCallout: AutoCancelCallout): ApiGatewayOp[List[AutoCancelRequest]] = {
+    import autoCancelCallout._
+
     for {
       accountSummary <- getAccountSummary(accountId).toApiGatewayOp("getAccountSummary").withLogging("getAccountSummary")
-      subToCancel <- getSubscriptionToCancel(accountSummary).withLogging("getSubscriptionToCancel")
-      cancellationDate <- getCancellationDateFromInvoices(accountSummary, now).withLogging("getCancellationDateFromInvoices")
-    } yield AutoCancelRequest(accountId, subToCancel, cancellationDate)
+      subsNames <- getSubsNamesOnInvoice(invoiceId).toApiGatewayOp("getSubsNamesOnInvoice").withLogging("getSubsNamesOnInvoice")
+      cancellationDate <- getCancellationDateFromInvoice(invoiceId, accountSummary, now).withLogging("getCancellationDateFromInvoice")
+    } yield subsNames
+      .map { subToCancel =>
+        AutoCancelRequest(accountId, subToCancel, cancellationDate)
+      }
   }
 
-  def getCancellationDateFromInvoices(accountSummary: AccountSummary, dateToday: LocalDate): ApiGatewayOp[LocalDate] = {
-    val unpaidAndOverdueInvoices = accountSummary.invoices.filter { invoice => invoiceOverdue(invoice, dateToday) }
-    if (unpaidAndOverdueInvoices.isEmpty) {
-      logger.error(s"Failed to find an unpaid invoice that was overdue. The invoices we got were: ${accountSummary.invoices}")
-      ReturnWithResponse(noActionRequired("No unpaid and overdue invoices found!"))
-    } else {
-      logger.info(s"Found at least one unpaid invoices for account: ${accountSummary.basicInfo.id}. Invoice id(s): ${unpaidAndOverdueInvoices.map(_.id)}")
-      implicit val localDateOrdering: Ordering[LocalDate] = Ordering.by(_.toEpochDay)
-      val earliestDueDate = unpaidAndOverdueInvoices.map(_.dueDate).min
-      logger.info(s"Earliest overdue invoice for account ${accountSummary.basicInfo.id} has due date: $earliestDueDate. Setting this as the cancellation date.")
-      ContinueProcessing(earliestDueDate)
-    }
+  def getCancellationDateFromInvoice(invoiceId: String, accountSummary: AccountSummary, dateToday: LocalDate): ApiGatewayOp[LocalDate] = {
+    accountSummary.invoices
+      .find(inv => {
+        logger.info(s"found callout invoice in accountSummary: $inv")
+        inv.id == invoiceId
+      })
+      .find(invoiceOverdue(_, dateToday)) match {
+        case None =>
+          logger.error(s"Failed on Validating Unpaid invoice that was overdue, invoiceId: $invoiceId")
+          ReturnWithResponse(noActionRequired("No unpaid and overdue invoices found!"))
+        case Some(inv) =>
+          logger.info(s"Found Valid Unpaid invoice for account: ${accountSummary.basicInfo.id}. Invoice: ${inv}")
+          ContinueProcessing(inv.dueDate)
+      }
   }
 
   def invoiceOverdue(invoice: Invoice, dateToday: LocalDate): Boolean = {
+    /**
+     * TODO invoice.balance > 0 may not be necessary as the trigger is on Invoice payment overdue event
+     * but i am not sure at this moment
+     */
     if (invoice.balance > 0 && invoice.status == "Posted") {
       val zuoraGracePeriod = 14L // This needs to match with the timeframe for the 3rd payment retry attempt in Zuora
       val invoiceOverdueDate = invoice.dueDate.plusDays(zuoraGracePeriod)
       logger.info(s"Zuora grace period is: $zuoraGracePeriod days. Due date for Invoice id ${invoice.id} is ${invoice.dueDate}, so it will be considered overdue on: $invoiceOverdueDate.")
       dateToday.isEqual(invoiceOverdueDate) || dateToday.isAfter(invoiceOverdueDate)
     } else false
-  }
-
-  def getSubscriptionToCancel(accountSummary: AccountSummary): ApiGatewayOp[SubscriptionId] = {
-    val activeSubs = accountSummary.subscriptions.filter(_.status == "Active")
-    activeSubs match {
-      case sub :: Nil =>
-        logger.info(s"Determined that we should cancel SubscriptionId: ${sub.id} (for AccountId: ${accountSummary.basicInfo.id})")
-        ContinueProcessing(sub.id)
-      case Nil =>
-        logger.error(s"Didn't find any active subscriptions. The full list of subs for this account was: ${accountSummary.subscriptions}")
-        ReturnWithResponse(noActionRequired("No Active subscriptions to cancel!"))
-      case subs =>
-        // This should be a pretty rare scenario, because the Billing Account to Sub relationship is (supposed to be) 1-to-1
-        logger.error(s"More than one subscription is Active on account: ${accountSummary.basicInfo.id}. Subscription ids are: ${activeSubs.map(_.id)}")
-        ReturnWithResponse(noActionRequired("More than one active sub found!")) // Don't continue because we don't know which active sub to cancel
-    }
   }
 
 }
--- handlers/zuora-callout-apis/src/main/scala/com/gu/util/zuora/ZuoraUpdateCancellationReason.scala
+++ handlers/zuora-callout-apis/src/main/scala/com/gu/util/zuora/ZuoraUpdateCancellationReason.scala
@@ -2,7 +2,6 @@ package com.gu.util.zuora
 
 import com.gu.util.resthttp.RestRequestMaker.Requests
 import com.gu.util.resthttp.Types.{ClientFailableOp, ClientSuccess}
-import com.gu.util.zuora.ZuoraGetAccountSummary.SubscriptionId
 import com.typesafe.scalalogging.LazyLogging
 import play.api.libs.json.{JsSuccess, Json, Reads, Writes}
 
@@ -19,15 +18,15 @@ object ZuoraUpdateCancellationReason extends LazyLogging {
   implicit val unitReads: Reads[Unit] =
     Reads(_ => JsSuccess(()))
 
-  private def toBodyAndPath(subscription: SubscriptionId) =
-    (SubscriptionUpdate("System AutoCancel"), s"subscriptions/${subscription.id}")
+  private def toBodyAndPath(subscription: SubscriptionNumber) =
+    (SubscriptionUpdate("System AutoCancel"), s"subscriptions/${subscription.value}")
 
-  def apply(requests: Requests)(subscription: SubscriptionId): ClientFailableOp[Unit] = {
+  def apply(requests: Requests)(subscription: SubscriptionNumber): ClientFailableOp[Unit] = {
     val (body, path) = toBodyAndPath(subscription)
     requests.put(body, path): ClientFailableOp[Unit]
   }
 
-  def dryRun(requests: Requests)(subscription: SubscriptionId): ClientFailableOp[Unit] = {
+  def dryRun(requests: Requests)(subscription: SubscriptionNumber): ClientFailableOp[Unit] = {
     val (body, path) = toBodyAndPath(subscription)
     val msg = s"DryRun for ZuoraUpdateCancellationReason: body=$body, path=$path"
     logger.info(msg)
