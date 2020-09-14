30d6c47b91ed134778aab6376ca4d19d86d0a8a2
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-09-14 13:45:35 +0100

    Add holiday credit products for Prod stage

--- handlers/holiday-stop-processor/src/test/scala/com/gu/holidaystopprocessor/HolidayCreditProductTest.scala
+++ handlers/holiday-stop-processor/src/test/scala/com/gu/holidaystopprocessor/HolidayCreditProductTest.scala
@@ -11,26 +11,27 @@ class HolidayCreditProductTest extends AnyFlatSpec with Matchers {
     val subscription = Fixtures.subscriptionFromJson("GuardianWeeklyWith6For6.json")
     HolidayCreditProduct.forStage(Dev)(subscription) shouldBe HolidayCreditProduct.Dev.GuardianWeekly
     HolidayCreditProduct.forStage(Code)(subscription) shouldBe HolidayCreditProduct.Code.GuardianWeekly
-    HolidayCreditProduct.forStage(Prod)(subscription) shouldBe HolidayCreditProduct.Prod
+    HolidayCreditProduct.forStage(Prod)(subscription) shouldBe HolidayCreditProduct.Prod.GuardianWeekly
   }
 
   it should "give correct credit product for a Home Delivery subscription" in {
     val subscription = Fixtures.subscriptionFromJson("EchoLegacySubscription.json")
     HolidayCreditProduct.forStage(Dev)(subscription) shouldBe HolidayCreditProduct.Dev.HomeDelivery
     HolidayCreditProduct.forStage(Code)(subscription) shouldBe HolidayCreditProduct.Code.HomeDelivery
-    HolidayCreditProduct.forStage(Prod)(subscription) shouldBe HolidayCreditProduct.Prod
+    HolidayCreditProduct.forStage(Prod)(subscription) shouldBe HolidayCreditProduct.Prod.HomeDelivery
   }
 
   it should "give correct credit product for a Voucher subscription" in {
     val subscription = Fixtures.subscriptionFromJson("SundayVoucherSubscription.json")
     HolidayCreditProduct.forStage(Dev)(subscription) shouldBe HolidayCreditProduct.Dev.Voucher
     HolidayCreditProduct.forStage(Code)(subscription) shouldBe HolidayCreditProduct.Code.Voucher
-    HolidayCreditProduct.forStage(Prod)(subscription) shouldBe HolidayCreditProduct.Prod
+    HolidayCreditProduct.forStage(Prod)(subscription) shouldBe HolidayCreditProduct.Prod.Voucher
   }
 
   it should "throw an exception when subscription has no applicable rate plan" in {
     val subscription = Fixtures.subscriptionFromJson("AlternativeSubscription.json")
     the[IllegalArgumentException] thrownBy HolidayCreditProduct.forStage(Dev)(subscription) should have message "No holiday credit product available for subscription A-S00051570"
     the[IllegalArgumentException] thrownBy HolidayCreditProduct.forStage(Code)(subscription) should have message "No holiday credit product available for subscription A-S00051570"
+    the[IllegalArgumentException] thrownBy HolidayCreditProduct.forStage(Prod)(subscription) should have message "No holiday credit product available for subscription A-S00051570"
   }
 }
