93ff6e9022548b64ba0cedfc4af70956bc80e6a9
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-09-14 12:02:50 +0100

    Rename Fixtures to avoid confusion with scalatest

--- handlers/delivery-problem-credit-processor/src/test/scala/com/gu/deliveryproblemcreditprocessor/TestFixtures.scala
+++ handlers/delivery-problem-credit-processor/src/test/scala/com/gu/deliveryproblemcreditprocessor/TestFixtures.scala
@@ -7,7 +7,7 @@ import org.scalatest.Assertions
 
 import scala.io.Source
 
-object Fixtures extends Assertions {
+object TestFixtures extends Assertions {
   def subscriptionFromJson(resource: String): Subscription = {
     val subscriptionRaw = Source.fromResource(resource).mkString
     decode[Subscription](subscriptionRaw).getOrElse(fail(s"Could not decode $subscriptionRaw"))
