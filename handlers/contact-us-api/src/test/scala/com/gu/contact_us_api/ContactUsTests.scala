b17a08f0da84d6e39cbb747b614308f22d38abb0
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-10-05 20:40:53 +0100

    Delete unneeded test file

--- handlers/contact-us-api/src/test/scala/com/gu/contact_us_api/ContactUsTests.scala
+++
@@ -1,8 +0,0 @@
-package com.gu.contact_us_api
-
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should
-
-class ContactUsTests extends AnyFlatSpec with should.Matchers {
-
-}
