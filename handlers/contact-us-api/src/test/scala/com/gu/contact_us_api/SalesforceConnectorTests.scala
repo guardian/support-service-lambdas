c9fd7cc1c50a994635f4072b283e3ac1b9d4e5a8
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-10-06 10:36:44 +0100

    Expand SalesforceConnector's unit tests

--- handlers/contact-us-api/src/test/scala/com/gu/contact_us_api/SalesforceConnectorTests.scala
+++ handlers/contact-us-api/src/test/scala/com/gu/contact_us_api/SalesforceConnectorTests.scala
@@ -1,12 +1,13 @@
 package com.gu.contact_us_api
 
 import com.gu.contact_us_api.models.ContactUsTestVars.{testEmail, testMessage, testName, testSubject, testTopic}
-import com.gu.contact_us_api.models.{ContactUsConfig, ContactUsError, SFCaseRequest, SFCompositeRequest}
+import com.gu.contact_us_api.models.{ContactUsConfig, ContactUsError, SFCaseRequest, SFCompositeRequest, SFErrorDetails}
+import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 import scalaj.http.{HttpRequest, HttpResponse}
 
-class SalesforceConnectorTests extends AnyFlatSpec with should.Matchers {
+class SalesforceConnectorTests extends AnyFlatSpec with should.Matchers with EitherValues {
 
   def getRunRequest(body: String, statusCode: Int = 200): HttpRequest => Either[ContactUsError, HttpResponse[String]] = {
     def mockedRunRequest(http: HttpRequest): Either[ContactUsError, HttpResponse[String]] = {
@@ -18,6 +19,7 @@ class SalesforceConnectorTests extends AnyFlatSpec with should.Matchers {
 
   val fakeConfg: ContactUsConfig = ContactUsConfig("clientID", "clientSecret", "username", "password", "token", "authEndpoint", "reqEndpoint")
   val fakeRequest: SFCompositeRequest = SFCompositeRequest(List(SFCaseRequest(testTopic, None, None, testName, testEmail, testSubject, testMessage)))
+  val invalidJson = "{}"
 
   val authToken = "THIS_IS_A_TOKEN"
   val successfulAuthRequestJson = s"""{ "access_token": "$authToken" }"""
@@ -27,12 +29,17 @@ class SalesforceConnectorTests extends AnyFlatSpec with should.Matchers {
   val failedAuthErrorDescription = "authentication failure"
   val failedAuthReqJson = s"""{ "error": "$failedAuthError", "error_description": "$failedAuthErrorDescription" }"""
 
-  val successfulCompositeReqJson = """{ "compositeResponse": [{ "httpStatusCode": 201, "referenceId": "newCase" }] }"""
+  val compositeReferenceId = "newCase"
+  val compositeErrorCode = "ERROR"
+  val compositeErrorMessage = "request failure"
+
+  val successfulCompositeReqJson = s"""{ "compositeResponse": [{ "httpStatusCode": 201, "referenceId": "$compositeReferenceId" }] }"""
+
+  val compositeErrorStatusCode = 400
+  val successfulCompositeReqWithErrorJson = s"""{ "compositeResponse": [{ "httpStatusCode": $compositeErrorStatusCode, "referenceId": "$compositeReferenceId", "body": [{ "errorCode": "$compositeErrorCode", "message": "$compositeErrorMessage" }] }] }"""
 
   val failedCompositeStatusCode = 500
-  val failedCompositeError = "ERROR"
-  val failedCompositeErrorDescription = "request failure"
-  val failedCompositeReqJson = s"""[{ "errorCode": "${failedCompositeError}", "message": "${failedCompositeErrorDescription}" }]"""
+  val failedCompositeReqJson = s"""[{ "errorCode": "$compositeErrorCode", "message": "$compositeErrorMessage" }]"""
 
   "auth" should "return a token when the request is successful " in {
     new SalesforceConnector(getRunRequest(successfulAuthRequestJson))
@@ -45,15 +52,57 @@ class SalesforceConnectorTests extends AnyFlatSpec with should.Matchers {
       Left(ContactUsError("Salesforce", s"Could not authenticate: Status code: $failedAuthStatusCode. $failedAuthError - $failedAuthErrorDescription"))
   }
 
+  it should "return a ContactUsError of type Decoder when it receives as unexpected response body in a 2xx response" in {
+    val result2xx = new SalesforceConnector(getRunRequest(invalidJson))
+      .auth(fakeConfg)
+
+    result2xx.isLeft shouldBe true
+    result2xx.left.value shouldBe a[ContactUsError]
+    result2xx.left.value.errorType shouldBe "Decode"
+  }
+
+  it should "return a ContactUsError of type Decoder when it receives as unexpected response body in a non-2xx response" in {
+    val result500 = new SalesforceConnector(getRunRequest(invalidJson, 500))
+      .auth(fakeConfg)
+
+    result500.isLeft shouldBe true
+    result500.left.value shouldBe a[ContactUsError]
+    result500.left.value.errorType shouldBe "Decode"
+  }
+
   "sendReq" should "return Right() when the request is successful " in {
     new SalesforceConnector(getRunRequest(successfulCompositeReqJson))
       .sendReq(fakeConfg, authToken, fakeRequest) shouldBe Right(())
   }
 
+  it should "return ContactUsError when the request is successful but reports an error" in {
+    new SalesforceConnector(getRunRequest(successfulCompositeReqWithErrorJson))
+      .sendReq(fakeConfg, authToken, fakeRequest) shouldBe
+      Left(ContactUsError("Salesforce", s"Could not complete composite request. Status code: 200. Status code for $compositeReferenceId: $compositeErrorStatusCode ${SFErrorDetails(compositeErrorCode, compositeErrorMessage).asString}"))
+  }
+
   it should "return a ContactUsError when the request fails" in {
     new SalesforceConnector(getRunRequest(failedCompositeReqJson, failedCompositeStatusCode))
       .sendReq(fakeConfg, authToken, fakeRequest) shouldBe
-      Left(ContactUsError("Salesforce", s"Could not complete request. Status code: $failedCompositeStatusCode. $failedCompositeError - $failedCompositeErrorDescription"))
+      Left(ContactUsError("Salesforce", s"Could not complete request. Status code: $failedCompositeStatusCode. $compositeErrorCode - $compositeErrorMessage"))
+  }
+
+  it should "return a ContactUsError of type Decoder when it receives as unexpected response body in a 2xx response" in {
+    val result2xx = new SalesforceConnector(getRunRequest(invalidJson))
+      .sendReq(fakeConfg, authToken, fakeRequest)
+
+    result2xx.isLeft shouldBe true
+    result2xx.left.value shouldBe a[ContactUsError]
+    result2xx.left.value.errorType shouldBe "Decode"
+  }
+
+  it should "return a ContactUsError of type Decoder when it receives as unexpected response body in a non-2xx response" in {
+    val result500 = new SalesforceConnector(getRunRequest(invalidJson, 500))
+      .sendReq(fakeConfg, authToken, fakeRequest)
+
+    result500.isLeft shouldBe true
+    result500.left.value shouldBe a[ContactUsError]
+    result500.left.value.errorType shouldBe "Decode"
   }
 
 }
