6ee96a7929664ee696bdfafd45794cf756dc5bdf
John Duffell <john.duffell@guardian.co.uk>
2020-10-06 13:39:32 +0100

    also clean test accounts as well as subs

--- handlers/dev-env-cleaner/src/main/scala/com/gu/cleaner/Handler.scala
+++ handlers/dev-env-cleaner/src/main/scala/com/gu/cleaner/Handler.scala
@@ -132,7 +132,7 @@ class Steps(log: String => Unit) {
       accounts_to_cancel,
       """select Id
         |from Account
-        |where billtocontact.WorkEmail = 'integration-test@gu.com' and Status = 'Active'
+        |where (billtocontact.WorkEmail = 'integration-test@gu.com' OR billtocontact.WorkEmail = 'test@gu.com') and Status = 'Active'
         |""".stripMargin
     )
     val request = AquaQueryRequest(
