2affd5af0ad4d32713d223e4897d7e390a8e2464
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-10-07 11:05:20 +0100

    Add Prod delivery-problem credit products (#697)

--- handlers/delivery-problem-credit-processor/src/test/scala/com/gu/deliveryproblemcreditprocessor/DeliveryCreditProductTest.scala
+++ handlers/delivery-problem-credit-processor/src/test/scala/com/gu/deliveryproblemcreditprocessor/DeliveryCreditProductTest.scala
@@ -10,19 +10,20 @@ class DeliveryCreditProductTest extends AnyFlatSpec with Matchers {
     val subscription = TestFixtures.subscriptionFromJson("GuardianWeeklyWith6For6.json")
     DeliveryCreditProduct.forStage(Dev)(subscription) shouldBe DeliveryCreditProduct.Dev.GuardianWeekly
     DeliveryCreditProduct.forStage(Code)(subscription) shouldBe DeliveryCreditProduct.Code.GuardianWeekly
-    DeliveryCreditProduct.forStage(Prod)(subscription) shouldBe DeliveryCreditProduct.Prod
+    DeliveryCreditProduct.forStage(Prod)(subscription) shouldBe DeliveryCreditProduct.Prod.GuardianWeekly
   }
 
   it should "give correct credit product for a Home Delivery subscription" in {
     val subscription = TestFixtures.subscriptionFromJson("EchoLegacySubscription.json")
     DeliveryCreditProduct.forStage(Dev)(subscription) shouldBe DeliveryCreditProduct.Dev.HomeDelivery
     DeliveryCreditProduct.forStage(Code)(subscription) shouldBe DeliveryCreditProduct.Code.HomeDelivery
-    DeliveryCreditProduct.forStage(Prod)(subscription) shouldBe DeliveryCreditProduct.Prod
+    DeliveryCreditProduct.forStage(Prod)(subscription) shouldBe DeliveryCreditProduct.Prod.HomeDelivery
   }
 
   it should "throw an exception when subscription has no applicable rate plan" in {
     val subscription = TestFixtures.subscriptionFromJson("AlternativeSubscription.json")
     the[IllegalArgumentException] thrownBy DeliveryCreditProduct.forStage(Dev)(subscription) should have message "No delivery credit product available for subscription A-S00051570"
     the[IllegalArgumentException] thrownBy DeliveryCreditProduct.forStage(Code)(subscription) should have message "No delivery credit product available for subscription A-S00051570"
+    the[IllegalArgumentException] thrownBy DeliveryCreditProduct.forStage(Prod)(subscription) should have message "No delivery credit product available for subscription A-S00051570"
   }
 }
