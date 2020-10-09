f67e4fb78d3170b77e2ad52f1d355d756c6cb5e1
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-10-09 17:05:41 +0100

    Linting

--- handlers/sf-billing-account-remover/src/main/scala/com/gu/sf_billing_account_remover/Config.scala
+++ handlers/sf-billing-account-remover/src/main/scala/com/gu/sf_billing_account_remover/Config.scala
@@ -2,13 +2,17 @@ package com.gu.sf_billing_account_remover
 
 case class Config(salesforceConfig: SalesforceConfig, zuoraConfig: ZuoraConfig)
 
-case class ZuoraConfig(apiAccessKeyId: String,
-                       apiSecretAccessKey: String,
-                       zuoraInstanceUrl: String)
+case class ZuoraConfig(
+  apiAccessKeyId: String,
+  apiSecretAccessKey: String,
+  zuoraInstanceUrl: String
+)
 
-case class SalesforceConfig(authUrl: String,
-                            clientId: String,
-                            clientSecret: String,
-                            userName: String,
-                            password: String,
-                            token: String)
+case class SalesforceConfig(
+  authUrl: String,
+  clientId: String,
+  clientSecret: String,
+  userName: String,
+  password: String,
+  token: String
+)
