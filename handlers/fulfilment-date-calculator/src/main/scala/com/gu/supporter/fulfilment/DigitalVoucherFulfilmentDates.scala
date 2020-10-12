b93b7e973c7bfc6b9c4ed6a0e7ed0601797589a6
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-10-12 11:50:35 +0100

    Process digital voucher suspensions day ahead (#735)

--- handlers/fulfilment-date-calculator/src/main/scala/com/gu/supporter/fulfilment/DigitalVoucherFulfilmentDates.scala
+++ handlers/fulfilment-date-calculator/src/main/scala/com/gu/supporter/fulfilment/DigitalVoucherFulfilmentDates.scala
@@ -13,40 +13,43 @@ import scala.collection.immutable.ListMap
 
 object DigitalVoucherFulfilmentDates {
 
-  lazy val VoucherHolidayStopNoticePeriodDays = 1
-  lazy val FulfilmentCutoffDays = NonEmptyList.of(DayOfWeek.MONDAY, DayOfWeek.THURSDAY)
-  lazy val WeekStartDay = DayOfWeek.MONDAY
+  private final val VoucherHolidayStopNoticePeriodDays: Int = 2
+  private final val FulfilmentCutoffDays: NonEmptyList[DayOfWeek] =
+    NonEmptyList.of(DayOfWeek.MONDAY, DayOfWeek.THURSDAY)
 
   def apply(today: LocalDate): ListMap[String, FulfilmentDates] =
     ListMap( // to preserve insertion order, so the file is easier to read
       List(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY).map(targetDayOfWeek =>
         targetDayOfWeek.getDisplayName(FULL, ENGLISH) -> FulfilmentDates(
-          today,
-          holidayStopFirstAvailableDate(today),
-          holidayStopProcessorTargetDate(targetDayOfWeek, today),
-          newSubscriptionEarliestStartDate(targetDayOfWeek, today)
+          today = today,
+          holidayStopFirstAvailableDate = holidayStopFirstAvailableDate(today),
+          holidayStopProcessorTargetDate = holidayStopProcessorTargetDate(targetDayOfWeek, today),
+          newSubscriptionEarliestStartDate = newSubscriptionEarliestStartDate(targetDayOfWeek, today)
         )): _*
     )
 
-  def holidayStopFirstAvailableDate(today: LocalDate): LocalDate = today plusDays VoucherHolidayStopNoticePeriodDays
+  private def holidayStopFirstAvailableDate(today: LocalDate): LocalDate =
+    today plusDays VoucherHolidayStopNoticePeriodDays
 
-  def holidayStopProcessorTargetDate(targetDayOfWeek: DayOfWeek, today: LocalDate): Option[LocalDate] = {
-    if (today.getDayOfWeek == targetDayOfWeek) {
-      Some(today) // we process voucher holiday stops on the day they're scheduled for
-    } else {
-      None
-    }
-  }
+  private def holidayStopProcessorTargetDate(targetDayOfWeek: DayOfWeek, today: LocalDate): Option[LocalDate] =
+    /*
+     * We process voucher holiday stops the day before they're scheduled
+     * to ensure they are disabled on the suspension date
+     */
+    Option.when(today.getDayOfWeek == targetDayOfWeek)(today.plusDays(1))
 
-  def newSubscriptionEarliestStartDate(issueDay: DayOfWeek, today: LocalDate) = {
+  private def newSubscriptionEarliestStartDate(issueDay: DayOfWeek, today: LocalDate): LocalDate = {
     //Fulfilment files are generated on Mondays and Thursdays
     //There is a delay of up to 7 days for the voucher cards to be printed and sent to the customer
     //The earliest start date will be the issue day on or after that date
     val soonestFulfilmentFileDate =
-      soonest(FulfilmentCutoffDays.map(fulfilmentCutoffDay => today `with` next(fulfilmentCutoffDay)))
+      soonest(
+        FulfilmentCutoffDays.map(fulfilmentCutoffDay => today `with` next(fulfilmentCutoffDay))
+      )
 
-    soonestFulfilmentFileDate plusDays (7) `with` nextOrSame(issueDay)
+    soonestFulfilmentFileDate plusDays 7 `with` nextOrSame(issueDay)
   }
 
-  def soonest(dates: NonEmptyList[LocalDate]): LocalDate = dates.toList.min[LocalDate](_ compareTo _)
+  private def soonest(dates: NonEmptyList[LocalDate]): LocalDate =
+    dates.toList.min[LocalDate](_ compareTo _)
 }
