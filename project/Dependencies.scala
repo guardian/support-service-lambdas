206970ccca4c56f0d97e3f5d85e7a4492854d941
Scala Steward <me@scala-steward.org>
2021-03-01 10:40:44 +0100

    Update scalatest to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
 
   // Testing
   val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.4.3" % Test
-  val scalatest = "org.scalatest" %% "scalatest" % "3.2.4" % Test
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.5" % Test
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.15.3" % Test
   val scalaMock = "org.scalamock" %% "scalamock" % "5.1.0" % Test
 
