ecfd4a923bfeae39af9c4ff6d863e5104f22974d
David Pepper <david.pepper@guardian.co.uk>
2021-03-05 17:54:33 +0000

    latest

---
+++ handlers/soft-opt-in-consent-setter/src/main/scala/com/gu/soft_opt_in_consent_setter/AssociatedSFSubscription.scala
@@ -0,0 +1,15 @@
+package com.gu.soft_opt_in_consent_setter
+
+object AssociatedSFSubscription {
+  case class RootInterface(
+      totalSize: Int,
+      done: Boolean,
+      records: Seq[Record]
+  )
+
+  case class Record(
+      Product__c: String,
+      IdentityID__c: String
+  )
+
+}
