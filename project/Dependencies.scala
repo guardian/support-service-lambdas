0d20af5aab0fe957e6c70fb0be99ecc0b2f87eb4
Scala Steward <me@scala-steward.org>
2021-03-30 10:37:34 +0200

    Update cats-core to 2.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val circeVersion = "0.13.0"
   val sttpVersion = "1.7.2"
   val http4sVersion = "0.21.20"
-  val catsVersion = "2.3.1"
+  val catsVersion = "2.5.0"
   val catsEffectVersion = "2.4.1"
 
   val logging = Seq(
