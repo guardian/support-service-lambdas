a9467c7d98b0dde3c617b4daa994daafc5dba9ae
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-06-21 15:40:52 +0100

    Test config

--- handlers/payment-failure-comms/src/main/scala/com.gu.payment_failure_comms/Handler.scala
+++ handlers/payment-failure-comms/src/main/scala/com.gu.payment_failure_comms/Handler.scala
@@ -2,12 +2,19 @@ package com.gu.payment_failure_comms
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
+import com.gu.payment_failure_comms.models.Config
 import com.gu.util.Logging
 
 class Handler extends RequestHandler[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent] with Logging {
 
   def handleRequest(event: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent = {
-    logger.info("Received request with body: " + event.getBody)
+
+    for {
+      config <- Config()
+    } yield {
+      logger.info(s"Read config with: ${config.braze.instanceUrl} ${config.braze.bearerToken.charAt(0)}")
+      logger.info("Received request with body: " + event.getBody)
+    }
 
     new APIGatewayProxyResponseEvent().withStatusCode(200)
   }
