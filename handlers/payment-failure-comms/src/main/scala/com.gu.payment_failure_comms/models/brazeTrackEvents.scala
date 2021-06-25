3c1c47a616677d43d556ed59b24979b82b4ab426
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-06-25 11:58:23 +0100

    Explicit property list

--- handlers/payment-failure-comms/src/main/scala/com.gu.payment_failure_comms/models/brazeTrackEvents.scala
+++ handlers/payment-failure-comms/src/main/scala/com.gu.payment_failure_comms/models/brazeTrackEvents.scala
@@ -3,4 +3,6 @@ package com.gu.payment_failure_comms.models
 case class BrazeTrackRequest(events: List[CustomEvent])
 
 // Based on https://www.braze.com/docs/api/objects_filters/event_object/
-case class CustomEvent(external_id: String, app_id: String, name: String, time: String, properties: Map[String, String])
+case class CustomEvent(external_id: String, app_id: String, name: String, time: String, properties: EventProperties)
+
+case class EventProperties(currency: String, amount: Double)
