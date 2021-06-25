237d3bd4dcf4e78e35312a6fbeb4f883a0948651
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-06-25 09:06:33 +0100

    Core logic

--- handlers/payment-failure-comms/src/main/scala/com.gu.payment_failure_comms/Handler.scala
+++ handlers/payment-failure-comms/src/main/scala/com.gu.payment_failure_comms/Handler.scala
@@ -2,20 +2,32 @@ package com.gu.payment_failure_comms
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
-import com.gu.payment_failure_comms.models.Config
+import com.gu.payment_failure_comms.models.{Config, CustomEvent, Failure}
 import com.gu.util.Logging
+import io.circe.generic.auto._
+import io.circe.syntax._
 
 class Handler extends RequestHandler[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent] with Logging {
 
   def handleRequest(event: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent = {
-    for {
+
+    logger.info("Received request with body: " + event.getBody)
+
+    (for {
       config <- Config()
-    } yield {
-      logger.info(s"Read config with: ${config.braze.instanceUrl} ${config.braze.bearerToken.charAt(0)} ${config.braze.zuoraAppId}")
-      logger.info("Received request with body: " + event.getBody)
+      customEvent <- convertToCustomEventRequest(event.getBody)
+      _ = BrazeConnector.sendCustomEvent(config.braze, customEvent.asJson.toString)
+    } yield ()) match {
+      case Right(_) => new APIGatewayProxyResponseEvent().withStatusCode(200)
+      case Left(failure) => {
+        logger.error(s"An error happened. ${failure.kind}: ${failure.details}")
+
+        new APIGatewayProxyResponseEvent().withStatusCode(500)
+      }
     }
 
-    new APIGatewayProxyResponseEvent().withStatusCode(200)
   }
-  
+
+  def convertToCustomEventRequest(body: String): Either[Failure, CustomEvent] = ???
+
 }
