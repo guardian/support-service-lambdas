46a63005ecadb5f00464f43e9424f76d6f38ed1f
Scala Steward <me@scala-steward.org>
2021-09-08 14:44:30 +0200

    Update scala-xml to 2.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -68,7 +68,7 @@ object Dependencies {
   // Other
   val zio = "dev.zio" %% "zio" % "1.0.7"
   val enumeratum = "com.beachape" %% "enumeratum" % "1.7.0"
-  val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
+  val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.0.1"
   val stripe = "com.stripe" % "stripe-java" % "20.74.0"
 
   // Testing
