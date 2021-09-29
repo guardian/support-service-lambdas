7bb9113cd5ba33cd82785bea20255fb3462efaa3
Scala Steward <me@scala-steward.org>
2021-09-29 18:32:11 +0200

    Update stripe-java to 20.79.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -68,7 +68,7 @@ object Dependencies {
   val zio = "dev.zio" %% "zio" % "1.0.7"
   val enumeratum = "com.beachape" %% "enumeratum" % "1.7.0"
   val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
-  val stripe = "com.stripe" % "stripe-java" % "20.74.0"
+  val stripe = "com.stripe" % "stripe-java" % "20.79.0"
 
   // Testing
   val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.5.3" % Test
