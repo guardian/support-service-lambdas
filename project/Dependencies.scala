5903afa7741961718c122fec3bf5c6226c373203
Scala Steward <me@scala-steward.org>
2021-10-02 18:53:39 +0200

    Update client3:async-http-client-backend-cats-ce2, ... to 3.3.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ import sbtassembly.PathList
 object Dependencies {
   val awsSdkVersion = "2.16.100"
   val circeVersion = "0.13.0"
-  val sttpVersion = "3.3.11"
+  val sttpVersion = "3.3.15"
   val http4sVersion = "0.21.28"
   val catsVersion = "2.6.1"
   val catsEffectVersion = "2.5.1"
