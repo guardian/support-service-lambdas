fd78ed569e0c5e8784adab91bbec825e21063a45
Scala Steward <me@scala-steward.org>
2021-10-11 14:52:58 +0200

    Update cats-effect to 3.2.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val sttpVersion = "3.3.15"
   val http4sVersion = "0.21.28"
   val catsVersion = "2.6.1"
-  val catsEffectVersion = "2.5.4"
+  val catsEffectVersion = "3.2.9"
 
   val logging = Seq(
     "ch.qos.logback" % "logback-classic" % "1.2.6",
