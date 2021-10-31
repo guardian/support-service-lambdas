07072ea781afa905019033a27b78396fa5e8a1eb
Scala Steward <me@scala-steward.org>
2021-10-31 14:15:14 +0100

    Update diffx-scalatest to 0.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
   val stripe = "com.stripe" % "stripe-java" % "20.85.0"
 
   // Testing
-  val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.5.6" % Test
+  val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.6.0" % Test
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.10" % Test
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.15.4" % Test
   val scalaMock = "org.scalamock" %% "scalamock" % "5.1.0" % Test
