a5d47ba01683a12c576a16564ebda9c6d984ece6
David Pepper <david.pepper@guardian.co.uk>
2021-12-13 12:29:52 +0000

    Add handler

---
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
@@ -0,0 +1,14 @@
+package com.gu.sf_emails_to_s3_exporter
+
+import com.typesafe.scalalogging.LazyLogging
+
+object Handler extends LazyLogging {
+
+  def main(args: Array[String]): Unit = {
+    handleRequest()
+  }
+
+  def handleRequest(): Unit = {
+    
+  }
+}
