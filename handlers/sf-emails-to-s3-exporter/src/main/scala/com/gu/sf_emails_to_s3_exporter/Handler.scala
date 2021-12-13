5a97c242d2219d0389ac8fe7c41b70398cd46a68
David Pepper <david.pepper@guardian.co.uk>
2021-12-13 16:31:22 +0000

    add auth and GET methods to new SF Connector class

--- handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
@@ -1,10 +1,9 @@
 package com.gu.sf_emails_to_s3_exporter
 
+import com.gu.sf_emails_to_s3_exporter.SFConnector.{auth, getEmailsFromSf}
 import com.typesafe.scalalogging.LazyLogging
-import io.circe.Error
 import io.circe.generic.auto.exportDecoder
 import io.circe.parser.decode
-import scalaj.http.{Http, HttpOptions}
 
 object Handler extends LazyLogging {
   case class SfAuthDetails(access_token: String, instance_url: String)
@@ -43,42 +42,6 @@ object Handler extends LazyLogging {
     } yield emailsForExportFromSf
 
     logger.info("emails:" + emails)
-  }
-
-  def getEmailsFromSf(sfAuthDetails: SfAuthDetails): Either[Error, EmailsFromSfResponse.Response] = {
-    logger.info("Getting emails from Salesforce...")
-
-    val query = GetEmailsQuery.query
-
-    val responseBody = doSfGetWithQuery(sfAuthDetails, query)
-    println("response body:" + responseBody)
-    decode[EmailsFromSfResponse.Response](responseBody)
-  }
-
-  def doSfGetWithQuery(sfAuthDetails: SfAuthDetails, query: String): String = {
-    Http(s"${sfAuthDetails.instance_url}/services/data/v50.0/query/")
-      .param("q", query)
-      .option(HttpOptions.readTimeout(30000))
-      .header("Authorization", s"Bearer ${sfAuthDetails.access_token}")
-      .method("GET")
-      .asString
-      .body
-  }
-
-  def auth(salesforceConfig: SalesforceConfig): String = {
-    logger.info("Authenticating with Salesforce...")
-    Http(s"${System.getenv("authUrl")}/services/oauth2/token")
-      .postForm(
-        Seq(
-          "grant_type" -> "password",
-          "client_id" -> salesforceConfig.clientId,
-          "client_secret" -> salesforceConfig.clientSecret,
-          "username" -> salesforceConfig.userName,
-          "password" -> s"${salesforceConfig.password}${salesforceConfig.token}"
-        )
-      )
-      .asString
-      .body
   }
 
 }
---
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/SFConnector.scala
@@ -0,0 +1,41 @@
+package com.gu.sf_emails_to_s3_exporter
+
+import com.gu.sf_emails_to_s3_exporter.Handler.SfAuthDetails
+import io.circe.Error
+import io.circe.generic.auto.exportDecoder
+import io.circe.parser.decode
+import scalaj.http.{Http, HttpOptions}
+
+object SFConnector {
+
+  def getEmailsFromSf(sfAuthDetails: SfAuthDetails): Either[Error, EmailsFromSfResponse.Response] = {
+    val responseBody = doSfGetWithQuery(sfAuthDetails, GetEmailsQuery.query)
+    println("response body:" + responseBody)
+    decode[EmailsFromSfResponse.Response](responseBody)
+  }
+
+  def doSfGetWithQuery(sfAuthDetails: SfAuthDetails, query: String): String = {
+    Http(s"${sfAuthDetails.instance_url}/services/data/v50.0/query/")
+      .param("q", query)
+      .option(HttpOptions.readTimeout(30000))
+      .header("Authorization", s"Bearer ${sfAuthDetails.access_token}")
+      .method("GET")
+      .asString
+      .body
+  }
+
+  def auth(salesforceConfig: SalesforceConfig): String = {
+    Http(s"${System.getenv("authUrl")}/services/oauth2/token")
+      .postForm(
+        Seq(
+          "grant_type" -> "password",
+          "client_id" -> salesforceConfig.clientId,
+          "client_secret" -> salesforceConfig.clientSecret,
+          "username" -> salesforceConfig.userName,
+          "password" -> s"${salesforceConfig.password}${salesforceConfig.token}"
+        )
+      )
+      .asString
+      .body
+  }
+}
