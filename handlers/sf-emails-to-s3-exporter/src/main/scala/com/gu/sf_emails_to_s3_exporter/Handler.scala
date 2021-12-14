6a5928dfdb67afadcd8345192b0633229b23833f
David Pepper <david.pepper@guardian.co.uk>
2021-12-14 15:34:43 +0000

    group emails and write to s3

--- handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
@@ -1,12 +1,11 @@
 package com.gu.sf_emails_to_s3_exporter
 
 import com.gu.sf_emails_to_s3_exporter.S3Connector.writeEmailsJsonToS3
-import com.gu.sf_emails_to_s3_exporter.SFConnector.{SfAuthDetails, auth, getEmailsFromSf, optConfig}
+import com.gu.sf_emails_to_s3_exporter.SFConnector.{SfAuthDetails, auth, getEmailsFromSf}
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
-import io.circe.generic.decoding.DerivedDecoder.deriveDecoder
-import io.circe.parser.decode
-import io.circe.syntax.EncoderOps
+import io.circe.parser._
+import io.circe.syntax._
 
 object Handler extends LazyLogging {
 
@@ -15,13 +14,13 @@ object Handler extends LazyLogging {
   }
 
   def handleRequest(): Unit = {
-    val emails  = for {
+    val emails = for {
       config <- SalesforceConfig.fromEnvironment.toRight("Missing config value")
       sfAuthDetails <- decode[SfAuthDetails](auth(config))
       emailsForExportFromSf <- getEmailsFromSf(sfAuthDetails)
     } yield emailsForExportFromSf
 
-    logger.info("emails:"+emails)
+    logger.info("emails:" + emails)
 
     emails match {
 
@@ -30,23 +29,17 @@ object Handler extends LazyLogging {
         throw new RuntimeException("Missing config value")
       }
 
-      case Right(success) => logger.info("Config successfully retrieved")
+      case Right(emailsFromSF) => {
+        val emailsGroupedByCaseNumber = emailsFromSF
+          .records
+          .groupBy(_.Parent.CaseNumber)
 
-    }
-    }{
-      emailsForExportFromSf
-        .records
-        .groupBy(_.Parent.CaseNumber)
-        .foreach(p =>
+        emailsGroupedByCaseNumber.foreach(p =>
           writeEmailsJsonToS3(
             p._1,
             p._2.asJson.toString()
-          )
-        )
+          ))
+      }
     }
   }
 }
-
-  }
-}
-
