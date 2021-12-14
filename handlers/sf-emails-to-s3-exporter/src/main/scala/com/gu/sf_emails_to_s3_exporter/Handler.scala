a18a95ac44d92639474d0934b4da816f12626d2d
David Pepper <david.pepper@guardian.co.uk>
2021-12-14 10:12:24 +0000

    Group emails by Parent CaseNumber and save to S3

--- handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
@@ -1,9 +1,12 @@
 package com.gu.sf_emails_to_s3_exporter
 
+import com.gu.sf_emails_to_s3_exporter.S3Connector.writeEmailsJsonToS3
 import com.gu.sf_emails_to_s3_exporter.SFConnector.{auth, getEmailsFromSf, optConfig}
 import com.typesafe.scalalogging.LazyLogging
+import io.circe.generic.auto._
 import io.circe.generic.decoding.DerivedDecoder.deriveDecoder
 import io.circe.parser.decode
+import io.circe.syntax.EncoderOps
 
 object Handler extends LazyLogging {
   case class SfAuthDetails(access_token: String, instance_url: String)
@@ -13,13 +16,20 @@ object Handler extends LazyLogging {
   }
 
   def handleRequest(): Unit = {
-    val emails = for {
+    for{
       config <- optConfig.toRight(new RuntimeException("Missing config value"))
       sfAuthDetails <- decode[SfAuthDetails](auth(config.salesforceConfig))
       emailsForExportFromSf <- getEmailsFromSf(sfAuthDetails)
-    } yield emailsForExportFromSf
-
-    logger.info("emails:" + emails)
+    }{
+      emailsForExportFromSf
+        .records
+        .groupBy(_.Parent.CaseNumber)
+        .foreach(p =>
+          writeEmailsJsonToS3(
+            p._1,
+            p._2.asJson.toString()
+          )
+        )
+    }
   }
-
 }
