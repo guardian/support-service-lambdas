fdcc9bac035fcebd9a2f35c9a8ff6b3cf073aca6
David Pepper <david.pepper@guardian.co.uk>
2022-01-11 15:51:26 +0000

    Handler - use for comprehension instead of map

--- handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
@@ -1,7 +1,7 @@
 package com.gu.sf_emails_to_s3_exporter
 
-import com.gu.sf_emails_to_s3_exporter.S3Connector.saveEmailsToS3
-import com.gu.sf_emails_to_s3_exporter.SFConnector._
+import com.gu.sf_emails_to_s3_exporter.S3Connector.saveEmailToS3
+import com.gu.sf_emails_to_s3_exporter.SFConnector.{getEmailsFromSfByRecordsetReference, _}
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
 import io.circe.parser._
@@ -23,21 +23,21 @@ object Handler extends LazyLogging {
   }
 
   def processEmails(sfAuthDetails: SfAuthDetails, emailsDataFromSF: EmailsFromSfResponse.Response): Unit = {
-    val emailIdsSuccessfullySavedToS3 = saveEmailsToS3(emailsDataFromSF)
 
-    writebackSuccessesToSf(sfAuthDetails: SfAuthDetails, emailIdsSuccessfullySavedToS3)
+    val emailIdsSuccessfullySavedToS3 = emailsDataFromSF.records.map(caseEmail => {
+      saveEmailToS3(caseEmail)
+    })
+
+    writebackSuccessesToSf(sfAuthDetails, emailIdsSuccessfullySavedToS3)
 
     //process more emails if they exist
     if (!emailsDataFromSF.done) {
-      val nextBatchOfEmails = getEmailsFromSfByRecordsetReference(sfAuthDetails, emailsDataFromSF.nextRecordsUrl.get)
 
-      nextBatchOfEmails
-        .map(
-          nextPageEmails => {
-            processEmails(sfAuthDetails, nextPageEmails)
-          }
-        )
+      for {
+        nextBatchOfEmails <- getEmailsFromSfByRecordsetReference(sfAuthDetails, emailsDataFromSF.nextRecordsUrl.get)
+        _ = processEmails(sfAuthDetails, nextBatchOfEmails)
+      } yield ()
+
     }
-
   }
 }
