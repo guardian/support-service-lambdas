15dff3790e937726982b8b042b87b2b0d0dee764
David Pepper <david.pepper@guardian.co.uk>
2022-01-19 15:45:48 +0000

    Delete SalesforceConfig and replace with Config including s3 config

---
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Config.scala
@@ -0,0 +1,50 @@
+package com.gu.sf_emails_to_s3_exporter
+
+case class Config(
+  sfConfig: SalesforceConfig,
+  s3Config: S3Config
+)
+
+case class SalesforceConfig(
+  authUrl: String,
+  clientId: String,
+  clientSecret: String,
+  userName: String,
+  password: String,
+  token: String,
+  apiVersion: String
+)
+
+case class S3Config(
+  bucketName: String
+)
+
+object Config {
+  lazy val fromEnvironment: Option[Config] = {
+    for {
+      sfUserName <- Option(System.getenv("username"))
+      sfClientId <- Option(System.getenv("clientId"))
+      sfClientSecret <- Option(System.getenv("clientSecret"))
+      sfPassword <- Option(System.getenv("password"))
+      sfToken <- Option(System.getenv("token"))
+      sfAuthUrl <- Option(System.getenv("authUrl"))
+      sfApiVersion <- Option(System.getenv("apiVersion"))
+      s3BucketName <- Option(System.getenv("bucketName"))
+      s3Stage <- Option(System.getenv("stage"))
+    } yield Config(
+      SalesforceConfig(
+        userName = sfUserName,
+        clientId = sfClientId,
+        clientSecret = sfClientSecret,
+        password = sfPassword,
+        token = sfToken,
+        authUrl = sfAuthUrl,
+        apiVersion = sfApiVersion
+      ),
+      S3Config(
+        bucketName = s3BucketName + "-" + s3Stage.toLowerCase()
+      )
+    )
+  }
+}
+
