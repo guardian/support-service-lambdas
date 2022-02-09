45959f5daa9e3998d6aa5ac4960cee474afd0c86
David Pepper <david.pepper@guardian.co.uk>
2022-02-09 14:58:29 +0000

    add log lines indicating failure/success to put metric

--- handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Metrics.scala
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Metrics.scala
@@ -2,14 +2,17 @@ package com.gu.sf_emails_to_s3_exporter
 
 import com.gu.aws.AwsCloudWatch
 import com.gu.aws.AwsCloudWatch._
+import com.typesafe.scalalogging.LazyLogging
 
-import scala.util.Try
+import scala.util.{Failure, Success}
 
-object Metrics {
+object Metrics extends LazyLogging{
 
   private val stage = sys.env.getOrElse("Stage", "DEV")
 
-  def put(event: String, value: Double = 1.0): Try[Unit] = {
+  def put(event: String, value: Double = 1.0): Unit = {
+
+    //currently fire and forget
     AwsCloudWatch.metricPut(
       MetricRequest(
         MetricNamespace("s3-emails-from-sf"),
@@ -17,7 +20,14 @@ object Metrics {
         Map(MetricDimensionName("Stage") -> MetricDimensionValue(stage)),
         value
       )
-    )
+    ) match {
+      case Failure(exception) => {
+        logger.warn(s"Error logging Metric $event. Error:", exception)
+      }
+      case Success(_) => {
+        logger.info(s"Metric $event logged successfully")
+      }
+    }
   }
 
 }
