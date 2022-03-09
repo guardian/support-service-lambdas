36d57d336efec3abfd249e852f2728c12a9d590f
David Pepper <david.pepper@guardian.co.uk>
2022-03-09 11:17:58 +0000

    Create AsyncProcessRecsFromSfResponse.scala

---
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/AsyncProcessRecsFromSfResponse.scala
@@ -0,0 +1,14 @@
+package com.gu.sf_emails_to_s3_exporter
+
+object AsyncProcessRecsFromSfResponse {
+  case class Response(
+    done: Boolean,
+    records: Seq[Records],
+    nextRecordsUrl: Option[String] = None
+  )
+
+  case class Records(
+    Id: String,
+    Record_Id__c: String
+  )
+}
