c1fa561a0d03a5ff811f8b45f70a63ed2d3d3345
David Pepper <david.pepper@guardian.co.uk>
2022-03-10 13:02:26 +0000

    Handler - refactor for clarity in processing batches of emails

--- handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
+++ handlers/sf-emails-to-s3-exporter/src/main/scala/com/gu/sf_emails_to_s3_exporter/Handler.scala
@@ -15,53 +15,43 @@ object Handler extends LazyLogging {
   }
 
   def handleRequest(): Unit = {
-
-    val getQueueItemsAttempt = for {
+    val authenticationAttempt = for {
       config <- Config.fromEnvironment.toRight("Missing config value")
       authentication <- auth(config.sfConfig)
       sfAuthDetails <- decode[SfAuthDetails](authentication)
-      sfQueueItems <- getRecordsFromSF[QueueItemsFromSfResponse.Response](
-        sfAuthDetails,
-        config.sfConfig.apiVersion,
-        GetQueueItemsQuery.query,
-        batchSize = 2000
-      )
-    } yield {
-      getQueueItemsAttempt match {
-        case Left(ex) => {
-          CustomFailure.toMetric(
-            "failed_to_get_records_from_sf",
-            s"Failed to get records from SF. Error:${ex}"
-          )
-        }
-        case Right(success) => {
-          deleteSfQueueItemsAndExportEmailsFromSfToS3(sfAuthDetails, config, sfQueueItems.records)
-        }
-      }
-    }
+    } yield fetchQueueItemsFromSfAndThenExportEmailsToS3(sfAuthDetails: SfAuthDetails, config: Config)
 
-
-  }
-
-  //Break down the Async Process Records (max 2000) returned from Salesforce into groups of 200 for processing (callouts back to Salesforce should contain maximum 200 records)
-  def deleteSfQueueItemsAndExportEmailsFromSfToS3(sfAuthDetails: SfAuthDetails, config: Config, queueItems: Seq[QueueItemsFromSfResponse.QueueItem]):Unit = {
-    if (!queueItems.isEmpty) {
-      val batchedQueueItems = batchQueueItems(queueItems, 200)
-
-      batchedQueueItems.map { queueItemBatch =>
-
-        deleteQueueItems(sfAuthDetails, queueItemBatch.map(record => record.Id))
-
-        fetchEmailsFromSalesforceAndExportToS3(
-          sfAuthDetails,
-          config,
-          queueItemBatch.map(record => record.Record_Id__c)
+    authenticationAttempt match {
+      case Left(ex) => {
+        CustomFailure.toMetric(
+          "failed_to_authenticate_with_sf",
+          s"Failed to authenticate with SF. Error:${ex}"
         )
       }
+      case Right(_) => {}
     }
   }
 
-  def fetchEmailsFromSalesforceAndExportToS3(sfAuthDetails: SfAuthDetails, config: Config, emailIds: Seq[String]): Unit = {
+  def fetchQueueItemsFromSfAndThenExportEmailsToS3(sfAuthDetails: SfAuthDetails, config: Config): Unit = {
+    for {
+      queueItems <- fetchQueueItemsFromSf(sfAuthDetails, config)
+    } yield deleteQueueItemsExportEmailsFromSfToS3InBatches(sfAuthDetails, config, batchQueueItems(queueItems, batchSize = 2))
+  }
+
+  def deleteQueueItemsExportEmailsFromSfToS3InBatches(sfAuthDetails: SfAuthDetails, config: Config, batchedQueueItems: Seq[Seq[QueueItemsFromSfResponse.QueueItem]]): Any = { //Either[CustomFailure, Seq[QueueItemsFromSfResponse.QueueItem]] = {
+
+    batchedQueueItems.map { queueItemBatch =>
+      deleteQueueItems(sfAuthDetails, queueItemBatch.map(record => record.Id))
+
+      fetchBatchOfEmailsFromSfAndThenExportToS3(
+        sfAuthDetails,
+        config,
+        queueItemBatch.map(record => record.Record_Id__c)
+      )
+    }
+  }
+
+  def fetchBatchOfEmailsFromSfAndThenExportToS3(sfAuthDetails: SfAuthDetails, config: Config, emailIds: Seq[String]): Unit = {
     val getEmailsAttempt = for {
       emailsFromSF <- getRecordsFromSF[EmailsFromSfResponse.Response](
         sfAuthDetails,
@@ -79,9 +69,74 @@ object Handler extends LazyLogging {
           s"Failed to get records from SF. Error:${ex.getMessage}"
         )
       }
-      case Right(success) => {
-        processEmails(sfAuthDetails, success, config.s3Config.bucketName)
+      case Right(batchOfEmailsFromSf) => {
+        saveBatchOfEmailsToS3AndThenWritebackSuccessesToSf(sfAuthDetails, batchOfEmailsFromSf, config.s3Config.bucketName)
         logger.info("Processing complete")
+      }
+    }
+  }
+
+  def saveBatchOfEmailsToS3AndThenWritebackSuccessesToSf(sfAuthDetails: SfAuthDetails, batchOfEmailsFromSf: EmailsFromSfResponse.Response, bucketName: String): Any = {
+    logger.info(s"Start processing ${batchOfEmailsFromSf.records.size} emails...")
+
+    val emailIdsSuccessfullySavedToS3 = saveBatchOfEmailsToS3(batchOfEmailsFromSf, bucketName)
+
+    if (!emailIdsSuccessfullySavedToS3.isEmpty) {
+      writebackSuccessesToSf(sfAuthDetails, emailIdsSuccessfullySavedToS3).map(responseArray =>
+
+        responseArray.map(responseArrayItem =>
+
+          responseArrayItem.success.getOrElse(None) match {
+            case true => {
+              logger.info(s"Successful write back to sf for record:${responseArrayItem.id}")
+            }
+            case false => {
+              CustomFailure.toMetric(
+                "failed_writeback_to_sf_record",
+                s"Failed to write back to sf for record:$responseArrayItem"
+              )
+            }
+            case none => {
+              CustomFailure.toMetric(
+                "failed_writeback_request_to_sf",
+                s"Failed write back Request. errorCode(${responseArrayItem.errorCode}), message: ${responseArrayItem.message}"
+              )
+            }
+          }))
+    }
+  }
+
+  def saveBatchOfEmailsToS3(emailsDataFromSF: EmailsFromSfResponse.Response, bucketName: String): Seq[String] = {
+
+    val saveToS3Attempts = for {
+      saveToS3Attempt <- emailsDataFromSF
+        .records
+        .map(email => saveEmailToS3(email, bucketName))
+    } yield saveToS3Attempt
+
+    saveToS3Attempts.collect { case Right(value) => value }
+  }
+
+  def fetchQueueItemsFromSf(sfAuthDetails: SfAuthDetails, config: Config): Either[CustomFailure, Seq[QueueItemsFromSfResponse.QueueItem]] = {
+    val getQueueItemsAttempt = for {
+      sfQueueItems <- getRecordsFromSF[QueueItemsFromSfResponse.Response](
+        sfAuthDetails,
+        config.sfConfig.apiVersion,
+        GetQueueItemsQuery.query,
+        batchSize = 2000
+      )
+    } yield sfQueueItems
+
+    getQueueItemsAttempt match {
+      case Left(ex) => {
+        Left(CustomFailure.toMetric(
+          "failed_to_get_records_from_sf",
+          s"Failed to get Queue Items from Async Process Record Object in SF. Error:${ex}"
+        ))
+      }
+      case Right(success) => {
+        logger.info(s"${success.records.size} Queue items retrieved from Salesforce")
+        Right(success.records)
       }
     }
   }
@@ -90,7 +145,7 @@ object Handler extends LazyLogging {
     queueItems.grouped(batchSize).toList
   }
 
-  def deleteQueueItems(sfAuthDetails: SfAuthDetails, recordIds: Seq[String]): Any = {
+  def deleteQueueItems(sfAuthDetails: SfAuthDetails, recordIds: Seq[String]): Unit = {
     val deleteAttempts = for {
       deletedRecs <- deleteQueueItemsInSf(
         sfAuthDetails,
@@ -116,51 +171,6 @@ object Handler extends LazyLogging {
         })
       }
     }
-  }
-
-  def processEmails(sfAuthDetails: SfAuthDetails, emailsDataFromSF: EmailsFromSfResponse.Response, bucketName: String): Any = {
-    logger.info(s"Start processing ${emailsDataFromSF.records.size} emails...")
-
-    val emailIdsSuccessfullySavedToS3 = saveEmailsToS3(emailsDataFromSF, bucketName)
-
-    if (!emailIdsSuccessfullySavedToS3.isEmpty) {
-      writebackSuccessesToSf(sfAuthDetails, emailIdsSuccessfullySavedToS3).map(
-
-        responseArray => responseArray.map(
-
-          responseArrayItem =>
-
-            responseArrayItem.success.getOrElse(None) match {
-              case true => {
-                logger.info(s"Successful write back to sf for record:${responseArrayItem.id}")
-              }
-              case false => {
-                CustomFailure.toMetric(
-                  "failed_writeback_to_sf_record",
-                  s"Failed to write back to sf for record:$responseArrayItem"
-                )
-              }
-              case none => {
-                CustomFailure.toMetric(
-                  "failed_writeback_request_to_sf",
-                  s"Failed write back Request. errorCode(${responseArrayItem.errorCode}), message: ${responseArrayItem.message}"
-                )
-              }
-            }
-        )
-      )
-    }
-  }
-
-  def saveEmailsToS3(emailsDataFromSF: EmailsFromSfResponse.Response, bucketName: String): Seq[String] = {
-
-    val saveToS3Attempts = for {
-      saveToS3Attempt <- emailsDataFromSF
-        .records
-        .map(email => saveEmailToS3(email, bucketName))
-    } yield saveToS3Attempt
-
-    saveToS3Attempts.collect { case Right(value) => value }
   }
 
   def safely[A](doSomething: => A): Either[CustomFailure, A] =
