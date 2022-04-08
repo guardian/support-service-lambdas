ec8898e82c6a7cfb29bcefd5a5122c3f16c4c5a4
Scala Steward <me@scala-steward.org>
2022-04-08 08:34:44 +0200

    Update scalacheck to 1.16.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
   // Testing
   val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.7.0" % Test
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.11" % Test
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.15.4" % Test
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.16.0" % Test
   val scalaMock = "org.scalamock" %% "scalamock" % "5.2.0" % Test
 
   /*
