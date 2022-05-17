fe27bf89cf6a4865169f12939aaecca90409f07b
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-05-17 09:39:48 +0100

    Tabs to spaces

--- handlers/soft-opt-in-consent-setter/src/main/scala/com/gu/soft_opt_in_consent_setter/SfQueries.scala
+++ handlers/soft-opt-in-consent-setter/src/main/scala/com/gu/soft_opt_in_consent_setter/SfQueries.scala
@@ -7,16 +7,16 @@ object SfQueries {
 
     s"""
        |SELECT
-       |	Id,
-       |	Name,
-       |	Product__c,
-       |	SF_Status__c,
-       |	Soft_Opt_in_Status__c,
-       |	Soft_Opt_in_Last_Stage_Processed__c,
-       |	Soft_Opt_in_Number_of_Attempts__c,
-       |	Buyer__r.IdentityID__c
+       |    Id,
+       |    Name,
+       |    Product__c,
+       |    SF_Status__c,
+       |    Soft_Opt_in_Status__c,
+       |    Soft_Opt_in_Last_Stage_Processed__c,
+       |    Soft_Opt_in_Number_of_Attempts__c,
+       |    Buyer__r.IdentityID__c
        |FROM
-       |	SF_Subscription__c
+       |    SF_Subscription__c
        |WHERE
        |    Soft_Opt_in_Number_of_Attempts__c < 5 AND
        |    Soft_Opt_in_Eligible__c = true AND
