3c22a4007e67dab0e4aa66502f1827a7c773a651
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-05-24 16:41:48 +0200

    Update logback-classic to 1.2.11 (#1430)
    * Update logback-classic to 1.2.11
    
    * Revert commit(s) eaa6b44c
    
    * Update logback-classic to 1.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val catsEffectVersion = "2.5.5"
 
   val logging = Seq(
-    "ch.qos.logback" % "logback-classic" % "1.2.10",
+    "ch.qos.logback" % "logback-classic" % "1.2.11",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
   )
 
