0f88f5bcdbef0e82b34d20aff2c8465ba1d35773
Scala Steward <scala-steward@users.noreply.github.com>
2022-06-11 07:37:05 +0000

    Update cloudwatch, lambda, s3, secretsmanager, ... to 2.17.209

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ import sbtassembly.AssemblyPlugin.autoImport.{
 import sbtassembly.PathList
 
 object Dependencies {
-  val awsSdkVersion = "2.17.194"
+  val awsSdkVersion = "2.17.209"
   val circeVersion = "0.13.0"
   val sttpVersion = "3.5.2"
   val http4sVersion = "0.21.33"
