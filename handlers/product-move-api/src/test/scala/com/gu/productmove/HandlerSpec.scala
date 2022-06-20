6cfb67fcbb83a4bd67728f9d65bbe3a2afc96cdd
John Duffell <john.duffell@guardian.co.uk>
2022-06-20 17:51:48 +0100

    fix test compile error

--- handlers/product-move-api/src/test/scala/com/gu/productmove/HandlerSpec.scala
+++ handlers/product-move-api/src/test/scala/com/gu/productmove/HandlerSpec.scala
@@ -1,6 +1,6 @@
 package com.gu.productmove
 
-import com.gu.productmove.HandlerTypes.{ExpectedInput, OutputBody}
+import com.gu.productmove.ProductMoveEndpointTypes.{ExpectedInput, OutputBody}
 import com.gu.productmove.zuora.{GetSubscription, TestGetSubscription}
 import com.gu.productmove.zuora.GetSubscription.GetSubscriptionResponse
 import zio.*
@@ -11,7 +11,7 @@ object HandlerSpec extends ZIOSpecDefault {
   def spec = suite("HandlerSpec")(
     test("runWithEnvironment doesn't do much yet") {
       for {
-        output <- Handler.runWithEnvironment(ExpectedInput(false))
+        output <- ProductMoveEndpoint.runWithEnvironment(ExpectedInput(false))
         requests <- TestGetSubscription.requests
       } yield assert(output)(equalTo(OutputBody("hello"))) &&
         assert(requests)(equalTo(List("A-S00339056")))
