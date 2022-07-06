3ff52d727f0cbee679d0cd69cca3293539f01062
Scala Steward <scala_steward@virtuslab.com>
2022-07-06 02:43:02 +0000

    Update http4s-circe, http4s-core, ... to 0.23.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val awsSdkVersion = "2.17.210"
   val circeVersion = "0.13.0"
   val sttpVersion = "3.6.2"
-  val http4sVersion = "0.21.33"
+  val http4sVersion = "0.23.13"
   val catsVersion = "2.7.0"
   val catsEffectVersion = "2.5.5"
 
