157e74fca630a7d48d854246d428f7b28da604ac
Rupert Bates <Rupert.Bates@guardian.co.uk>
2023-01-24 13:58:26 +0000

    Always use the DEV environment to load config

--- handlers/product-move-api/src/main/scala/com/gu/productmove/GuReaderRevenuePrivateS3.scala
+++ handlers/product-move-api/src/main/scala/com/gu/productmove/GuReaderRevenuePrivateS3.scala
@@ -1,14 +1,18 @@
 package com.gu.productmove
 
 import com.gu.productmove.GuStageLive.Stage
+import com.gu.productmove.GuStageLive.Stage.{CODE, DEV}
 
 object GuReaderRevenuePrivateS3 {
   val bucket = "gu-reader-revenue-private"
 
   def key(configFileName: String, stage: Stage, version: Int = 1) = {
-    val basePath = s"membership/support-service-lambdas/$stage"
 
-    val versionString = if (stage == Stage.DEV) "" else s".v${version}"
+    // We always want product-move-api to fetch config from DEV not CODE
+    val stageUsingDevForCode = if (stage == CODE) DEV else stage
+    val basePath = s"membership/support-service-lambdas/$stageUsingDevForCode"
+
+    val versionString = if (stage == Stage.DEV) "" else s".v$version"
     val relativePath = s"$configFileName-$stage$versionString.json"
     s"$basePath/$relativePath"
   }
