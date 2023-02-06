2af9fbb6f9257c5fb0f3bce694515bf3e7074a95
Rupert Bates <Rupert.Bates@guardian.co.uk>
2023-02-06 16:19:48 +0000

    Handle product moves for subscriptions with multiple invoice items

---
+++ handlers/product-move-api/src/main/scala/com/gu/productmove/zuora/BuildPreviewResult.scala
@@ -0,0 +1,33 @@
+package com.gu.productmove.zuora
+
+import zio.{IO, ZIO}
+import zio.*
+import zio.Clock
+import com.gu.productmove.GuStageLive.Stage
+import com.gu.productmove.endpoint.move.ProductMoveEndpointTypes.PreviewResult
+
+object BuildPreviewResult {
+  def getPreviewResult(
+      invoice: SubscriptionUpdateInvoice,
+      ids: SupporterPlusRatePlanIds,
+  ): ZIO[Stage, String, PreviewResult] =
+    invoice.invoiceItems.partition(_.productRatePlanChargeId == ids.ratePlanChargeId) match
+      case (supporterPlusInvoice :: Nil, contributionInvoices) =>
+        for {
+          date <- Clock.currentDateTime.map(_.toLocalDate)
+          contributionRefundInvoice = contributionInvoices
+            .filter(invoiceItem =>
+              invoiceItem.totalAmount < 0 &&
+                invoiceItem.serviceStartDate == date,
+            )
+            .head
+        } yield PreviewResult(
+          supporterPlusInvoice.totalAmount - contributionRefundInvoice.totalAmount.abs,
+          contributionRefundInvoice.totalAmount,
+          supporterPlusInvoice.totalAmount,
+        )
+      case _ =>
+        ZIO.fail(
+          s"Unexpected invoice item structure was returned from a Zuora preview call. Invoice data was: $invoice",
+        )
+}
---
+++ handlers/product-move-api/src/test/scala/com/gu/productmove/zuora/Fixtures.scala
@@ -0,0 +1,69 @@
+package com.gu.productmove.zuora
+
+import com.gu.productmove.zuora.SubscriptionUpdateSpec.test
+
+import java.time.LocalDate
+
+object Fixtures {
+  val supporterPlusRatePlanChargeId = "8ad09fc281de1ce70181de3b253e36a6"
+  val contributionRatePlanChargeId = "2c92c0f85a6b1352015a7fcf35ab397c"
+
+  val invoiceWithMultipleInvoiceItems = SubscriptionUpdateInvoice(
+    amount = 42,
+    amountWithoutTax = 42,
+    taxAmount = 0,
+    invoiceItems = List(
+      SubscriptionUpdateInvoiceItem(
+        LocalDate.parse("2022-11-19"),
+        16,
+        0,
+        contributionRatePlanChargeId,
+      ),
+      SubscriptionUpdateInvoiceItem(
+        LocalDate.parse("2022-12-19"),
+        16,
+        0,
+        contributionRatePlanChargeId,
+      ),
+      SubscriptionUpdateInvoiceItem( //Supporter plus
+        LocalDate.parse("2023-01-19"),
+        10,
+        0,
+        supporterPlusRatePlanChargeId,
+      ),
+      SubscriptionUpdateInvoiceItem(
+        LocalDate.parse("2023-01-19"),
+        -16,
+        0,
+        contributionRatePlanChargeId,
+      ),
+      SubscriptionUpdateInvoiceItem(
+        LocalDate.parse("2023-01-19"),
+        16,
+        0,
+        contributionRatePlanChargeId,
+      ),
+    ),
+  )
+
+  val invoiceWithTax = SubscriptionUpdateInvoice(
+    amount = -10,
+    amountWithoutTax = -10.91,
+    taxAmount = 0.91,
+    invoiceItems = List(
+      SubscriptionUpdateInvoiceItem(
+        serviceStartDate = LocalDate.parse("2023-02-06"),
+        chargeAmount = -20,
+        taxAmount = -0,
+        productRatePlanChargeId = contributionRatePlanChargeId,
+      ),
+      SubscriptionUpdateInvoiceItem(
+        serviceStartDate = LocalDate.parse("2023-02-06"),
+        chargeAmount = 9.09,
+        taxAmount = 0.91,
+        productRatePlanChargeId = supporterPlusRatePlanChargeId,
+      ),
+    )
+  )
+
+}
