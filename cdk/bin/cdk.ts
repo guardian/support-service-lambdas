1d3cd2eaf685014342d9fbcbfd93120223af586a
Michael Jacobson <michael.jacobson@guardian.co.uk>
2023-05-05 14:20:50 +0100

    adjusting ci pipeline

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,5 +3,5 @@ import { App } from "aws-cdk-lib";
 import { NewProductApi } from "../lib/new-product-api";
 
 const app = new App();
-new NewProductApi(app, "NewProductApi-CODE", { stack: "membership", stage: "CODE" });
-new NewProductApi(app, "NewProductApi-PROD", { stack: "membership", stage: "PROD" });
+new NewProductApi(app, "new-product-api-CODE", { stack: "membership", stage: "CODE" });
+new NewProductApi(app, "new-product-api-PROD", { stack: "membership", stage: "PROD" });
---
+++ script/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR="${DIR}/.."
+cd "$ROOT_DIR"
+
+echo "$1"
+
+#yarn clean
+#yarn install
+#yarn tsc
+#yarn lint
+#yarn build
+#
+#cp package.json target
+#cp riff-raff.yaml target
+#
+#pushd target
+#yarn install --production
+#popd
+#
+#cd target
+#
+#zip -FSr "${ROOT_DIR}/support-reminders.zip" ./*
