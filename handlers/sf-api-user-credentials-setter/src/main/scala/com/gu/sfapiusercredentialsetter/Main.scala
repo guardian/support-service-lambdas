e2bc00f03469260f15f0f6a67b25d7c6c77d74a6
Pascal Honoré <pascal@alseyn.net>
2023-05-11 14:13:42 +0100

    use secrets manager at sf-api-user-credentials-setter

--- handlers/sf-api-user-credentials-setter/src/main/scala/com/gu/sfapiusercredentialsetter/Main.scala
+++ handlers/sf-api-user-credentials-setter/src/main/scala/com/gu/sfapiusercredentialsetter/Main.scala
@@ -34,24 +34,18 @@ object Main extends App with LazyLogging {
   )
 
   lazy val optConfig = for {
-    sfUserName <- Option(System.getenv("username"))
-    sfClientId <- Option(System.getenv("clientId"))
-    sfClientSecret <- Option(System.getenv("clientSecret"))
-    sfPassword <- Option(System.getenv("password"))
-    sfToken <- Option(System.getenv("token"))
-    sfAuthUrl <- Option(System.getenv("authUrl"))
-    stage <- Option(System.getenv("stageName"))
+    secrets <- Secrets.getAwsCredentialsSetterSecrets
   } yield Config(
     SalesforceConfig(
-      userName = sfUserName,
-      clientId = sfClientId,
-      clientSecret = sfClientSecret,
-      password = sfPassword,
-      token = sfToken,
-      authUrl = sfAuthUrl,
+      userName = secrets.username,
+      clientId = secrets.clientId,
+      clientSecret = secrets.clientSecret,
+      password = secrets.password,
+      token = secrets.token,
+      authUrl = secrets.authUrl,
     ),
     AwsConfig(
-      stageName = stage,
+      stageName = secrets.stageName,
     ),
   )
 
