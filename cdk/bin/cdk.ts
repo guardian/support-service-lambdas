7a15c6e34e2c069f39b06f5052a477dcf3f5f43e
Michael Jacobson <michael.jacobson@guardian.co.uk>
2023-05-11 12:56:50 +0100

    re-checking into version control

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,43 @@
 import "source-map-support/register";
 import { App } from "aws-cdk-lib";
+import type { NewProductApiProps } from "../lib/new-product-api";
 import { NewProductApi } from "../lib/new-product-api";
 
 const app = new App();
-new NewProductApi(app, "new-product-api-CODE", { stack: "membership", stage: "CODE" });
-new NewProductApi(app, "new-product-api-PROD", { stack: "membership", stage: "PROD" });
+
+export const devProps: NewProductApiProps = {
+    stack: "support",
+    stage: "DEV",
+    domainName: "new-product-api-dev.membership.guardianapis.com",
+    hostedZoneId: "Z1E4V12LQGXFEC",
+    certificateId: "c1efc564-9ff8-4a03-be48-d1990a3d79d2",
+    apiGatewayTargetDomainName: "d-papcvkgmc9.execute-api.eu-west-1.amazonaws.com",
+    zuoraCatalogLocation: "arn:aws:s3:::gu-zuora-catalog/CODE/Zuora-DEV/catalog.json",
+    fulfilmentDateCalculatorS3Resource: "arn:aws:s3:::fulfilment-date-calculator-dev/*"
+};
+
+export const codeProps: NewProductApiProps = {
+    stack: "support",
+    stage: "CODE",
+    domainName: "new-product-api-code.membership.guardianapis.com",
+    hostedZoneId: "Z1E4V12LQGXFEC",
+    certificateId: "c1efc564-9ff8-4a03-be48-d1990a3d79d2",
+    apiGatewayTargetDomainName: "d-ecyddyj7nk.execute-api.eu-west-1.amazonaws.com",
+    zuoraCatalogLocation: "arn:aws:s3:::gu-zuora-catalog/CODE/Zuora-UAT/catalog.json",
+    fulfilmentDateCalculatorS3Resource: "arn:aws:s3:::fulfilment-date-calculator-code/*"
+};
+
+export const prodProps: NewProductApiProps = {
+    stack: "support",
+    stage: "PROD",
+    domainName: "new-product-api-prod.membership.guardianapis.com",
+    hostedZoneId: "Z1E4V12LQGXFEC",
+    certificateId: "c1efc564-9ff8-4a03-be48-d1990a3d79d2",
+    apiGatewayTargetDomainName: "d-yyh9pmqphi.execute-api.eu-west-1.amazonaws.com",
+    zuoraCatalogLocation: "arn:aws:s3:::gu-zuora-catalog/PROD/Zuora-PROD/catalog.json",
+    fulfilmentDateCalculatorS3Resource: "arn:aws:s3:::fulfilment-date-calculator-prod/*"
+};
+
+new NewProductApi(app, "new-product-api-DEV", devProps);
+new NewProductApi(app, "new-product-api-CODE", codeProps);
+new NewProductApi(app, "new-product-api-PROD", prodProps);
