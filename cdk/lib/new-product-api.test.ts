4ed404d10dccac15f8d7125b1625e5163ce04681
Michael Jacobson <michael.jacobson@guardian.co.uk>
2023-05-17 14:03:15 +0100

    checking into clean branch

--- cdk/lib/new-product-api.test.ts
+++ cdk/lib/new-product-api.test.ts
@@ -1,12 +1,28 @@
 import { App } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
+import { codeProps, devProps, prodProps } from "../bin/cdk";
 import { NewProductApi } from "./new-product-api";
 
 describe("The NewProductApi stack", () => {
   it("matches the snapshot", () => {
     const app = new App();
-    const stack = new NewProductApi(app, "NewProductApi", { stack: "membership", stage: "TEST" });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+    const devStack = new NewProductApi(
+        app,
+        "new-product-api-DEV",
+        devProps
+    );
+    const codeStack = new NewProductApi(
+        app,
+        "new-product-api-CODE",
+        codeProps
+    );
+    const prodStack = new NewProductApi(
+        app,
+        "new-product-api-PROD",
+        prodProps
+    );
+    expect(Template.fromStack(devStack).toJSON()).toMatchSnapshot();
+    expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
+    expect(Template.fromStack(prodStack).toJSON()).toMatchSnapshot();
   });
 });
