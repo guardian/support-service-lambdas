4b60138c0797e849cd7893e4d15f224f7d81950e
David Pepper <david.pepper@guardian.co.uk>
2023-08-24 18:42:39 +0100

    Add core logic to generate composite json

--- handlers/failed-national-deliveries-processor/src/index.ts
+++ handlers/failed-national-deliveries-processor/src/index.ts
@@ -1,5 +1,154 @@
+import fs from 'fs';
+import { parse } from 'csv-parse/sync';
+
 export const main = async (): Promise<string> => {
-	const message = "hello world";
-	console.debug(message);
-	return Promise.resolve(message);
+		
+		const records : FileRow[] = await getFileRows();
+		
+		const recordsForCompositeJson : FileRow[] = records.filter(
+			record=>record.reason_code==='F'
+		);
+		
+		const json = generateJson(recordsForCompositeJson);
+		
+		console.log('json:',json);
+	
+	return Promise.resolve('');
 };
+
+function generateJson(recordsForCompositeJson : FileRow[]){
+
+	
+	var compositeRequests : CompositeRequest[] = [];
+	recordsForCompositeJson.forEach(
+		record => {
+			compositeRequests.push(createJsonForPatchDeliveryWithNotes(record.sub, record['Delivery Date']))
+			compositeRequests.push(createJsonForGetSFSubFromDelivery(record.sub, record['Delivery Date']))
+			compositeRequests.push(createJsonForCreateCase(record.sub, record['Delivery Date']))
+			compositeRequests.push(createJsonForUpdateDeliveryWithCase(record.sub, record['Delivery Date']))
+		}
+	); 
+	
+	const request : Request = {
+		allOrNone:true, 
+		compositeRequest:compositeRequests
+	};
+
+	const requestJson : string = JSON.stringify(request);
+	return requestJson;
+}
+
+function createJsonForPatchDeliveryWithNotes(subName:string, deliveryDate:string){
+	const compositeKey : string = generateCompositeKey(subName, deliveryDate);
+	const deliveryUpdateUrl : string = generateDeliveryUpdateUrl(compositeKey);
+	const referenceId = 'UpdateDeliveryNotes_'+compositeKey.replace('A-S', '').replaceAll('-', '_');
+	console.log('referenceId:'+referenceId);
+	return {
+		method : 'PATCH',
+		url : deliveryUpdateUrl,
+		referenceId : referenceId,
+		body : {Notes__c : 'updating ' + compositeKey}
+	}
+}
+
+function createJsonForGetSFSubFromDelivery(subName:string, deliveryDate:string){
+	const compositeKey : string = generateCompositeKey(subName, deliveryDate);
+	return {
+		method : 'GET',
+		referenceId : "GetDelivery_"+compositeKey.replace('A-S', '').replaceAll('-', '_'),
+        url : "/services/data/v58.0/sobjects/Delivery__c/@{UpdateDeliveryNotes_"+compositeKey.replace('A-S', '').replaceAll('-', '_')+".id}?fields=SF_Subscription__c",
+	}
+}
+
+function createJsonForCreateCase(subName:string, deliveryDate:string){
+	const compositeKey : string = generateCompositeKey(subName, deliveryDate);
+	return {
+        method : "POST",
+        referenceId : "CreateCase_"+compositeKey.replace('A-S', '').replaceAll('-', '_'),
+        url : "/services/data/v58.0/sobjects/Case",
+        body : {  
+            description : "Case from composite api",
+            SF_Subscription__c : "@{GetDelivery_"+compositeKey.replace('A-S', '').replaceAll('-', '_')+".SF_Subscription__c}"
+        }
+    }
+}
+
+function createJsonForUpdateDeliveryWithCase(subName:string, deliveryDate:string){
+	const compositeKey : string = generateCompositeKey(subName, deliveryDate);
+	const deliveryUpdateUrl : string = generateDeliveryUpdateUrl(compositeKey);
+	return {
+        method : "PATCH",
+        url : deliveryUpdateUrl,
+        referenceId : "UpdateDeliveryCase_"+compositeKey.replace('A-S', '').replaceAll('-', '_'),
+        body : {  
+            Case__c : "@{CreateCase_"+compositeKey.replace('A-S', '').replaceAll('-', '_')+".id}"
+        }
+    }
+}
+
+function generateDeliveryUpdateUrl(compositeKey : string){
+	return '/services/data/v58.0/sobjects/Delivery__c/Composite_Key__c/'+compositeKey;
+}
+
+function generateCompositeKey(subName : string, deliveryDate : string){
+	const year = deliveryDate.substring(6,10);
+	const month = deliveryDate.substring(3,5);
+	const day = deliveryDate.substring(0,2);
+	return subName + '-' + year + '-' + month + '-' + day;
+}
+
+function generateCompositeJson(records: string[]){
+	// var myArray :string[] = [];
+
+	// for (const record of records){
+	// 	console.log('record:',record);
+	// 	if(record.reason_code){
+
+	// 	}
+	// 	myArray.push(record);
+	// }
+	// console.log('myArray:',myArray);
+
+}
+
+
+async function getFileRows(){
+	return parse(
+		fs.readFileSync('../gnm_failed_deliveries_11_07_23.csv', 'utf-8'), 
+		{columns: true, skip_empty_lines: true}
+	);
+}
+
+type FileRow = {
+	sub: string;
+	"Delivery Quantity": string;
+	"Delivery Date": string;
+	reason_code : string;	
+	"Extended Reason": string;
+};
+
+type DeliveryUpdate = {
+	compositeKey: string;
+};
+
+type Request = {
+    allOrNone: boolean
+    compositeRequest: CompositeRequest[]
+}
+
+type CompositeRequest = {
+    method: string
+    url: string
+    referenceId: string
+    body?: Body
+}
+
+type Body = {
+	Case__c?: string
+	description?: string
+	SF_Subscription__c?: string
+	Notes__c?: string
+}
+
+main();
+
