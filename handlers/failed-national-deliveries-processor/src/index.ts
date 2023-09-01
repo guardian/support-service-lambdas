6e2a85703d600f401e92d89f2eda5b1510fe6516
David Pepper <david.pepper@guardian.co.uk>
2023-09-01 12:25:09 +0100

    use const instead of var

--- handlers/failed-national-deliveries-processor/src/index.ts
+++ handlers/failed-national-deliveries-processor/src/index.ts
@@ -6,7 +6,8 @@ export const main = async (): Promise<string> => {
 	
 	const allFileRows = await getFileContents();
 	const failedDeliveryRows : FileRow[] = getFailedDeliveryRowsFromFileContents(allFileRows);		
-	var requests : Request[] = generateRequestsFromFailedDeliveryRows(failedDeliveryRows);
+	
+	const requests : Request[] = generateRequestsFromFailedDeliveryRows(failedDeliveryRows);
 	
 	requests.forEach(
 		request => console.log('request: '+ JSON.stringify(request))
