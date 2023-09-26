a62b59a1c57bbd11707be2468d69b79c4fe5251a
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2023-09-26 15:58:45 +0100

    chore: remove import

--- cdk/lib/single-contributions-processor.ts
+++ cdk/lib/single-contributions-processor.ts
@@ -1,6 +1,5 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import { CfnOutput } from 'aws-cdk-lib';
 import type { App } from 'aws-cdk-lib';
 import { Queue, QueueEncryption } from 'aws-cdk-lib/aws-sqs';
 
