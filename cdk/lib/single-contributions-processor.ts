61f5e7b6c68207e7ddc984c69303ebaa765c1d85
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2023-09-26 14:51:10 +0100

    ops: add encryption at rest using SQS managed keys

--- cdk/lib/single-contributions-processor.ts
+++ cdk/lib/single-contributions-processor.ts
@@ -2,7 +2,7 @@ import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { CfnOutput } from 'aws-cdk-lib';
 import type { App } from 'aws-cdk-lib';
-import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Queue, QueueEncryption } from 'aws-cdk-lib/aws-sqs';
 
 export const APP_NAME = 'single-contributions-processor';
 
@@ -13,10 +13,12 @@ export class SingleContributionsProcessorStack extends GuStack {
 		// Resources
 		const deadLetterQueue = new Queue(this, `dead-letters-${APP_NAME}-queue`, {
 			queueName: `dead-letters-${APP_NAME}-queue-${props.stage}`,
+			encryption: QueueEncryption.SQS_MANAGED,
 		});
 
 		const queue = new Queue(this, `${APP_NAME}-queue`, {
 			queueName: `${APP_NAME}-queue-${props.stage}`,
+			encryption: QueueEncryption.SQS_MANAGED,
 			deadLetterQueue: {
 				queue: deadLetterQueue,
 				maxReceiveCount: 2,
