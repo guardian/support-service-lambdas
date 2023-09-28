90c104742c8ea78bf344013c8f90820140ef0793
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2023-09-28 11:06:51 +0100

    refactor: SQS queues are SSE-SQS encrypted by default

--- cdk/lib/single-contribution-salesforce-writes.ts
+++ cdk/lib/single-contribution-salesforce-writes.ts
@@ -1,7 +1,7 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
-import { Queue, QueueEncryption } from 'aws-cdk-lib/aws-sqs';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 
 export const APP_NAME = 'single-contribution-salesforce-writes';
 
@@ -11,12 +11,10 @@ export class SingleContributionSalesforceWrites extends GuStack {
 
 		const deadLetterQueue = new Queue(this, `dead-letters-${APP_NAME}-queue`, {
 			queueName: `dead-letters-${APP_NAME}-queue-${props.stage}`,
-			encryption: QueueEncryption.SQS_MANAGED,
 		});
 
 		new Queue(this, `${APP_NAME}-queue`, {
 			queueName: `${APP_NAME}-queue-${props.stage}`,
-			encryption: QueueEncryption.SQS_MANAGED,
 			deadLetterQueue: {
 				queue: deadLetterQueue,
 				maxReceiveCount: 3,
