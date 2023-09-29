0a284a2e4df5bce26c249f8afdc0d3ec9dc9aba1
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2023-09-29 11:32:27 +0100

    ops: add rule to acquisition bus to send payment-api messages to the single-contribution-salesforce-writes-queue

--- cdk/lib/single-contribution-salesforce-writes.ts
+++ cdk/lib/single-contribution-salesforce-writes.ts
@@ -1,6 +1,9 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
+import { EventBus, Rule } from 'aws-cdk-lib/aws-events';
+import { SqsQueue } from 'aws-cdk-lib/aws-events-targets';
+import { Effect, PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 
 export const APP_NAME = 'single-contribution-salesforce-writes';
@@ -13,12 +16,55 @@ export class SingleContributionSalesforceWrites extends GuStack {
 			queueName: `dead-letters-${APP_NAME}-queue-${props.stage}`,
 		});
 
-		new Queue(this, `${APP_NAME}-queue`, {
+		const queue = new Queue(this, `${APP_NAME}-queue`, {
 			queueName: `${APP_NAME}-queue-${props.stage}`,
 			deadLetterQueue: {
 				queue: deadLetterQueue,
 				maxReceiveCount: 3,
 			},
 		});
+
+		enum AcquisitionBusSource {
+			PaymentApi = 'payment-api',
+		}
+
+		const acquisitionBusName = `acquisitions-bus-${props.stage}`;
+
+		const acquisitionBus = EventBus.fromEventBusArn(
+			this,
+			'AcquisitionBus',
+			`arn:aws:events:${this.region}:${this.account}:event-bus/${acquisitionBusName}`,
+		);
+
+		const rule = new Rule(
+			this,
+			'AcquisitionBusToSingleContributionSalesforceWritesQueueRule',
+			{
+				description:
+					'Send payment api events to the single-contribution-salesforce-writes-queue',
+				eventPattern: {
+					region: [this.region],
+					account: [this.account],
+					source: [AcquisitionBusSource.PaymentApi],
+				},
+				eventBus: acquisitionBus,
+				targets: [new SqsQueue(queue)],
+			},
+		);
+
+		const policyStatement = new PolicyStatement({
+			sid: 'Allow acquisition bus to send messages to the single-contribution-salesforce-writes-queue',
+			principals: [new ServicePrincipal('events.amazonaws.com')],
+			effect: Effect.ALLOW,
+			resources: [queue.queueArn],
+			actions: ['sqs:SendMessage'],
+			conditions: {
+				ArnEquals: {
+					'aws:SourceArn': rule.ruleArn,
+				},
+			},
+		});
+
+		queue.addToResourcePolicy(policyStatement);
 	}
 }
