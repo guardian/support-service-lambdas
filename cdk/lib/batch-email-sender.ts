ba9a6f5bb3a5e134e362475954d29c569526e038
Michael Jacobson <michael.jacobson@guardian.co.uk>
2023-10-18 14:58:06 +0100

    generate cfn templates using cfninclude

--- cdk/lib/batch-email-sender.ts
+++ cdk/lib/batch-email-sender.ts
@@ -1,10 +1,15 @@
 import {GuStack, GuStackProps} from "@guardian/cdk/lib/constructs/core";
 import {App} from "aws-cdk-lib";
+import {CfnInclude} from "aws-cdk-lib/cloudformation-include";
 
 export interface BatchEmailSenderProps extends GuStackProps {}
 
 export class BatchEmailSender extends GuStack {
     constructor(scope: App, id: string, props: BatchEmailSenderProps) {
         super(scope, id, props);
+        const yamlTemplateFilePath = `${__dirname}/../../handlers/batch-email-sender/cfn.yaml`;
+        new CfnInclude(this, "YamlTemplate", {
+            templateFile: yamlTemplateFilePath,
+        });
     }
 }
