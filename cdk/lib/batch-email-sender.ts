896feb7a339e82820a48ad59b35aaafa1e84f4a0
Michael Jacobson <michael.jacobson@guardian.co.uk>
2023-10-23 16:26:01 +0100

    Dual stack cloudformation (Step 2) (#2079)
    * work in progress
    
    * only comment out `BatchEmailSenderUsagePlanKey` in original cfn yaml template
    
    * update jest snapshots
    
    * add inline role policies
    
    * ensure unique resource names where needed
    
    * add `EmailQueueName` environment variable

--- cdk/lib/batch-email-sender.ts
+++ cdk/lib/batch-email-sender.ts
@@ -1,6 +1,14 @@
+import {GuApiGatewayWithLambdaByPath} from "@guardian/cdk";
+import {GuAlarm} from "@guardian/cdk/lib/constructs/cloudwatch";
 import type { GuStackProps} from "@guardian/cdk/lib/constructs/core";
 import {GuStack} from "@guardian/cdk/lib/constructs/core";
+import {GuLambdaFunction} from "@guardian/cdk/lib/constructs/lambda";
 import type {App} from "aws-cdk-lib";
+import {Duration, Fn} from "aws-cdk-lib";
+import {ApiKey, CfnUsagePlanKey, Cors, UsagePlan} from "aws-cdk-lib/aws-apigateway";
+import {ComparisonOperator, Metric} from "aws-cdk-lib/aws-cloudwatch";
+import {Effect, Policy, PolicyStatement} from "aws-cdk-lib/aws-iam";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 import {CfnInclude} from "aws-cdk-lib/cloudformation-include";
 
 export class BatchEmailSender extends GuStack {
@@ -10,5 +18,149 @@ export class BatchEmailSender extends GuStack {
         new CfnInclude(this, "YamlTemplate", {
             templateFile: yamlTemplateFilePath,
         });
+
+
+        // ---- Miscellaneous constants ---- //
+        const isProd = this.stage === 'PROD';
+        const app = "batch-email-sender";
+
+
+        // ---- API-triggered lambda functions ---- //
+        const batchEmailSenderLambda = new GuLambdaFunction(this, "BatchEmailSenderLambda", {
+            app,
+            handler: "com.gu.batchemailsender.api.batchemail.Handler::apply",
+            functionName: `batch-email-sender-${this.stage}-CDK`,
+            runtime: Runtime.JAVA_11,
+            fileName: "batch-email-sender.jar",
+            memorySize: 1536,
+            timeout: Duration.seconds(300),
+            environment: {
+                "Stage": this.stage,
+                "EmailQueueName": Fn.importValue(`comms-${this.stage}-EmailQueueName`)
+            },
+        });
+
+
+        // ---- API gateway ---- //
+        const batchEmailSenderApi = new GuApiGatewayWithLambdaByPath(this, {
+            app,
+            defaultCorsPreflightOptions: {
+                allowOrigins: Cors.ALL_ORIGINS,
+                allowMethods: Cors.ALL_METHODS,
+                allowHeaders: ["Content-Type"],
+            },
+            monitoringConfiguration: { noMonitoring: true },
+            targets: [
+                {
+                    path: "/email-batch",
+                    httpMethod: "POST",
+                    lambda: batchEmailSenderLambda,
+                    apiKeyRequired: true,
+                },
+            ],
+        })
+
+
+        // ---- Usage plan and API key ---- //
+        const usagePlan = new UsagePlan(this, "BatchEmailSenderUsagePlan", {
+            name: `batch-email-sender-api-usage-plan-${this.stage}`,
+            apiStages: [
+                {
+                    api: batchEmailSenderApi.api,
+                    stage: batchEmailSenderApi.api.deploymentStage
+                }
+            ]
+        })
+
+        const apiKey = new ApiKey(this, "BatchEmailSenderApiKey", {
+            apiKeyName: `batch-email-sender-api-key-${this.stage}-CDK`,
+            description: "Key required to call batch email sender API",
+            enabled: true,
+        })
+
+        new CfnUsagePlanKey(this, "BatchEmailSenderUsagePlanKey-CDK", {
+            keyId: apiKey.keyId,
+            keyType: "API_KEY",
+            usagePlanId: usagePlan.usagePlanId,
+        })
+
+
+        // ---- Alarms ---- //
+        new GuAlarm(this, 'FailedEmailApiAlarm', {
+            app,
+            alarmName: "URGENT 9-5 - PROD: Failed to send email triggered by Salesforce - 5XXError (CDK)",
+            alarmDescription: "API responded with 5xx to Salesforce meaning some emails failed to send. Logs at /aws/lambda/batch-email-sender-PROD repo at https://github.com/guardian/support-service-lambdas/blob/main/handlers/batch-email-sender/",
+            evaluationPeriods: 1,
+            threshold: 1,
+            actionsEnabled: isProd,
+            snsTopicName: "retention-dev",
+            comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+            metric: new Metric({
+                metricName: "5XXError",
+                namespace: "AWS/ApiGateway",
+                statistic: "Sum",
+                period: Duration.seconds(60),
+                dimensionsMap: {
+                    ApiName: `BatchEmailSender-${this.stage}`,
+                }
+            }),
+        });
+
+        new GuAlarm(this, 'FailedEmailLambdaAlarm', {
+            app,
+            alarmName: "URGENT 9-5 - PROD: Failed to send email triggered by Salesforce - Lambda crash (CDK)",
+            alarmDescription: "Lambda crashed unexpectedely meaning email message sent from Salesforce to the Service Layer could not be processed. Logs at /aws/lambda/batch-email-sender-PROD repo at https://github.com/guardian/support-service-lambdas/blob/main/handlers/batch-email-sender/",
+            evaluationPeriods: 1,
+            threshold: 1,
+            actionsEnabled: isProd,
+            snsTopicName: "retention-dev",
+            comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+            metric: new Metric({
+                metricName: "Errors",
+                namespace: "AWS/Lambda",
+                statistic: "Sum",
+                period: Duration.seconds(300),
+                dimensionsMap: {
+                    FunctionName: batchEmailSenderLambda.functionName,
+                }
+            }),
+        });
+
+
+        // ---- Apply policies ---- //
+        const cloudwatchLogsInlinePolicy: Policy = new Policy(this, "cloudwatch-logs-inline-policy", {
+            statements: [
+                new PolicyStatement({
+                    effect: Effect.ALLOW,
+                    actions: [
+                        "logs:CreateLogGroup",
+                        "logs:CreateLogStream",
+                        "logs:PutLogEvents",
+                        "lambda:InvokeFunction"
+                    ],
+                    resources: [
+                        `arn:aws:logs:${this.region}:${this.account}:log-group:/aws/lambda/batch-email-sender-${this.stage}:log-stream:*`,
+                    ]
+                }),
+            ],
+        })
+
+        const sqsInlinePolicy: Policy = new Policy(this, "sqs-inline-policy", {
+            statements: [
+                new PolicyStatement({
+                    effect: Effect.ALLOW,
+                    actions: [
+                        "sqs:GetQueueUrl",
+                        "sqs:SendMessage",
+                    ],
+                    resources: [
+                        `arn:aws:sqs:${this.region}:${this.account}:braze-emails-${this.stage}`,
+                    ]
+                }),
+            ],
+        })
+
+        batchEmailSenderLambda.role?.attachInlinePolicy(cloudwatchLogsInlinePolicy)
+        batchEmailSenderLambda.role?.attachInlinePolicy(sqsInlinePolicy)
     }
 }
