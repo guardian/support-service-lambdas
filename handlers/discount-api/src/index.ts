3e2a27095abddab1fd39b3b4906f118aab020cdf
Rupert Bates <Rupert.Bates@guardian.co.uk>
2023-11-16 15:46:27 +0000

    CI workflow

--- handlers/discount-api/src/index.ts
+++ handlers/discount-api/src/index.ts
@@ -1,5 +1,15 @@
-export const handler = async () => {
+import type {
+	APIGatewayProxyEvent,
+	APIGatewayProxyResult,
+	Handler,
+} from 'aws-lambda';
+
+export const handler: Handler = async (
+	event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+	console.log(`Input is ${JSON.stringify(event)}`);
 	return await Promise.resolve({
-		success: true,
+		body: 'Hello World',
+		statusCode: 200,
 	});
 };
