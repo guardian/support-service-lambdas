5e3e06e652d3ae5fa77ade1fa03399727947c53a
Rupert Bates <Rupert.Bates@guardian.co.uk>
2023-12-15 14:46:11 +0000

    Correct discounts in PROD

--- handlers/discount-api/src/productToDiscountMapping.ts
+++ handlers/discount-api/src/productToDiscountMapping.ts
@@ -67,9 +67,9 @@ const ProductToDiscountMapping: {
 	},
 	PROD: {
 		Month: {
-			discountProductRatePlanId: '8a128adf8b64bcfd018b6b6fdc7674f5',
-			name: 'Cancellation Save Discount - 25% off for 12 months',
-			effectiveStartDate: '2023-10-26',
+			discountProductRatePlanId: '2c92a0ff64176cd40164232c8ec97661',
+			name: 'Cancellation Save Discount - 25% off for 3 months',
+			effectiveStartDate: '2018-06-22',
 			effectiveEndDate: '2099-03-08',
 			eligibleProductRatePlanIds: [
 				'2c92a0fb4edd70c8014edeaa4eae220a',
@@ -77,9 +77,9 @@ const ProductToDiscountMapping: {
 			],
 		},
 		Quarter: {
-			discountProductRatePlanId: '8a128adf8b64bcfd018b6b6fdc7674f5',
-			name: 'Cancellation Save Discount - 25% off for 12 months',
-			effectiveStartDate: '2023-10-26',
+			discountProductRatePlanId: '2c92a0ff64176cd40164232c8ec97661',
+			name: 'Cancellation Save Discount - 25% off for 3 months',
+			effectiveStartDate: '2018-06-22',
 			effectiveEndDate: '2099-03-08',
 			eligibleProductRatePlanIds: [
 				'2c92a0fb4edd70c8014edeaa4eae220a',
@@ -87,9 +87,9 @@ const ProductToDiscountMapping: {
 			],
 		},
 		Annual: {
-			discountProductRatePlanId: '2c92a0ff64176cd40164232c8ec97661',
-			name: 'Cancellation Save Discount - 25% off for 3 months',
-			effectiveStartDate: '2018-06-22',
+			discountProductRatePlanId: '8a128adf8b64bcfd018b6b6fdc7674f5',
+			name: 'Cancellation Save Discount - 25% off for 12 months',
+			effectiveStartDate: '2023-10-26',
 			effectiveEndDate: '2099-03-08',
 			eligibleProductRatePlanIds: ['2c92a0fb4edd70c8014edeaa4e972204'],
 		},
