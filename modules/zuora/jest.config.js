c2c6f44a8559f54833352748a18f4fe627c76ae1
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-01-12 11:28:39 +0000

    Fix module resolution in tests

--- modules/zuora/jest.config.js
+++ modules/zuora/jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	runner: 'groups',
+	moduleNameMapper: {
+		'@modules/(.*)/(.*)$': '<rootDir>/../../modules/$1/src/$2',
+		'@modules/(.*)$': '<rootDir>/../../modules/$1',
+	},
 };
