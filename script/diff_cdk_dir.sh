a819db340ac882389299f4576714ff243130705d
Michael Jacobson <michael.jacobson@guardian.co.uk>
2024-01-30 18:00:08 +0000

    tweaking git diff (3)

--- script/diff_cdk_dir.sh
+++ script/diff_cdk_dir.sh
@@ -4,7 +4,7 @@ CDK_DIR=$1
 BASE=$2
 HEAD=$3
 
-git diff --quiet $BASE...$HEAD $CDK_DIR
+git diff --quiet $BASE..$HEAD $CDK_DIR
 if [ $? -eq 0 ]; then
   echo "CDK_CHANGES=false" >> $GITHUB_ENV
 else
