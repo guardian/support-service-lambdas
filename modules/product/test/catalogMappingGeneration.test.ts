f87af4ea780bd6e25c8309140599b51f218b0440
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-02-07 14:49:34 +0000

    Fix tests

--- modules/product/test/catalogMappingGeneration.test.ts
+++ modules/product/test/catalogMappingGeneration.test.ts
@@ -1,8 +1,5 @@
 import { catalogSchema } from '@modules/catalog/catalogSchema';
-import {
-	generateCatalogMapping,
-	writeMappingsToFile,
-} from '@modules/product/catalogMappingGeneration';
+import { generateCatalogMapping } from '@modules/product/catalogMappingGeneration';
 import codeMapping from '@modules/product/codeCatalogMapping.json';
 import prodMapping from '@modules/product/prodCatalogMapping.json';
 import code from '../../catalog/test/fixtures/catalog-code.json';
@@ -13,12 +10,6 @@ test('We can generate the catalog mapping from a catalog file', () => {
 	const codeCatalogMapping = generateCatalogMapping(codeCatalog);
 	const prodCatalog = catalogSchema.parse(prod);
 	const prodCatalogMapping = generateCatalogMapping(prodCatalog);
-	console.log(JSON.stringify(codeCatalogMapping, null, 2));
-	console.log(JSON.stringify(prodCatalogMapping, null, 2));
 	expect(codeCatalogMapping).toEqual(codeMapping);
 	expect(prodCatalogMapping).toEqual(prodMapping);
-});
-
-test('We can write the catalog mapping to a file', async () => {
-	await writeMappingsToFile();
 });
