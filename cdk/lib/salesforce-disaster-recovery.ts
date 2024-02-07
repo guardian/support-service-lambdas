d738d74337b656a7cb9608ba00fb1c54f64544c6
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-07 14:13:18 +0000

    fix: update salesforce domain url

--- cdk/lib/salesforce-disaster-recovery.ts
+++ cdk/lib/salesforce-disaster-recovery.ts
@@ -49,8 +49,8 @@ export class SalesforceDisasterRecovery extends GuStack {
 
 		const salesforceApiDomain =
 			this.stage === 'PROD'
-				? 'https://gnmtouchpoint--dev1.sandbox.my.salesforce.com'
-				: 'https://gnmtouchpoint.my.salesforce.com';
+				? 'https://gnmtouchpoint.my.salesforce.com'
+				: 'https://gnmtouchpoint--dev1.sandbox.my.salesforce.com';
 
 		const createSalesforceQueryJob = new CustomState(
 			this,
