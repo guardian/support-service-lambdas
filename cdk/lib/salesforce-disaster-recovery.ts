d54b6649c25ea9cffcc2824e5dde95b2be8a7d72
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-09 14:15:28 +0000

    fix: manage imports

--- cdk/lib/salesforce-disaster-recovery.ts
+++ cdk/lib/salesforce-disaster-recovery.ts
@@ -1,6 +1,6 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import { type App } from 'aws-cdk-lib';
+import { type App, Duration } from 'aws-cdk-lib';
 import { Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import {
 	CustomState,
