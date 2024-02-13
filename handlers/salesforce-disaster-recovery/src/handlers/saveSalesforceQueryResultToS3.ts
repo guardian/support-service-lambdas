f541deaa6afe5cdfe649bd6c7fd3190cbc4df841
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-13 17:10:59 +0000

    build: install @aws-sdk/client-s3

--- handlers/salesforce-disaster-recovery/src/handlers/saveSalesforceQueryResultToS3.ts
+++ handlers/salesforce-disaster-recovery/src/handlers/saveSalesforceQueryResultToS3.ts
@@ -1,3 +1,7 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+const client = new S3Client({ region: process.env.region });
+
 export const handler = (event: { queryJobId: string }) => {
 	console.log(event);
 	console.log('Inside lambda...');
