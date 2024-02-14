36e6ae8eb0804b9637775dcf3ad576ba7f3e73ac
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-14 17:36:36 +0000

    test: add logs

--- handlers/salesforce-disaster-recovery/src/handlers/saveSalesforceQueryResultToS3.ts
+++ handlers/salesforce-disaster-recovery/src/handlers/saveSalesforceQueryResultToS3.ts
@@ -10,67 +10,12 @@ const secretsManagerClient = new SecretsManagerClient({
 });
 
 export const handler = async (event: { queryJobId: string }) => {
-	console.log(event);
-
 	if (!process.env.SALESFORCE_API_DOMAIN) {
 		throw new Error('No env');
 	}
 
-	// Get secret
-	const input = {
-		SecretId:
-			'events!connection/salesforce-disaster-recovery-CODE-salesforce-api/e2792d75-414a-48f3-89a1-5e8eac15f627',
-	};
-	const command = new GetSecretValueCommand(input);
-	const response = await secretsManagerClient.send(command);
+	const token = await fetchToken();
 
-	if (!response.SecretString) throw new Error('No secret');
-
-	const secretValue = JSON.parse(response.SecretString) as {
-		authorization_endpoint: string;
-		client_id: string;
-		client_secret: string;
-		oauth_http_parameters: {
-			body_parameters: Array<{ key: string; value: string }>;
-		};
-	};
-
-	let token = '';
-
-	// Get Salesforce token
-	try {
-		const formData = new URLSearchParams([
-			['client_id', secretValue.client_id],
-			['client_secret', secretValue.client_secret],
-		]);
-
-		secretValue.oauth_http_parameters.body_parameters.forEach((param) => {
-			formData.append(param.key, param.value);
-		});
-
-		const response = await fetch(secretValue.authorization_endpoint, {
-			method: 'POST',
-			headers: {
-				// 'Content-Type': 'application/json',
-				Accept: 'application/json',
-			},
-			body: formData,
-		});
-
-		const json = (await response.json()) as { access_token: string };
-		console.log(json);
-		console.log(Object.keys(json));
-		console.log(Object.entries(json));
-		console.log(Object.values(json));
-		console.log(typeof json['access_token']);
-		console.log(typeof json.access_token);
-		token = json['access_token'];
-	} catch (error) {
-		console.error('Error during request: ', error);
-		throw new Error('Failed to get access token');
-	}
-
-	// Get Query result
 	try {
 		const response = await fetch(
 			`${process.env.SALESFORCE_API_DOMAIN}/services/data/v59.0/jobs/query/${event.queryJobId}/results`,
@@ -91,4 +36,55 @@ export const handler = async (event: { queryJobId: string }) => {
 	}
 
 	// Save CSV to S3
+};
+
+const fetchToken = async () => {
+	try {
+		const input = {
+			SecretId:
+				'events!connection/salesforce-disaster-recovery-CODE-salesforce-api/e2792d75-414a-48f3-89a1-5e8eac15f627',
+		};
+		const command = new GetSecretValueCommand(input);
+		const response1 = await secretsManagerClient.send(command);
+
+		if (!response1.SecretString) throw new Error('No secret');
+
+		const secretValue = JSON.parse(response1.SecretString) as {
+			authorization_endpoint: string;
+			client_id: string;
+			client_secret: string;
+			oauth_http_parameters: {
+				body_parameters: Array<{ key: string; value: string }>;
+			};
+		};
+		const formData = new URLSearchParams([
+			['client_id', secretValue.client_id],
+			['client_secret', secretValue.client_secret],
+		]);
+
+		secretValue.oauth_http_parameters.body_parameters.forEach((param) => {
+			formData.append(param.key, param.value);
+		});
+
+		const response2 = await fetch(secretValue.authorization_endpoint, {
+			method: 'POST',
+			// headers: {
+			// 	// 'Content-Type': 'application/json',
+			// 	Accept: 'application/json',
+			// },
+			body: formData,
+		});
+
+		const json = (await response2.json()) as { access_token: string };
+		console.log(json);
+		console.log(Object.keys(json));
+		console.log(Object.entries(json));
+		console.log(Object.values(json));
+		console.log(typeof json['access_token']);
+		console.log(typeof json.access_token);
+		return json['access_token'];
+	} catch (error) {
+		console.error('Error during request: ', error);
+		throw new Error('Failed to get access token');
+	}
 };
