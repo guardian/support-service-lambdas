c416a748478c696048f2e0f2e1d373b203a44b0c
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-19 16:59:56 +0000

    fix: update schema

--- modules/zuora/src/actionUpdate.ts
+++ modules/zuora/src/actionUpdate.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 import type { ZuoraClient } from './zuoraClient';
 
-const successResponseSchema = z.object({
+const successObjectSchema = z.object({
 	Id: z.string(),
 	Success: z.literal(true),
 });
 
-const errorResponseSchema = z.object({
+const errorObjectSchema = z.object({
 	Errors: z.array(
 		z.object({
 			Message: z.string(),
@@ -16,9 +16,16 @@ const errorResponseSchema = z.object({
 	Success: z.literal(false),
 });
 
-const responseSchema = z.union([successResponseSchema, errorResponseSchema]);
+const responseSchema = z.array(
+	z.union([successObjectSchema, errorObjectSchema]),
+);
 
-export const actionUpdate = async (zuoraClient: ZuoraClient, body: string) => {
+export type ActionUpdateResponse = z.infer<typeof responseSchema>;
+
+export const actionUpdate = async (
+	zuoraClient: ZuoraClient,
+	body: string,
+): Promise<ActionUpdateResponse> => {
 	const path = 'v1/action/update';
 
 	return zuoraClient.post(path, body, responseSchema);
