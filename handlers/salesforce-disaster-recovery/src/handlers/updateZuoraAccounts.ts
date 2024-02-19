7b8ce868be739b4ae6b485167d4e861d69091a5c
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-19 15:29:34 +0000

    test: use zuora client from modules

--- handlers/salesforce-disaster-recovery/src/handlers/updateZuoraAccounts.ts
+++ handlers/salesforce-disaster-recovery/src/handlers/updateZuoraAccounts.ts
@@ -1,3 +1,5 @@
+import { getSubscription } from '@modules/zuora/getSubscription';
+import { ZuoraClient } from '@modules/zuora/zuoraClient';
 import { convertCsvToAccountRows, getFileFromS3 } from '../services';
 
 export const handler = async (event: { filePath: string }) => {
@@ -14,8 +16,19 @@ export const handler = async (event: { filePath: string }) => {
 
 	const rows = convertCsvToAccountRows({ csvString: fileContent });
 	console.log(rows[0]);
+	await testZuoraServiceCall();
 
 	return {
 		StatusCode: 200,
 	};
+};
+
+const testZuoraServiceCall = async () => {
+	const zuoraClient = await ZuoraClient.create('CODE');
+
+	const subscription = await getSubscription(
+		zuoraClient,
+		'8ad09c9f8db5ab95018dc1fb9c3944e7',
+	);
+	console.log(subscription);
 };
