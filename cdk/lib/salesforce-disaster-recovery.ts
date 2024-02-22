b164cc1b262a1ac99bb9bca244f31336642c20b9
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-22 16:46:46 +0000

    infra: test map state

--- cdk/lib/salesforce-disaster-recovery.ts
+++ cdk/lib/salesforce-disaster-recovery.ts
@@ -165,7 +165,7 @@ export class SalesforceDisasterRecovery extends GuStack {
 		const createBatches = new Pass(this, 'CreateBatches', {
 			result: Result.fromObject({
 				// 'batches.$': JsonPath.array(JsonPath.arrayRange(0, 20, 4)),
-				'batches.$': JsonPath.arrayRange(0, 20, 4),
+				batches: JsonPath.arrayRange(0, 20, 4),
 				// batches: [1, 4, 644, 6, 4, 5],
 				// batches: Array.from(
 				// 	{ length: Math.ceil(834085 / 330) },
