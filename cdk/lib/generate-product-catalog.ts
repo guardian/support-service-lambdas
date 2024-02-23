52f0d4df08340735e09a7c94010cdd5b53d1ed28
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-02-23 14:29:26 +0000

    S3 changes

--- cdk/lib/generate-product-catalog.ts
+++ cdk/lib/generate-product-catalog.ts
@@ -1,12 +1,16 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuCname } from '@guardian/cdk/lib/constructs/dns';
 import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda/lambda';
 import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
+import { CfnBasePathMapping, CfnDomainName } from 'aws-cdk-lib/aws-apigateway';
 import { Effect, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { CfnRecordSet } from 'aws-cdk-lib/aws-route53';
 import { Bucket, BucketEncryption, EventType } from 'aws-cdk-lib/aws-s3';
 import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { supportApisDomain, supportCertificateId } from '../bin/cdk';
 
 export interface GenerateProductCatalogProps extends GuStackProps {
 	stack: string;
@@ -60,7 +64,8 @@ export class GenerateProductCatalog extends GuStack {
 			bucketName: productCatalogBucketName,
 			encryption: BucketEncryption.S3_MANAGED,
 			enforceSSL: true,
-			versioned: false,
+			versioned: true,
+			publicReadAccess: true,
 		});
 
 		const s3InlinePolicy: Policy = new Policy(this, 'S3 inline policy', {
@@ -82,5 +87,38 @@ export class GenerateProductCatalog extends GuStack {
 		});
 
 		lambda.role?.attachInlinePolicy(s3InlinePolicy);
+
+		// ---- DNS ---- //
+		// const domainName = `product-catalog-${this.stage}.${supportApisDomain}`;
+
+		// new GuCname(this, 'Cname', {
+		// 	app,
+		// 	domainName,
+		// 	ttl: Duration.minutes(1),
+		// 	resourceRecord: 'dualstack.guardian.map.fastly.net.',
+		// });
+
+		// const certificateArn = `arn:aws:acm:eu-west-1:${this.account}:certificate/${supportCertificateId}`;
+		// const cfnDomainName = new CfnDomainName(this, 'DomainName', {
+		// 	domainName: domainName,
+		// 	regionalCertificateArn: certificateArn,
+		// 	endpointConfiguration: {
+		// 		types: ['REGIONAL'],
+		// 	},
+		// });
+		//
+		// new CfnBasePathMapping(this, 'BasePathMapping', {
+		// 	domainName: cfnDomainName.ref,
+		// 	restApiId: lambda.api.restApiId,
+		// 	stage: lambda.api.deploymentStage.stageName,
+		// });
+		//
+		// new CfnRecordSet(this, 'DNSRecord', {
+		// 	name: domainName,
+		// 	type: 'CNAME',
+		// 	hostedZoneId: props.hostedZoneId,
+		// 	ttl: '120',
+		// 	resourceRecords: [cfnDomainName.attrRegionalDomainName],
+		// });
 	}
 }
