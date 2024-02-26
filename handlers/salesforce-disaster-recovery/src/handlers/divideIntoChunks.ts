b233b75a5ba842a084337ff331c7f06cf5585cab
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-26 11:10:31 +0000

    chore: add logs

--- handlers/salesforce-disaster-recovery/src/handlers/divideIntoChunks.ts
+++ handlers/salesforce-disaster-recovery/src/handlers/divideIntoChunks.ts
@@ -6,22 +6,35 @@ export const handler = (event: {
 	console.log('HERERE');
 	console.log(event);
 	const { filePath, concurrency, numberOfRecords } = event;
+	console.log(filePath);
 	const chunkSize = Math.ceil(numberOfRecords / concurrency);
 
-	const chunks = [];
+	const chunks: Array<{
+		filePath: string;
+		startIndex: number;
+		chunkSize: number;
+	}> = [];
+
+	console.log(chunks);
 
 	for (let i = 0; i < concurrency; i++) {
+		console.log(i);
 		const startIndex = i * chunkSize;
 		const endIndex = Math.min(startIndex + chunkSize - 1, numberOfRecords - 1);
+		console.log(startIndex, endIndex);
 
 		const chunk = {
 			filePath,
 			startIndex,
 			chunkSize: endIndex - startIndex + 1,
 		};
+		console.log(chunk);
 
 		chunks.push(chunk);
+		console.log(chunk);
 	}
+	console.log('final');
+	console.log(chunks);
 
 	return chunks;
 };
