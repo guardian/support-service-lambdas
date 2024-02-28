fc3b66d55027d4905033fc1e0f994480341fd652
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-28 16:46:18 +0000

    test: revert changes

--- handlers/salesforce-disaster-recovery/test/services/secretsManager.test.ts
+++ handlers/salesforce-disaster-recovery/test/services/secretsManager.test.ts
@@ -1,58 +1,54 @@
-// import {
-// 	GetSecretValueCommand,
-// 	SecretsManagerClient,
-// } from '@aws-sdk/client-secrets-manager';
-// import { mockClient } from 'aws-sdk-client-mock';
-// import { getSecretValue } from '../../src/services/secretsManager';
+import {
+	GetSecretValueCommand,
+	SecretsManagerClient,
+} from '@aws-sdk/client-secrets-manager';
+import { mockClient } from 'aws-sdk-client-mock';
+import { getSecretValue } from '../../src/services';
 
-// const secretsManagerClientMock = mockClient(SecretsManagerClient);
+const secretsManagerClientMock = mockClient(SecretsManagerClient);
 
 describe('getSecretValue', () => {
-	// const secretName = 'test-secret';
-	// const secretValue = { password: 'test-password' };
-	// const secretString = JSON.stringify(secretValue);
+	const secretName = 'test-secret';
+	const secretValue = { password: 'test-password' };
+	const secretString = JSON.stringify(secretValue);
 
 	beforeEach(() => {
-		// secretsManagerClientMock.reset();
+		secretsManagerClientMock.reset();
 		jest.resetAllMocks();
 		console.error = jest.fn();
 	});
 
-	test('test', () => {
-		expect(2).toBe(2);
+	test('should get secret value from Secrets Manager', async () => {
+		secretsManagerClientMock.on(GetSecretValueCommand).resolves({
+			SecretString: secretString,
+		});
+
+		const result = await getSecretValue<{ password: string }>({ secretName });
+
+		expect(secretsManagerClientMock.calls().length).toEqual(1);
+		const getSecretArgs = secretsManagerClientMock.call(0)
+			.firstArg as GetSecretValueCommand;
+		expect(getSecretArgs.input.SecretId).toEqual(secretName);
+		expect(result).toEqual(secretValue);
 	});
 
-	// test('should get secret value from Secrets Manager', async () => {
-	// 	secretsManagerClientMock.on(GetSecretValueCommand).resolves({
-	// 		SecretString: secretString,
-	// 	});
+	test('should throw error if no secret found', async () => {
+		secretsManagerClientMock.on(GetSecretValueCommand).resolves({});
 
-	// 	const result = await getSecretValue<{ password: string }>({ secretName });
+		await expect(
+			getSecretValue<{ password: string }>({ secretName }),
+		).rejects.toThrow('No secret found');
+	});
 
-	// 	expect(secretsManagerClientMock.calls().length).toEqual(1);
-	// 	const getSecretArgs = secretsManagerClientMock.call(0)
-	// 		.firstArg as GetSecretValueCommand;
-	// 	expect(getSecretArgs.input.SecretId).toEqual(secretName);
-	// 	expect(result).toEqual(secretValue);
-	// });
+	test('should throw error if Secrets Manager request fails', async () => {
+		const errorMessage = 'Failed to get secret value';
 
-	// test('should throw error if no secret found', async () => {
-	// 	secretsManagerClientMock.on(GetSecretValueCommand).resolves({});
+		secretsManagerClientMock
+			.on(GetSecretValueCommand)
+			.rejects(new Error(errorMessage));
 
-	// 	await expect(
-	// 		getSecretValue<{ password: string }>({ secretName }),
-	// 	).rejects.toThrow('No secret found');
-	// });
-
-	// test('should throw error if Secrets Manager request fails', async () => {
-	// 	const errorMessage = 'Failed to get secret value';
-
-	// 	secretsManagerClientMock
-	// 		.on(GetSecretValueCommand)
-	// 		.rejects(new Error(errorMessage));
-
-	// 	await expect(
-	// 		getSecretValue<{ password: string }>({ secretName }),
-	// 	).rejects.toThrow('Failed to get secret value');
-	// });
+		await expect(
+			getSecretValue<{ password: string }>({ secretName }),
+		).rejects.toThrow('Failed to get secret value');
+	});
 });
