2809744ba12ad19398b1cb9db50a5a37f8de8fc1
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-03-01 17:05:29 +0000

    feat: test distributed map

--- handlers/salesforce-disaster-recovery/src/handlers/updateZuoraAccounts.ts
+++ handlers/salesforce-disaster-recovery/src/handlers/updateZuoraAccounts.ts
@@ -1,52 +1,23 @@
-// import { type Stage } from '@modules/stage';
-// import { ZuoraClient } from '@modules/zuora/zuoraClient';
-import {
-	type AccountRow,
-	// batchUpdateZuoraAccounts,
-	// convertCsvToAccountRows,
-	// getFileFromS3,
-} from '../services';
+import { type Stage } from '@modules/stage';
+import { ZuoraClient } from '@modules/zuora/zuoraClient';
+import { type AccountRow, batchUpdateZuoraAccounts } from '../services';
 
 export const handler = async (event: { Items: AccountRow[] }) => {
 	console.log(event);
-	await Promise.resolve();
-	for (let i = 0; i < event.Items.length; i += 50) {
-		console.log(event.Items[i]);
-		// const batch = event.Items.slice(i, i + 50);
-		// console.log(batch);
+	const stage = process.env.STAGE;
+
+	if (!stage) {
+		throw new Error('Environment variables not set');
 	}
-	// console.log(event);
-	// const stage = process.env.STAGE;
-	// const bucketName = process.env.S3_BUCKET;
 
-	// if (!stage || !bucketName) {
-	// 	throw new Error('Environment variables not set');
-	// }
+	const zuoraClient = await ZuoraClient.create(stage as Stage);
 
-	// const fileContent = await getFileFromS3({
-	// 	bucketName,
-	// 	filePath: event.filePath,
-	// });
+	for (let i = 0; i < event.Items.length; i += 50) {
+		const batch = event.Items.slice(i, i + 50);
 
-	// const rows = convertCsvToAccountRows({ csvString: fileContent }).filter(
-	// 	(row) => row.Zuora__Zuora_Id__c,
-	// );
-
-	// const zuoraClient = await ZuoraClient.create(stage as Stage);
-
-	// const startIndex = event.startIndex;
-	// const endIndex = Math.min(startIndex + event.chunkSize, rows.length - 1);
-
-	// for (let i = startIndex; i < endIndex; i += 50) {
-	// 	const batch = rows.slice(i, i + 50);
-
-	// 	await batchUpdateZuoraAccounts({
-	// 		zuoraClient,
-	// 		accountRows: batch,
-	// 	});
-	// }
-
-	// return {
-	// 	StatusCode: 200,
-	// };
+		await batchUpdateZuoraAccounts({
+			zuoraClient,
+			accountRows: batch,
+		});
+	}
 };
