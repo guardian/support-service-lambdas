64e12a007c83426d11acbf9a431b1cec7328d64c
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-03-01 17:49:18 +0000

    fix: reduce MaxItemsPerBatch

--- cdk/lib/salesforce-disaster-recovery.ts
+++ cdk/lib/salesforce-disaster-recovery.ts
@@ -200,7 +200,7 @@ export class SalesforceDisasterRecovery extends GuStack {
 						},
 					},
 					ItemBatcher: {
-						MaxItemsPerBatch: 5000,
+						MaxItemsPerBatch: 2500,
 					},
 					ItemProcessor: {
 						ProcessorConfig: {
