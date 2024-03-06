66ba41ec3a9495613e185a91293992d9c7246f23
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-03-06 16:45:16 +0000

    refactor: remove comments

--- handlers/salesforce-disaster-recovery/test/services/csv.test.ts
+++ handlers/salesforce-disaster-recovery/test/services/csv.test.ts
@@ -18,30 +18,4 @@ describe('convertArrayToCsv', () => {
 
 		expect(result).toEqual(expectedResult);
 	});
-
-	// it('should handle special characters in data', () => {
-	// 	const mockData = [
-	// 		{ id: 1, name: 'John "Doe"', age: 30 },
-	// 		{ id: 2, name: 'Alice, Bob', age: 25 },
-	// 	];
-
-	// 	const expectedResult = `id,name,age\n"1","John ""Doe""","30"\n"2","Alice, Bob","25"`;
-
-	// 	const result = convertArrayToCsv({ arr: mockData });
-
-	// 	expect(result).toEqual(expectedResult);
-	// });
-
-	// it('should handle empty values in data', () => {
-	// 	const mockData = [
-	// 		{ id: 1, name: '', age: null },
-	// 		{ id: 2, name: 'Alice', age: undefined },
-	// 	];
-
-	// 	const expectedResult = `id,name,age\n1,"",\n2,"Alice",`;
-
-	// 	const result = convertArrayToCsv({ arr: mockData });
-
-	// 	expect(result).toEqual(expectedResult);
-	// });
 });
