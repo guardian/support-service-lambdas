6f668c0d3611c3a0d0a1f9523ee5c099d8104226
Tom Wey <tom.wey@theguardian.com>
2024-03-14 11:26:19 +0000

    Continue defining alarm gchat cdk

--- cdk/lib/alarms-gchat.ts
+++ cdk/lib/alarms-gchat.ts
@@ -1,11 +1,16 @@
+import type {
+	GuLambdaErrorPercentageMonitoringProps,
+	NoMonitoring,
+} from '@guardian/cdk/lib/constructs/cloudwatch';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
-import {GuStack, GuStringParameter} from '@guardian/cdk/lib/constructs/core';
+import { GuStack, GuStringParameter } from '@guardian/cdk/lib/constructs/core';
+import { GuSnsLambdaExperimental } from '@guardian/cdk/lib/experimental/patterns';
 import type { App } from 'aws-cdk-lib';
-import {CfnMapping, Duration} from 'aws-cdk-lib';
+import { CfnMapping, Duration } from 'aws-cdk-lib';
+import { SnsTopic } from 'aws-cdk-lib/aws-events-targets';
 import { Effect, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import {GuSnsLambdaExperimental} from "@guardian/cdk/lib/experimental/patterns";
-import {GuLambdaErrorPercentageMonitoringProps, NoMonitoring} from "@guardian/cdk/lib/constructs/cloudwatch";
+import { Topic } from 'aws-cdk-lib/aws-sns';
 
 export interface AlarmsGchatProps extends GuStackProps {
 	stack: string;
@@ -26,62 +31,73 @@ export class AlarmsGchat extends GuStack {
 		};
 
 		const parameters = {
-			webhook: new GuStringParameter(this, "webhook", {
-				description: "Google Chat webhook for lambda to fire to",
+			webhook: new GuStringParameter(this, 'webhook', {
+				description: 'Google Chat webhook for lambda to fire to',
 			}),
 		};
-		const stageMapping = new CfnMapping(this, "stageMapping", {
-			mapping: {
-				CODE: { alarmActionsEnabled: "FALSE" },
-				PROD: { alarmActionsEnabled: "TRUE" },
-			},
+		// const stageMapping = new CfnMapping(this, 'stageMapping', {
+		// 	mapping: {
+		// 		CODE: { alarmActionsEnabled: 'FALSE' },
+		// 		PROD: { alarmActionsEnabled: 'TRUE' },
+		// 	},
+		// });
+
+		const snsTopic = new Topic(this, `${app}-topic`, {
+			topicName: `${app}-topic-${props.stage}`,
 		});
 
-		const getMonitoringConfiguration = ():
-			| NoMonitoring
-			| GuLambdaErrorPercentageMonitoringProps => {
-			if (this.stage === "PROD"|| this.stage === "CODE") {
-				return {
-					alarmName: `Failed to raise alarm via Google Chat in ${this.stage}`,
-					alarmDescription: "Please check the CloudWatch Alarms dashboard to see which alarms are firing",
-					toleratedErrorPercentage: 0,
-					snsTopicName: "mobile-server-side-email-alert",
-					// We can't use ternaries or other code-based ways of doing this as we synthesise this before applying, meaning this will always return false and therefore CODE
-					actionsEnabled: (stageMapping.findInMap(this.stage, "alarmActionsEnabled") as unknown) as boolean,
-				}
-			}
-			return {
-				noMonitoring: true,
-			};
-		};
+		// const getMonitoringConfiguration = ():
+		// 	| NoMonitoring
+		// 	| GuLambdaErrorPercentageMonitoringProps => {
+		// 	if (this.stage === 'PROD' || this.stage === 'CODE') {
+		// 		return {
+		// 			alarmName: `Failed to raise alarm via Google Chat in ${this.stage}`,
+		// 			alarmDescription:
+		// 				'Please check the CloudWatch Alarms dashboard to see which alarms are firing',
+		// 			toleratedErrorPercentage: 0,
+		// 			snsTopicName: 'mobile-server-side-email-alert',
+		// 			// We can't use ternaries or other code-based ways of doing this as we synthesise this before applying, meaning this will always return false and therefore CODE
+		// 			actionsEnabled: stageMapping.findInMap(
+		// 				this.stage,
+		// 				'alarmActionsEnabled',
+		// 			) as unknown as boolean,
+		// 		};
+		// 	}
+		// 	return {
+		// 		noMonitoring: true,
+		// 	};
+		// };
 
-
-
-		const lambda =  new GuSnsLambdaExperimental(this, "google-chat-bot-sns-to-gchat", {
-			description:
-				'An API Gateway triggered lambda generated in the support-service-lambdas repo',
-			functionName: nameWithStage,
-			fileName: `${app}.zip`,
-			handler: "index.handler",
-			runtime: Runtime.NODEJS_18_X,
-			memorySize: 1024,
-			environment: {
-				WEBHOOK: parameters.webhook.valueAsString,
-				...commonEnvironmentVariables,
+		const lambda = new GuSnsLambdaExperimental(
+			this,
+			'google-chat-bot-sns-to-gchat',
+			{
+				description:
+					'An API Gateway triggered lambda generated in the support-service-lambdas repo',
+				functionName: nameWithStage,
+				fileName: `${app}.zip`,
+				handler: 'index.handler',
+				runtime: Runtime.NODEJS_18_X,
+				memorySize: 1024,
+				environment: {
+					WEBHOOK: parameters.webhook.valueAsString,
+					...commonEnvironmentVariables,
+				},
+				timeout: Duration.seconds(15),
+				existingSnsTopic: { externalTopicName: snsTopic.topicName },
+				monitoringConfiguration: { noMonitoring: true },
+				app: app,
 			},
-			timeout: Duration.seconds(15),
-			existingSnsTopic: { externalTopicName: "mobile-server-side" },
-			monitoringConfiguration: getMonitoringConfiguration(),
-			app: app,
-		});
-
+		);
 
 		const s3InlinePolicy: Policy = new Policy(this, 'S3 inline policy', {
 			statements: [
 				new PolicyStatement({
 					effect: Effect.ALLOW,
 					actions: ['s3:GetObject'],
-					resources: [`arn:aws:s3::*:membership-dist/${this.stack}/${this.stage}/${app}/`],
+					resources: [
+						`arn:aws:s3::*:membership-dist/${this.stack}/${this.stage}/${app}/`,
+					],
 				}),
 			],
 		});
