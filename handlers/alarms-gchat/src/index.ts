fc6cf8b7f039e45e7050eb957315ac6ab8887445
Tom Forbes <tom.forbes@theguardian.com>
2024-03-21 10:32:27 +0000

    use webhook

--- handlers/alarms-gchat/src/index.ts
+++ handlers/alarms-gchat/src/index.ts
@@ -1,17 +1,38 @@
 import type {
-	APIGatewayProxyEvent,
-	APIGatewayProxyResult,
 	Handler,
+	SNSEvent
 } from 'aws-lambda';
 
 export const handler: Handler = async (
-	event: APIGatewayProxyEvent,
-): Promise<APIGatewayProxyResult> => {
+	event: SNSEvent,
+): Promise<null> => {
 	console.log(`Input is ${JSON.stringify(event)}`);
-	return await Promise.resolve({
-		body: 'Hello World',
-		statusCode: 200,
+
+	const webhookUrl = process.env.WEBHOOK;
+
+	if (!webhookUrl) {
+		console.error('No webhook provided');
+		return null;
+	}
+
+	const messages: string[] = event.Records.map(record => {
+		const message = JSON.parse(record.Sns.Message);
+		return `Uh oh, ${message.AlarmName} has triggered!`
 	});
+
+	const responses = messages.map(message => {
+		console.log(`Sending: ${message}`);
+		fetch(webhookUrl, {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				text: message,
+			}),
+		});
+	});
+
+	return Promise.all(responses).then((responses) => {
+		console.log(`Responses: ${responses}`);
+		return null
+	})
 };
-
-
