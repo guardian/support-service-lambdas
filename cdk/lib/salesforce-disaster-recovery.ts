6fa1093d4f43107772aeda07eb36b111b4f3ded6
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-03-21 16:22:45 +0000

    feat: place links in message parameter

--- cdk/lib/salesforce-disaster-recovery.ts
+++ cdk/lib/salesforce-disaster-recovery.ts
@@ -418,9 +418,10 @@ export class SalesforceDisasterRecovery extends GuStack {
 						TopicArn: snsTopic.topicArn,
 						Subject: `Salesforce Disaster Recovery Re-syncing Procedure Completed For ${this.stage}`,
 						'Message.$': JsonPath.format(
-							`State machine execution details: {}\nAccounts that failed to update ({}): {}
+							`State machine execution details: {}\nAccounts to update (?): {}\nAccounts that failed to update ({}): {}
 						`,
 							JsonPath.stringAt('$.stateMachineExecutionDetailsUrl'),
+							JsonPath.stringAt('$.queryResultFileUrl'),
 							JsonPath.stringAt('$.failedRowsCount'),
 							JsonPath.stringAt('$.failedRowsFileUrl'),
 						),
