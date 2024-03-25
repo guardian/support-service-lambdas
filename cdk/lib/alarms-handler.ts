636f8534818910efde92282080e8f89468b4eac5
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-03-25 15:03:19 +0000

    feat: use sns -> sqs -> lambda pattern

--- cdk/lib/alarms-handler.ts
+++ cdk/lib/alarms-handler.ts
@@ -1,107 +1,60 @@
-// import type {
-// 	GuLambdaErrorPercentageMonitoringProps,
-// 	NoMonitoring,
-// } from '@guardian/cdk/lib/constructs/cloudwatch';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack, GuStringParameter } from '@guardian/cdk/lib/constructs/core';
-import { GuSnsLambdaExperimental } from '@guardian/cdk/lib/experimental/patterns';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
-// import { SnsTopic } from 'aws-cdk-lib/aws-events-targets';
-import { Effect, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { Topic } from 'aws-cdk-lib/aws-sns';
-
-export interface AlarmsHandlerProps extends GuStackProps {
-	stack: string;
-	stage: string;
-}
+import { SqsSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 
 export class AlarmsHandler extends GuStack {
-	constructor(scope: App, id: string, props: AlarmsHandlerProps) {
+	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
 		const app = 'alarms-handler';
-		const nameWithStage = `${app}-${this.stage}`;
 
-		const commonEnvironmentVariables = {
-			App: app,
-			Stack: this.stack,
-			Stage: this.stage,
-		};
+		const deadLetterQueue = new Queue(this, `dead-letters-${app}-queue`, {
+			queueName: `dead-letters-${app}-queue-${this.stage}`,
+			retentionPeriod: Duration.days(14),
+		});
+
+		const queue = new Queue(this, `${app}-queue`, {
+			queueName: `${app}-queue-${this.stage}`,
+			deadLetterQueue: {
+				queue: deadLetterQueue,
+				maxReceiveCount: 3,
+			},
+		});
 
 		const parameters = {
-			webhook: new GuStringParameter(this, 'webhook', {
-				description: 'Google Chat webhook for lambda to fire to',
+			webhook: new GuStringParameter(this, `${app}-webhook`, {
+				description: 'Google Chat webhook',
 			}),
 		};
-		// const stageMapping = new CfnMapping(this, 'stageMapping', {
-		// 	mapping: {
-		// 		CODE: { alarmActionsEnabled: 'FALSE' },
-		// 		PROD: { alarmActionsEnabled: 'TRUE' },
-		// 	},
-		// });
+
+		new GuLambdaFunction(this, `${app}-lambda`, {
+			app,
+			memorySize: 1024,
+			fileName: `${app}.zip`,
+			runtime: Runtime.NODEJS_20_X,
+			timeout: Duration.seconds(15),
+			handler: 'index.handler',
+			functionName: `${app}-${this.stage}`,
+			events: [new SqsEventSource(queue)],
+			environment: {
+				APP: app,
+				STACK: this.stack,
+				STAGE: this.stage,
+				WEBHOOK: parameters.webhook.valueAsString,
+			},
+		});
 
 		const snsTopic = new Topic(this, `${app}-topic`, {
-			topicName: `${app}-topic-${props.stage}`,
+			topicName: `${app}-topic-${this.stage}`,
 		});
 
-		// const getMonitoringConfiguration = ():
-		// 	| NoMonitoring
-		// 	| GuLambdaErrorPercentageMonitoringProps => {
-		// 	if (this.stage === 'PROD' || this.stage === 'CODE') {
-		// 		return {
-		// 			alarmName: `Failed to raise alarm via Google Chat in ${this.stage}`,
-		// 			alarmDescription:
-		// 				'Please check the CloudWatch Alarms dashboard to see which alarms are firing',
-		// 			toleratedErrorPercentage: 0,
-		// 			snsTopicName: 'mobile-server-side-email-alert',
-		// 			// We can't use ternaries or other code-based ways of doing this as we synthesise this before applying, meaning this will always return false and therefore CODE
-		// 			actionsEnabled: stageMapping.findInMap(
-		// 				this.stage,
-		// 				'alarmActionsEnabled',
-		// 			) as unknown as boolean,
-		// 		};
-		// 	}
-		// 	return {
-		// 		noMonitoring: true,
-		// 	};
-		// };
-
-		const lambda = new GuSnsLambdaExperimental(
-			this,
-			'google-chat-bot-sns-to-gchat',
-			{
-				description:
-					'An API Gateway triggered lambda generated in the support-service-lambdas repo',
-				functionName: nameWithStage,
-				fileName: `${app}.zip`,
-				handler: 'index.handler',
-				runtime: Runtime.NODEJS_18_X,
-				memorySize: 1024,
-				environment: {
-					WEBHOOK: parameters.webhook.valueAsString,
-					...commonEnvironmentVariables,
-				},
-				timeout: Duration.seconds(15),
-				existingSnsTopic: { externalTopicName: snsTopic.topicName },
-				monitoringConfiguration: { noMonitoring: true },
-				app: app,
-			},
-		);
-
-		const s3InlinePolicy: Policy = new Policy(this, 'S3 inline policy', {
-			statements: [
-				new PolicyStatement({
-					effect: Effect.ALLOW,
-					actions: ['s3:GetObject'],
-					resources: [
-						`arn:aws:s3::*:membership-dist/${this.stack}/${this.stage}/${app}/`,
-					],
-				}),
-			],
-		});
-
-		lambda.role?.attachInlinePolicy(s3InlinePolicy);
+		snsTopic.addSubscription(new SqsSubscription(queue));
 	}
 }
--- handlers/alarms-handler/src/index.ts
+++ handlers/alarms-handler/src/index.ts
@@ -1,46 +1,73 @@
-import type {
-	Handler,
-	SNSEvent
-} from 'aws-lambda';
+import type { SQSEvent, SQSRecord } from 'aws-lambda';
+import { checkDefined } from '@modules/nullAndUndefined';
 
-interface AlarmMessage {
-	AlarmName: string;
-	AlarmDescription?: string;
-	NewStateReason: string;
-}
+// interface AlarmMessage {
+// 	AlarmName: string;
+// 	AlarmDescription?: string;
+// 	NewStateReason: string;
+// }
 
-export const handler: Handler = async (
-	event: SNSEvent,
-): Promise<null> => {
-	console.log(`Input is ${JSON.stringify(event)}`);
+export const handler = async (event: SQSEvent): Promise<void> => {
+	const webhookUrl = checkDefined<string>(
+		process.env.WEBHOOK,
+		'WEBHOOK environment variable not set',
+	);
 
-	const webhookUrl = process.env.WEBHOOK;
+	console.log(webhookUrl);
 
-	if (!webhookUrl) {
-		console.error('No webhook provided');
-		return null;
+	try {
+		for (const record of event.Records) {
+			const recordBody = JSON.parse(record.body) as SQSRecord;
+			console.log(recordBody);
+			// const snsMessage = recordBody.body as SNSEventRecord;
+			// const stripeEvent = body.detail as Stripe.CheckoutSessionCompletedEvent;
+			// const sessionId = stripeEvent.data.object.id;
+			// const { error } = await sendEmail({ sessionId });
+
+			// if (error) {
+			// 	console.error(error);
+
+			// 	throw new Error('Could not process SQS event record');
+			// }
+		}
+	} catch (error) {
+		console.error(error);
+		throw error;
 	}
-
-	const messages: string[] = event.Records.map(record => {
-		const message = JSON.parse(record.Sns.Message) as AlarmMessage;
-		return `*ALARM:* ${message.AlarmName} has triggered!\n\n*Description:* ${message.AlarmDescription ?? ''}\n\n*Reason:* ${message.NewStateReason}}`
-	});
-
-	const responses = messages.map(message => {
-		console.log(`Sending: ${message}`);
-		return fetch(webhookUrl, {
-			method: 'POST',
-			headers: {'Content-Type': 'application/json'},
-			body: JSON.stringify({
-				text: message,
-			}),
-		});
-	});
-
-	return Promise.all(responses).then((responses) => {
-		responses.forEach(response => {
-			console.log(`Response: ${response.status}`);
-		});
-		return null
-	})
 };
+
+// export const handler: Handler = async (event: SQSEvent): Promise<null> => {
+// 	console.log(`Input is ${JSON.stringify(event)}`);
+
+// 	const webhookUrl = process.env.WEBHOOK;
+
+// 	if (!webhookUrl) {
+// 		console.error('No webhook provided');
+// 		return null;
+// 	}
+
+// 	const messages: string[] = event.Records.map((record) => {
+// 		const message = JSON.parse(record.Sns.Message) as AlarmMessage;
+// 		return `*ALARM:* ${message.AlarmName} has triggered!\n\n*Description:* ${
+// 			message.AlarmDescription ?? ''
+// 		}\n\n*Reason:* ${message.NewStateReason}}`;
+// 	});
+
+// 	const responses = messages.map((message) => {
+// 		console.log(`Sending: ${message}`);
+// 		return fetch(webhookUrl, {
+// 			method: 'POST',
+// 			headers: { 'Content-Type': 'application/json' },
+// 			body: JSON.stringify({
+// 				text: message,
+// 			}),
+// 		});
+// 	});
+
+// 	return Promise.all(responses).then((responses) => {
+// 		responses.forEach((response) => {
+// 			console.log(`Response: ${response.status}`);
+// 		});
+// 		return null;
+// 	});
+// };
