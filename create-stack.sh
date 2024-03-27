57fc8eba4a454976e1a8e9903d053ab728093904
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-03-27 11:23:00 +0000

    Make the create script copy lambda files to S3 before trying to create the stack

--- create-stack.sh
+++ create-stack.sh
@@ -6,8 +6,20 @@ set -e
 PROJECT_NAME="$1"
 echo "Creating stack $PROJECT_NAME"
 
+echo "Building lambda package"
+pnpm --filter $PROJECT_NAME package
+
+s3Bucket="membership-dist"
+s3Path="support/CODE/$PROJECT_NAME/$PROJECT_NAME.zip"
+zipFile="./handlers/$PROJECT_NAME/target/$PROJECT_NAME.zip"
+
+echo "Copying lambda package to S3"
+aws s3 cp $zipFile s3://$s3Bucket/$s3Path --profile membership --region eu-west-1
+
+echo "Building CDK"
 pnpm --filter cdk package
 
+echo "Creating stack"
 aws cloudformation create-stack \
   --capabilities '["CAPABILITY_AUTO_EXPAND", "CAPABILITY_NAMED_IAM", "CAPABILITY_IAM"]'  \
   --stack-name "support-CODE-$PROJECT_NAME" \
