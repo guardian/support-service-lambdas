674128e2ca4fb107286175de14e2f427ecb1ecb5
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-03-27 15:11:06 +0000

    Remove bucket name

--- update-lambda.sh
+++ update-lambda.sh
@@ -18,11 +18,17 @@ echo "Updating lambda $PROJECT_NAME"
 
 pnpm --filter $PROJECT_NAME package
 
-s3Bucket="membership-dist"
+s3Bucket=`aws ssm get-parameter --name /account/services/artifact.bucket --query "Parameter.Value" --output text`
 s3Path="support/CODE/$PROJECT_NAME/$PROJECT_NAME.zip"
 zipFile="./handlers/$PROJECT_NAME/target/$PROJECT_NAME.zip"
 
 aws s3 cp $zipFile s3://$s3Bucket/$s3Path --profile membership --region eu-west-1
-aws lambda update-function-code --function-name $PROJECT_NAME-CODE --s3-bucket $s3Bucket --s3-key $s3Path --profile membership --region eu-west-1 > /dev/null
+aws lambda update-function-code \
+  --function-name $PROJECT_NAME-CODE \
+  --s3-bucket $s3Bucket \
+  --s3-key $s3Path \
+  --profile membership \
+  --region eu-west-1 \
+  > /dev/null
 
 echo "Update complete"
