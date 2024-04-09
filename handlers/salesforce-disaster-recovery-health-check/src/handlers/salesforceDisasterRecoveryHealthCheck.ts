faea07fc642d1cae2eee682ad763c19fdbe5cf4e
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-04-09 16:13:01 +0100

    feat: write lambda to run step function

--- handlers/salesforce-disaster-recovery-health-check/src/handlers/salesforceDisasterRecoveryHealthCheck.ts
+++ handlers/salesforce-disaster-recovery-health-check/src/handlers/salesforceDisasterRecoveryHealthCheck.ts
@@ -1,11 +1,45 @@
-import type { APIGatewayProxyResult, Handler } from 'aws-lambda';
+import { checkDefined } from '@modules/nullAndUndefined';
+import type { Handler } from 'aws-lambda';
+import { StepFunctions } from 'aws-sdk';
 
-export const handler: Handler = async (
-	event: unknown,
-): Promise<APIGatewayProxyResult> => {
+const stepfunctions = new StepFunctions();
+
+export const handler: Handler = async (event: unknown) => {
 	console.log(`Input is ${JSON.stringify(event)}`);
-	return await Promise.resolve({
-		body: 'Hello World',
-		statusCode: 200,
-	});
+
+	const input = JSON.stringify(event);
+
+	const stateMachineArn = checkDefined<string>(
+		process.env.STATE_MACHINE_ARN,
+		'STATE_MACHINE_ARN environment variable not set',
+	);
+
+	try {
+		const startExecutionResponse = await stepfunctions
+			.startExecution({
+				stateMachineArn: stateMachineArn,
+				input: input,
+			})
+			.promise();
+
+		const executionArn = startExecutionResponse.executionArn;
+		console.log('Execution started:', executionArn);
+
+		let status = 'RUNNING';
+		while (status === 'RUNNING') {
+			const describeExecutionResponse = await stepfunctions
+				.describeExecution({
+					executionArn: executionArn,
+				})
+				.promise();
+
+			status = describeExecutionResponse.status;
+			if (status !== 'RUNNING') {
+				console.log('Execution result:', describeExecutionResponse);
+			}
+			await new Promise((resolve) => setTimeout(resolve, 5000));
+		}
+	} catch (error) {
+		console.error('Failed to execute state machine:', error);
+	}
 };
