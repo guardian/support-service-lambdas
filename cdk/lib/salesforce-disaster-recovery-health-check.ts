26f8943aba4d30a4514d2fe9164254e61cf29d25
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-04-09 15:09:31 +0100

    chore: add log

--- cdk/lib/salesforce-disaster-recovery-health-check.ts
+++ cdk/lib/salesforce-disaster-recovery-health-check.ts
@@ -14,6 +14,7 @@ export class SalesforceDisasterRecoveryHealthCheck extends GuStack {
 		super(scope, id, props);
 
 		const app = 'salesforce-disaster-recovery-health-check';
+		// Comment
 
 		const lambdaDefaultConfig: Pick<
 			GuFunctionProps,
