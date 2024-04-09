58b75234ee14336cc0c57cefa7ee508a89d3b8df
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-04-09 15:56:18 +0100

    infra: reference state machine to execute

--- cdk/lib/salesforce-disaster-recovery-health-check.ts
+++ cdk/lib/salesforce-disaster-recovery-health-check.ts
@@ -1,34 +1,36 @@
 import { GuScheduledLambda } from '@guardian/cdk';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import { type GuFunctionProps } from '@guardian/cdk/lib/constructs/lambda';
 import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
 import { Schedule } from 'aws-cdk-lib/aws-events';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-
-export interface Props extends GuStackProps {}
+import { StateMachine } from 'aws-cdk-lib/aws-stepfunctions';
 
 export class SalesforceDisasterRecoveryHealthCheck extends GuStack {
-	constructor(scope: App, id: string, props: Props) {
+	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
 		const app = 'salesforce-disaster-recovery-health-check';
 
-		const lambdaDefaultConfig: Pick<
-			GuFunctionProps,
-			'app' | 'memorySize' | 'fileName' | 'runtime' | 'timeout' | 'environment'
-		> = {
+		const stateMachine = StateMachine.fromStateMachineName(
+			this,
+			'sdf',
+			`salesforce-disaster-recovery-${this.stage}`,
+		);
+
+		new GuScheduledLambda(this, 'salesforce-disaster-recovery-health-check', {
 			app,
 			memorySize: 1024,
 			fileName: `${app}.zip`,
 			runtime: Runtime.NODEJS_20_X,
 			timeout: Duration.minutes(5),
-			environment: { APP: app, STACK: this.stack, STAGE: this.stage },
-		};
-
-		new GuScheduledLambda(this, 'salesforce-disaster-recovery-health-check', {
-			...lambdaDefaultConfig,
+			environment: {
+				APP: app,
+				STACK: this.stack,
+				STAGE: this.stage,
+				STATE_MACHINE_ARN: stateMachine.stateMachineArn,
+			},
 			handler: 'salesforceDisasterRecoveryHealthCheck.handler',
 			functionName: `${app}-${this.stage}`,
 			rules: [{ schedule: Schedule.cron({ minute: '0', hour: '9' }) }],
