40709839d6feccbf5e87020a32431de7acb69590
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-04-11 15:58:50 +0100

    feat: add subject to publishSnsMessage function

--- handlers/salesforce-disaster-recovery-health-check/src/services/sns.ts
+++ handlers/salesforce-disaster-recovery-health-check/src/services/sns.ts
@@ -3,14 +3,17 @@ import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
 const snsClient = new SNSClient({ region: process.env.region });
 
 export const publishSnsMessage = async ({
+	subject,
 	message,
 	topicArn,
 }: {
+	subject: string;
 	message: string;
 	topicArn: string;
 }) => {
 	try {
 		const command = new PublishCommand({
+			Subject: subject,
 			Message: message,
 			TopicArn: topicArn,
 		});
