1ff0fb344ded7b0e246fd8ed4fcdb37d4c4ba009
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-04-16 17:00:18 +0100

    feat: improve comments and messages

--- cdk/lib/salesforce-disaster-recovery.ts
+++ cdk/lib/salesforce-disaster-recovery.ts
@@ -359,8 +359,12 @@ export class SalesforceDisasterRecovery extends GuStack {
 						TopicArn: snsTopic.topicArn,
 						Subject: `Salesforce Disaster Recovery Re-syncing Procedure Completed For ${this.stage}`,
 						'Message.$': JsonPath.format(
-							`This health check is in place to make sure the Salesforce Disaster Recovery resyncing tool is available at all times.\nPlease review the state machine execution details below to figure out the cause of the failure:\n{}`,
+							`State machine execution details:\n{}\n\nAccounts to sync:\n{}\n\nAccounts that failed to update ({}):\n{}
+						`,
 							JsonPath.stringAt('$.stateMachineExecutionDetailsUrl'),
+							JsonPath.stringAt('$.queryResultFileUrl'),
+							JsonPath.stringAt('$.failedRowsCount'),
+							JsonPath.stringAt('$.failedRowsFileUrl'),
 						),
 					},
 					ResultPath: JsonPath.stringAt('$.TaskResult'),
@@ -427,9 +431,7 @@ export class SalesforceDisasterRecovery extends GuStack {
 																					'SomeAccountsHaveFailedToUpdate',
 																					{
 																						stateName:
-																							'SomeAccountsHaveFailedToUpdate\n(read state comment for details on how to debug)',
-																						comment:
-																							"To debug the failed account updates, please check the 'failedRowsFileUrl' key in the input data for this stage. It contains the URL to a CSV file named 'failed-rows.csv'. Download and open this file to review the rows that failed to update.",
+																							"SomeAccountsHaveFailedToUpdate - Open state input 'failedRowsFileUrl' to debug",
 																					},
 																				),
 																			),
