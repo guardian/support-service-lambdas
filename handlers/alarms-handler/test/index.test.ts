ebf162bf766dc616a3f38f88717ea5092ac317cd
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-05-15 11:17:47 +0100

    test: add test to check for fetch HTTP call failure

--- handlers/alarms-handler/test/index.test.ts
+++ handlers/alarms-handler/test/index.test.ts
@@ -12,6 +12,35 @@ describe('Handler', () => {
 		SRE_WEBHOOK: 'sre-webhook-url',
 	};
 
+	const mockCloudWatchAlarmEvent = {
+		Records: [
+			{
+				body: JSON.stringify({
+					Message: JSON.stringify({
+						AlarmArn: 'mock-arn',
+						AlarmName: 'mock-alarm',
+						NewStateReason: 'mock-reason',
+						AlarmDescription: 'description',
+					}),
+				}),
+			},
+		],
+	} as SQSEvent;
+
+	const mockSnsPublishMessageEvent = {
+		Records: [
+			{
+				body: JSON.stringify({
+					Message: 'mock-message',
+					MessageAttributes: {
+						app: { Type: 'String', Value: 'mock-app' },
+						stage: { Type: 'String', Value: 'CODE' },
+					},
+				}),
+			},
+		],
+	} as SQSEvent;
+
 	beforeEach(() => {
 		jest.resetModules();
 		jest.resetAllMocks();
@@ -20,54 +49,35 @@ describe('Handler', () => {
 	});
 
 	it('should handle CloudWatch alarm message', async () => {
-		const mockEvent = {
-			Records: [
-				{
-					body: JSON.stringify({
-						Message: JSON.stringify({
-							AlarmArn: 'mock-arn',
-							AlarmName: 'mock-alarm',
-							NewStateReason: 'mock-reason',
-							AlarmDescription: 'description',
-						}),
-					}),
-				},
-			],
-		};
-
 		(getAppNameTag as jest.Mock).mockResolvedValueOnce('mock-app');
 
 		jest
 			.spyOn(global, 'fetch')
 			.mockResolvedValue(Promise.resolve(new Response(JSON.stringify({}))));
 
-		await handler(mockEvent as SQSEvent);
+		await handler(mockCloudWatchAlarmEvent);
 
 		expect(getAppNameTag).toHaveBeenCalledWith('mock-arn');
 		expect(fetch).toHaveBeenCalledWith(mockEnv.SRE_WEBHOOK, expect.any(Object));
 	});
 
 	it('should handle SNS publish message', async () => {
-		const mockEvent = {
-			Records: [
-				{
-					body: JSON.stringify({
-						Message: 'mock-message',
-						MessageAttributes: {
-							app: { Type: 'String', Value: 'mock-app' },
-							stage: { Type: 'String', Value: 'CODE' },
-						},
-					}),
-				},
-			],
-		};
-
 		jest
 			.spyOn(global, 'fetch')
 			.mockResolvedValue(Promise.resolve(new Response(JSON.stringify({}))));
 
-		await handler(mockEvent as SQSEvent);
+		await handler(mockSnsPublishMessageEvent);
 
 		expect(fetch).toHaveBeenCalledWith(mockEnv.SRE_WEBHOOK, expect.any(Object));
+	});
+
+	it('should throw error if the fetch HTTP call fails', async () => {
+		jest
+			.spyOn(global, 'fetch')
+			.mockResolvedValue(Promise.reject(new Error('Fetch error')));
+
+		await expect(handler(mockCloudWatchAlarmEvent)).rejects.toThrow(
+			'Fetch error',
+		);
 	});
 });
