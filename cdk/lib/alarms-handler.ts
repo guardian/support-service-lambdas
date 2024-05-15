9c31d0c68c89c4ea02337da8e089fb604f685633
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-05-15 14:57:00 +0100

    test: temporarily enable actions for CODE

--- cdk/lib/alarms-handler.ts
+++ cdk/lib/alarms-handler.ts
@@ -107,7 +107,7 @@ export class AlarmsHandler extends GuStack {
 			comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
 			threshold: 0,
 			evaluationPeriods: 24,
-			actionsEnabled: this.stage === 'PROD',
+			// actionsEnabled: this.stage === 'PROD',
 		});
 	}
 }
