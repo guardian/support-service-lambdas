3e6e46fdc561bcd03155023963c601bbfc8ff89b
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-05-21 12:29:58 +0100

    feat: add mapping for canonical-config

--- handlers/alarms-handler/src/alarmMappings.ts
+++ handlers/alarms-handler/src/alarmMappings.ts
@@ -44,6 +44,7 @@ const teamToAppMappings: Record<Team, string[]> = {
 	],
 	SRE: ['gchat-test-app'],
 	PP: [
+		'canonical-config',
 		'frontend',
 		'salesforce-disaster-recovery',
 		'salesforce-disaster-recovery-health-check',
