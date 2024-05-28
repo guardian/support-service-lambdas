debd83ac106db38c10e8482869d82384abac728c
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-05-28 16:56:37 +0100

    Correct the incoming url path

--- handlers/update-supporter-plus-amount/src/urlParsing.ts
+++ handlers/update-supporter-plus-amount/src/urlParsing.ts
@@ -1,6 +1,6 @@
 import { checkDefined } from '@modules/nullAndUndefined';
 
-const regex = /\/product-move\/update-supporter-plus-amount\/(A-S\d+)/;
+const regex = /\/update-supporter-plus-amount\/(A-S\d+)/;
 export const getSubscriptionNumberFromUrl = (urlPath: string) => {
 	const match = urlPath.match(regex);
 	return checkDefined(
