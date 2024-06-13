a6bcf0bc4815c43e0e65038923ae8a3fbd64c26f
John Duffell <john.duffell@guardian.co.uk>
2024-06-13 16:53:03 +0100

    use correct relative directory

--- modules/jest.config.js
+++ modules/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	testEnvironment: 'node',
 	runner: 'groups',
 	moduleNameMapper: {
-		'@modules/(.*)/(.*)$': '<rootDir>/../../modules/$1/src/$2',
-		'@modules/(.*)$': '<rootDir>/../../modules/$1',
+		'@modules/(.*)/(.*)$': '<rootDir>/../modules/$1/src/$2',
+		'@modules/(.*)$': '<rootDir>/../modules/$1',
 	},
 };
