db2ff0b5d7b40c056e2f28c881a5896179876f3f
Tom Wey <tom.wey@theguardian.com>
2024-06-19 15:21:12 +0100

    Deliberately break TS in a module

--- modules/product-catalog/src/api.ts
+++ modules/product-catalog/src/api.ts
@@ -14,6 +14,7 @@ export const getProductCatalogFromApi = async (
 	});
 
 	const json = await response.json();
+	return 1;
 	if (response.ok) {
 		console.log(`Response from catalog api was: ${JSON.stringify(json)}`);
 		return productCatalogSchema.parse(json);
