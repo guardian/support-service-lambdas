31c17e23487a2959608075c6755ba829512cab99
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-06-20 16:20:46 +0100

    create lambda

--- cdk/lib/zuora-salesforce-link-remover.ts
+++ cdk/lib/zuora-salesforce-link-remover.ts
@@ -1,13 +1,26 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import { type App } from 'aws-cdk-lib';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
-// const app = 'zuora-salesforce-link-remover';
+const appName = 'zuora-salesforce-link-remover';
 
 export class SalesforceDisasterRecovery extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
-	}
 
+		new GuLambdaFunction(
+			this,
+			'get-billing-accounts-lambda',
+			{
+				app: appName,
+				functionName: `${appName}-get-billing-accounts-lambda-${this.stage}`,
+				runtime: Runtime.NODEJS_20_X,
+				handler: 'getBillingAccounts.handler',
+				fileName: `${appName}.zip`,
+			}
+		)
+	}
 	
 }
---
+++ handlers/zuora-salesforce-link-remover/src/getBillingAccounts.ts
@@ -0,0 +1,5 @@
+export function handler(){
+    console.log('hello david and andrew');
+
+    return 'abcdef';
+}
