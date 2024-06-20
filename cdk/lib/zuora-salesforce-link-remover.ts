f6b119a6788f06e8bf08ad5c21af3dc5c18e3466
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-06-20 14:32:44 +0100

    Update zuora-salesforce-link-remover.ts

--- cdk/lib/zuora-salesforce-link-remover.ts
+++ cdk/lib/zuora-salesforce-link-remover.ts
@@ -2,7 +2,7 @@ import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { type App } from 'aws-cdk-lib';
 
-const app = 'zuora-salesforce-link-remover';
+// const app = 'zuora-salesforce-link-remover';
 
 export class SalesforceDisasterRecovery extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
