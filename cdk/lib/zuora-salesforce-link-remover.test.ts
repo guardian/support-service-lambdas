75de2677edbfa755456acf844f6a21908a4d07c1
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-06-24 13:16:47 +0100

    Update zuora-salesforce-link-remover.test.ts

--- cdk/lib/zuora-salesforce-link-remover.test.ts
+++ cdk/lib/zuora-salesforce-link-remover.test.ts
@@ -1,20 +1,20 @@
-// import { App } from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import { ZuoraSalesforceLinkRemover } from './zuora-salesforce-link-remover';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { ZuoraSalesforceLinkRemover } from './zuora-salesforce-link-remover';
 
-describe('The ZuoraSalesforceLinkRemover stack', () => {
+describe('The zuora-salesforce-link-remover stack', () => {
 	it('matches the snapshot', () => {
-		// const app = new App();
-		// const codeStack = new ZuoraSalesforceLinkRemover(
-		// 	app,
-		// 	'get-billing-accounts-lambda',
-		// 	{
-		// 		stack: 'membership',
-		// 		stage: 'CODE',
-		// 	},
-		// );
+		const app = new App();
+		const codeStack = new ZuoraSalesforceLinkRemover(app, 'zuora-salesforce-link-remover-CODE', {
+			stack: 'membership',
+			stage: 'CODE',
+		});
+		const prodStack = new ZuoraSalesforceLinkRemover(app, 'zuora-salesforce-link-remover-PROD', {
+			stack: 'membership',
+			stage: 'PROD',
+		});
 
-		// expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
-		expect(true);
+		expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
+		expect(Template.fromStack(prodStack).toJSON()).toMatchSnapshot();
 	});
 });
