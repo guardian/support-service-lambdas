9ce58db6e70428f997b8c05cbe57184cba98b20e
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-06-24 13:21:51 +0100

    Update zuora-salesforce-link-remover.test.ts

--- cdk/lib/zuora-salesforce-link-remover.test.ts
+++ cdk/lib/zuora-salesforce-link-remover.test.ts
@@ -5,14 +5,22 @@ import { ZuoraSalesforceLinkRemover } from './zuora-salesforce-link-remover';
 describe('The zuora-salesforce-link-remover stack', () => {
 	it('matches the snapshot', () => {
 		const app = new App();
-		const codeStack = new ZuoraSalesforceLinkRemover(app, 'zuora-salesforce-link-remover-CODE', {
-			stack: 'membership',
-			stage: 'CODE',
-		});
-		const prodStack = new ZuoraSalesforceLinkRemover(app, 'zuora-salesforce-link-remover-PROD', {
-			stack: 'membership',
-			stage: 'PROD',
-		});
+		const codeStack = new ZuoraSalesforceLinkRemover(
+			app,
+			'zuora-salesforce-link-remover-CODE',
+			{
+				stack: 'membership',
+				stage: 'CODE',
+			},
+		);
+		const prodStack = new ZuoraSalesforceLinkRemover(
+			app,
+			'zuora-salesforce-link-remover-PROD',
+			{
+				stack: 'membership',
+				stage: 'PROD',
+			},
+		);
 
 		expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
 		expect(Template.fromStack(prodStack).toJSON()).toMatchSnapshot();
