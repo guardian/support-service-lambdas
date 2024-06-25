e824fc705ffbd0322a78741a24c385d4deb9c05e
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-06-25 10:57:09 +0100

    get secrets from secrets manager

--- handlers/zuora-salesforce-link-remover/src/getBillingAccounts.ts
+++ handlers/zuora-salesforce-link-remover/src/getBillingAccounts.ts
@@ -1,5 +1,44 @@
+import { GetSecretValueCommand, SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
+
 export function handler() {
-	console.log('hello david and andrew');
+	console.log('getting secret...');
+	const secretName = "DEV/Salesforce/ConnectedApp/AwsConnectorSandbox"
+
+	const secret = getSecretValue({secretName});
 
 	return 'abcdef';
 }
+
+
+export const getSecretValue = async <T>({
+	secretName,
+}: {
+	secretName: string;
+}): Promise<T> => {
+	console.log('1. secretName:', secretName);
+
+	const secretsManagerClient = new SecretsManagerClient({
+		region: process.env.region,
+	});
+
+	try {
+		const command = new GetSecretValueCommand({
+			SecretId: secretName,
+		});
+
+		const response = await secretsManagerClient.send(command);
+		console.log('2. secret response:', response);
+		if (!response.SecretString) {
+			throw new Error('No secret found');
+		}
+		console.log('3. response.SecretString:', response.SecretString);
+
+		const secretValue = JSON.parse(response.SecretString) as T;
+		console.log('4. secretValue:', secretValue);
+
+		return secretValue;
+	} catch (error) {
+		console.error('error:',error);
+		throw error;
+	}
+};
