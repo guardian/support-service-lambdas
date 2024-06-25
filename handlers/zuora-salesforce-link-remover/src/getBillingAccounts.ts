4ae647809e71fa6fdf9173c8cee9eabbbd192af0
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-06-25 11:59:07 +0100

    tidy

--- handlers/zuora-salesforce-link-remover/src/getBillingAccounts.ts
+++ handlers/zuora-salesforce-link-remover/src/getBillingAccounts.ts
@@ -4,45 +4,35 @@ import {
 } from '@aws-sdk/client-secrets-manager';
 
 export async function handler() {
-	console.log('getting secret...');
-	const secretName = 'DEV/Salesforce/ConnectedApp/AwsConnectorSandbox';
+	const secretName = 'DEV/Salesforce/ConnectedApp/AwsConnectorSandbox'; //TODO in future pr: add a function that returns appropriate secret depending on env
 
-	const secret = await getSecretValue({ secretName });
-	console.log('secret = ', secret);
+	await getSecretValue( secretName );
+
 	return 'abcdef';
 }
 
-export const getSecretValue = async ({
-	secretName,
-}: {
-	secretName: string;
-}): Promise<ConnectedAppSecret> => {
-
-	const secretsManagerClient = new SecretsManagerClient({
-		region: process.env.region,
-	});
-
+export async function getSecretValue(secretName: string): Promise<ConnectedAppSecret>{
 	try {
+		const secretsManagerClient = new SecretsManagerClient({
+			region: process.env.region,
+		});
+
 		const command = new GetSecretValueCommand({
 			SecretId: secretName,
 		});
 
 		const response = await secretsManagerClient.send(command);
+		
 		if (!response.SecretString) {
-			throw new Error('No secret found');
+			throw new Error(`No secret found with name:$secretName`);
 		}
 
-		const secretValue = JSON.parse(response.SecretString) as ConnectedAppSecret;
-		
-		console.log('secretValue.name:', secretValue.name);
-		console.log('secretValue.authUrl:', secretValue.authUrl);
-
-		return secretValue;
+		return JSON.parse(response.SecretString) as ConnectedAppSecret;
 	} catch (error) {
 		console.error('error:', error);
 		throw error;
 	}
-};
+}
 
 type ConnectedAppSecret = {
 	name: string;
