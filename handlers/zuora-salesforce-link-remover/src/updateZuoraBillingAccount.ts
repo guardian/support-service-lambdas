48f74cf2a74805fd355568057751ccbd28e6f390
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-07-01 16:03:58 +0100

    throw error on no billing account id

--- handlers/zuora-salesforce-link-remover/src/updateZuoraBillingAccount.ts
+++ handlers/zuora-salesforce-link-remover/src/updateZuoraBillingAccount.ts
@@ -14,8 +14,10 @@ export const handler: Handler = async (event: Event) => {
 		throw Error('Invalid stage value');
 	}
 
+	if (!event.Zuora__External_Id__c) {
+		throw Error('No Billing Account Id has been provided');
+	}
 	const billingAccountId = event.Zuora__External_Id__c;
-
 	const secretName = getZuoraSecretName(stage);
 
 	const { clientId, clientSecret } =
