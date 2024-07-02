15e8ab66cebca1ff92259fd36c1c5f090f75be80
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-07-02 13:11:24 +0100

    tidy

--- handlers/zuora-salesforce-link-remover/src/updateZuoraBillingAccount.ts
+++ handlers/zuora-salesforce-link-remover/src/updateZuoraBillingAccount.ts
@@ -1,11 +1,7 @@
-import { ZuoraClient } from '@modules/zuora/zuoraClient';
-import { zuoraSuccessResponseSchema } from '@modules/zuora/zuoraSchemas';
+import type { ZuoraSuccessResponse } from '@modules/zuora/zuoraSchemas';
 import type { Handler } from 'aws-lambda';
 import { z } from 'zod';
-
-// import { getSecretValue, getZuoraSecretName } from './secrets';
-// import type { ZuoraSecret } from './secrets';
-// import { doZuoraAuth, updateBillingAccountInZuora } from './zuoraHttp';
+import { updateBillingAccountInZuora } from './zuoraHttp';
 
 export const handler: Handler = async (event: Event) => {
 
@@ -17,53 +13,17 @@ export const handler: Handler = async (event: Event) => {
 		throw new Error(parseError);
 	}
 
-	const billingAccountId = parseResponse.data.Zuora__External_Id__c;
-	console.log('billingAccountId:',billingAccountId);
+	const zuoraBillingAccountId = parseResponse.data.Zuora__External_Id__c;
+	console.log('billingAccountId:',zuoraBillingAccountId);
 
-	// const stage = process.env.STAGE;
-	// if (!stage) {
-	// 	throw Error('Stage not defined');
-	// }
-
-	// if (!isValidStage(stage)) {
-	// 	throw Error('Invalid stage value');
-	// }
-	// const secretName = getZuoraSecretName(stage);
-
-	// const { clientId, clientSecret } =
-	// 	await getSecretValue<ZuoraSecret>(secretName);
-
-		const zuoraClient = await ZuoraClient.create('CODE');
-		const path = `v1/accounts/${billingAccountId}`;
+	const zuoraBillingAccountUpdateResponse: ZuoraSuccessResponse = await updateBillingAccountInZuora(zuoraBillingAccountId);
+	console.log('zuoraBillingAccountUpdateResponse:',zuoraBillingAccountUpdateResponse);
 	
-		const body = JSON.stringify({
-			crmId: '',
-		});
-
-		const abc = await zuoraClient.put(path, body, zuoraSuccessResponseSchema);
-		console.log('abc:',abc);
-	// const zuoraAccessToken = await doZuoraAuth({
-	// 	client_id: clientId,
-	// 	client_secret: clientSecret,
-	// 	grant_type: 'client_credentials',
-	// });
-
-	// const zuoraBillingAccountUpdateResponse = await updateBillingAccountInZuora(
-	// 	zuoraAccessToken,
-	// 	billingAccountId,
-	// );
-	
-	// return {
-	// 	billingAccountId,
-	// 	...zuoraBillingAccountUpdateResponse,
-	// };
-
-	return {};
+	return {
+		zuoraBillingAccountId,
+		...zuoraBillingAccountUpdateResponse,
+	};
 };
-
-// function isValidStage(value: unknown): value is 'CODE' | 'PROD' {
-// 	return value === 'CODE' || value === 'PROD';
-// }
 
 const EventSchema = z.object({
 	Zuora__External_Id__c: z.string(),
