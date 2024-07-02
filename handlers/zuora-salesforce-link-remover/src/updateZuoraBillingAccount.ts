25be37551e2b6bc492b7ef508efc10ae433d4e3b
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-07-02 12:35:57 +0100

    use zuoraClient

--- handlers/zuora-salesforce-link-remover/src/updateZuoraBillingAccount.ts
+++ handlers/zuora-salesforce-link-remover/src/updateZuoraBillingAccount.ts
@@ -1,8 +1,11 @@
+import { ZuoraClient } from '@modules/zuora/zuoraClient';
+import { zuoraSuccessResponseSchema } from '@modules/zuora/zuoraSchemas';
 import type { Handler } from 'aws-lambda';
 import { z } from 'zod';
-import { getSecretValue, getZuoraSecretName } from './secrets';
-import type { ZuoraSecret } from './secrets';
-import { doZuoraAuth, updateBillingAccountInZuora } from './zuoraHttp';
+
+// import { getSecretValue, getZuoraSecretName } from './secrets';
+// import type { ZuoraSecret } from './secrets';
+// import { doZuoraAuth, updateBillingAccountInZuora } from './zuoraHttp';
 
 export const handler: Handler = async (event: Event) => {
 
@@ -16,34 +19,45 @@ export const handler: Handler = async (event: Event) => {
 
 	const billingAccountId = parseResponse.data.Zuora__External_Id__c;
 	
-	const stage = process.env.STAGE;
-	if (!stage) {
-		throw Error('Stage not defined');
-	}
+	// const stage = process.env.STAGE;
+	// if (!stage) {
+	// 	throw Error('Stage not defined');
+	// }
 
-	if (!isValidStage(stage)) {
-		throw Error('Invalid stage value');
-	}
-	const secretName = getZuoraSecretName(stage);
+	// if (!isValidStage(stage)) {
+	// 	throw Error('Invalid stage value');
+	// }
+	// const secretName = getZuoraSecretName(stage);
 
-	const { clientId, clientSecret } =
-		await getSecretValue<ZuoraSecret>(secretName);
+	// const { clientId, clientSecret } =
+	// 	await getSecretValue<ZuoraSecret>(secretName);
 
-	const zuoraAccessToken = await doZuoraAuth({
-		client_id: clientId,
-		client_secret: clientSecret,
-		grant_type: 'client_credentials',
-	});
-
-	const zuoraBillingAccountUpdateResponse = await updateBillingAccountInZuora(
-		zuoraAccessToken,
-		billingAccountId,
-	);
+		const zuoraClient = await ZuoraClient.create('CODE');
+		const path = `v1/accounts/${billingAccountId}`;
 	
-	return {
-		billingAccountId,
-		...zuoraBillingAccountUpdateResponse,
-	};
+		const body = JSON.stringify({
+			crmId: '',
+		});
+
+		const abc = zuoraClient.post(path, body, zuoraSuccessResponseSchema);
+		console.log('abc:',abc);
+	// const zuoraAccessToken = await doZuoraAuth({
+	// 	client_id: clientId,
+	// 	client_secret: clientSecret,
+	// 	grant_type: 'client_credentials',
+	// });
+
+	// const zuoraBillingAccountUpdateResponse = await updateBillingAccountInZuora(
+	// 	zuoraAccessToken,
+	// 	billingAccountId,
+	// );
+	
+	// return {
+	// 	billingAccountId,
+	// 	...zuoraBillingAccountUpdateResponse,
+	// };
+
+	return {};
 };
 
 function isValidStage(value: unknown): value is 'CODE' | 'PROD' {
--- handlers/zuora-salesforce-link-remover/src/zuoraHttp.ts
+++ handlers/zuora-salesforce-link-remover/src/zuoraHttp.ts
@@ -1,44 +1,60 @@
+import { ZuoraClient } from '@modules/zuora/zuoraClient';
 import { z } from 'zod';
 
-export async function doZuoraAuth(zuoraAuth: ZuoraAuth): Promise<string> {
-	console.log('authenticating with Zuora...');
-	const req = {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		body: new URLSearchParams(zuoraAuth).toString(),
-	};
+// export async function doZuoraAuth(): Promise<string> {
+// 	console.log('authenticating with Zuora...');
 
-	try {
-		const result = await fetch(
-			'https://rest.apisandbox.zuora.com/oauth/token',
-			req,
-		);
+// 	const zuoraClient = await ZuoraClient.create('CODE');
+// 	const path = `v1/accounts/${zuoraBillingAccountId}`;
 
-		if (!result.ok) {
-			//can we get the error text out of the response rather than outputting full response?
-			const errorMessage = `Error retrieving access token from Zuora: ${JSON.stringify(result)}`;
-			console.error(errorMessage);
-			throw new Error(errorMessage);
-		}
-		console.log('successfully authenticated with Zuora');
+// 	const body = JSON.stringify({
+// 		accountNumber,
+// 		targetDate: zuoraDateFormat(targetDate),
+// 		assumeRenewal: 'Autorenew',
+// 	});
+// 	return zuoraClient.post(path, body, billingPreviewSchema);
+// 	return '';
+// }
 
-		const zuoraAuthResponse = (await result.json()) as ZuoraAuthResponse;
+// export async function doZuoraAuth(zuoraAuth: ZuoraAuth): Promise<string> {
+// 	console.log('authenticating with Zuora...');
+// 	const req = {
+// 		method: 'POST',
+// 		headers: {
+// 			'Content-Type': 'application/x-www-form-urlencoded',
+// 		},
+// 		body: new URLSearchParams(zuoraAuth).toString(),
+// 	};
 
-		const parseResult = ZuoraAuthResponseSchema.safeParse(zuoraAuthResponse);
+// 	try {
+// 		const result = await fetch(
+// 			'https://rest.apisandbox.zuora.com/oauth/token',
+// 			req,
+// 		);
 
-		if (!parseResult.success) {
-			const parseError = `Error parsing response from Zuora: ${JSON.stringify(parseResult.error.format())}`;
-			console.error(parseError);
-			throw new Error(parseError);
-		}
+// 		if (!result.ok) {
+// 			//can we get the error text out of the response rather than outputting full response?
+// 			const errorMessage = `Error retrieving access token from Zuora: ${JSON.stringify(result)}`;
+// 			console.error(errorMessage);
+// 			throw new Error(errorMessage);
+// 		}
+// 		console.log('successfully authenticated with Zuora');
 
-		return parseResult.data.access_token;
-	} catch (error) {
-		throw new Error(String(error));
-	}
-}
+// 		const zuoraAuthResponse = (await result.json()) as ZuoraAuthResponse;
+
+// 		const parseResult = ZuoraAuthResponseSchema.safeParse(zuoraAuthResponse);
+
+// 		if (!parseResult.success) {
+// 			const parseError = `Error parsing response from Zuora: ${JSON.stringify(parseResult.error.format())}`;
+// 			console.error(parseError);
+// 			throw new Error(parseError);
+// 		}
+
+// 		return parseResult.data.access_token;
+// 	} catch (error) {
+// 		throw new Error(String(error));
+// 	}
+// }
 
 export type ZuoraAuth = {
 	client_id: string;
