04a729bf5b912d271b5a4c5a26677c761ed6db69
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-07-08 12:06:09 +0100

    output parsed response

--- handlers/zuora-salesforce-link-remover/src/updateZuoraBillingAccount.ts
+++ handlers/zuora-salesforce-link-remover/src/updateZuoraBillingAccount.ts
@@ -5,16 +5,21 @@ import { updateBillingAccountInZuora } from './zuoraHttp';
 
 export const handler: Handler = async (event: Event) => {
 	const parseResponse = EventSchema.safeParse(event);
-
+	console.log('parseResponse:',parseResponse);
 	if (!parseResponse.success) {
 		throw new Error(
 			`Error parsing billing account id from input: ${JSON.stringify(parseResponse.error.format())}`,
 		);
 	}
 	const zuoraBillingAccountId = parseResponse.data.item.Zuora__External_Id__c;
+	console.log('zuoraBillingAccountId:',zuoraBillingAccountId);
+
 	const sfBillingAccountId = parseResponse.data.item.Id;
+	console.log('sfBillingAccountId:',sfBillingAccountId);
+
 	const zuoraBillingAccountUpdateResponse: ZuoraSuccessResponse =
 		await updateBillingAccountInZuora(zuoraBillingAccountId);
+	console.log('zuoraBillingAccountUpdateResponse:',zuoraBillingAccountUpdateResponse);
 
 	return {
 		zuoraBillingAccountId,
