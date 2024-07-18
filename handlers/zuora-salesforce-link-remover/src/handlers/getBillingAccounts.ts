e782e0080e526a8b3bac1fd6c9726500d0583e4a
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-07-18 14:11:55 +0100

    fix query

--- handlers/zuora-salesforce-link-remover/src/handlers/getBillingAccounts.ts
+++ handlers/zuora-salesforce-link-remover/src/handlers/getBillingAccounts.ts
@@ -35,7 +35,7 @@ export async function handler() {
 		const sfAuthResponse = await doSfAuth(sfApiUserAuth, sfConnectedAppAuth);
 
 		const limit = 200;
-		const query = `SELECT Id, Zuora__Account__c, GDPR_Removal_Attempts__c, Zuora__External_Id__c FROM Zuora__CustomerAccount__c WHERE Zuora__External_Id__c != null AND Zuora__Account__r.GDPR_Billing_Accounts_Ready_for_Removal__c = true AND GDPR_Removal_Attempts__c < $maxAttempts ORDER BY Zuora__Account__r.GDPR_Date_Successfully_Removed_Related__c desc LIMIT ${limit}`
+		const query = `SELECT Id, Zuora__Account__c, GDPR_Removal_Attempts__c, Zuora__External_Id__c FROM Zuora__CustomerAccount__c WHERE Zuora__External_Id__c != null AND Zuora__Account__r.GDPR_Billing_Accounts_Ready_for_Removal__c = true AND GDPR_Removal_Attempts__c < 5 ORDER BY Zuora__Account__r.GDPR_Date_Successfully_Removed_Related__c desc LIMIT ${limit}`
 		console.log('query:',query);
 		const response: SalesforceQueryResponse = await executeSalesforceQuery(
 			sfAuthResponse,
