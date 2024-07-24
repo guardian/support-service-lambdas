2a4731c1e1c906f328a2785e5dd05940dcef7d7e
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-07-24 14:55:09 +0100

    add comment explaining v59 as default for api version

--- modules/salesforce/src/config.ts
+++ modules/salesforce/src/config.ts
@@ -2,7 +2,7 @@ export const sfApiVersion = (): string => {
 	const sfApiVersion = process.env.SF_API_VERSION;
 
 	if (!sfApiVersion) {
-		return 'v54.0';
+		return 'v59.0'; //latest version as of July '24
 	}
 	return sfApiVersion;
 };
