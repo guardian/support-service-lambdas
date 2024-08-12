6a758f96495386f6694716a5ce87bd9593d97904
Michael McNamara <michael.mcnamara@theguardian.com>
2024-08-12 18:02:55 +0100

    restore deleted tests and add comment to show they are placeholders for future work

--- handlers/ticket-tailor-webhook/test/firstIntegration.test.ts
+++ handlers/ticket-tailor-webhook/test/firstIntegration.test.ts
@@ -4,6 +4,9 @@
  * This makes it useful for testing things that require credentials which are available locally but not on the CI server.
  *
  * @group integration
+ *
+ * This specific test is a placeholder and will be updated at a later date when the project has matured such that there is something to test.
+ * The test is retained here simply to prevent the build from failing
  */
 test('my app', () => {
 	expect(1 + 1).toEqual(2);
--- handlers/ticket-tailor-webhook/test/firstTest.test.ts
+++ handlers/ticket-tailor-webhook/test/firstTest.test.ts
@@ -1,6 +1,9 @@
 /**
  * This is a unit test, it can be run by the `pnpm test` command, and will be run by the CI/CD pipeline
  *
+ *
+ * This specific test is a placeholder and will be updated at a later date when the project has matured such that there is something to test.
+ * The test is retained here simply to prevent the build from failing
  */
 test('my app', () => {
 	expect(1 + 1).toEqual(2);
