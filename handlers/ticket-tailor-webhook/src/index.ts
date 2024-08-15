eb5561d188e17a4a3e272ad2a61373b287eedb3c
Joe Griffiths <joe.griffiths@guardian.co.uk>
2024-08-15 12:08:25 +0100

    refactor

--- handlers/ticket-tailor-webhook/src/index.ts
+++ handlers/ticket-tailor-webhook/src/index.ts
@@ -1,43 +1,19 @@
-//import getWebhookValidationSecret from './hMacKey';
-import { createHmac, timingSafeEqual } from 'node:crypto';
 import type { APIGatewayProxyResult, Handler, SQSEvent } from 'aws-lambda';
-import { getWebhookValidationSecret } from './hMacKey';
-
-//import type { Stage } from '@modules/stage'
+import { signAndVerify } from './signAndVerify';
 
 export const handler: Handler = async (
 	event: SQSEvent,
 ): Promise<APIGatewayProxyResult> => {
 	console.log(`Input is ${JSON.stringify(event)}`);
 
-	// const stage =  Stage("CODE") ???//
-	const webhookValidationSecret = await getWebhookValidationSecret('CODE');
-
-	interface BuyerDetails {
-		buyer_details:{
-		  email: string;
-		};
-	  }
-	event.Records.forEach((record) => {
-			  
-		const parsed = JSON.parse(record.body) as BuyerDetails;
-		const email = parsed.buyer_details.email;
-		const hash = createHmac('sha256', webhookValidationSecret)
-			.update(record.body)
-			.digest('hex');
-
-			const signature = record.messageAttributes['Tickettailor-Webhook-Signature']?.stringValue
-
-			if (typeof signature === 'string') {
-				timingSafeEqual(Buffer.from(hash), Buffer.from(signature)) ? callIdapi(email) : console.error("HMAC signatures do not match");
-				
-			}
-			record
-
-		
-
-		console.log(hash);
-		//todo - remove the next line before you have an actual secret
+	event.Records.forEach( (record) => {
+		const email = signAndVerify(record)
+		if (typeof email === "string") {
+			callIdapi(email)
+		}
+		else {
+			console.log("error")
+		}
 	});
 
 	return Promise.resolve({
@@ -46,6 +22,6 @@ export const handler: Handler = async (
 	});
 };
 
-export const callIdapi =  (email: string): void => {
-	console.log(email)
+export const callIdapi =  (email: string) => {
+	console.log(`email for idapi ${email}`)
 }
--- handlers/ticket-tailor-webhook/test/firstTest.test.ts
+++ handlers/ticket-tailor-webhook/test/firstTest.test.ts
@@ -1,3 +1,5 @@
+import { handler } from "../src";
+
 /**
  * This is a unit test, it can be run by the `pnpm test` command, and will be run by the CI/CD pipeline
  *
@@ -6,5 +8,15 @@
  * The test is retained here simply to prevent the build from failing
  */
 test('my app', () => {
+
+	// {
+	// 	"buyer_details": {
+	// 	  "email": "john@example.com",
+	// 	  "first_name": "John",
+	// 	  "last_name": "Doe",
+	// 	  "name": "John Doe",
+	// 	  "phone": "07123456789"
+	// 	}
+	//   }
 	expect(1 + 1).toEqual(2);
 });
