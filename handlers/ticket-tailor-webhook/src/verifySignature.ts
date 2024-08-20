c74da5cde6c3ebede229ad9f13448961d23bbf1e
Joe Griffiths <joe.griffiths@guardian.co.uk>
2024-08-20 14:03:28 +0100

    refactor

---
+++ handlers/ticket-tailor-webhook/src/verifySignature.ts
@@ -0,0 +1,43 @@
+import { createHmac, timingSafeEqual } from 'crypto';
+import type { Stage } from '@modules/stage';
+import type { SQSRecord } from 'aws-lambda';
+import { getWebhookValidationSecret } from './hMacKey';
+
+const stage = process.env.STAGE as Stage;
+
+export interface Payload {
+	payload: {
+		buyer_details: {
+			email: string;
+		};
+	};
+}
+
+export const hasMatchingSignature = async (
+	record: SQSRecord,
+): Promise<boolean> => {
+	const webhookValidationSecret = await getWebhookValidationSecret(stage);
+
+	const signatureWithTs =
+		record.messageAttributes['tickettailor-webhook-signature']?.stringValue;
+
+	if (typeof signatureWithTs === 'string') {
+		const signature = signatureWithTs.split(',')[1]?.split('=')[1];
+		const ts = signatureWithTs.split(',')[0]?.split('=')[1];
+		if (typeof signature === 'string' && typeof ts === 'string') {
+			const hash = createHmac('sha256', webhookValidationSecret.secret)
+				.update(ts.concat(record.body))
+				.digest('hex');
+
+			console.log(`SignatureWithTS (split out) is:
+			 ${signature}`);
+			console.log(`generated hash is: 
+			${hash}`);
+			return timingSafeEqual(Buffer.from(hash), Buffer.from(signature));
+		} else {
+			throw new Error('No ts on incoming request');
+		}
+	} else {
+		throw new Error('No ts on incoming request');
+	}
+};
