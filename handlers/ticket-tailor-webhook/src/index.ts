de232e3b127b888368ec4445192d7e9d31fcf290
Joe Griffiths <joe.griffiths@guardian.co.uk>
2024-08-20 13:58:43 +0100

    refactor

--- handlers/ticket-tailor-webhook/src/index.ts
+++ handlers/ticket-tailor-webhook/src/index.ts
@@ -1,13 +1,6 @@
 import type { APIGatewayProxyResult, Handler, SQSEvent } from 'aws-lambda';
-import { hasMatchingSignature } from './signAndVerify';
-
-export interface Payload {
-	payload: {
-		buyer_details: {
-			email: string;
-		};
-	};
-}
+import type { Payload } from './verifySignature';
+import { hasMatchingSignature } from './verifySignature';
 
 export const handler: Handler = async (
 	event: SQSEvent,
@@ -15,10 +8,10 @@ export const handler: Handler = async (
 	console.log(`Input is ${JSON.stringify(event)}`);
 
 	const res = await event.Records.flatMap(async (record) => {
-		const payload = JSON.parse(record.body) as Payload;
-		const email = payload.payload.buyer_details.email;
 		const matches = await hasMatchingSignature(record);
 		if (matches) {
+			const payload = JSON.parse(record.body) as Payload;
+			const email = payload.payload.buyer_details.email;
 			return callIdapi(email);
 		} else {
 			throw new Error('Signatures do not match');
