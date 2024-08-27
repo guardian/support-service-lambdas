fb105919949b63c46a66008164ebd7c2d3035a94
Joe Griffiths <joe.griffiths@guardian.co.uk>
2024-08-27 12:55:34 +0100

    fix copy paste error

--- cdk/lib/ticket-tailor-webhook.ts
+++ cdk/lib/ticket-tailor-webhook.ts
@@ -153,8 +153,8 @@ export class TicketTailorWebhook extends GuStack {
 					new PolicyStatement({
 						actions: ['secretsmanager:GetSecretValue'],
 						resources: [
-							`arn:aws:secretsmanager:${this.region}:${this.account}:secret::${this.stage}/TicketTailor/Webhook-validation-*`,
-							`arn:aws:secretsmanager:${this.region}:${this.account}:secret::${this.stage}/TicketTailor/IdApi-token-*`,
+							`arn:aws:secretsmanager:${this.region}:${this.account}:secret:${this.stage}/TicketTailor/Webhook-validation-*`,
+							`arn:aws:secretsmanager:${this.region}:${this.account}:secret:${this.stage}/TicketTailor/IdApi-token-*`,
 						],
 					}),
 				],
