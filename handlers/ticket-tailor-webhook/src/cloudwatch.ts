9843116033f8db4fceb4e2ac658917d5d3b7d740
Michael McNamara <michael.mcnamara@theguardian.com>
2024-09-16 17:57:53 +0100

    update handling of stage in cloudwatch.ts, set local.env to DEV in index.test.ts

--- handlers/ticket-tailor-webhook/src/cloudwatch.ts
+++ handlers/ticket-tailor-webhook/src/cloudwatch.ts
@@ -7,9 +7,24 @@ import {
 	CloudWatchClient,
 	PutMetricDataCommand,
 } from '@aws-sdk/client-cloudwatch';
-import { stageFromEnvironment } from '@modules/stage';
+
+export type Stage = 'CODE' | 'DEV' | 'PROD';
+
+const getStage = (): Stage | undefined => {
+	const stage = process.env.Stage;
+	if (stage === undefined) {
+		throw new Error('Stage is not defined as an environment variable');
+	}
+	return stage as Stage;
+};
 
 export async function putMetric(metricName: string): Promise<void> {
+	const stage = getStage();
+	if (stage == 'DEV') {
+		console.log('No metrics sent as running local test');
+		return;
+	}
+
 	const cloudwatch = new CloudWatchClient({
 		region: process.env.AWS_REGION ?? 'eu-west-1',
 	});
@@ -21,7 +36,7 @@ export async function putMetric(metricName: string): Promise<void> {
 		},
 		{
 			Name: 'Stage',
-			Value: stageFromEnvironment(),
+			Value: stage,
 		},
 	];
 
