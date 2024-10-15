0f27d3e33957bd74e78fb7fefe388a21bc01cb86
John Duffell <john.duffell@guardian.co.uk>
2024-10-15 14:44:08 +0100

    new test for the lazy behaviour of then method

--- handlers/discount-api/test/lazy.test.ts
+++ handlers/discount-api/test/lazy.test.ts
@@ -25,3 +25,16 @@ test('it should handle failed promises', async () => {
 	expect(sut.get()).rejects.toEqual('boom');
 	expect(log).toEqual(1);
 });
+
+test('it should only call the function when its used even if its mapped', async () => {
+	let log = 0;
+	const sut = new Lazy<string>(() => {
+		log++;
+		return Promise.resolve('hi');
+	}, 'testing').then((value) => value);
+	expect(log).toEqual(0);
+	expect(sut.get()).resolves.toEqual('hi');
+	expect(log).toEqual(1);
+	expect(sut.get()).resolves.toEqual('hi');
+	expect(log).toEqual(1);
+});
