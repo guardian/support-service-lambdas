b01af678d8ff204de0d2bea0e120518e816025b0
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-10-18 16:27:59 +0100

    add Member xml schema and builder function

---
+++ handlers/press-reader-entitlements/test/xmlBuilder.test.ts
@@ -0,0 +1,41 @@
+import { buildXml, Member } from "../src/xmlBuilder";
+
+test('creates Member schema xml', () => {
+	const expectedXml = `<?xml version="1.0" encoding="utf-8"?>
+<userID>123456</userID>
+<firstname>John</firstname>
+<lastname>Doe</lastname>
+<products>
+  <productID>123</productID>
+  <startdate>2022-01-01</startdate>
+  <enddate>2022-12-31</enddate>
+</products>
+<products>
+  <productID>456</productID>
+  <startdate>2022-01-01</startdate>
+  <enddate>2022-12-31</enddate>
+</products>
+`;
+
+const member: Member = 
+		{
+			userID: "123456",
+			firstname: "John",
+			lastname: "Doe",
+			products: [
+				{
+					productID: "123",
+					startdate: "2022-01-01",
+					enddate: "2022-12-31"
+				},
+				{
+					productID: "456",
+					startdate: "2022-01-01",
+					enddate: "2022-12-31"}
+			]
+		};
+
+	const result = buildXml(member);
+	
+	expect(result).toEqual(expectedXml);
+});
