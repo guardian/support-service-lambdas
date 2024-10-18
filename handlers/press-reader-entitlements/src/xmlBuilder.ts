1c598bd9074d966a8f69d53f16e9f0ec17d572ef
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-10-18 16:27:59 +0100

    add Member xml schema and builder function

---
+++ handlers/press-reader-entitlements/src/xmlBuilder.ts
@@ -0,0 +1,21 @@
+import { XMLBuilder } from "fast-xml-parser";
+
+type Product = {
+	productID: string;
+	startdate: string;
+	enddate: string;
+}
+
+export type Member = {
+	userID: string;
+	firstname: string;
+	lastname: string;
+	products: Product[];
+};
+
+export function buildXml(member: Member){	
+	const builder = new XMLBuilder({format: true});
+	const xmlContent = builder.build(member);
+
+	return `<?xml version="1.0" encoding="utf-8"?>\n${xmlContent}`;
+}
