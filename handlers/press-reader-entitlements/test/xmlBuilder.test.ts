29219acd8fc83ac67064c7075b72d8128f307f76
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-10-21 09:19:33 +0100

    prettier fix

--- handlers/press-reader-entitlements/test/xmlBuilder.test.ts
+++ handlers/press-reader-entitlements/test/xmlBuilder.test.ts
@@ -1,4 +1,5 @@
-import { buildXml, Member } from "../src/xmlBuilder";
+import type { Member } from '../src/xmlBuilder';
+import { buildXml } from '../src/xmlBuilder';
 
 test('creates Member schema xml', () => {
 	const expectedXml = `<?xml version="1.0" encoding="utf-8"?>
@@ -17,25 +18,25 @@ test('creates Member schema xml', () => {
 </products>
 `;
 
-const member: Member = 
-		{
-			userID: "123456",
-			firstname: "John",
-			lastname: "Doe",
-			products: [
-				{
-					productID: "123",
-					startdate: "2022-01-01",
-					enddate: "2022-12-31"
-				},
-				{
-					productID: "456",
-					startdate: "2022-01-01",
-					enddate: "2022-12-31"}
-			]
-		};
+	const member: Member = {
+		userID: '123456',
+		firstname: 'John',
+		lastname: 'Doe',
+		products: [
+			{
+				productID: '123',
+				startdate: '2022-01-01',
+				enddate: '2022-12-31',
+			},
+			{
+				productID: '456',
+				startdate: '2022-01-01',
+				enddate: '2022-12-31',
+			},
+		],
+	};
 
 	const result = buildXml(member);
-	
+
 	expect(result).toEqual(expectedXml);
 });
