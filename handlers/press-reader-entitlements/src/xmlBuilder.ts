0840b15e6acf7617610b2fb15780f5fa9f73ed12
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-10-21 09:09:05 +0100

    ignore lint rule for unsafe assignment

--- handlers/press-reader-entitlements/src/xmlBuilder.ts
+++ handlers/press-reader-entitlements/src/xmlBuilder.ts
@@ -15,7 +15,8 @@ export type Member = {
 
 export function buildXml(member: Member){	
 	const builder = new XMLBuilder({format: true});
-	const xmlContent: string = builder.build(member);
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- fast-xml-parser returns any when it should be string */ 
+	const xmlContent = builder.build(member);
 
 	return `<?xml version="1.0" encoding="utf-8"?>\n${xmlContent}`;
 }
