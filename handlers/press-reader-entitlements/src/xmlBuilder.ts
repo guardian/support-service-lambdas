9446f14315d8e55d567425b08a3df944c1337e25
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-10-21 09:19:33 +0100

    prettier fix

--- handlers/press-reader-entitlements/src/xmlBuilder.ts
+++ handlers/press-reader-entitlements/src/xmlBuilder.ts
@@ -1,10 +1,10 @@
-import { XMLBuilder } from "fast-xml-parser";
+import { XMLBuilder } from 'fast-xml-parser';
 
 type Product = {
 	productID: string;
 	startdate: string;
 	enddate: string;
-}
+};
 
 export type Member = {
 	userID: string;
@@ -13,9 +13,9 @@ export type Member = {
 	products: Product[];
 };
 
-export function buildXml(member: Member){	
-	const builder = new XMLBuilder({format: true});
-    /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- fast-xml-parser returns any when it should be string */ 
+export function buildXml(member: Member) {
+	const builder = new XMLBuilder({ format: true });
+	/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- fast-xml-parser returns any when it should be string */
 	const xmlContent = builder.build(member);
 
 	return `<?xml version="1.0" encoding="utf-8"?>\n${xmlContent}`;
