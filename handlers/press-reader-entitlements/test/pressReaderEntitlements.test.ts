c969ac25adf8c34fd2e7677a38fae76c061656cc
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-10-29 16:36:16 +0000

    refactor tests, add empty products case

--- handlers/press-reader-entitlements/test/pressReaderEntitlements.test.ts
+++ handlers/press-reader-entitlements/test/pressReaderEntitlements.test.ts
@@ -6,7 +6,7 @@ import type { Member } from '../src/xmlBuilder';
 import { buildXml } from '../src/xmlBuilder';
 
 describe('xmlBuilder', () => {
-	test('creates Member schema xml', () => {
+	it('creates Member schema xml with products', () => {
 		const expectedXml = `<?xml version="1.0" encoding="utf-8"?>
 <userID>123456</userID>
 <firstname>John</firstname>
@@ -51,44 +51,66 @@ describe('xmlBuilder', () => {
 
 		expect(result).toEqual(expectedXml);
 	});
+
+	it('creates creates Member schema xml with no products', () => {
+		const expectedXml = `<?xml version="1.0" encoding="utf-8"?>
+<userID>123456</userID>
+<firstname>John</firstname>
+<lastname>Doe</lastname>
+<products></products>
+`;
+
+		const member: Member = {
+			userID: '123456',
+			firstname: 'John',
+			lastname: 'Doe',
+			products: [],
+		};
+
+		const result = buildXml(member);
+
+		expect(result).toEqual(expectedXml);
+	});
 });
 
 const codeProductCatalog = generateProductCatalog(codeZuoraCatalog);
 
-test('checkForValidEntitlements', () => {
-	const supporterData: SupporterRatePlanItem[] = [
-		{
-			subscriptionName: 'A-S00123456',
-			contractEffectiveDate: '2024-01-01',
-			termEndDate: '2024-01-31',
-			identityId: '110001234',
-			productRatePlanId: 'Invalid Product Rate Plan ID',
-			productRatePlanName: 'Fake Product',
-		},
-		{
-			subscriptionName: 'A-S00123456',
-			contractEffectiveDate: '2024-02-01',
-			termEndDate: '2025-01-01',
-			identityId: '110001234',
-			productRatePlanId: '8ad096ca8992481d018992a363bd17ad',
-			productRatePlanName: 'NationalDelivery',
-		},
-		{
-			subscriptionName: 'A-S00123456',
-			contractEffectiveDate: '2024-02-01',
-			termEndDate: '2025-02-01',
-			identityId: '110001234',
-			productRatePlanId: '8ad08cbd8586721c01858804e3275376',
-			productRatePlanName: 'SupporterPlus',
-		},
-	];
+describe('getLatestValidSubscription', () => {
+	it('gets latest subscription by term end date', () => {
+		const supporterData: SupporterRatePlanItem[] = [
+			{
+				subscriptionName: 'A-S00123456',
+				contractEffectiveDate: '2024-01-01',
+				termEndDate: '2024-01-31',
+				identityId: '110001234',
+				productRatePlanId: 'Invalid Product Rate Plan ID',
+				productRatePlanName: 'Fake Product',
+			},
+			{
+				subscriptionName: 'A-S00123456',
+				contractEffectiveDate: '2024-02-01',
+				termEndDate: '2025-01-01',
+				identityId: '110001234',
+				productRatePlanId: '8ad096ca8992481d018992a363bd17ad',
+				productRatePlanName: 'NationalDelivery',
+			},
+			{
+				subscriptionName: 'A-S00123456',
+				contractEffectiveDate: '2024-02-01',
+				termEndDate: '2025-02-01',
+				identityId: '110001234',
+				productRatePlanId: '8ad08cbd8586721c01858804e3275376',
+				productRatePlanName: 'SupporterPlus',
+			},
+		];
 
-	const subscription = getLatestValidSubscription(
-		codeProductCatalog,
-		supporterData,
-	);
+		const subscription = getLatestValidSubscription(
+			codeProductCatalog,
+			supporterData,
+		);
 
-	expect(subscription).toBeDefined();
-	expect(subscription?.termEndDate).toBe('2025-02-01');
-	expect(subscription?.productRatePlanName).toBe('SupporterPlus');
+		expect(subscription).toBeDefined();
+		expect(subscription?.termEndDate).toBe('2025-02-01');
+		expect(subscription?.productRatePlanName).toBe('SupporterPlus');
+	});
 });
