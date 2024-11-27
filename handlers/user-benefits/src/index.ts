a9a1636ffed33727c2f567476fa985a7ac1edb5a
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-11-27 16:16:08 +0000

    index.ts

--- handlers/user-benefits/src/index.ts
+++ handlers/user-benefits/src/index.ts
@@ -1,19 +1,36 @@
 import { ValidationError } from '@modules/errors';
+import { getProductCatalogFromApi } from '@modules/product-catalog/api';
+import { ProductCatalogHelper } from '@modules/product-catalog/productCatalog';
+import type { Stage } from '@modules/stage';
 import type {
 	APIGatewayProxyEvent,
 	APIGatewayProxyResult,
 	Handler,
 } from 'aws-lambda';
+import { getUserBenefits } from './userBenefits';
+
+const stage = process.env.STAGE as Stage;
 
 export const handler: Handler = async (
 	event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
 	console.log(`Input is ${JSON.stringify(event)}`);
 	try {
-		return await Promise.resolve({
-			body: 'Hello World',
+		const productCatalog = await getProductCatalogFromApi(stage);
+		const identityId = event.queryStringParameters?.identityId;
+		if (identityId === undefined) {
+			throw new ValidationError('IdentityId is required');
+		}
+
+		const userBenefitsResponse = await getUserBenefits(
+			stage,
+			new ProductCatalogHelper(productCatalog),
+			identityId,
+		);
+		return {
+			body: JSON.stringify(userBenefitsResponse),
 			statusCode: 200,
-		});
+		};
 	} catch (error) {
 		console.log('Caught exception with message: ', error);
 		if (error instanceof ValidationError) {
