fd5077a2a3d2b15afe068e9b68ee3dcb142e7c06
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-11-27 16:08:21 +0000

    Trial information

--- handlers/user-benefits/src/trialInformation.ts
+++ handlers/user-benefits/src/trialInformation.ts
@@ -1,15 +1,19 @@
 // This information is used by the apps to work out what offer to show to the user
-export type TrialInformation = {
+import type { ProductBenefit } from './productBenefit';
+
+export type TrialInformation = Partial<Record<ProductBenefit, Trial>>;
+
+export type Trial = {
 	iosSubscriptionGroup: string;
 	androidOfferTag: string;
 };
 
-export const feastExtendedTrial: TrialInformation = {
+export const feastExtendedTrial: Trial = {
 	iosSubscriptionGroup: '21445388',
 	androidOfferTag: 'initial_supporter_launch_offer',
 };
 
-export const feastRegularTrial: TrialInformation = {
+export const feastRegularSubscription: Trial = {
 	iosSubscriptionGroup: '21396030',
 	androidOfferTag: 'initial_supporter_launch_offer',
 };
--- handlers/user-benefits/src/userBenefits.ts
+++ handlers/user-benefits/src/userBenefits.ts
@@ -1,51 +1,76 @@
-import type { ProductCatalogHelper } from '@modules/product-catalog/productCatalog';
+import type {
+	ProductCatalogHelper,
+	ProductKey,
+} from '@modules/product-catalog/productCatalog';
 import type { Stage } from '@modules/stage';
-import type { SupporterRatePlanItem } from '@modules/supporter-product-data/supporterProductData';
 import { getSupporterProductData } from '@modules/supporter-product-data/supporterProductData';
 import type { ProductBenefit } from './productBenefit';
 import { productBenefitMapping } from './productBenefit';
 import type { TrialInformation } from './trialInformation';
-import { feastExtendedTrial } from './trialInformation';
+import {
+	feastExtendedTrial,
+	feastRegularSubscription,
+} from './trialInformation';
 
 export type UserBenefitsResponse = {
 	benefits: ProductBenefit[];
-	trials: Partial<Record<ProductBenefit, TrialInformation>>;
+	trials: TrialInformation;
 };
+
 export const getUserBenefits = async (
 	stage: Stage,
 	productCatalogHelper: ProductCatalogHelper,
 	identityId: string,
 ): Promise<UserBenefitsResponse> => {
+	const userProducts = await getUserProducts(
+		stage,
+		productCatalogHelper,
+		identityId,
+	);
+	const benefits = getBenefits(userProducts);
+	const trials = getTrialInformation(benefits, userProducts);
+	return {
+		benefits,
+		trials,
+	};
+};
+
+const getUserProducts = async (
+	stage: Stage,
+	productCatalogHelper: ProductCatalogHelper,
+	identityId: string,
+) => {
 	const supporterProductDataItems = await getSupporterProductData(
 		stage,
 		identityId,
 	);
-
-	return Promise.resolve({
-		benefits: getBenefits(productCatalogHelper, supporterProductDataItems),
-		trials: {
-			feastApp: feastExtendedTrial,
-		},
-	});
-};
-
-export const getBenefits = (
-	productCatalogHelper: ProductCatalogHelper,
-	supporterProductDataItems: SupporterRatePlanItem[] | undefined,
-): ProductBenefit[] => {
 	if (supporterProductDataItems === undefined) {
+		console.log('No supporter product data found');
 		return [];
 	}
-	const userProducts = supporterProductDataItems
+	return supporterProductDataItems
 		.flatMap(
 			(item) =>
 				productCatalogHelper.findProductDetails(item.productRatePlanId)
 					?.zuoraProduct,
 		)
 		.filter((product) => product !== undefined);
+};
 
-	const userProductBenefits = userProducts.flatMap(
-		(product) => productBenefitMapping[product],
-	);
-	return userProductBenefits;
+export const getBenefits = (userProducts: ProductKey[]): ProductBenefit[] =>
+	userProducts.flatMap((product) => productBenefitMapping[product]);
+
+export const getTrialInformation = (
+	productBenefits: ProductBenefit[],
+	userProducts: ProductKey[],
+): TrialInformation => {
+	if (productBenefits.includes('feastApp')) {
+		return {};
+	}
+	if (userProducts.find((product) => product === 'GuardianWeeklyDomestic')) {
+		return { feastApp: feastExtendedTrial };
+	}
+	return {
+		feastApp: feastRegularSubscription,
+	};
 };
