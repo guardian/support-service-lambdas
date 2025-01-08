f550bcc0b119c7911e1d300f81ef7e66090ec3ab
Tom Forbes <tom.forbes@theguardian.com>
2025-01-08 09:07:46 +0000

    move to portfolio

--- handlers/alarms-handler/src/alarmMappings.ts
+++ handlers/alarms-handler/src/alarmMappings.ts
@@ -82,6 +82,7 @@ const teamToAppMappings: Record<Team, string[]> = {
 		'payment-api',
 
 		// support-service-lambdas
+		'catalog-service',
 		'generate-product-catalog',
 		'metric-push-api',
 		'press-reader-entitlements',
@@ -108,7 +109,6 @@ const teamToAppMappings: Record<Team, string[]> = {
 		'invoicing-api',
 		'zuora-callout-apis',
 		'zuora-oracle-fusion',
-		'catalog-service',
 
 		// stripe
 		'stripe-patrons-data',
