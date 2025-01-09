4e15b942f9867216295bfb36aff2c5ff85cfba06
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-09 15:20:10 +0000

    Create discount-expiry-notifier.test.ts

---
+++ cdk/lib/discount-expiry-notifier.test.ts
@@ -0,0 +1,28 @@
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { DiscountExpiryNotifier } from './discount-expiry-notifier';
+
+describe('The zuora-salesforce-link-remover stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const codeStack = new DiscountExpiryNotifier(
+			app,
+			'discount-expiry-notifier-CODE',
+			{
+				stack: 'membership',
+				stage: 'CODE',
+			},
+		);
+		const prodStack = new DiscountExpiryNotifier(
+			app,
+			'discount-expiry-notifier-PROD',
+			{
+				stack: 'membership',
+				stage: 'PROD',
+			},
+		);
+
+		expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
+		expect(Template.fromStack(prodStack).toJSON()).toMatchSnapshot();
+	});
+});
