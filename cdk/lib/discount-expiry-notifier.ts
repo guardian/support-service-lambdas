17ec23ca3d7a0041f9c471c420d1bce27579eb9b
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-09 15:20:07 +0000

    Create discount-expiry-notifier.ts

---
+++ cdk/lib/discount-expiry-notifier.ts
@@ -0,0 +1,29 @@
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import type { App } from 'aws-cdk-lib';
+import {
+	DefinitionBody,
+	Pass,
+	StateMachine,
+} from 'aws-cdk-lib/aws-stepfunctions';
+
+export class DiscountExpiryNotifier extends GuStack {
+	constructor(scope: App, id: string, props: GuStackProps) {
+		super(scope, id, props);
+		
+		const appName = 'discount-expiry-notifier';
+
+		const passState = new Pass(this, 'generic pass state')
+		const definitionBody = DefinitionBody.fromChainable(
+			passState
+		);
+
+		new StateMachine(
+			this,
+			`${appName}-state-machine-${this.stage}`,
+			{
+				definitionBody: definitionBody,
+			},
+		);
+	}
+}
