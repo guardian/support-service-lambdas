334a5e838e779f84e86458f82ae71d4b582f33cd
Tom Wey <tom.wey@theguardian.com>
2025-01-09 11:36:31 +0000

    Add Guardian Ad-Lite to the product catalog
    For now this is alongside the old Guardian Light product, which will be
    removed in a future PR.

--- modules/product-catalog/test/generateProductCatalog.test.ts
+++ modules/product-catalog/test/generateProductCatalog.test.ts
@@ -1,6 +1,7 @@
 import { zuoraCatalogSchema } from '@modules/zuora-catalog/zuoraCatalogSchema';
 import { generateProductCatalog } from '@modules/product-catalog/generateProductCatalog';
 import { generateTypeObject } from '@modules/product-catalog/generateTypeObject';
+import code from '../../zuora-catalog/test/fixtures/catalog-code.json';
 import prod from '../../zuora-catalog/test/fixtures/catalog-prod.json';
 
 test('Generated product catalog matches snapshot', () => {
@@ -14,4 +15,19 @@ test('Generated product catalog types match snapshot', () => {
 	const prodZuoraCatalog = zuoraCatalogSchema.parse(prod);
 	const prodTypeObject = generateTypeObject(prodZuoraCatalog);
 	expect(prodTypeObject).toMatchSnapshot();
+});
+
+describe('code', () => {
+	test('Generated product catalog matches snapshot', () => {
+		const codeZuoraCatalog = zuoraCatalogSchema.parse(code);
+		const codeProductCatalog = generateProductCatalog(codeZuoraCatalog);
+		console.log(JSON.stringify(codeProductCatalog));
+		expect(codeProductCatalog).toMatchSnapshot();
+	});
+
+	test('Generated product catalog types match snapshot', () => {
+		const codeZuoraCatalog = zuoraCatalogSchema.parse(code);
+		const codeTypeObject = generateTypeObject(codeZuoraCatalog);
+		expect(codeTypeObject).toMatchSnapshot();
+	});
 });
