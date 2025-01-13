9761e62b0e42d762d7a46692faacb593a2f48440
Tom Wey <tom.wey@theguardian.com>
2025-01-13 11:42:55 +0000

    Add Guardian Ad-Lite to the product catalog (#2618)
    For now this is alongside the old Guardian Light product, which will be
    removed in a future PR.

--- modules/product-catalog/test/generateProductCatalog.test.ts
+++ modules/product-catalog/test/generateProductCatalog.test.ts
@@ -1,17 +1,35 @@
 import { zuoraCatalogSchema } from '@modules/zuora-catalog/zuoraCatalogSchema';
 import { generateProductCatalog } from '@modules/product-catalog/generateProductCatalog';
 import { generateTypeObject } from '@modules/product-catalog/generateTypeObject';
+import code from '../../zuora-catalog/test/fixtures/catalog-code.json';
 import prod from '../../zuora-catalog/test/fixtures/catalog-prod.json';
 
-test('Generated product catalog matches snapshot', () => {
-	const prodZuoraCatalog = zuoraCatalogSchema.parse(prod);
-	const prodProductCatalog = generateProductCatalog(prodZuoraCatalog);
-	console.log(JSON.stringify(prodProductCatalog));
-	expect(prodProductCatalog).toMatchSnapshot();
+describe('prod', () => {
+	test('Generated product catalog matches snapshot', () => {
+		const prodZuoraCatalog = zuoraCatalogSchema.parse(prod);
+		const prodProductCatalog = generateProductCatalog(prodZuoraCatalog);
+		console.log(JSON.stringify(prodProductCatalog));
+		expect(prodProductCatalog).toMatchSnapshot();
+	});
+
+	test('Generated product catalog types match snapshot', () => {
+		const prodZuoraCatalog = zuoraCatalogSchema.parse(prod);
+		const prodTypeObject = generateTypeObject(prodZuoraCatalog);
+		expect(prodTypeObject).toMatchSnapshot();
+	});
 });
 
-test('Generated product catalog types match snapshot', () => {
-	const prodZuoraCatalog = zuoraCatalogSchema.parse(prod);
-	const prodTypeObject = generateTypeObject(prodZuoraCatalog);
-	expect(prodTypeObject).toMatchSnapshot();
+describe('code', () => {
+	test('Generated product catalog matches snapshot', () => {
+		const codeZuoraCatalog = zuoraCatalogSchema.parse(code);
+		const codeProductCatalog = generateProductCatalog(codeZuoraCatalog);
+		console.log(JSON.stringify(codeProductCatalog));
+		expect(codeProductCatalog).toMatchSnapshot();
+	});
+
+	test('Generated product catalog types match snapshot', () => {
+		const codeZuoraCatalog = zuoraCatalogSchema.parse(code);
+		const codeTypeObject = generateTypeObject(codeZuoraCatalog);
+		expect(codeTypeObject).toMatchSnapshot();
+	});
 });
