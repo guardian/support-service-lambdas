eb234e259bfb8bf1e79f6c5cfc7880b90d0f5765
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-13 11:00:43 +0000

    Update discount-expiry-notifier.ts

--- cdk/lib/discount-expiry-notifier.ts
+++ cdk/lib/discount-expiry-notifier.ts
@@ -1,11 +1,14 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import type { App } from 'aws-cdk-lib';
+import { Architecture } from 'aws-cdk-lib/aws-lambda';
 import {
 	DefinitionBody,
-	Pass,
 	StateMachine,
 } from 'aws-cdk-lib/aws-stepfunctions';
+import { LambdaInvoke } from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { nodeVersion } from './node-version';
 
 export class DiscountExpiryNotifier extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
@@ -13,8 +16,32 @@ export class DiscountExpiryNotifier extends GuStack {
 
 		const appName = 'discount-expiry-notifier';
 
-		const passState = new Pass(this, 'generic pass state');
-		const definitionBody = DefinitionBody.fromChainable(passState);
+		const getSubsWithExpiringDiscountsLambda = new GuLambdaFunction(
+			this,
+			'get-subs-with-expiring-discounts-lambda',
+			{
+				app: appName,
+				functionName: `${appName}-get-subs-with-expiring-discounts-${this.stage}`,
+				runtime: nodeVersion,
+				environment: {
+					Stage: this.stage,
+				},
+				handler: 'getSubsWithExpiringDiscounts.handler',
+				fileName: `${appName}.zip`,
+				architecture: Architecture.ARM_64,
+			},
+		);
+
+		const getSubsWithExpiringDiscountsLambdaTask = new LambdaInvoke(
+			this,
+			'Get Salesforce Billing Accounts',
+			{
+				lambdaFunction: getSubsWithExpiringDiscountsLambda,
+				outputPath: '$.Payload',
+			},
+		);
+
+		const definitionBody = DefinitionBody.fromChainable(getSubsWithExpiringDiscountsLambdaTask);
 
 		new StateMachine(this, `${appName}-state-machine-${this.stage}`, {
 			definitionBody: definitionBody,
