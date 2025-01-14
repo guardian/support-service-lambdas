18fe07e9ba02e68614ba260df319ac840c933d86
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2025-01-14 16:51:32 +0000

    feat: add request to idapi

--- handlers/stripe-checkout-spike/src/handlers/checkoutSessionCompletedEventHandler.ts
+++ handlers/stripe-checkout-spike/src/handlers/checkoutSessionCompletedEventHandler.ts
@@ -1,3 +1,31 @@
-export const handler = (event: unknown) => {
-	console.log(event);
+import { type SQSEvent } from 'aws-lambda';
+import type Stripe from 'stripe';
+import { getSecretValue } from '../services';
+
+export const handler = async (event: SQSEvent): Promise<void> => {
+	try {
+		for (const record of event.Records) {
+			const body = JSON.parse(record.body) as {
+				detail: Stripe.CheckoutSessionCompletedEvent;
+			};
+
+			console.log(JSON.stringify(body));
+
+			const accessToken = await getSecretValue({ secretName: '' });
+
+			const response = await fetch(
+				`$https://idapi.theguardian.com/user?emailAddress=${body.detail.data.object.customer_details?.email}`,
+				{
+					method: 'GET',
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+					},
+				},
+			);
+			console.log(response);
+		}
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
 };
