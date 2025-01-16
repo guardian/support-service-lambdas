0f992e66df66fdf6df15b1d4b929472e422613ab
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-16 15:48:09 +0000

    add buildEmailPayload lambda

--- cdk/lib/discount-expiry-notifier.ts
+++ cdk/lib/discount-expiry-notifier.ts
@@ -9,47 +9,75 @@ import { LambdaInvoke } from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { nodeVersion } from './node-version';
 
 export class DiscountExpiryNotifier extends GuStack {
-	constructor(scope: App, id: string, props: GuStackProps) {
-		super(scope, id, props);
+    constructor(scope: App, id: string, props: GuStackProps) {
+        super(scope, id, props);
 
-		const appName = 'discount-expiry-notifier';
+        const appName = 'discount-expiry-notifier';
 
-		new Bucket(this, 'Bucket', {
-			bucketName: `${appName}-${this.stage.toLowerCase()}`,
-		});
+        new Bucket(this, 'Bucket', {
+            bucketName: `${appName}-${this.stage.toLowerCase()}`,
+        });
 
-		const getSubsWithExpiringDiscountsLambda = new GuLambdaFunction(
-			this,
-			'get-subs-with-expiring-discounts-lambda',
-			{
-				app: appName,
-				functionName: `${appName}-get-subs-with-expiring-discounts-${this.stage}`,
-				runtime: nodeVersion,
-				environment: {
-					Stage: this.stage,
-				},
-				handler: 'getSubsWithExpiringDiscounts.handler',
-				fileName: `${appName}.zip`,
-				architecture: Architecture.ARM_64,
-			},
-		);
+        const getSubsWithExpiringDiscountsLambda = new GuLambdaFunction(
+            this,
+            'get-subs-with-expiring-discounts-lambda',
+            {
+                app: appName,
+                functionName: `${appName}-get-subs-with-expiring-discounts-${this.stage}`,
+                runtime: nodeVersion,
+                environment: {
+                    Stage: this.stage,
+                },
+                handler: 'getSubsWithExpiringDiscounts.handler',
+                fileName: `${appName}.zip`,
+                architecture: Architecture.ARM_64,
+            },
+        );
 
-		const getSubsWithExpiringDiscountsLambdaTask = new LambdaInvoke(
-			this,
-			'Get Subs With Expiring Discounts',
-			{
-				lambdaFunction: getSubsWithExpiringDiscountsLambda,
-				outputPath: '$.Payload',
-			},
-		);
+        const buildEmailPayloadLambda = new GuLambdaFunction(
+            this,
+            'build-email-payload-lambda',
+            {
+                app: appName,
+                functionName: `${appName}-build-email-payload-${this.stage}`,
+                runtime: nodeVersion,
+                environment: {
+                    Stage: this.stage,
+                },
+                handler: 'buildEmailPayload.handler',
+                fileName: `${appName}.zip`,
+                architecture: Architecture.ARM_64,
+            },
+        );
 
-		const definitionBody = DefinitionBody.fromChainable(
-			getSubsWithExpiringDiscountsLambdaTask,
-		);
+        const getSubsWithExpiringDiscountsLambdaTask = new LambdaInvoke(
+            this,
+            'Get Subs With Expiring Discounts',
+            {
+                lambdaFunction: getSubsWithExpiringDiscountsLambda,
+                outputPath: '$.Payload',
+            },
+        );
 
-		new StateMachine(this, `${appName}-state-machine-${this.stage}`, {
-			stateMachineName: `${appName}-${this.stage}`,
-			definitionBody: definitionBody,
-		});
-	}
+        const buildEmailPayloadLambdaTask = new LambdaInvoke(
+            this,
+            'Build Email Payload',
+            {
+                lambdaFunction: buildEmailPayloadLambda,
+                outputPath: '$.Payload',
+            },
+        );
+
+        const definitionBody = DefinitionBody.fromChainable(
+            getSubsWithExpiringDiscountsLambdaTask.next(
+                buildEmailPayloadLambdaTask,
+            ),
+        );
+
+        new StateMachine(this, `${appName}-state-machine-${this.stage}`, {
+            stateMachineName: `${appName}-${this.stage}`,
+            definitionBody: definitionBody,
+        });
+    }
 }
+
