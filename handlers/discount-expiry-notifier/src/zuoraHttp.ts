7ca3e4a50bfcc09b9408f793619f79328ce3e840
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-21 11:34:05 +0000

    Create zuoraHttp.ts

---
+++ handlers/discount-expiry-notifier/src/zuoraHttp.ts
@@ -0,0 +1,20 @@
+import { stageFromEnvironment } from '@modules/stage';
+import { ZuoraClient } from '@modules/zuora/zuoraClient';
+import { zuoraSubscriptionSchema } from '@modules/zuora/zuoraSchemas';
+import type { ZuoraSubscription } from '@modules/zuora/zuoraSchemas';
+
+export async function getSub(
+	subName: string,
+): Promise<ZuoraSubscription> {
+	console.log(
+		`retrieving sub ${subName} from Zuora ...`,
+	);
+
+	const zuoraClient = await ZuoraClient.create(stageFromEnvironment());
+	const path = `/v1/subscriptions/${subName}`;
+
+	const zuoraGetSubResponse: ZuoraSubscription =
+		await zuoraClient.get(path, zuoraSubscriptionSchema);
+
+	return zuoraGetSubResponse;
+}
