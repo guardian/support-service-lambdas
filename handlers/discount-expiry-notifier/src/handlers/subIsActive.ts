12df490335afaf38911499cdc68aeb840679d019
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-21 11:34:08 +0000

    Update subIsActive.ts

--- handlers/discount-expiry-notifier/src/handlers/subIsActive.ts
+++ handlers/discount-expiry-notifier/src/handlers/subIsActive.ts
@@ -1,3 +1,19 @@
-export const handler = () => {
-    
+import { getSub } from "../zuoraHttp";
+
+export const handler = async () => {
+	try {
+        const subName = process.env.SUB_NAME ?? "A-S00954053";
+
+        console.log("subName:", subName);
+
+		const getSubResponse = await getSub(subName);
+
+		return {
+			status: getSubResponse.status,
+		};
+	} catch (error) {
+		throw new Error(
+			`Error retrieving sub from Zuora: ${JSON.stringify(error)}`,
+		);
+	}
 };
