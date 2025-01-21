1aebe34ce880ac2dd2b6874e64c07410426f33de
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-21 16:32:55 +0000

    Update bigquery.ts

--- handlers/discount-expiry-notifier/src/bigquery.ts
+++ handlers/discount-expiry-notifier/src/bigquery.ts
@@ -29,7 +29,7 @@ export const runQuery = async (
 	});
 
 	const query =
-		"SELECT tier.id, tier.tier, charge.id FROM `datatech-fivetran.zuora.rate_plan_charge_tier` tier JOIN `datatech-fivetran.zuora.rate_plan_charge` charge ON charge.id = tier.rate_plan_charge_id WHERE tier.id = '8a12926292c35f1d0192f3ca2e3b7a09'";
+		"SELECT tier.id, tier.tier, charge.id, DATE_ADD(charge.effective_start_date, INTERVAL charge.up_to_periods MONTH) as calculated_end_date, DATE_DIFF(charge.effective_end_date, charge.effective_start_date, MONTH) as months_diff, FROM `datatech-fivetran.zuora.rate_plan_charge_tier` tier JOIN `datatech-fivetran.zuora.rate_plan_charge` charge ON charge.id = tier.rate_plan_charge_id WHERE tier.id = '8a12926292c35f1d0192f3ca2e3b7a09'";
 
 	const result = await bigquery.query(query);
 	console.log('result', result);
