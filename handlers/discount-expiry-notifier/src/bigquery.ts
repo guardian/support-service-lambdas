0da68a8a9bd2396b38fadb0d7f7b4ac1f6cd236b
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-21 14:47:28 +0000

    fix formatting

--- handlers/discount-expiry-notifier/src/bigquery.ts
+++ handlers/discount-expiry-notifier/src/bigquery.ts
@@ -1,29 +1,38 @@
 import { BigQuery } from '@google-cloud/bigquery';
-import type { BaseExternalAccountClient, ExternalAccountClientOptions } from 'google-auth-library';
+import type {
+	BaseExternalAccountClient,
+	ExternalAccountClientOptions,
+} from 'google-auth-library';
 import { ExternalAccountClient } from 'google-auth-library';
 
-export const buildAuthClient = (clientConfig: string): Promise<BaseExternalAccountClient> => new Promise((resolve, reject) => {
-    const parsedConfig = JSON.parse(clientConfig) as ExternalAccountClientOptions;
-    const authClient = ExternalAccountClient.fromJSON(parsedConfig);
-    if (authClient) {
-        resolve(authClient);
-    } else {
-        reject(new Error('Failed to create Google Auth Client'));
-    }
-});
+export const buildAuthClient = (
+	clientConfig: string,
+): Promise<BaseExternalAccountClient> =>
+	new Promise((resolve, reject) => {
+		const parsedConfig = JSON.parse(
+			clientConfig,
+		) as ExternalAccountClientOptions;
+		const authClient = ExternalAccountClient.fromJSON(parsedConfig);
+		if (authClient) {
+			resolve(authClient);
+		} else {
+			reject(new Error('Failed to create Google Auth Client'));
+		}
+	});
 
 export const runQuery = async (
-    authClient: BaseExternalAccountClient,
+	authClient: BaseExternalAccountClient,
 ): Promise<number> => {
-    const bigquery = new BigQuery({
-        projectId: `datatech-fivetran`,
-        authClient,
-    });
+	const bigquery = new BigQuery({
+		projectId: `datatech-fivetran`,
+		authClient,
+	});
 
-    const query = "SELECT tier.id FROM `datatech-fivetran.zuora.rate_plan_charge_tier` tier WHERE tier.id = '8a12926292c35f1d0192f3ca2e3b7a09'"
+	const query =
+		"SELECT tier.id FROM `datatech-fivetran.zuora.rate_plan_charge_tier` tier WHERE tier.id = '8a12926292c35f1d0192f3ca2e3b7a09'";
 
-    const result = await bigquery.query(query);
-    console.log('result',result);
+	const result = await bigquery.query(query);
+	console.log('result', result);
 
-    return 1;
-}
+	return 1;
+};
--- handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
+++ handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
@@ -1,9 +1,8 @@
 import { buildAuthClient, runQuery } from '../bigquery';
 
 export const handler = async () => {
-
 	const gcpConfig = process.env.GCP_CONFIG ?? '';
-    const authClient = await buildAuthClient(gcpConfig);
-    const result = await runQuery(authClient);
-    console.log('result: ', result);
+	const authClient = await buildAuthClient(gcpConfig);
+	const result = await runQuery(authClient);
+	console.log('result: ', result);
 };
