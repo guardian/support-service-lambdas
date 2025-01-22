f443603ea860664b62dc0a6105e694980446a654
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-22 13:22:53 +0000

    Update bigquery.ts

--- handlers/discount-expiry-notifier/src/bigquery.ts
+++ handlers/discount-expiry-notifier/src/bigquery.ts
@@ -90,55 +90,12 @@ export const runQuery = async (
 			charge.up_to_periods IS NOT NULL AND 
 			sub.is_latest_version = true AND 
 			sub.status = 'Active' AND 
-			DATE_ADD(charge.effective_start_date, INTERVAL charge.up_to_periods MONTH) = DATE_ADD(current_date(), INTERVAL 32 DAY)
+			DATE_ADD(charge.effective_start_date, INTERVAL charge.up_to_periods MONTH) = DATE_ADD(current_date(), INTERVAL 32 DAY) AND
+			(tierId='8a129b5d9349013f01935a2d75854d0f' OR tierId = '8a1294769343a965019343cc4cf23590')
 		ORDER BY 
 			sub.name desc
 		`;
-	// WHERE
-	// 	tier.id = '8a12926292c35f1d0192f3ca2e3b7a09'
-	// 		WHERE product.name = 'Discounts'
-	// AND charge.charge_type = 'Recurring'
-	// AND charge.up_to_periods IS NOT NULL
-	// AND sub.is_latest_version = true
-	// AND sub.status = 'Active'
-	// AND DATE_ADD(charge.effective_start_date, INTERVAL charge.up_to_periods MONTH) = DATE_ADD(current_date(), INTERVAL 32 DAY)
 
-	console.log('query:', query);
-	// const query = `
-	// SELECT
-	// 	tier.id as tierId,
-	// 	tier.tier as tier,
-	// 	charge.id as chargeId,
-	// 	charge.name as chargeName,
-	// 	charge.charge_type as chargeType,
-	// 	charge.up_to_periods as upToPeriods,
-	// 	charge.up_to_periods_type as upToPeriodsType,
-	// 	charge.effective_start_date as effectiveStartDate,
-	// 	charge.effective_end_date as effectiveEndDate,
-	// DATE_ADD(charge.effective_start_date, INTERVAL charge.up_to_periods MONTH) as calculated_end_date,
-	// DATE_DIFF(charge.effective_end_date, charge.effective_start_date, MONTH) as months_diff,
-	// 	tier.discount_percentage as discountPercentage,
-	// 	tier.price as price,
-	// 	sub.name as sub_name,
-	// 	sub.is_latest_version as isLatestVersion,
-	// 	sub.status as subStatus
-
-	// FROM datatech-fivetran.zuora.rate_plan_charge_tier tier
-	// 	JOIN datatech-fivetran.zuora.rate_plan_charge charge ON charge.id = tier.rate_plan_charge_id
-	// 	JOIN datatech-fivetran.zuora.rate_plan rate_plan ON rate_plan.id = charge.rate_plan_id
-	// 	JOIN datatech-fivetran.zuora.product product ON product.id = tier.product_id
-	// 	JOIN datatech-fivetran.zuora.subscription sub ON sub.id = tier.subscription_id
-
-	// WHERE
-	// 	product.name = 'Discounts' AND
-	// 	charge.charge_type = 'Recurring' AND
-	// 	charge.up_to_periods IS NOT NULL AND
-	// 	sub.is_latest_version = true AND
-	// 	sub.status = 'Active' AND
-	// 	DATE_ADD(charge.effective_start_date, INTERVAL charge.up_to_periods MONTH) = DATE_ADD(current_date(), INTERVAL 32 DAY)
-	// ORDER BY
-	// 	sub.name desc
-	// 	`;
 	const result = await bigquery.query(query);
 	console.log('result', result);
 
