2c14fae45890186126f47a19538a527f5ff28d00
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-22 11:42:43 +0000

    Update bigquery.ts

--- handlers/discount-expiry-notifier/src/bigquery.ts
+++ handlers/discount-expiry-notifier/src/bigquery.ts
@@ -4,86 +4,116 @@ import type {
 	ExternalAccountClientOptions,
 } from 'google-auth-library';
 import { ExternalAccountClient } from 'google-auth-library';
+import { z } from 'zod';
 
-export const buildAuthClient = (
+export const buildAuthClient = async (
 	clientConfig: string,
-): Promise<BaseExternalAccountClient> =>
-	new Promise((resolve, reject) => {
-		try {
-			const parsedConfig = JSON.parse(
-				clientConfig,
-			) as ExternalAccountClientOptions;
-			const authClient = ExternalAccountClient.fromJSON(parsedConfig);
-			if (authClient) {
-				resolve(authClient);
-			} else {
-				reject(new Error('Failed to create Google Auth Client'));
-			}
-		} catch (error) {
-			// Narrow the error type
-			if (error instanceof Error) {
-				reject(new Error(`Error parsing client config: ${error.message}`));
-			} else {
-				reject(
-					new Error('Error parsing client config: An unknown error occurred'),
-				);
-			}
+): Promise<BaseExternalAccountClient> => {
+	try {
+		const parsedConfig = JSON.parse(
+			clientConfig,
+		) as ExternalAccountClientOptions;
+		const authClient = ExternalAccountClient.fromJSON(parsedConfig);
+
+		if (!authClient) {
+			throw new Error('Failed to create Google Auth Client');
 		}
-	});
+
+		return await Promise.resolve(authClient);
+	} catch (error) {
+		throw new Error(`Error building auth client: ${(error as Error).message}`);
+	}
+};
+
+// const BigQueryDateSchema = z
+// 	.string()
+// 	.regex(/^\d{4}-\d{2}-\d{2}$/, 'Invalid date format. Expected YYYY-MM-DD.')
+// 	.transform((date) => new Date(date));
+export const BigQueryDate1 = z
+  .string()
+  .regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid date format. Expected YYYY-MM-DD.")
+  .transform((date) => new Date(date));
+
+export const BigQueryResultDataSchema = z.array(
+	z.object({
+		tierId: z.string(),
+		tier: z.number(),
+		chargeId: z.string(),
+		chargeName: z.string(),
+		chargeType: z.string(),
+		upToPeriods: z.number(),
+		upToPeriodsType: z.string(),
+		effectiveStartDate: BigQueryDate1,
+	}),
+);
 
 export const runQuery = async (
 	authClient: BaseExternalAccountClient,
 ): Promise<number> => {
 	const bigquery = new BigQuery({
-		projectId: 'datatech-platform-code',
+		projectId: `datatech-platform-code`,
 		authClient,
 	});
 
 	const query = `
 		SELECT 
-			tier.id as id, 
+			tier.id as tierId, 
+			tier.tier as tier,
+			charge.id as chargeId,
+			charge.name as chargeName,
+			
+			charge.charge_type as chargeType,
+			charge.up_to_periods as upToPeriods,
+			charge.up_to_periods_type as upToPeriodsType,
 			charge.effective_start_date as effectiveStartDate
 		FROM 
 			datatech-fivetran.zuora.rate_plan_charge_tier tier 
 		JOIN 
-			datatech-fivetran.zuora.rate_plan_charge charge 
-		ON 
-			charge.id = tier.rate_plan_charge_id 
+			datatech-fivetran.zuora.rate_plan_charge charge ON charge.id = tier.rate_plan_charge_id
 		WHERE 
 			tier.id = '8a12926292c35f1d0192f3ca2e3b7a09'
-	`;
+		`;
+	console.log('query:', query);
+	// const query = `
+	// SELECT
+	// 	tier.id as tierId,
+	// 	tier.tier as tier,
+	// 	charge.id as chargeId,
+	// 	charge.name as chargeName,
+	// 	charge.charge_type as chargeType,
+	// 	charge.up_to_periods as upToPeriods,
+	// 	charge.up_to_periods_type as upToPeriodsType,
+	// 	charge.effective_start_date as effectiveStartDate,
+	// 	charge.effective_end_date as effectiveEndDate,
+	// 	DATE_ADD(charge.effective_start_date, INTERVAL charge.up_to_periods MONTH) as calculated_end_date,
+	// 	DATE_DIFF(charge.effective_end_date, charge.effective_start_date, MONTH) as months_diff,
+	// 	tier.discount_amount as discountAmount,
+	// 	tier.discount_percentage as discountPercentage,
+	// 	tier.price as price,
+	// 	sub.name as sub_name,
+	// 	sub.is_latest_version as isLatestVersion,
+	// 	sub.status as subStatus
 
-	try {
-		// Execute the query
-		const [rows] = await bigquery.query({ query });
+	// FROM datatech-fivetran.zuora.rate_plan_charge_tier tier
+	// 	JOIN datatech-fivetran.zuora.rate_plan_charge charge ON charge.id = tier.rate_plan_charge_id
+	// 	JOIN datatech-fivetran.zuora.rate_plan rate_plan ON rate_plan.id = charge.rate_plan_id
+	// 	JOIN datatech-fivetran.zuora.product product ON product.id = tier.product_id
+	// 	JOIN datatech-fivetran.zuora.subscription sub ON sub.id = tier.subscription_id
 
-		// Type the rows as QueryResult[]
-		const typedRows = rows as QueryResult[];
+	// WHERE
+	// 	product.name = 'Discounts' AND
+	// 	charge.charge_type = 'Recurring' AND
+	// 	charge.up_to_periods IS NOT NULL AND
+	// 	sub.is_latest_version = true AND
+	// 	sub.status = 'Active' AND
+	// 	DATE_ADD(charge.effective_start_date, INTERVAL charge.up_to_periods MONTH) = DATE_ADD(current_date(), INTERVAL 32 DAY)
+	// ORDER BY
+	// 	sub.name desc
+	// 	`;
+	const result = await bigquery.query(query);
+	console.log('result', result);
 
-		// Log the results
-		if (typedRows.length > 0) {
-			const firstRow = typedRows[0] ?? null; // Safely assign firstRow or null
-
-			if (firstRow && firstRow.effectiveStartDate instanceof BigQueryDate) {
-				console.log(
-					'calculated_end_date value:',
-					firstRow.effectiveStartDate.value,
-				);
-			} else if (firstRow) {
-				console.log('calculated_end_date:', firstRow.effectiveStartDate);
-			}
-		} else {
-			console.log('No rows returned from the query.');
-		}
-
-		return 1;
-	} catch (error) {
-		console.error('Error running query:', (error as Error).message);
-		throw error;
-	}
+	const resultData = BigQueryResultDataSchema.parse(result[0]);
+	console.log('resultData', resultData);
+	return 1;
 };
-
-interface QueryResult {
-	id: string;
-	effectiveStartDate: BigQueryDate;
-}
