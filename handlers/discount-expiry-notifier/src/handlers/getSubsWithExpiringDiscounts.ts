7fe9af28c0ba791e9bbdd9b54f92693c854f1b4d
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-23 14:58:20 +0000

    formatting

--- handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
+++ handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
@@ -2,14 +2,15 @@ import { buildAuthClient, runQuery } from '../bigquery';
 import { getSSMParam } from '../ssm';
 
 export const handler = async () => {
-
-    const stage = process.env.Stage;
+	const stage = process.env.Stage;
 
 	if (!stage || (stage !== 'CODE' && stage !== 'PROD')) {
-        return Promise.reject(new Error(`Invalid or missing stage: '${stage ?? ''}'`));
-    }
+		return Promise.reject(
+			new Error(`Invalid or missing stage: '${stage ?? ''}'`),
+		);
+	}
 
-    const gcpConfig = await getSSMParam('gcp-credentials-config', stage);
+	const gcpConfig = await getSSMParam('gcp-credentials-config', stage);
 	const authClient = await buildAuthClient(gcpConfig);
 	const result = await runQuery(authClient);
 	console.log('result: ', result);
--- handlers/discount-expiry-notifier/src/ssm.ts
+++ handlers/discount-expiry-notifier/src/ssm.ts
@@ -1,20 +1,23 @@
 import * as AWS from 'aws-sdk';
 
-export const getSSMParam = (key: string, stage: 'CODE' | 'PROD'): Promise<string> => {
-    const ssm = new AWS.SSM({ region: 'eu-west-1' });
-    return ssm
-        .getParameter({
-            Name: `/discount-expiry-notifier/${stage}/${key}`,
-            WithDecryption: true,
-        })
-        .promise()
-        .then((result) => {
-            const value = result.Parameter?.Value;
+export const getSSMParam = (
+	key: string,
+	stage: 'CODE' | 'PROD',
+): Promise<string> => {
+	const ssm = new AWS.SSM({ region: 'eu-west-1' });
+	return ssm
+		.getParameter({
+			Name: `/discount-expiry-notifier/${stage}/${key}`,
+			WithDecryption: true,
+		})
+		.promise()
+		.then((result) => {
+			const value = result.Parameter?.Value;
 
-            if (value) {
-                return value;
-            }
+			if (value) {
+				return value;
+			}
 
-            throw new Error(`Failed to retrieve config from parameter store: ${key}`);
-        });
+			throw new Error(`Failed to retrieve config from parameter store: ${key}`);
+		});
 };
