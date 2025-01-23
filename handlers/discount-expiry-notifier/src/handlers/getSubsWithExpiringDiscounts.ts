ce7fa36b59e6fb2f9b9c9c220185bb2317c5a470
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-23 17:23:07 +0000

    Update getSubsWithExpiringDiscounts.ts

--- handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
+++ handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
@@ -3,8 +3,10 @@ import { buildAuthClient, runQuery } from '../bigquery';
 import { getSSMParam } from '../ssm';
 
 export const handler = async () => {
-
-	const gcpConfig = await getSSMParam('gcp-credentials-config', stageFromEnvironment());
+	const gcpConfig = await getSSMParam(
+		'gcp-credentials-config',
+		stageFromEnvironment(),
+	);
 	const authClient = await buildAuthClient(gcpConfig);
 	const result = await runQuery(authClient);
 	console.log('result: ', result);
