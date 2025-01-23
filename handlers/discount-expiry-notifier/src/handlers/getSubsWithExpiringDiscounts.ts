9c83bcea965719c18f9bcd541deae26d1e9708fb
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-23 17:17:24 +0000

    use stageFromEnvironment

--- handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
+++ handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
@@ -1,16 +1,10 @@
+import { stageFromEnvironment } from '@modules/stage';
 import { buildAuthClient, runQuery } from '../bigquery';
 import { getSSMParam } from '../ssm';
 
 export const handler = async () => {
-	const stage = process.env.Stage;
 
-	if (!stage || (stage !== 'CODE' && stage !== 'PROD')) {
-		return Promise.reject(
-			new Error(`Invalid or missing stage: '${stage ?? ''}'`),
-		);
-	}
-
-	const gcpConfig = await getSSMParam('gcp-credentials-config', stage);
+	const gcpConfig = await getSSMParam('gcp-credentials-config', stageFromEnvironment());
 	const authClient = await buildAuthClient(gcpConfig);
 	const result = await runQuery(authClient);
 	console.log('result: ', result);
