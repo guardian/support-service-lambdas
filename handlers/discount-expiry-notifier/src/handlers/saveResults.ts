8e61d25590f808eb3b15c2bae4da4ef27c52dede
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-27 14:37:00 +0000

    Update saveResults.ts

--- handlers/discount-expiry-notifier/src/handlers/saveResults.ts
+++ handlers/discount-expiry-notifier/src/handlers/saveResults.ts
@@ -1,8 +1,8 @@
 import { convertArrayToCsv } from '../csv';
 import { uploadFileToS3 } from '../s3';
 
-export const handler = async (event) => {
-    console.log('event:',event);
+export const handler = async () => {
+    // console.log('event:',event);
 	const bucketName = 'discount-expiry-notifier-code';
 
 	const failedRows = [
