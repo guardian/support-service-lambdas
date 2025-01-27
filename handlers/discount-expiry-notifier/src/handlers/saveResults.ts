beb398d039519b75566ee713ad91900620e7a87f
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-27 11:04:54 +0000

    Update saveResults.ts

--- handlers/discount-expiry-notifier/src/handlers/saveResults.ts
+++ handlers/discount-expiry-notifier/src/handlers/saveResults.ts
@@ -1,1 +1,30 @@
-export const handler = () => {};
+import { uploadFileToS3 } from '../s3';
+
+export const handler = async () => {
+
+	const bucketName = 'discount-expiry-notifier-code';
+
+	const failedRows = [
+        {
+            name:'David',
+            subName: 'a-S11111111'
+        },
+        {
+            name:'Rachel',
+            subName: 'a-S22222222'
+        }
+    ];
+
+    const filePath = 'abc/def';
+
+	await uploadFileToS3({
+		bucketName,
+		filePath,
+		content: 'csv contents here...',
+	});
+
+	return {
+		failedRowsCount: failedRows.length,
+		failedRowsFilePath: filePath,
+	};
+};
