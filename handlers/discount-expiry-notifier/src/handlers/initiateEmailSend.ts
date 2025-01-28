8c6e8473e4dde912f94fa98ea298a4bee50b6a22
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-28 14:15:57 +0000

    Update initiateEmailSend.ts

--- handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
+++ handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
@@ -1,41 +1,33 @@
-import type {
-	EmailMessage,
-	EmailMessageWithSfContactId,
-	EmailMessageWithUserId,
-	EmailPayload,
-} from '@modules/email/email';
 import { DataExtensionNames, sendEmail } from '@modules/email/email';
 import { stageFromEnvironment } from '@modules/stage';
 
 export const handler = async () => {
-	const emailPayload: EmailPayload = {
-		Address: 'david.pepper@guardian.co.uk',
-		ContactAttributes: {
-			SubscriberAttributes: {
-				EmailAddress: 'david.pepper@guardian.co.uk',
-				paymentAmount: '70.00',
-				first_name: 'David',
-				date_of_payment: '28 February 2025',
-				paymentFrequency: 'month',
+
+	const emailMessageWithUserId = {
+		...{
+			To: {
+				Address: 'david.pepper@guardian.co.uk',
+				ContactAttributes: {
+					SubscriberAttributes: {
+						EmailAddress: 'david.pepper@guardian.co.uk',
+						paymentAmount: '70.00',
+						first_name: 'David',
+						date_of_payment: '28 February 2025',
+						paymentFrequency: 'month',
+					},
+				},
 			},
+			DataExtensionName: DataExtensionNames.subscriptionCancelledEmail,
 		},
-	};
-	console.log('emailPayload:', emailPayload);
-
-	const emailMessage: EmailMessage = {
-		To: emailPayload,
-		DataExtensionName: DataExtensionNames.subscriptionCancelledEmail,
-	};
-	console.log('emailMessage:', emailMessage);
-
-	const emailMessageWithSfContactId: EmailMessageWithSfContactId = {
-		...emailMessage,
 		SfContactId: '0039E00001HiIGlQAN',
 	};
-	const emailMessageWithUserId: EmailMessageWithUserId =
-		emailMessageWithSfContactId;
 
-	const emailSend = await sendEmail(stageFromEnvironment(), emailMessageWithUserId);
+	const emailSend = await sendEmail(
+		stageFromEnvironment(),
+		emailMessageWithUserId,
+	);
 
 	console.log('emailSend:', emailSend);
+
+	return emailSend;
 };
