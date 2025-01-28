17bac13403f09e1edc76e6e6441d14fa3ab24ec1
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-28 12:29:58 +0000

    Update saveResults.ts

--- handlers/discount-expiry-notifier/src/handlers/saveResults.ts
+++ handlers/discount-expiry-notifier/src/handlers/saveResults.ts
@@ -29,7 +29,7 @@ export const handler = async (event: LambdaInput) => {
 		const year = now.getFullYear();
 		const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
 		const day = String(now.getDate()).padStart(2, '0');
-		
+
 		return `${year}-${month}-${day}`;
 	};
 
