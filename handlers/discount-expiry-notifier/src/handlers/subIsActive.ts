96bcba1bde8dbdf6ba8072977f1d67ada02401df
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-28 14:47:54 +0000

    formatting

--- handlers/discount-expiry-notifier/src/handlers/subIsActive.ts
+++ handlers/discount-expiry-notifier/src/handlers/subIsActive.ts
@@ -2,14 +2,24 @@ import { stageFromEnvironment } from '@modules/stage';
 import { getSubscription } from '@modules/zuora/getSubscription';
 import { ZuoraClient } from '@modules/zuora/zuoraClient';
 
-export const handler = async () => {
+type Item = {
+	subName: string;
+	firstName: string;
+	paymentAmount: number;
+	paymentFrequency: string;
+	nextPaymentDate: string;
+};
+
+export const handler = async (event: Item[]) => {
 	try {
+		console.log('event:', event);
 		//sub name will be passed in via json path in state machine
 		const subName = process.env.SUB_NAME ?? 'A-S00954053';
 
 		const zuoraClient = await ZuoraClient.create(stageFromEnvironment());
 		const getSubResponse = await getSubscription(zuoraClient, subName);
 		return {
+			...event,
 			status: getSubResponse.status,
 		};
 	} catch (error) {
