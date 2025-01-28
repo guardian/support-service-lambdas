89885307d76c0c6f968939da79012688480cbc72
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-28 13:55:34 +0000

    Update discount-expiry-notifier.ts

--- cdk/lib/discount-expiry-notifier.ts
+++ cdk/lib/discount-expiry-notifier.ts
@@ -2,7 +2,13 @@ import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import type { App } from 'aws-cdk-lib';
-import { Effect, Policy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import {
+	Effect,
+	Policy,
+	PolicyStatement,
+	Role,
+	ServicePrincipal,
+} from 'aws-cdk-lib/aws-iam';
 import { Architecture } from 'aws-cdk-lib/aws-lambda';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import {
@@ -212,7 +218,7 @@ export class DiscountExpiryNotifier extends GuStack {
 		});
 
 		initiateEmailSendLambda.role?.attachInlinePolicy(sqsInlinePolicy);
-				
+
 		new StateMachine(this, `${appName}-state-machine-${this.stage}`, {
 			stateMachineName: `${appName}-${this.stage}`,
 			definitionBody: definitionBody,
