7b20dae011cfe96893879274b961f69a8b06d441
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-01-28 11:50:21 +0000

    add email send logic

--- handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
+++ handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
@@ -1,1 +1,41 @@
-export const handler = () => {};
+import type {
+	EmailMessage,
+	EmailMessageWithSfContactId,
+	EmailMessageWithUserId,
+	EmailPayload,
+} from '@modules/email/email';
+import { DataExtensionNames, sendEmail } from '@modules/email/email';
+import { stageFromEnvironment } from '@modules/stage';
+
+export const handler = () => {
+	const emailPayload: EmailPayload = {
+		Address: 'david.pepper@guardian.co.uk',
+		ContactAttributes: {
+			SubscriberAttributes: {
+				EmailAddress: 'david.pepper@guardian.co.uk',
+				paymentAmount: '70.00',
+				first_name: 'David',
+				date_of_payment: '28 February 2025',
+				paymentFrequency: 'month',
+			},
+		},
+	};
+	console.log('emailPayload:', emailPayload);
+
+	const emailMessage: EmailMessage = {
+		To: emailPayload,
+		DataExtensionName: DataExtensionNames.subscriptionCancelledEmail,
+	};
+	console.log('emailMessage:', emailMessage);
+
+	const emailMessageWithSfContactId: EmailMessageWithSfContactId = {
+		...emailMessage,
+		SfContactId: 'someSfContactIdValue',
+	};
+	const emailMessageWithUserId: EmailMessageWithUserId =
+		emailMessageWithSfContactId;
+
+	const emailSend = sendEmail(stageFromEnvironment(), emailMessageWithUserId);
+
+	console.log('emailSend:', emailSend);
+};
