8c1f74d86c269bb1ca6e1c4e49c398615b40ab00
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-01-29 11:52:31 +0000

    Back to returning an asynchronous response

--- handlers/user-benefits/src/benefitsList.ts
+++ handlers/user-benefits/src/benefitsList.ts
@@ -1,9 +1,9 @@
 import { productBenefitMapping } from '@modules/product-benefits/productBenefit';
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const benefitsListHandler = (
+export const benefitsListHandler = async (
 	event: APIGatewayProxyEvent,
-): APIGatewayProxyResult => {
+): Promise<APIGatewayProxyResult> => {
 	console.log(`Input is ${JSON.stringify(event)}`);
 
 	const returnHtml = event.headers.accept?.includes('text/html');
@@ -16,15 +16,15 @@ export const benefitsListHandler = (
 const getHttpResponse = (
 	body: string,
 	contentType?: string,
-): APIGatewayProxyResult => {
-	return {
+): Promise<APIGatewayProxyResult> => {
+	return Promise.resolve({
 		statusCode: 200,
 		headers: {
 			'Content-Type': contentType ?? 'application/json',
 			'Cache-Control': 'max-age=60',
 		},
 		body,
-	};
+	});
 };
 
 const getHtmlBody = (): string => {
