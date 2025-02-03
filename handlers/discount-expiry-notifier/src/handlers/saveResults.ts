4d04a82955397e66ea1d168fb93e03fb49248eb9
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-03 13:48:40 +0000

    add logic for unique file path

--- handlers/discount-expiry-notifier/src/handlers/saveResults.ts
+++ handlers/discount-expiry-notifier/src/handlers/saveResults.ts
@@ -1,43 +1,31 @@
 import { getIfDefined } from '@modules/nullAndUndefined';
 import { uploadFileToS3 } from '../s3';
 
-type ExpiringDiscountToProcess = {
-	firstName: string;
-	nextPaymentDate: string;
-	paymentAmount: number;
-	paymentCurrency: string;
-	paymentFrequency: string;
-	productName: string;
-	sfContactId: string;
-	subName: string;
-	workEmail: string;
-};
-
-type ExpiringDiscountProcessingAttempt = {
-	status: string;
-};
-
-type LambdaInput = {
-	expiringDiscountsToProcess: ExpiringDiscountToProcess[];
-	expiringDiscountProcessingAttempts: ExpiringDiscountProcessingAttempt[];
-};
-
-export const handler = async (event: LambdaInput) => {
+export const handler = async (event: {
+	expiringDiscountsToProcess: Array<{
+		firstName: string;
+		nextPaymentDate: string;
+		paymentAmount: number;
+		paymentCurrency: string;
+		paymentFrequency: string;
+		productName: string;
+		sfContactId: string;
+		subName: string;
+		workEmail: string;
+	}>;
+	expiringDiscountProcessingAttempts: Array<{
+		status: string;
+	}>;
+}) => {
 	const bucketName = getIfDefined<string>(
 		process.env.S3_BUCKET,
 		'S3_BUCKET environment variable not set',
 	);
 
-	const getCurrentDateString = (): string => {
-		const now = new Date();
-		const year = now.getFullYear();
-		const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-		const day = String(now.getDate()).padStart(2, '0');
+	const runForDate = '2024-10-13';
+	const executionDateTime = new Date().toISOString();
 
-		return `${year}-${month}-${day}`;
-	};
-
-	const filePath = getCurrentDateString();
+	const filePath = `${runForDate}/${executionDateTime}`;
 
 	//TODO add a precheck to find out if the file exists already and either append or create a separate file
 	await uploadFileToS3({
