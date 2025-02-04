490bd487b6e78ff3429fba39984ca7a340a5efb0
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-04 15:24:17 +0000

    remove dev testing logic

--- handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
+++ handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
@@ -1,11 +1,5 @@
-import type { SendMessageCommandOutput } from '@aws-sdk/client-sqs';
-import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
-import { awsConfig } from '@modules/aws/config';
-import { DataExtensionNames } from '@modules/email/email';
-import type { EmailMessageWithUserId } from '@modules/email/email';
-import { prettyPrint } from '@modules/prettyPrint';
+import { DataExtensionNames, sendEmail } from '@modules/email/email';
 import { stageFromEnvironment } from '@modules/stage';
-import type { Stage } from '@modules/stage';
 
 export const handler = async (event: {
 	firstName: string;
@@ -40,8 +34,7 @@ export const handler = async (event: {
 		SfContactId: event.sfContactId,
 	};
 	try {
-		const succeed = event.subName === 'A-S00886188';
-		const response = await sendEmail(stageFromEnvironment(), payload, succeed);
+		const response = await sendEmail(stageFromEnvironment(), payload);
 
 		return {
 			detail: event,
@@ -63,28 +56,6 @@ export const handler = async (event: {
 	}
 };
 
-export const sendEmail = async (
-	stage: Stage,
-	emailMessage: EmailMessageWithUserId,
-	succeed: boolean,
-	log: (messsage: string) => void = console.log,
-): Promise<SendMessageCommandOutput> => {
-	const queueName = succeed
-		? `braze-emails-${stage}`
-		: `braze-emails-${stage}-blah`;
-	const client = new SQSClient(awsConfig);
-	log(
-		`Sending email message ${prettyPrint(emailMessage)} to queue ${queueName}`,
-	);
-	const command = new SendMessageCommand({
-		QueueUrl: queueName,
-		MessageBody: JSON.stringify(emailMessage),
-	});
-
-	const response = await client.send(command);
-	log(`Response from email send was ${prettyPrint(response)}`);
-	return response;
-};
 function formatDate(inputDate: string): string {
 	return new Date(inputDate).toLocaleDateString('en-GB', {
 		day: '2-digit',
