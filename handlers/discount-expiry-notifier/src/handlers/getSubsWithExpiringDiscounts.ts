9523c7d4710d329dbccf877a6bfeaca6d27f7630
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-04 17:00:19 +0000

    Update getSubsWithExpiringDiscounts.ts

--- handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
+++ handlers/discount-expiry-notifier/src/handlers/getSubsWithExpiringDiscounts.ts
@@ -2,6 +2,7 @@ import { getIfDefined } from '@modules/nullAndUndefined';
 import { stageFromEnvironment } from '@modules/stage';
 import { buildAuthClient, runQuery } from '../bigquery';
 import { getSSMParam } from '../ssm';
+import { testQueryResponse } from '../testQueryResponse';
 
 //to manually run the state machine for a specified discount expiry date, enter {"discountExpiresOnDate":"2025-11-23"} in aws console
 export const handler = async (event: { discountExpiresOnDate?: string }) => {
@@ -17,10 +18,10 @@ export const handler = async (event: { discountExpiresOnDate?: string }) => {
 
 	const query = getQuery(discountExpiresOnDate);
 	const result = await runQuery(authClient, query);
-
+	console.log('result', result);
 	return {
 		discountExpiresOnDate,
-		expiringDiscountsToProcess: result,
+		expiringDiscountsToProcess: testQueryResponse,
 	};
 };
 
