f050eb988b3d6be7c1f3f16711492ab77c475888
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-04 11:23:25 +0000

    Update initiateEmailSend.ts

--- handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
+++ handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
@@ -1,5 +1,12 @@
-import { DataExtensionNames, sendEmail } from '@modules/email/email';
+import type { SendMessageCommandOutput } from '@aws-sdk/client-sqs';
+import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
+import { awsConfig } from '@modules/aws/config';
+import { DataExtensionNames } from '@modules/email/email';
+import type { EmailMessageWithUserId } from '@modules/email/email';
+import { prettyPrint } from '@modules/prettyPrint';
 import { stageFromEnvironment } from '@modules/stage';
+import type { Stage } from '@modules/stage';
+import { CustomError } from '../CustomError';
 
 export const handler = async (event: {
 	firstName: string;
@@ -17,10 +24,10 @@ export const handler = async (event: {
 	const payload = {
 		...{
 			To: {
-				Address: 'david.pepper@guardian.co.uk', // hard code this during testing
+				Address: event.workEmail, // hard code this during testing
 				ContactAttributes: {
 					SubscriberAttributes: {
-						EmailAddress: 'david.pepper@guardian.co.uk', // hard code this during testing
+						EmailAddress: event.workEmail, // hard code this during testing
 						paymentAmount: `${currencySymbol}${event.paymentAmount}`,
 						first_name: event.firstName,
 						date_of_payment: formatDate(event.nextPaymentDate),
@@ -33,12 +40,23 @@ export const handler = async (event: {
 		},
 		SfContactId: event.sfContactId,
 	};
+	try {
+		const emailSend = await sendEmail(stageFromEnvironment(), payload);
 
-	const emailSend = await sendEmail(stageFromEnvironment(), payload);
+		console.log('emailSend:', emailSend);
 
-	console.log('emailSend:', emailSend);
+		return emailSend;
+	} catch (error) {
+		// throw new Error(
+		// 	`Error initiating email send in membership workflow: ${JSON.stringify(error)}`,
 
-	return emailSend;
+		// );
+		throw new CustomError(
+			'SubscriptionService',
+			payload,
+			JSON.stringify(error),
+		);
+	}
 };
 
 function formatDate(inputDate: string): string {
@@ -60,3 +78,23 @@ function getCurrencySymbol(currencyCode: string): string {
 	};
 	return symbols[currencyCode] ?? '';
 }
+
+export const sendEmail = async (
+	stage: Stage,
+	emailMessage: EmailMessageWithUserId,
+	log: (messsage: string) => void = console.log,
+): Promise<SendMessageCommandOutput> => {
+	const queueName = `braze-emails-${stage}-'blah`;
+	const client = new SQSClient(awsConfig);
+	log(
+		`Sending email message ${prettyPrint(emailMessage)} to queue ${queueName}`,
+	);
+	const command = new SendMessageCommand({
+		QueueUrl: queueName,
+		MessageBody: JSON.stringify(emailMessage),
+	});
+
+	const response = await client.send(command);
+	log(`Response from email send was ${prettyPrint(response)}`);
+	return response;
+};
