8fc88bb1fd53f834d394796b037da228354f9133
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-05 15:35:08 +0000

    update test query response with countries

--- handlers/discount-expiry-notifier/src/testQueryResponse.ts
+++ handlers/discount-expiry-notifier/src/testQueryResponse.ts
@@ -9,6 +9,10 @@ export const testQueryResponse = [
 		sfContactId: '0039E00001HiIGlQAN',
 		subName: 'A-S00886188', // Active sub in dev sandbox
 		workEmail: 'david.pepper+david@guardian.co.uk',
+		sfBuyerContactMailingCountry: 'United Kingdom',
+		sfBuyerContactOtherCountry: 'United Kingdom',
+		sfRecipientContactMailingCountry: 'United Kingdom',
+		sfRecipientContactOtherCountry: 'United Kingdom',
 	},
 	{
 		firstName: 'Mary',
@@ -20,6 +24,10 @@ export const testQueryResponse = [
 		sfContactId: '0039E00001HwhM9QAJ',
 		subName: 'A-S00886159', // Active subscription in dev sandbox
 		workEmail: 'graham.hopgood@guardian.co.uk',
+		sfBuyerContactMailingCountry: 'US',
+		sfBuyerContactOtherCountry: 'US',
+		sfRecipientContactMailingCountry: 'US',
+		sfRecipientContactOtherCountry: 'US',
 	},
 	{
 		firstName: 'Robert',
@@ -31,5 +39,9 @@ export const testQueryResponse = [
 		sfContactId: '0039E00001lNoXrQAK',
 		subName: 'A-S00515481', // cancelled subscription in dev sandbox
 		workEmail: 'david.pepper+3003+1@guardian.co.uk',
+		sfBuyerContactMailingCountry: 'Germany',
+		sfBuyerContactOtherCountry: 'Germany',
+		sfRecipientContactMailingCountry: 'Germany',
+		sfRecipientContactOtherCountry: 'Germany',
 	},
 ];
