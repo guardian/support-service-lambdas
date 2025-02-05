987f6acb87cc8cef687ea1983c9d8128ea143377
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-05 10:42:05 +0000

    revert cdk changes and fix syntax of lambda input

--- handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
+++ handlers/discount-expiry-notifier/src/handlers/initiateEmailSend.ts
@@ -1,22 +1,24 @@
 import { DataExtensionNames, sendEmail } from '@modules/email/email';
 import { stageFromEnvironment } from '@modules/stage';
 
-export const handler = async (event: {
-	firstName: string;
-	nextPaymentDate: string;
-	paymentAmount: number;
-	paymentCurrency: string;
-	paymentFrequency: string;
-	productName: string;
-	sfContactId: string;
-	subName: string;
-	workEmail: string;
-	subStatus: string;
+export const handler = async (event: { 
+	item: {	
+		firstName: string;
+		nextPaymentDate: string;
+		paymentAmount: number;
+		paymentCurrency: string;
+		paymentFrequency: string;
+		productName: string;
+		sfContactId: string;
+		subName: string;
+		workEmail: string;
+		subStatus: string;
+	};
 }) => {
 	console.log('event:', event);
-	// console.log('event.firstName:', event.firstName);
-	// console.log('event.subStatus:', event.subStatus);
-	if (event.subStatus === 'Cancelled') {
+	console.log('event.item.firstName:', event.item.firstName);
+	console.log('event.item.subStatus:', event.item.subStatus);
+	if (event.item.subStatus === 'Cancelled') {
 		return {
 			detail: event,
 			emailSendAttempt: {
@@ -26,26 +28,26 @@ export const handler = async (event: {
 			},
 		};
 	}
-	const currencySymbol = getCurrencySymbol(event.paymentCurrency);
+	const currencySymbol = getCurrencySymbol(event.item.paymentCurrency);
 
 	const payload = {
 		...{
 			To: {
-				Address: event.workEmail,
+				Address: event.item.workEmail,
 				ContactAttributes: {
 					SubscriberAttributes: {
-						EmailAddress: event.workEmail,
-						paymentAmount: `${currencySymbol}${event.paymentAmount}`,
-						first_name: event.firstName,
-						date_of_payment: formatDate(event.nextPaymentDate),
-						paymentFrequency: event.paymentFrequency,
+						EmailAddress: event.item.workEmail,
+						paymentAmount: `${currencySymbol}${event.item.paymentAmount}`,
+						first_name: event.item.firstName,
+						date_of_payment: formatDate(event.item.nextPaymentDate),
+						paymentFrequency: event.item.paymentFrequency,
 					},
 				},
 			},
 			// subscriptionCancelledEmail used for testing. will update data extension to active one when published
 			DataExtensionName: DataExtensionNames.subscriptionCancelledEmail,
 		},
-		SfContactId: event.sfContactId,
+		SfContactId: event.item.sfContactId,
 	};
 
 	const emailSendAttempt = await sendEmail(stageFromEnvironment(), payload);
