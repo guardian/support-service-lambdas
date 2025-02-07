e07f137744402a9e4a7982c8fb64e74c67b3769e
Tom Wey <tom.wey@theguardian.com>
2025-02-07 15:21:03 +0000

    Add simple routing/request handling

--- handlers/metric-push-api/src/index.ts
+++ handlers/metric-push-api/src/index.ts
@@ -1,12 +1,17 @@
-import type {
-	APIGatewayProxyEvent,
-	APIGatewayProxyResult,
-	Handler,
-} from 'aws-lambda';
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { Router } from '@modules/routing/router';
 
-export const handler: Handler = async (
+const router = new Router([
+	{
+		httpMethod: 'GET',
+		path: '/',
+		handler: (event: APIGatewayProxyEvent) => {
+			console.log(`Input is ${JSON.stringify(event)}`);
+			return Promise.resolve({ statusCode: 200, body: '' });
+		},
+	},
+]);
+
+export const handler = async (
 	event: APIGatewayProxyEvent,
-): Promise<APIGatewayProxyResult> => {
-	console.log(`Input is ${JSON.stringify(event)}`);
-	return Promise.resolve({ statusCode: 200, body: '' });
-};
+): Promise<APIGatewayProxyResult> => router.routeRequest(event);
