26518155e12cd8a343ee6e1af93f81f1a66bc3c7
Tom Wey <tom.wey@theguardian.com>
2025-02-07 15:38:49 +0000

    Distinguish between valid and non-valid referrers

--- handlers/metric-push-api/src/index.ts
+++ handlers/metric-push-api/src/index.ts
@@ -1,13 +1,27 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { Router } from '@modules/routing/router';
 
+const validReferers = [
+	'https://support.thegulocal.com/',
+	'https://support.code.dev-theguardian.com/',
+	'https://support.theguardian.com/',
+];
+
 const router = new Router([
 	{
 		httpMethod: 'GET',
 		path: '/',
 		handler: (event: APIGatewayProxyEvent) => {
 			console.log(`Input is ${JSON.stringify(event)}`);
-			return Promise.resolve({ statusCode: 200, body: '' });
+
+			if (
+				event.headers?.referer &&
+				validReferers.includes(event.headers.referer)
+			) {
+				return Promise.resolve({ statusCode: 201, body: '' });
+			} else {
+				return Promise.resolve({ statusCode: 204, body: '' });
+			}
 		},
 	},
 ]);
