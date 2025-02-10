78a55f4e978f792138bc36eb29b5ff2c0919ddd5
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-10 11:19:24 +0000

    Update filterSubs.test.ts

--- handlers/discount-expiry-notifier/test/handlers/filterSubs.test.ts
+++ handlers/discount-expiry-notifier/test/handlers/filterSubs.test.ts
@@ -44,33 +44,33 @@ describe('filterSubs handler', () => {
 		expect(result.filteredSubs.some((sub) => sub.zuoraSubName === 'A-S00515481'),).toBe(false);
 	});
 
-	// it('should return an empty array if no subscriptions match the regions', async () => {
-	// 	(getIfDefined as jest.Mock).mockReturnValue('UK');
+	it('should return an empty array if no subscriptions match the regions', async () => {
+		(getIfDefined as jest.Mock).mockReturnValue('UK');
 
-	// 	const event = {
-	// 		discountExpiresOnDate: '2024-03-21',
-	// 		expiringDiscountsToProcess: testQueryResponse,
-	// 	};
+		const event = {
+			discountExpiresOnDate: '2024-03-21',
+			expiringDiscountsToProcess: testQueryResponse,
+		};
 
-	// 	const result = await handler(event);
+		const result = await handler(event);
 
-	// 	expect(result).toBeDefined();
-	// 	expect(result.filteredSubs).toBeInstanceOf(Array);
-	// 	expect(result.filteredSubs.length).toBe(0);
-	// });
+		expect(result).toBeDefined();
+		expect(result.filteredSubs).toBeInstanceOf(Array);
+		expect(result.filteredSubs.length).toBe(0);
+	});
 
-	// it('should throw an error if FILTER_BY_REGIONS is not set', async () => {
-	// 	(getIfDefined as jest.Mock).mockImplementation(() => {
-	// 		throw new Error('FILTER_BY_REGIONS environment variable not set');
-	// 	});
+	it('should throw an error if FILTER_BY_REGIONS is not set', async () => {
+		(getIfDefined as jest.Mock).mockImplementation(() => {
+			throw new Error('FILTER_BY_REGIONS environment variable not set');
+		});
 
-	// 	const event = {
-	// 		discountExpiresOnDate: '2024-03-21',
-	// 		expiringDiscountsToProcess: testQueryResponse,
-	// 	};
+		const event = {
+			discountExpiresOnDate: '2024-03-21',
+			expiringDiscountsToProcess: testQueryResponse,
+		};
 
-	// 	await expect(handler(event)).rejects.toThrow(
-	// 		'FILTER_BY_REGIONS environment variable not set',
-	// 	);
-	// });
+		await expect(handler(event)).rejects.toThrow(
+			'FILTER_BY_REGIONS environment variable not set',
+		);
+	});
 });
