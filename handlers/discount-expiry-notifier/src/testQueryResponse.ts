1c51732c7f416bf71b3b8e8019ca81607e8dc9c2
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-12 14:48:39 +0000

    Update testQueryResponse.ts

--- handlers/discount-expiry-notifier/src/testQueryResponse.ts
+++ handlers/discount-expiry-notifier/src/testQueryResponse.ts
@@ -6,9 +6,9 @@ export const testQueryResponse = [
 		paymentCurrency: 'GBP',
 		paymentFrequency: 'Month',
 		productName: 'GW',
-		sfContactId: '0039E00001HiIGlQAN',
-		zuoraSubName: 'A-S00886188', // Active sub in dev sandbox
-		workEmail: 'david.pepper+david@guardian.co.uk',
+		sfContactId: '0030J000022u2hrQAA',
+		zuoraSubName: 'A-S02360124', // Active sub in dev sandbox
+		workEmail: 'david.pepper@guardian.co.uk',
 		contactCountry: 'united states of america',
 		sfBuyerContactMailingCountry: 'united states of america',
 		sfBuyerContactOtherCountry: 'united states of america',
@@ -16,14 +16,14 @@ export const testQueryResponse = [
 		sfRecipientContactOtherCountry: 'united states of america',
 	},
 	{
-		firstName: 'Mary',
+		firstName: 'Graham',
 		nextPaymentDate: '2025-02-28',
 		paymentAmount: 4.56,
 		paymentCurrency: 'USD',
 		paymentFrequency: 'Month',
 		productName: 'Supporter Plus',
-		sfContactId: '0039E00001HwhM9QAJ',
-		zuoraSubName: 'A-S00886159', // Active subscription in dev sandbox
+		sfContactId: '0035I00000UUFyHQAX	',
+		zuoraSubName: 'A-S02360127', // Active subscription in dev sandbox
 		workEmail: 'graham.hopgood@guardian.co.uk',
 		contactCountry: 'United States',
 		sfBuyerContactMailingCountry: 'United States',
@@ -31,20 +31,20 @@ export const testQueryResponse = [
 		sfRecipientContactMailingCountry: 'United States',
 		sfRecipientContactOtherCountry: 'United States',
 	},
-	{
-		firstName: 'Robert',
-		nextPaymentDate: '2025-02-28',
-		paymentAmount: 7.89,
-		paymentCurrency: 'USD',
-		paymentFrequency: 'Month',
-		productName: 'Supporter Plus',
-		sfContactId: '0039E00001lNoXrQAK',
-		zuoraSubName: 'A-S00515481', // cancelled subscription in dev sandbox
-		workEmail: 'david.pepper+3003+1@guardian.co.uk',
-		contactCountry: 'Germany',
-		sfBuyerContactMailingCountry: 'Germany',
-		sfBuyerContactOtherCountry: 'Germany',
-		sfRecipientContactMailingCountry: 'Germany',
-		sfRecipientContactOtherCountry: 'Germany',
-	},
+	// {
+	// 	firstName: 'Robert',
+	// 	nextPaymentDate: '2025-02-28',
+	// 	paymentAmount: 7.89,
+	// 	paymentCurrency: 'USD',
+	// 	paymentFrequency: 'Month',
+	// 	productName: 'Supporter Plus',
+	// 	sfContactId: '0039E00001lNoXrQAK',
+	// 	zuoraSubName: 'A-S00515481', // cancelled subscription in dev sandbox
+	// 	workEmail: 'david.pepper+3003+1@guardian.co.uk',
+	// 	contactCountry: 'Germany',
+	// 	sfBuyerContactMailingCountry: 'Germany',
+	// 	sfBuyerContactOtherCountry: 'Germany',
+	// 	sfRecipientContactMailingCountry: 'Germany',
+	// 	sfRecipientContactOtherCountry: 'Germany',
+	// },
 ];
