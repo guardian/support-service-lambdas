c05aee02057c13e7f11a28dee9c2c36f7858919a
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-24 16:24:50 +0000

    comment out sendEmail command

--- handlers/discount-expiry-notifier/src/handlers/sendEmail.ts
+++ handlers/discount-expiry-notifier/src/handlers/sendEmail.ts
@@ -1,5 +1,6 @@
-import { DataExtensionNames, sendEmail } from '@modules/email/email';
-import { stageFromEnvironment } from '@modules/stage';
+// import { DataExtensionNames, sendEmail } from '@modules/email/email';
+import { DataExtensionNames } from '@modules/email/email';
+// import { stageFromEnvironment } from '@modules/stage';
 import type { z } from 'zod';
 import { BaseRecordForEmailSendSchema } from '../types';
 
@@ -51,11 +52,11 @@ export const handler = async (event: SendEmailInput) => {
 	};
 
 	try {
-		const response = await sendEmail(stageFromEnvironment(), request);
+		// const response = await sendEmail(stageFromEnvironment(), request);
 
-		if (response.$metadata.httpStatusCode !== 200) {
-			throw new Error('Failed to send email');
-		}
+		// if (response.$metadata.httpStatusCode !== 200) {
+		// 	throw new Error('Failed to send email');
+		// }
 		return {
 			record: parsedEvent,
 			emailSendEligibility,
