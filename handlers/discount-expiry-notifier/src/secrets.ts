c64eba08d8170e80bb665fed79421a4659e55554
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-26 14:43:18 +0000

    Delete secrets.ts

--- handlers/discount-expiry-notifier/src/secrets.ts
+++
@@ -1,9 +0,0 @@
-import type { Stage } from '@modules/stage';
-
-export function getZuoraSecretName(stage: Stage): string {
-	return `${stage}/Zuora-OAuth/SupportServiceLambdas`;
-}
-export type ZuoraSecret = {
-	clientId: string;
-	clientSecret: string;
-};
