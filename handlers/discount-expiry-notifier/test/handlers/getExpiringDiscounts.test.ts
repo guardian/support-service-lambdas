981ca20bc88f053df603ecf682d2f0dd8120c69b
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-02-27 15:38:19 +0000

    Update getExpiringDiscounts.test.ts

--- handlers/discount-expiry-notifier/test/handlers/getExpiringDiscounts.test.ts
+++ handlers/discount-expiry-notifier/test/handlers/getExpiringDiscounts.test.ts
@@ -1,8 +1,7 @@
 import { getSSMParam } from '@modules/aws/ssm';
 import { buildAuthClient, runQuery } from '@modules/bigquery/src/bigquery';
 import { stageFromEnvironment } from '@modules/stage';
-// import { addDays, handler } from '../../src/handlers/getExpiringDiscounts';
-import { handler } from '../../src/handlers/getExpiringDiscounts';
+import { addDays, handler } from '../../src/handlers/getExpiringDiscounts';
 import { testQueryResponse } from '../../src/testQueryResponse';
 
 jest.mock('@modules/bigquery/src/bigquery');
@@ -43,46 +42,49 @@ describe('getExpiringDiscounts handler', () => {
 		});
 	});
 
-	// test('should return results when discountExpiresOnDate is not provided', async () => {
-	// 	const event = {};
-	// 	const mockDate = new Date(2025, 10, 23);
-	// 	jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
-	// 	const result = await handler(event);
+	test('should return results when discountExpiresOnDate is not provided', async () => {
+		const event = {};
+		const mockDate = new Date(2025, 10, 23);
+		jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
+		const result = await handler(event);
 
-	// 	expect(getSSMParam).toHaveBeenCalledWith(
-	// 		'gcp-credentials-config',
-	// 		'test-stage',
-	// 	);
-	// 	expect(buildAuthClient).toHaveBeenCalledWith(mockGcpConfig);
-	// 	expect(runQuery).toHaveBeenCalledWith(mockAuthClient, expect.any(String));
-	// 	expect(result).toEqual({
-	// 		discountExpiresOnDate: '2025-12-25',
-	// 		allRecordsFromBigQueryCount: 2,
-	// 		allRecordsFromBigQuery: mockQueryResult,
-	// 	});
-	// });
+		expect(getSSMParam).toHaveBeenCalledWith(
+			'/discount-expiry-notifier/test-stage/gcp-credentials-config',
+		);
+		expect(buildAuthClient).toHaveBeenCalledWith(mockGcpConfig);
+		expect(runQuery).toHaveBeenCalledWith(
+			mockAuthClient,
+			expect.any(String),
+			expect.any(String),
+		);
+		expect(result).toEqual({
+			discountExpiresOnDate: '2025-12-25',
+			allRecordsFromBigQueryCount: 3,
+			allRecordsFromBigQuery: mockQueryResult,
+		});
+	});
 
-	// test('should throw an error when an exception occurs', async () => {
-	// 	const event = { discountExpiresOnDate: '2025-11-23' };
-	// 	(runQuery as jest.Mock).mockRejectedValue(new Error('Query failed'));
+	test('should throw an error when an exception occurs', async () => {
+		const event = { discountExpiresOnDate: '2025-11-23' };
+		(runQuery as jest.Mock).mockRejectedValue(new Error('Query failed'));
 
-	// 	await expect(handler(event)).rejects.toThrow('Query failed');
-	// });
+		await expect(handler(event)).rejects.toThrow('Query failed');
+	});
 });
 
-// describe('addDays function', () => {
-// 	beforeEach(() => {
-// 		jest.restoreAllMocks();
-// 	});
-// 	test('should add days to the given date', () => {
-// 		const date = new Date('2025-11-23');
-// 		const result = addDays(date, 10);
-// 		expect(result).toBe('2025-12-03');
-// 	});
+describe('addDays function', () => {
+	beforeEach(() => {
+		jest.restoreAllMocks();
+	});
+	test('should add days to the given date', () => {
+		const date = new Date('2025-11-23');
+		const result = addDays(date, 10);
+		expect(result).toBe('2025-12-03');
+	});
 
-// 	test('should handle leap year correctly', () => {
-// 		const date = new Date('2024-02-28');
-// 		const result = addDays(date, 1);
-// 		expect(result).toBe('2024-02-29');
-// 	});
-// });
+	test('should handle leap year correctly', () => {
+		const date = new Date('2024-02-28');
+		const result = addDays(date, 1);
+		expect(result).toBe('2024-02-29');
+	});
+});
