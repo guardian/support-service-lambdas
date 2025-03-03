13a1e1f4e27065530cd5ec4b3c9d395f1ebaaaaa
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-03-03 16:20:43 +0000

    Update getOldPaymentAmount.ts

--- handlers/discount-expiry-notifier/src/handlers/getOldPaymentAmount.ts
+++ handlers/discount-expiry-notifier/src/handlers/getOldPaymentAmount.ts
@@ -1,8 +1,6 @@
 import { stageFromEnvironment } from '@modules/stage';
 import { doQuery } from '@modules/zuora/query';
 import { ZuoraClient } from '@modules/zuora/zuoraClient';
-import type { InvoiceItem } from '@modules/zuora/zuoraSchemas';
-import dayjs from 'dayjs';
 import type { z } from 'zod';
 import { BaseRecordForEmailSendSchema } from '../types';
 
@@ -29,20 +27,4 @@ export const handler = async (event: GetOldPaymentAmountInput) => {
 				error instanceof Error ? error.message : JSON.stringify(error, null, 2),
 		};
 	}
-};
-
-const filterRecords = (
-	invoiceItems: InvoiceItem[],
-	subscriptionName: string,
-	nextPaymentDate: string,
-): InvoiceItem[] => {
-	return invoiceItems.filter(
-		(item) =>
-			item.subscriptionName === subscriptionName &&
-			dayjs(item.serviceStartDate).isSame(dayjs(nextPaymentDate), 'day'),
-	);
-};
-
-const getNewPaymentAmount = (invoiceItems: InvoiceItem[]): number => {
-	return invoiceItems.reduce((total, item) => total + item.paymentAmount, 0);
 };
