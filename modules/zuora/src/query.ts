2d89fc48f7385aafaeb037d97b54cfb072c9237a
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-03-03 16:44:42 +0000

    syntax fix

--- modules/zuora/src/query.ts
+++ modules/zuora/src/query.ts
@@ -7,7 +7,7 @@ export const doQuery = async (zuoraClient: ZuoraClient): Promise<void> => {
 		'/v1/action/query',
 		JSON.stringify({
 			queryString:
-				"SELECT ChargeAmount, TaxAmount, ServiceStartDate, SubscriptionNumber FROM InvoiceItem WHERE SubscriptionNumber = 'A-S00424163' and ChargeName!='Delivery-problem credit' and ChargeName!='Holiday Credit' and ServiceStartDate = '2025-02-22'' ",
+				"SELECT ChargeAmount, TaxAmount, ServiceStartDate, SubscriptionNumber FROM InvoiceItem WHERE SubscriptionNumber = 'A-S00424163' and ChargeName!='Delivery-problem credit' and ChargeName!='Holiday Credit' and ServiceStartDate = '2025-02-22' ",
 		}),
 		invoiceItemResponseSchema,
 	);
