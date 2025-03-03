ecae6b7e9749da7a456bc783022687f9c304ed0f
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-03-03 10:36:40 +0000

    update test data

--- handlers/discount-expiry-notifier/src/handlers/getExpiringDiscounts.ts
+++ handlers/discount-expiry-notifier/src/handlers/getExpiringDiscounts.ts
@@ -2,6 +2,7 @@ import { getSSMParam } from '@modules/aws/ssm';
 import { buildAuthClient, runQuery } from '@modules/bigquery/src/bigquery';
 import { getIfDefined } from '@modules/nullAndUndefined';
 import { stageFromEnvironment } from '@modules/stage';
+import { functionalTestQueryResponse } from '../../test/handlers/data/functionalTestQueryResponse';
 import { BigQueryResultDataSchema } from '../types';
 
 //to manually run the state machine for a specified discount expiry date, enter {"discountExpiresOnDate":"2025-11-23"} in aws console
@@ -25,10 +26,14 @@ export const handler = async (event: { discountExpiresOnDate?: string }) => {
 		const resultData = BigQueryResultDataSchema.parse(result[0]);
 		console.log('resultData', resultData);
 
+		const records =
+			stageFromEnvironment() === 'PROD'
+				? resultData
+				: functionalTestQueryResponse;
 		return {
 			discountExpiresOnDate,
-			allRecordsFromBigQueryCount: resultData.length,
-			allRecordsFromBigQuery: resultData,
+			allRecordsFromBigQueryCount: records.length,
+			allRecordsFromBigQuery: records,
 		};
 	} catch (error) {
 		console.error('Error:', error);
