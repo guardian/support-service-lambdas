e304cbe20cd1fb42e8a0e3d54103e85efda37dd2
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-03-04 08:56:24 +0000

    generate old payment amount

--- handlers/discount-expiry-notifier/src/handlers/getOldPaymentAmount.ts
+++ handlers/discount-expiry-notifier/src/handlers/getOldPaymentAmount.ts
@@ -1,21 +1,32 @@
 import { stageFromEnvironment } from '@modules/stage';
 import { doQuery } from '@modules/zuora/query';
 import { ZuoraClient } from '@modules/zuora/zuoraClient';
+import type { InvoiceItemRecord } from '@modules/zuora/zuoraSchemas';
 import type { z } from 'zod';
 import { BaseRecordForEmailSendSchema } from '../types';
 
 export type GetOldPaymentAmountInput = z.infer<
 	typeof BaseRecordForEmailSendSchema
 >;
-
+const calculateTotalAmount = (records: InvoiceItemRecord[]) => {
+	return records.reduce(
+		(total, record) => total + record.ChargeAmount + record.TaxAmount,
+		0,
+	);
+};
 export const handler = async (event: GetOldPaymentAmountInput) => {
 	try {
 		const parsedEvent = BaseRecordForEmailSendSchema.parse(event);
 		const zuoraClient = await ZuoraClient.create(stageFromEnvironment());
 		const getInvoiceItemsResponse = await doQuery(zuoraClient);
+		const oldPaymentAmount = calculateTotalAmount(
+			getInvoiceItemsResponse.records,
+		);
+
 		console.log('getInvoiceItemsResponse:', getInvoiceItemsResponse);
 		return {
 			...parsedEvent,
+			oldPaymentAmount,
 		};
 	} catch (error) {
 		console.log('error:', error);
