c932919563dc4fad75206e220518c04721b383f2
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2025-03-13 16:58:46 +0000

    infra: 20 max items per batch

--- cdk/lib/write-off-unpaid-invoices.ts
+++ cdk/lib/write-off-unpaid-invoices.ts
@@ -85,7 +85,7 @@ export class WriteOffUnpaidInvoices extends GuStack {
 						},
 					},
 					ItemBatcher: {
-						MaxItemsPerBatch: 1,
+						MaxItemsPerBatch: 20,
 					},
 					ItemProcessor: {
 						ProcessorConfig: {
