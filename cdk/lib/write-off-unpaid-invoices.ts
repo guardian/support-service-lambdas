e9c26328fbc491a16ce5e8701b3c5d36386352c2
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2025-03-17 12:04:14 +0000

    infra: increase batch item max and lambda max execution timeout

--- cdk/lib/write-off-unpaid-invoices.ts
+++ cdk/lib/write-off-unpaid-invoices.ts
@@ -85,7 +85,7 @@ export class WriteOffUnpaidInvoices extends GuStack {
 						},
 					},
 					ItemBatcher: {
-						MaxItemsPerBatch: 250,
+						MaxItemsPerBatch: 300,
 					},
 					ItemProcessor: {
 						ProcessorConfig: {
