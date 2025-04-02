0c01b212344dfc8e140e4d8e5fa48ca9116588b9
Michael Jacobson <michael.jacobson@guardian.co.uk>
2025-04-02 19:47:23 +0100

    recreate existing CFN stack in new Typescript file using CDK

--- cdk/lib/soft-opt-in-consent-setter.ts
+++ cdk/lib/soft-opt-in-consent-setter.ts
@@ -1,19 +1,392 @@
-import { join } from 'path';
+import { GuScheduledLambda } from '@guardian/cdk';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import type { App } from 'aws-cdk-lib';
-import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
+import {
+	aws_dynamodb,
+	aws_lambda,
+	CfnCondition,
+	Duration,
+	Fn,
+	Tags,
+} from 'aws-cdk-lib';
+import { CfnAlarm } from 'aws-cdk-lib/aws-cloudwatch';
+import { Schedule } from 'aws-cdk-lib/aws-events';
+import {
+	AccountPrincipal,
+	Effect,
+	ManagedPolicy,
+	PolicyDocument,
+	PolicyStatement,
+	Role,
+	ServicePrincipal,
+} from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import type { IConstruct } from 'constructs';
+
+export interface SoftOptInConsentSetterProps extends GuStackProps {
+	mobileAccountIdSSMParam: string;
+	schedule: string;
+}
 
 export class SoftOptInConsentSetter extends GuStack {
-	constructor(scope: App, id: string, props: GuStackProps) {
+	constructor(scope: App, id: string, props: SoftOptInConsentSetterProps) {
 		super(scope, id, props);
-		const yamlTemplateFilePath = join(
-			__dirname,
-			'../..',
-			'/handlers/soft-opt-in-consent-setter/cfn.yaml',
+
+		// SSM Params
+		const mobileAccountId = StringParameter.fromStringParameterName(
+			this,
+			'MobileAccountId',
+			props.mobileAccountIdSSMParam,
+		).stringValue;
+
+		// Conditions
+		const isProd = new CfnCondition(this, 'IsProd', {
+			expression: Fn.conditionEquals(this.stage, 'PROD'),
+		});
+
+		// SQS Queues
+		const softOptInsDeadLetterQueue = new Queue(
+			this,
+			'SoftOptInsDeadLetterQueue',
+			{
+				queueName: `soft-opt-in-consent-setter-dead-letter-queue-${this.stage}`,
+				retentionPeriod: Duration.seconds(864000),
+			},
 		);
-		new CfnInclude(this, 'YamlTemplate', {
-			templateFile: yamlTemplateFilePath,
+		const softOptInsQueue = new Queue(this, 'SoftOptInsQueue', {
+			queueName: `soft-opt-in-consent-setter-queue-${this.stage}`,
+			visibilityTimeout: Duration.seconds(3000),
+			deadLetterQueue: {
+				maxReceiveCount: 3,
+				queue: softOptInsDeadLetterQueue,
+			},
+		});
+
+		// IAM Roles
+		new Role(this, 'SoftOptInsQueueCrossAccountRole', {
+			roleName: `${this.stackName}-QueueCrossAccountRole`,
+			assumedBy: new AccountPrincipal(mobileAccountId),
+			inlinePolicies: {
+				SQSAccess: new PolicyDocument({
+					statements: [
+						new PolicyStatement({
+							actions: [
+								'sqs:SendMessage',
+								'sqs:ReceiveMessage',
+								'sqs:DeleteMessage',
+								'sqs:GetQueueAttributes',
+							],
+							resources: [softOptInsQueue.queueArn],
+							effect: Effect.ALLOW,
+						}),
+					],
+				}),
+			},
+		});
+
+		const lambdaFunctionRole = new Role(this, 'LambdaFunctionRole', {
+			assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+			managedPolicies: [
+				ManagedPolicy.fromAwsManagedPolicyName(
+					'service-role/AWSLambdaBasicExecutionRole',
+				),
+			],
+		});
+		lambdaFunctionRole.addToPolicy(
+			new PolicyStatement({
+				actions: ['cloudwatch:PutMetricData'],
+				resources: ['*'],
+			}),
+		);
+		lambdaFunctionRole.addToPolicy(
+			new PolicyStatement({
+				sid: 'readDeployedArtefact',
+				actions: ['s3:GetObject'],
+				resources: ['arn:aws:s3::*:membership-dist/*'],
+			}),
+		);
+
+		const lambdaFunctionIAPRole = new Role(this, 'LambdaFunctionIAPRole', {
+			assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+			managedPolicies: [
+				ManagedPolicy.fromAwsManagedPolicyName(
+					'service-role/AWSLambdaBasicExecutionRole',
+				),
+			],
+		});
+		lambdaFunctionIAPRole.addToPolicy(
+			new PolicyStatement({
+				actions: ['dynamodb:PutItem'],
+				resources: [
+					`arn:aws:dynamodb:${this.region}:${this.account}:table/soft-opt-in-consent-setter-${this.stage}-logging`,
+				],
+			}),
+		);
+		lambdaFunctionIAPRole.addToPolicy(
+			new PolicyStatement({
+				actions: ['cloudwatch:PutMetricData'],
+				resources: ['*'],
+			}),
+		);
+		lambdaFunctionIAPRole.addToPolicy(
+			new PolicyStatement({
+				actions: ['s3:GetObject'],
+				resources: ['arn:aws:s3::*:membership-dist/*'],
+			}),
+		);
+		lambdaFunctionIAPRole.addToPolicy(
+			new PolicyStatement({
+				actions: [
+					'sqs:DeleteMessage',
+					'sqs:GetQueueAttributes',
+					'sqs:ReceiveMessage',
+				],
+				resources: [softOptInsQueue.queueArn],
+			}),
+		);
+		lambdaFunctionIAPRole.addToPolicy(
+			new PolicyStatement({
+				actions: [
+					'secretsmanager:DescribeSecret',
+					'secretsmanager:GetSecretValue',
+				],
+				resources: [
+					'arn:aws:secretsmanager:eu-west-1:865473395570:secret:CODE/Salesforce/ConnectedApp/AwsConnectorSandbox-jaCgRl',
+					'arn:aws:secretsmanager:eu-west-1:865473395570:secret:PROD/Salesforce/ConnectedApp/TouchpointUpdate-lolLqP',
+					'arn:aws:secretsmanager:eu-west-1:865473395570:secret:CODE/Salesforce/User/SoftOptInConsentSetterAPIUser-KjHQBG',
+					'arn:aws:secretsmanager:eu-west-1:865473395570:secret:PROD/Salesforce/User/SoftOptInConsentSetterAPIUser-EonJb0',
+					'arn:aws:secretsmanager:eu-west-1:865473395570:secret:CODE/Identity/SoftOptInConsentAPI-n7Elrb',
+					'arn:aws:secretsmanager:eu-west-1:865473395570:secret:PROD/Identity/SoftOptInConsentAPI-sJJo2s',
+					'arn:aws:secretsmanager:eu-west-1:865473395570:secret:CODE/MobilePurchasesAPI/User/GetSubscriptions-iCUzGN',
+					'arn:aws:secretsmanager:eu-west-1:865473395570:secret:PROD/MobilePurchasesAPI/User/GetSubscriptions-HZuC6H',
+				],
+			}),
+		);
+
+		// Lambda Functions
+		const lambdaFunction = new GuScheduledLambda(this, 'LambdaFunction', {
+			monitoringConfiguration: {
+				noMonitoring: true,
+			},
+			rules: [
+				{
+					schedule: Schedule.expression(props.schedule),
+					description: 'Runs Soft Opt-In Consent Setter',
+				},
+			],
+			app: '',
+			fileName: '',
+			role: lambdaFunctionRole,
+			functionName: `soft-opt-in-consent-setter-${this.stage}`,
+			runtime: Runtime.JAVA_11,
+			handler: 'com.gu.soft_opt_in_consent_setter.Handler::handleRequest',
+			memorySize: 512,
+			timeout: Duration.seconds(900),
+			environment: {
+				Stage: this.stage,
+				sfApiVersion: 'v46.0',
+			},
+		});
+
+		const lambdaFunctionIAP = new GuLambdaFunction(this, 'LambdaFunctionIAP', {
+			app: '',
+			fileName: '',
+			role: lambdaFunctionIAPRole,
+			functionName: `soft-opt-in-consent-setter-IAP-${this.stage}`,
+			runtime: Runtime.JAVA_11,
+			handler: 'com.gu.soft_opt_in_consent_setter.HandlerIAP::handleRequest',
+			memorySize: 512,
+			timeout: Duration.seconds(300),
+			environment: {
+				Stage: this.stage,
+				sfApiVersion: 'v56.0',
+			},
+		});
+
+		// SQS Triggers
+		new aws_lambda.EventSourceMapping(this, 'SQSTrigger', {
+			eventSourceArn: softOptInsQueue.queueArn,
+			target: lambdaFunctionIAP,
+			batchSize: 1,
+			enabled: true,
+		});
+
+		// DynamoDB Tables
+		const softOptInsLoggingTable = new aws_dynamodb.Table(
+			this,
+			'SoftOptInsLoggingTable',
+			{
+				tableName: `soft-opt-in-consent-setter-${this.stage}-logging`,
+				billingMode: aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+				partitionKey: {
+					name: 'identityId',
+					type: aws_dynamodb.AttributeType.STRING,
+				},
+				sortKey: { name: 'timestamp', type: aws_dynamodb.AttributeType.NUMBER },
+				pointInTimeRecovery: true,
+				encryption: aws_dynamodb.TableEncryption.AWS_MANAGED,
+			},
+		);
+		softOptInsLoggingTable.addGlobalSecondaryIndex({
+			indexName: 'subscriptionId-index',
+			partitionKey: {
+				name: 'subscriptionId',
+				type: aws_dynamodb.AttributeType.STRING,
+			},
+			projectionType: aws_dynamodb.ProjectionType.ALL,
+		});
+		Tags.of(softOptInsLoggingTable).add('Stage', this.stage);
+		Tags.of(softOptInsLoggingTable).add('devx-backup-enabled', 'true');
+
+		// Cloudwatch Alarms
+		const snsArn = `arn:aws:sns:${this.region}:${this.account}:alarms-handler-topic-PROD`;
+
+		const failedRunAlarm = new CfnAlarm(this, 'failedRunAlarm', {
+			alarmActions: [snsArn],
+			alarmName: `soft-opt-in-consent-setter-${this.stage} failed to run`,
+			alarmDescription:
+				'Five or more runs found an error and were unable to complete. See GitHub README for details.',
+			comparisonOperator: 'GreaterThanOrEqualToThreshold',
+			dimensions: [
+				{
+					name: 'FunctionName',
+					value: lambdaFunction.functionName,
+				},
+			],
+			evaluationPeriods: 2,
+			metricName: 'Errors',
+			namespace: 'AWS/Lambda',
+			period: 3600,
+			statistic: 'Sum',
+			threshold: 5,
+			treatMissingData: 'notBreaching',
+		});
+		failedRunAlarm.cfnOptions.condition = isProd;
+
+		const exceptionsAlarmIAP = new CfnAlarm(this, 'exceptionsAlarmIAP', {
+			alarmActions: [snsArn],
+			alarmName: `soft-opt-in-consent-setter-IAP-${this.stage} threw an exception`,
+			alarmDescription:
+				'Five or more errors for the IAP Lambda. See GitHub README for details.',
+			comparisonOperator: 'GreaterThanOrEqualToThreshold',
+			dimensions: [
+				{
+					name: 'FunctionName',
+					value: lambdaFunctionIAP.functionName,
+				},
+			],
+			evaluationPeriods: 2,
+			metricName: 'Errors',
+			namespace: 'AWS/Lambda',
+			period: 3600,
+			statistic: 'Sum',
+			threshold: 5,
+			treatMissingData: 'notBreaching',
+		});
+		exceptionsAlarmIAP.cfnOptions.condition = isProd;
+
+		const deadLetterBuildUpAlarmIAP = new CfnAlarm(
+			this,
+			'deadLetterBuildUpAlarmIAP',
+			{
+				alarmActions: [snsArn],
+				alarmName: `soft-opt-in-consent-setter-IAP-${this.stage} failed and sent a message to the dead letter queue.`,
+				alarmDescription:
+					'Alarm when the dead letter queue accumulates messages.',
+				comparisonOperator: 'GreaterThanOrEqualToThreshold',
+				dimensions: [
+					{
+						name: 'QueueName',
+						value: softOptInsDeadLetterQueue.queueName,
+					},
+				],
+				period: 300,
+				evaluationPeriods: 1,
+				metricName: 'ApproximateNumberOfMessagesVisible',
+				namespace: 'AWS/SQS',
+				statistic: 'Sum',
+				threshold: 5,
+				treatMissingData: 'notBreaching',
+			},
+		);
+		deadLetterBuildUpAlarmIAP.cfnOptions.condition = isProd;
+
+		const failedUpdateAlarm = new CfnAlarm(this, 'failedUpdateAlarm', {
+			alarmActions: [snsArn],
+			alarmName: `soft-opt-in-consent-setter-${this.stage} failed to update Salesforce records`,
+			alarmDescription:
+				'A run failed to update some Salesforce records in the last hour.',
+			comparisonOperator: 'GreaterThanOrEqualToThreshold',
+			dimensions: [
+				{
+					name: 'Stage',
+					value: this.stage,
+				},
+			],
+			evaluationPeriods: 1,
+			metricName: 'failed_salesforce_update',
+			namespace: 'soft-opt-in-consent-setter',
+			period: 3600,
+			statistic: 'Sum',
+			threshold: 1,
+			treatMissingData: 'notBreaching',
+		});
+		failedUpdateAlarm.cfnOptions.condition = isProd;
+
+		const failedDynamoUpdateAlarm = new CfnAlarm(
+			this,
+			'failedDynamoUpdateAlarm',
+			{
+				alarmActions: [snsArn],
+				alarmName: `soft-opt-in-consent-setter-${this.stage} failed to update the Dynamo logging table.`,
+				alarmDescription:
+					'A run failed to update the Dynamo logging table in the last hour.',
+				comparisonOperator: 'GreaterThanOrEqualToThreshold',
+				dimensions: [
+					{
+						name: 'Stage',
+						value: this.stage,
+					},
+				],
+				evaluationPeriods: 1,
+				metricName: 'failed_dynamo_update',
+				namespace: 'soft-opt-in-consent-setter',
+				period: 3600,
+				statistic: 'Sum',
+				threshold: 1,
+				treatMissingData: 'notBreaching',
+			},
+		);
+		failedDynamoUpdateAlarm.cfnOptions.condition = isProd;
+
+		// Logical ID overrides
+		const resourcesKeepingExistingLogicalIds: Array<{
+			construct: IConstruct;
+			forcedLogicalId: string;
+		}> = [
+			{
+				construct: softOptInsQueue,
+				forcedLogicalId: 'SoftOptInsQueue',
+			},
+			{
+				construct: softOptInsDeadLetterQueue,
+				forcedLogicalId: 'SoftOptInsDeadLetterQueue',
+			},
+			{
+				construct: softOptInsLoggingTable,
+				forcedLogicalId: 'SoftOptInsLoggingTable',
+			},
+		];
+		resourcesKeepingExistingLogicalIds.forEach((resource) => {
+			this.overrideLogicalId(resource.construct, {
+				// Sets the first letter to uppercase but keeps the rest of the string unchanged
+				logicalId: resource.forcedLogicalId,
+				reason: 'Retaining a stateful resource previously defined in YAML',
+			});
 		});
 	}
 }
