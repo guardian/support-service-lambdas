5a1329a195c9e8c8322468b20fe2aa7d918a9f15
Michael Jacobson <michael.jacobson@guardian.co.uk>
2025-05-06 15:10:08 +0100

    partially revert changes to ConsentsMapping.scala

--- handlers/soft-opt-in-consent-setter/src/main/scala/com/gu/soft_opt_in_consent_setter/models/ConsentsMapping.scala
+++ handlers/soft-opt-in-consent-setter/src/main/scala/com/gu/soft_opt_in_consent_setter/models/ConsentsMapping.scala
@@ -2,6 +2,7 @@ package com.gu.soft_opt_in_consent_setter.models
 
 object ConsentsMapping {
   val yourSupportOnboarding = "your_support_onboarding"
+  val similarGuardianProducts = "similar_guardian_products"
   val supporterNewsletter = "supporter_newsletter"
   val subscriberPreview = "subscriber_preview"
   val guardianWeeklyNewsletter = "guardian_weekly_newsletter"
@@ -26,46 +27,56 @@ object ConsentsMapping {
   val consentsMapping: Map[String, Set[String]] = Map(
     "Membership" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
     ),
     "Supporter" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
     ),
     "Contribution" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
     ),
     "Recurring Contribution" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
     ),
     "Contributor" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
     ),
     "newspaper" -> Set(
       yourSupportOnboarding,
-      subscriberPreview,
-      supporterNewsletter,
-    ),
-    "Newspaper - Home Delivery" -> Set(
-      yourSupportOnboarding,
-      subscriberPreview,
-      supporterNewsletter,
-    ),
-    "Newspaper - Voucher Book" -> Set(
-      yourSupportOnboarding,
-      subscriberPreview,
-      supporterNewsletter,
-    ),
-    "Newspaper - Digital Voucher" -> Set(
-      yourSupportOnboarding,
+      similarGuardianProducts,
       subscriberPreview,
       supporterNewsletter,
     ),
     "Newspaper - National Delivery" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
+      supporterNewsletter,
+      subscriberPreview,
+    ),
+    "Newspaper - Home Delivery" -> Set(
+      yourSupportOnboarding,
+      similarGuardianProducts,
+      subscriberPreview,
+      supporterNewsletter,
+    ),
+    "Newspaper - Voucher Book" -> Set(
+      yourSupportOnboarding,
+      similarGuardianProducts,
+      subscriberPreview,
+      supporterNewsletter,
+    ),
+    "Newspaper - Digital Voucher" -> Set(
+      yourSupportOnboarding,
+      similarGuardianProducts,
       subscriberPreview,
       supporterNewsletter,
     ),
@@ -75,23 +86,28 @@ object ConsentsMapping {
     ),
     "Digital Pack" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
     ),
     "Supporter Plus" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
     ),
     "Tier Three" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
       guardianWeeklyNewsletter,
     ),
     "InAppPurchase" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
       supporterNewsletter,
     ),
     "FeastInAppPurchase" -> Set(
       yourSupportOnboarding,
+      similarGuardianProducts,
     ),
     "Guardian Ad-Lite" -> Set(
       yourSupportOnboarding,
