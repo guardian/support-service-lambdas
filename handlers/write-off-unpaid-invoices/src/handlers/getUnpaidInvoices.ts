a2f0c6b20782f13071b6939ff92294a275a2d394
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2025-05-23 17:42:06 +0300

    chore: add CODE

--- handlers/write-off-unpaid-invoices/src/handlers/getUnpaidInvoices.ts
+++ handlers/write-off-unpaid-invoices/src/handlers/getUnpaidInvoices.ts
@@ -14,8 +14,7 @@ export const handler = async ({ filePath }: { filePath: string }) => {
 
 	// console.log('result', result);
 
-	const result = `invoice_id,invoice_number,invoice_amount,invoice_balance,invoice_currency,invoice_date,account_id,contact_sold_to_country,product_rate_plan_charge_product_codes,product_rate_plan_analysis_codes,accounting_code_project_codes,invoice_items_data\n
-xxx,INVxxx,6.0,0.0,GBP,2024-04-05,8a129e72xxx,United Kingdom,P1010,A101,0000,6!!!8a1282d6xxx!!!ABC-000xxx`;
+	const result = `invoice_id,invoice_number,invoice_amount,invoice_balance,invoice_currency,invoice_date,account_id,contact_sold_to_country,product_rate_plan_charge_product_codes,product_rate_plan_analysis_codes,accounting_code_project_codes,invoice_items_data\nxxx,INVxxx,6.0,0.0,GBP,2024-04-05,8a129e72xxx,United Kingdom,P1010,A101,0000,6!!!8a1282d6xxx!!!ABC-000xxx`;
 
 	const s3UploadAttempt = await uploadFileToS3({
 		bucketName: process.env.BUCKET_NAME!,
