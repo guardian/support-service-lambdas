588aa4c36126ccb436353bdd81a72a8e0f4739fa
John Duffell <john.duffell@guardian.co.uk>
2025-05-28 19:49:49 +0100

    extract invoice id before passing to zuora

--- handlers/product-move-api/src/main/scala/com/gu/productmove/zuora/GetInvoice.scala
+++ handlers/product-move-api/src/main/scala/com/gu/productmove/zuora/GetInvoice.scala
@@ -1,26 +1,11 @@
 package com.gu.productmove.zuora
 
-import com.gu.newproduct.api.productcatalog.{Annual, BillingPeriod, Monthly}
-import com.gu.productmove.AwsS3
-import com.gu.productmove.GuStageLive.Stage
-import com.gu.productmove.endpoint.move.ProductMoveEndpointTypes.ErrorResponse
 import com.gu.productmove.zuora.GetInvoice.GetInvoiceResponse
 import com.gu.productmove.zuora.model.InvoiceId
-import com.gu.productmove.zuora.rest.ZuoraRestBody.ZuoraSuccessCheck.None
 import com.gu.productmove.zuora.rest.{ZuoraGet, ZuoraRestBody}
-import sttp.capabilities.zio.ZioStreams
-import sttp.capabilities.{Effect, WebSockets}
 import sttp.client3.*
-import sttp.client3.httpclient.zio.HttpClientZioBackend
-import sttp.client3.ziojson.*
-import sttp.model.Uri
 import zio.json.*
-import zio.{IO, RIO, Task, URLayer, ZIO, ZLayer}
-
-import java.time.format.DateTimeFormatter
-import java.time.{LocalDate, LocalDateTime}
-import scala.collection.immutable.ListMap
-import scala.math.Ordered.orderingToOrdered
+import zio.{RIO, Task, URLayer, ZIO, ZLayer}
 
 object GetInvoiceLive {
   val layer: URLayer[ZuoraGet, GetInvoice] = ZLayer.fromFunction(GetInvoiceLive(_))
@@ -29,7 +14,7 @@ object GetInvoiceLive {
 private class GetInvoiceLive(zuoraGet: ZuoraGet) extends GetInvoice {
   override def get(invoiceId: InvoiceId): Task[GetInvoiceResponse] =
     zuoraGet.get[GetInvoiceResponse](
-      uri"invoices/$invoiceId",
+      uri"invoices/${invoiceId.id}",
       ZuoraRestBody.ZuoraSuccessCheck.None,
     )
 }
