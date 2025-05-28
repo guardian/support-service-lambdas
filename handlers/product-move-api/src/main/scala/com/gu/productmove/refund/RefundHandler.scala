7136adae77c2093db4917cf42de19974467b4045
John Duffell <john.duffell@guardian.co.uk>
2025-05-28 19:50:18 +0100

    stop cloudwatch splitting multi line logging into separate log messages

--- handlers/product-move-api/src/main/scala/com/gu/productmove/refund/RefundHandler.scala
+++ handlers/product-move-api/src/main/scala/com/gu/productmove/refund/RefundHandler.scala
@@ -2,38 +2,49 @@ package com.gu.productmove.refund
 
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage
-import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.amazonaws.services.lambda.runtime.{Context, LambdaRuntime, RequestHandler}
 import com.gu.productmove.SecretsLive
 import com.gu.productmove.invoicingapi.InvoicingApiRefundLive
 import com.gu.productmove.refund.*
-import com.gu.productmove.zuora.{
-  CreditBalanceAdjustmentLive,
-  GetInvoiceLive,
-  InvoiceItemAdjustment,
-  InvoiceItemAdjustmentLive,
-  InvoiceItemQueryLive,
-  PostInvoicesLive,
-  RunBillingLive,
-}
+import com.gu.productmove.zuora.{CreditBalanceAdjustmentLive, GetInvoiceLive, InvoiceItemAdjustment, InvoiceItemAdjustmentLive, InvoiceItemQueryLive, PostInvoicesLive, RunBillingLive}
 import com.gu.productmove.zuora.rest.{ZuoraClientLive, ZuoraGetLive}
 import com.gu.productmove.{AwsCredentialsLive, AwsS3Live, GuStageLive, SttpClientLive}
 import zio.json.*
 import zio.{Exit, Runtime, Unsafe, ZIO}
 
+import java.io.{OutputStream, PrintStream}
 import scala.jdk.CollectionConverters.*
 
 class RefundHandler extends RequestHandler[SQSEvent, Unit] {
 
+  val printStream = new PrintStream(new OutputStream() {
+    override def write(b: Int): Unit =
+      LambdaRuntime.getLogger.log(Array(b.toByte))
+
+    override def write(b: Array[Byte]): Unit =
+      LambdaRuntime.getLogger.log(b)
+
+    override def write(b: Array[Byte], off: Int, len: Int): Unit =
+      LambdaRuntime.getLogger.log(b.slice(off, off + len))
+  })
+
   override def handleRequest(input: SQSEvent, context: Context): Unit = {
+
+    // try to avoid the logs getting split up too much
+    System.setOut(printStream)
+    System.setErr(printStream)
+
     val records: List[SQSEvent.SQSMessage] = input.getRecords.asScala.toList
 
     records.foreach { record =>
-      val maybeRefundInput = record.getBody.fromJson[RefundInput]
+      val body = record.getBody
+      context.getLogger.log("Processing SQS record:\n" + body)
+      val maybeRefundInput = body.fromJson[RefundInput]
 
       maybeRefundInput match {
         case Right(refundInput) => runZio(refundInput, context)
         case Left(ex) =>
-          context.getLogger.log(s"Error '$ex' when decoding JSON to RefundInput with body: ${record.getBody}")
+          context.getLogger.log(s"Error '$ex' when decoding JSON to RefundInput with body: $body")
       }
     }
   }
