3e3db80f68de6be5dbdac7ef9ef5cf357c756b29
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-05-30 12:39:08 +0100

    Implemented router on mParticle API

--- handlers/mparticle-api/src/index.ts
+++ handlers/mparticle-api/src/index.ts
@@ -1,3 +1,4 @@
+import { Router } from '@modules/routing/router';
 import type {
 	APIGatewayProxyEvent,
 	APIGatewayProxyResult,
@@ -5,17 +6,15 @@ import type {
 } from 'aws-lambda';
 import type { DataSubjectRequestForm } from '../interfaces/data-subject-request-form';
 import { getStatusOfDataSubjectRequest, submitDataSubjectRequest } from './apis/data-subject-request';
-import { HttpError } from './http';
+// import { HttpError } from './http';
 
-export const handler: Handler = async (
-	event: APIGatewayProxyEvent,
-): Promise<APIGatewayProxyResult> => {
-	try {
-		const method = event.httpMethod;
-		const path = event.path;
-		const pathParameters = event.pathParameters ?? {};
-
-		if (method === 'POST' && path === '/requests') {
+const router = new Router([
+	{
+		httpMethod: 'POST',
+		path: '/requests',
+		handler: async (
+			event: APIGatewayProxyEvent,
+		): Promise<APIGatewayProxyResult> => {
 			let payload: unknown;
 
 			try {
@@ -32,9 +31,14 @@ export const handler: Handler = async (
 				body: JSON.stringify(await submitDataSubjectRequest(payload as DataSubjectRequestForm)),
 			};
 		}
-
-		if (method === 'GET' && path.match(/^\/requests\/[a-zA-Z0-9-]+$/)) {
-			const requestId = pathParameters.requestId ?? path.split('/')[2];
+	},
+	{
+		httpMethod: 'GET',
+		path: '/requests/{requestId}',
+		handler: async (
+			event: APIGatewayProxyEvent,
+		): Promise<APIGatewayProxyResult> => {
+			const requestId = event.pathParameters?.requestId;
 
 			if (!requestId) {
 				return {
@@ -48,22 +52,42 @@ export const handler: Handler = async (
 				body: JSON.stringify(await getStatusOfDataSubjectRequest(requestId))
 			};
 		}
+	},
+]);
 
-		return {
-			statusCode: 404,
-			body: 'Not Found',
-		};
-	} catch (err: unknown) {
-		if (err instanceof HttpError) {
-			return {
-				statusCode: 500,
-				body: JSON.stringify(err)
-			};
-		}
+export const handler: Handler = async (
+	event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+	return router.routeRequest(event);
+	// try {
+	// 	console.log(JSON.stringify(router))
+	// 	const method = event.httpMethod;
+	// 	const path = event.path;
+	// 	const pathParameters = event.pathParameters ?? {};
 
-		return {
-			statusCode: 500,
-			body: JSON.stringify({ error: 'Internal Server Error', details: (err as Error).message }),
-		};
-	}
+	// 	if (method === 'POST' && path === '/requests') {
+
+	// 	}
+
+	// 	if (method === 'GET' && path.match(/^\/requests\/[a-zA-Z0-9-]+$/)) {
+
+	// 	}
+
+	// 	return {
+	// 		statusCode: 404,
+	// 		body: 'Not Found',
+	// 	};
+	// } catch (err: unknown) {
+	// 	if (err instanceof HttpError) {
+	// 		return {
+	// 			statusCode: 500,
+	// 			body: JSON.stringify(err)
+	// 		};
+	// 	}
+
+	// 	return {
+	// 		statusCode: 500,
+	// 		body: JSON.stringify({ error: 'Internal Server Error', details: (err as Error).message }),
+	// 	};
+	// }
 };
