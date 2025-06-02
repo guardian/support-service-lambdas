859d22bd0a12f3a054acdc97017dd000bfee67e8
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-02 18:16:58 +0100

    Create functionalTestQueryResponse.ts

---
+++ handlers/negative-invoices-processor/test/handlers/data/functionalTestQueryResponse.ts
@@ -0,0 +1,11 @@
+//Used for dev testing since there is no dev sandbox for bigquery tables
+export const functionalTestQueryResponse = [
+	{
+		id: '1c91a098497a553401499606fa9c4a9c',
+		account_id: '3c93a0f948873db801488d5dab5a08d6',
+		invoice_date: '2015-04-20',
+		currency: 'GBP',
+		invoice_amount: -30,
+		invoice_balance: -30,
+	},
+];
