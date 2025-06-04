e4319502e3522f4dba495e17cb06ec957e206793
Michael Jacobson <michael.jacobson@guardian.co.uk>
2025-06-04 18:39:27 +0100

    initial commit in fresh branch

---
+++ cdk/lib/mparticle-api.ts
@@ -0,0 +1,17 @@
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import type { App } from 'aws-cdk-lib';
+
+export class MParticleApi extends GuStack {
+    constructor(scope: App, id: string, props: GuStackProps) {
+        super(scope, id, props);
+
+        const app = 'mparticle-api';
+        const nameWithStage = `${app}-${this.stage}`;
+        const commonEnvironmentVariables = {
+            App: app,
+            Stack: this.stack,
+            Stage: this.stage,
+        };
+    }
+}
