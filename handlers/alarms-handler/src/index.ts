06712bf52fcd795c04be6beec5b273437bf7f57f
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2025-06-05 16:36:02 +0100

    test'

--- handlers/alarms-handler/src/index.ts
+++ handlers/alarms-handler/src/index.ts
@@ -76,7 +76,7 @@ export async function getChatMessages(
 		});
 	}
 
-	if (message) {
+	if (message.app) {
 		const teams = alarmMappings.getTeams(message.app);
 		console.log(`app ${message.app} is assigned to teams ${teams.join(', ')}`);
 		const webhookUrls = teams.map(alarmMappings.getTeamWebhookUrl);
