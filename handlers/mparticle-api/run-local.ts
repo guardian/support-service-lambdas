60f99c92494b4ad9a76ca132ea42fbf4baf9d4e7
Michael Jacobson <michael.jacobson@guardian.co.uk>
2025-06-05 20:32:57 +0100

    rebase and add placeholder test suite

--- handlers/mparticle-api/run-local.ts
+++ handlers/mparticle-api/run-local.ts
@@ -1,54 +1,51 @@
 // test-local.ts
-import type {
-    APIGatewayProxyEvent,
-    Callback,
-    Context
-} from 'aws-lambda';
+import type { APIGatewayProxyEvent, Callback, Context } from 'aws-lambda';
 import { handler } from './src/index';
 
 const run = async ({
-    httpMethod,
-    path,
-    body
+	httpMethod,
+	path,
+	body,
 }: {
-    httpMethod: 'GET' | 'POST';
-    path: string;
-    body?: string;
+	httpMethod: 'GET' | 'POST';
+	path: string;
+	body?: string;
 }) => {
-    const result: unknown = await handler({
-        httpMethod,
-        path,
-        body
-    } as APIGatewayProxyEvent, {} as Context, (() => { }) as Callback<unknown>);
-    return result;
-}
+	const result: unknown = await handler(
+		{
+			httpMethod,
+			path,
+			body,
+		} as APIGatewayProxyEvent,
+		{} as Context,
+		(() => {}) as Callback<unknown>,
+	);
+	return result;
+};
 // run({
 //     httpMethod: 'GET',
 //     path: '/data-subject-requests/38689d80-4ae9-40f8-a628-b41077e3d62c'
 // })
 run({
-    httpMethod: 'POST',
-    path: '/events',
-    body: JSON.stringify({
-        events: [
-            {
-                data: {
-                },
-                eventType: "commerce_event"
-            }
-        ],
-        deviceInfo: {},
-        userAttributes: {},
-        deletedUserAttributes: [],
-        userIdentities: {},
-        applicationInfo: {},
-        schemaVersion: 2,
-        environment: "production",
-        context: {},
-        ip: "172.217.12.142"
-    })
+	httpMethod: 'POST',
+	path: '/events',
+	body: JSON.stringify({
+		events: [
+			{
+				data: {},
+				eventType: 'commerce_event',
+			},
+		],
+		deviceInfo: {},
+		userAttributes: {},
+		deletedUserAttributes: [],
+		userIdentities: {},
+		applicationInfo: {},
+		schemaVersion: 2,
+		environment: 'production',
+		context: {},
+		ip: '172.217.12.142',
+	}),
 })
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-
-
+	.then((response) => console.log(response))
+	.catch((err) => console.error(err));
