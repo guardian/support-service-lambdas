f1fd4a1f2f243ac71cfdb0e9604b4055dbe6165d
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-05 17:15:21 +0100

    be strict about things

--- handlers/negative-invoices-processor/src/types.ts
+++ handlers/negative-invoices-processor/src/types.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
-export const BigQueryRecordSchema = z.object({
-	id: z.string(),
-	account_id: z.string(),
-});
+export const BigQueryRecordSchema = z
+	.object({
+		id: z.string(),
+		account_id: z.string(),
+	})
+	.strict();
 export type BigQueryRecord = z.infer<typeof BigQueryRecordSchema>;
 
 export const BigQueryResultDataSchema = z.array(BigQueryRecordSchema);
