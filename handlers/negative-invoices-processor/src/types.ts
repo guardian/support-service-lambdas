e4f01a3a043eca1baae8a386b3e0eaea8dbd8b44
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-05 17:07:41 +0100

    Update types.ts

--- handlers/negative-invoices-processor/src/types.ts
+++ handlers/negative-invoices-processor/src/types.ts
@@ -4,7 +4,7 @@ export const BigQueryRecordSchema = z.object({
 	id: z.string(),
 	account_id: z.string(),
 	invoice_date: z.string(),
-	// currency: z.string().nullable(),
+	currency: z.string(),
 	invoice_amount: z.number(),
 	invoice_balance: z.number(),
 });
