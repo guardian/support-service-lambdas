52e0f147cac9a2525acd56802c05195590502bae
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-09 11:51:36 +0100

    Update getInvoices.ts

--- handlers/negative-invoices-processor/src/handlers/getInvoices.ts
+++ handlers/negative-invoices-processor/src/handlers/getInvoices.ts
@@ -45,19 +45,22 @@ export const getPRODData = async (): Promise<BigQueryRecord[]> => {
 
 const query = (): string =>
 	`
-    SELECT
+	SELECT
         inv.id,
-        inv.account_id AS account_id,
-        inv.balance AS invoice_balance
+        STRING_AGG(distinct inv.account_id, ',') AS account_id,
+        MAX(inv.balance) AS invoice_balance
     FROM 
         datatech-fivetran.zuora.invoice inv
     INNER JOIN 
+        datatech-fivetran.zuora.invoice_item item 
+        ON item.invoice_id = inv.id
+    INNER JOIN
         datatech-fivetran.zuora.subscription sub
-        ON inv.account_id = sub.account_id
+        ON item.subscription_id = sub.id AND sub.is_latest_version
     WHERE 
         inv.amount < 0
         AND inv.balance != 0
         AND sub.status = 'Active'
-    ORDER BY 
-        inv.invoice_date
+    GROUP BY 
+        inv.id
 `;
