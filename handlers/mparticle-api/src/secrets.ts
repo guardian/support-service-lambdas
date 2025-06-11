17dfd3f7f65d92c58d0b419eaa94b26da78c294f
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-06-11 17:00:45 +0100

    Change console.warn to console.info for improved logging clarity in getSecretValue function

--- handlers/mparticle-api/src/secrets.ts
+++ handlers/mparticle-api/src/secrets.ts
@@ -6,12 +6,12 @@ export async function getSecretValue(parameterStorePath: string, fallbackEnvVarK
         value = await getSSMParam(parameterStorePath);
     } catch (error: unknown) {
         const errorMessage = (error instanceof Error) ? error.message : String(error);
-        console.warn(`It was not possible to obtain secret value '${parameterStorePath}' from AWS Systems Manager Parameter Store: ${errorMessage}`);
+        console.info(`It was not possible to obtain secret value '${parameterStorePath}' from AWS Systems Manager Parameter Store: ${errorMessage}`);
     }
 
     if (!value && fallbackEnvVarKey) {
         // Fallback to Env vars
-        console.warn(`Secret value '${parameterStorePath}' fallback to Env var value '${fallbackEnvVarKey}'.`);
+        console.info(`Secret value '${parameterStorePath}' fallback to Env var value '${fallbackEnvVarKey}'.`);
         value = process.env[fallbackEnvVarKey];
     }
 
