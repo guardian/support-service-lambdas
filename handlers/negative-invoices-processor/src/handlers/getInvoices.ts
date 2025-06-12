59163086724c54d36bbacb6970a645544a85ddc3
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-12 14:27:26 +0100

    fix alias

--- handlers/negative-invoices-processor/src/handlers/getInvoices.ts
+++ handlers/negative-invoices-processor/src/handlers/getInvoices.ts
@@ -48,7 +48,7 @@ const query = (): string =>
 	SELECT
         inv.id,
         STRING_AGG(distinct inv.account_id, ',') AS accountId,
-        MAX(inv.balance) AS invoice_balance
+        MAX(inv.balance) AS invoiceBalance
     FROM 
         datatech-fivetran.zuora.invoice inv
     INNER JOIN 
