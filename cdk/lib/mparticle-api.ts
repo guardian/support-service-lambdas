1b09c97ef56c12198f8e5127d76549b0597c7a2c
Michael Jacobson <michael.jacobson@guardian.co.uk>
2025-06-12 11:26:23 +0100

    reinstate some lost changes

--- cdk/lib/mparticle-api.ts
+++ cdk/lib/mparticle-api.ts
@@ -3,15 +3,7 @@ import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
 
 export class MParticleApi extends GuStack {
-    constructor(scope: App, id: string, props: GuStackProps) {
-        super(scope, id, props);
-
-        const app = 'mparticle-api';
-        const nameWithStage = `${app}-${this.stage}`;
-        const commonEnvironmentVariables = {
-            App: app,
-            Stack: this.stack,
-            Stage: this.stage,
-        };
-    }
+	constructor(scope: App, id: string, props: GuStackProps) {
+		super(scope, id, props);
+	}
 }
