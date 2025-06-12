c6609059b93b048c66b59d087c68c0afa317595b
Michael Jacobson <michael.jacobson@guardian.co.uk>
2025-06-12 10:41:42 +0100

    revert changes to allow merging basic CI/CD config

--- cdk/lib/mparticle-api.ts
+++ cdk/lib/mparticle-api.ts
@@ -1,68 +1,9 @@
-import { GuApiLambda } from '@guardian/cdk';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
-import { Duration } from 'aws-cdk-lib';
-import { ApiKeySourceType } from 'aws-cdk-lib/aws-apigateway';
-import { nodeVersion } from './node-version';
 
 export class MParticleApi extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
-
-		const app = 'mparticle-api';
-		const nameWithStage = `${app}-${this.stage}`;
-		const commonEnvironmentVariables = {
-			App: app,
-			Stack: this.stack,
-			Stage: this.stage,
-		};
-
-		// ---- API-triggered lambda functions ---- //
-		const lambda = new GuApiLambda(this, `${app}-lambda`, {
-			fileName: 'product-switch-api.zip',
-			handler: 'index.handler',
-			runtime: nodeVersion,
-			memorySize: 1024,
-			timeout: Duration.seconds(300),
-			environment: commonEnvironmentVariables,
-			monitoringConfiguration: {
-				http5xxAlarm: { tolerated5xxPercentage: 5 },
-				snsTopicName: 'alerts-topic',
-			},
-			app,
-			api: {
-				id: nameWithStage,
-				restApiName: nameWithStage,
-				description: `API Gateway endpoint for the ${nameWithStage} lambda`,
-				proxy: true,
-				deployOptions: {
-					stageName: this.stage,
-				},
-				apiKeySourceType: ApiKeySourceType.HEADER,
-				defaultMethodOptions: {
-					apiKeyRequired: true,
-				},
-			},
-		});
-
-		const usagePlan = lambda.api.addUsagePlan('UsagePlan', {
-			name: nameWithStage,
-			description: 'REST endpoints for product-switch-api',
-			apiStages: [
-				{
-					stage: lambda.api.deploymentStage,
-					api: lambda.api,
-				},
-			],
-		});
-
-		// create api key
-		const apiKey = lambda.api.addApiKey(`${app}-key-${this.stage}`, {
-			apiKeyName: `${app}-key-${this.stage}`,
-		});
-
-		// associate api key to plan
-		usagePlan.addApiKey(apiKey);
 	}
 }
