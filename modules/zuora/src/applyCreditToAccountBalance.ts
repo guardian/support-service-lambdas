cba7983ddba7787a640573c98c032e62afd67b57
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-12 15:23:07 +0100

    Create applyCreditToAccountBalance.ts

---
+++ modules/zuora/src/applyCreditToAccountBalance.ts
@@ -0,0 +1,11 @@
+import type { ZuoraClient } from './zuoraClient';
+import type { ZuoraSuccessResponse } from './zuoraSchemas';
+import { zuoraSuccessResponseSchema } from './zuoraSchemas';
+
+export const applyCreditToAccountBalance = async (
+	zuoraClient: ZuoraClient,
+	body: string,
+): Promise<ZuoraSuccessResponse> => {
+	const path = `/v1/credit-memos`;
+	return zuoraClient.post(path, body, zuoraSuccessResponseSchema);
+};
