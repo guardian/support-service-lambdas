eded77d48c639ba4533a4d7f1e7e43569401c761
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-06-16 12:36:10 +0100

    Create mParticle lambda function with CDK

--- cdk/lib/mparticle-api.ts
+++ cdk/lib/mparticle-api.ts
@@ -1,9 +1,52 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import type { App } from 'aws-cdk-lib';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
+import { Duration, type App } from 'aws-cdk-lib';
+import { nodeVersion } from './node-version';
+import { Effect, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class MParticleApi extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
+
+		const app = 'mparticle-api';
+		const lambda = new GuLambdaFunction(this, `${app}-lambda`, {
+			app,
+			memorySize: 1024,
+			fileName: `${app}.zip`,
+			runtime: nodeVersion,
+			timeout: Duration.seconds(15),
+			handler: 'index.handler',
+			functionName: `${app}-${this.stage}`,
+			events: [],
+			environment: {
+				APP: app,
+				STACK: this.stack,
+				STAGE: this.stage,
+			},
+		});
+
+		lambda.role?.attachInlinePolicy(
+			new Policy(this, `${app}-cloudwatch-policy`, {
+				statements: [
+					new PolicyStatement({
+						actions: ['cloudwatch:ListTagsForResource'],
+						resources: ['*'],
+					}),
+				],
+			}),
+		);
+
+		// Allow the lambda to assume the roles that allow cross-account fetching of tags
+		lambda.addToRolePolicy(
+			new PolicyStatement({
+				actions: ['sts:AssumeRole'],
+				effect: Effect.ALLOW,
+				resources: [
+					// mobileAccountRoleArn.valueAsString,
+					// targetingAccountRoleArn.valueAsString,
+				],
+			}),
+		);
 	}
 }
