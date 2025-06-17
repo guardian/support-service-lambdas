c58f3e04c1b2ed9d6ca20d4f9a0b41e3d02970cc
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-17 15:29:40 +0100

    Update doCreditBalanceRefund.ts

--- modules/zuora/src/doCreditBalanceRefund.ts
+++ modules/zuora/src/doCreditBalanceRefund.ts
@@ -6,6 +6,6 @@ export const doCreditBalanceRefund = async (
 	zuoraClient: ZuoraClient,
 	body: string,
 ): Promise<ZuoraUpperCaseSuccessResponse> => {
-	const path = `/v1/object/refund`;
+	const path = `/v1/object/credit-balance-refund`;
 	return zuoraClient.post(path, body, zuoraUpperCaseSuccessResponseSchema);
 };
