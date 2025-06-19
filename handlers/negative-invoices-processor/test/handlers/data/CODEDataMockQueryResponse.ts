19890796e94247ce74cb590b5ce34a816c6305ce
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-19 17:24:58 +0100

    Update CODEDataMockQueryResponse.ts

--- handlers/negative-invoices-processor/test/handlers/data/CODEDataMockQueryResponse.ts
+++ handlers/negative-invoices-processor/test/handlers/data/CODEDataMockQueryResponse.ts
@@ -1,10 +1,10 @@
 //Used for dev testing since there is no dev sandbox for bigquery tables
 export const CODEDataMockQueryResponse = [
 	{
-		accountId: '8ad0823f80d18e2b0180d2812d231d80',
-		invoiceId: '8ad0877b80d18e230180d66ba9dc28bc',
-		invoiceNumber: 'INV00230814',
-		invoiceBalance: -69.99,
+		accountId: '8ad0823f82343ddd01823588667a28b1',
+		invoiceId: '8ad0877b82343ddd0182359fa1344711',
+		invoiceNumber: 'INV00247415',
+		invoiceBalance: -15.83,
 	},
 	{
 		accountId: '3c93a0ff5bec3ec3015c0ca913e7415b',
