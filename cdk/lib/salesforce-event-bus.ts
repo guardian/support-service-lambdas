939e1bed2157628be35aadce2d9d507836970e80
Graham Hopgood <graham.hopgood@guardian.co.uk>
2025-06-19 13:46:55 +0100

    Log all events to Cloudwatch

--- cdk/lib/salesforce-event-bus.ts
+++ cdk/lib/salesforce-event-bus.ts
@@ -2,9 +2,10 @@ import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import { type App, Duration } from 'aws-cdk-lib';
-import { EventBus, Rule } from 'aws-cdk-lib/aws-events';
+import { EventBus, Match, Rule } from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
 import { Effect, PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { nodeVersion } from './node-version';
 
@@ -32,6 +33,21 @@ export class SalesforceEventBus extends GuStack {
 				: `arn:aws:events:${this.region}::event-source/aws.partner/salesforce.com/00D9E0000004jvhUAA/0YLUD00000008Ll4AI`,
 		);
 
+		const logGroup = new LogGroup(
+			this,
+			`/aws/events/sf-event-bus-${props.stage}`,
+		);
+
+		new Rule(this, 'EventToCloudWatch', {
+			description:
+				'Send an event received by the SF bus to a Cloudwatch log group',
+			eventPattern: {
+				source: Match.prefix('aws.partner/salesforce.com'),
+			},
+			eventBus: salesforceBus,
+			targets: [new targets.CloudWatchLogGroup(logGroup)],
+		});
+
 		const deadLetterQueue = new Queue(this, `dead-letters-${app}-queue`, {
 			queueName: `dead-letters-${app}-queue-${props.stage}`,
 			retentionPeriod: Duration.days(14),
@@ -43,17 +59,17 @@ export class SalesforceEventBus extends GuStack {
 			`arn:aws:sqs:eu-west-1:865473395570:salesforce-outbound-messages-${props.stage}`,
 		);
 
-		const rule = new Rule(this, 'SfBusToContactUpdateQueue', {
+		const contactUpdateToSqsRule = new Rule(this, 'SfBusToContactUpdateQueue', {
 			description:
 				'Send an SF Contact Update event from the SF bus to the salesforce-outbound-messages-[STAGE] SQS queue for consumption by membership-workflow',
 			eventPattern: {
-				source: ['aws.partner/salesforce.com'],
+				source: Match.prefix('aws.partner/salesforce.com'),
 				detailType: ['Contact_Update__e'],
 			},
 			eventBus: salesforceBus,
 		});
 
-		rule.addTarget(
+		contactUpdateToSqsRule.addTarget(
 			new targets.SqsQueue(sfOutboundMessageQueue, {
 				deadLetterQueue: deadLetterQueue,
 				maxEventAge: Duration.hours(2),
@@ -69,7 +85,7 @@ export class SalesforceEventBus extends GuStack {
 			actions: ['sqs:SendMessage'],
 			conditions: {
 				ArnEquals: {
-					'aws:SourceArn': rule.ruleArn,
+					'aws:SourceArn': contactUpdateToSqsRule.ruleArn,
 				},
 			},
 		});
