0fd4c100d2c053951a05d7e248299a030a59060f
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-19 16:14:03 +0100

    update cdk

--- cdk/lib/negative-invoices-processor.ts
+++ cdk/lib/negative-invoices-processor.ts
@@ -219,7 +219,7 @@ export class NegativeInvoicesProcessor extends GuStack {
 		)
 			.when(
 				Condition.booleanEquals('$.hasActivePaymentMethod', true),
-				applyCreditToAccountBalanceLambdaTask,
+				new Pass(this, 'do refund lambda will go here'),
 			)
 			.otherwise(new Pass(this, 'check for valid email lambda will go here'));
 
