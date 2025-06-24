d17770a9ad7aed0fc3ee19f48c4f6568f79beb09
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-24 11:48:59 +0100

    Update CODEDataMockQueryResponse.ts

--- handlers/negative-invoices-processor/test/handlers/data/CODEDataMockQueryResponse.ts
+++ handlers/negative-invoices-processor/test/handlers/data/CODEDataMockQueryResponse.ts
@@ -1,12 +1,14 @@
 //Used for dev testing since there is no dev sandbox for bigquery tables
 export const CODEDataMockQueryResponse = [
 	{
+		//actual records from the Zuora dev sandbox. Update as needed.
 		accountId: '8ad0823f82343ddd018235c2f178596d',
 		invoiceId: '8ad0877b82343ddd018235fde13d683f',
 		invoiceNumber: 'INV00247462',
 		invoiceBalance: -15.83,
 	},
 	{
+		//records that do not exist in Zuora dev sandbox. Used for error testing.
 		accountId: '3c93a0ff5bec3ec3015c0ca913e7415b',
 		invoiceId: '1c91a0ad795f6bcc017966151113650d',
 		invoiceNumber: 'XXX',
