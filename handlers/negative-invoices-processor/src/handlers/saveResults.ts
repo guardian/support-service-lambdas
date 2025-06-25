61b48321a2074e4a50a7efdb5872cc4dc0c38f79
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-25 15:29:03 +0100

    Create saveResults.ts

---
+++ handlers/negative-invoices-processor/src/handlers/saveResults.ts
@@ -0,0 +1,53 @@
+import { uploadFileToS3 } from '@modules/aws/s3';
+import { getIfDefined } from '@modules/nullAndUndefined';
+import { z } from 'zod';
+import { InvoiceSchema, ProcessedInvoiceSchema } from '../types';
+
+export const saveResultsInputSchema = z.object({
+	invoicesCount: z.number(),
+	invoices: z.array(InvoiceSchema),
+	processedInvoices: z.array(ProcessedInvoiceSchema),
+});
+export type SaveResultsInput = z.infer<typeof saveResultsInputSchema>;
+
+export const handler = async (event: SaveResultsInput) => {
+	try {
+		const parsedEventResult = saveResultsInputSchema.safeParse(event);
+		if (!parsedEventResult.success) {
+			throw new Error('Invalid event data');
+		}
+		const parsedEvent = parsedEventResult.data;
+		const bucketName = getIfDefined<string>(
+			process.env.S3_BUCKET,
+			'S3_BUCKET environment variable not set',
+		);
+
+		const discountExpiresOnDate = '2025-06-24';
+
+		const executionDateTime = new Date().toISOString();
+
+		const filePath = `${discountExpiresOnDate}/${executionDateTime}`;
+
+		const s3UploadAttempt = await uploadFileToS3({
+			bucketName,
+			filePath,
+			content: JSON.stringify(parsedEvent, null, 2),
+		});
+
+		if (s3UploadAttempt.$metadata.httpStatusCode !== 200) {
+			throw new Error('Failed to upload to S3');
+		}
+		return {
+			...parsedEvent,
+			s3UploadAttemptStatus: 'success',
+			filePath,
+		};
+	} catch (error) {
+		return {
+			...event,
+			s3UploadAttemptStatus: 'error',
+			errorDetail:
+				error instanceof Error ? error.message : JSON.stringify(error, null, 2),
+		};
+	}
+};
