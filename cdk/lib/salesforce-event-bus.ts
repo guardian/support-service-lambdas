f7de2d104b471047379531572b9c6ae38a506253
Graham Hopgood <graham.hopgood@guardian.co.uk>
2025-06-26 14:05:17 +0100

    Remove placeholder lambda

--- cdk/lib/salesforce-event-bus.ts
+++ cdk/lib/salesforce-event-bus.ts
@@ -1,7 +1,6 @@
 import { GuAlarm } from '@guardian/cdk/lib/constructs/cloudwatch';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import { type App, Duration } from 'aws-cdk-lib';
 import {
 	ComparisonOperator,
@@ -16,23 +15,12 @@ import {
 } from 'aws-cdk-lib/aws-events';
 import { SqsQueue } from 'aws-cdk-lib/aws-events-targets';
 import { CfnQueuePolicy, Queue } from 'aws-cdk-lib/aws-sqs';
-import { nodeVersion } from './node-version';
 
 export class SalesforceEventBus extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
 		const app = 'salesforce-event-bus';
-
-		new GuLambdaFunction(this, 'SalesforceEventBusPlaceholder', {
-			app,
-			memorySize: 1024,
-			fileName: `${app}.zip`,
-			runtime: nodeVersion,
-			timeout: Duration.seconds(300),
-			handler: 'salesforceEventBusPlaceholder.handler',
-			functionName: `${app}-placeholder-${this.stage}`,
-		});
 
 		const busId = `arn:aws:events:${this.region}::event-source/aws.partner/salesforce.com/${this.stage === 'PROD' ? '00D20000000nq5gEAA/0YLQv00000000zJOAQ' : '00D9E0000004jvhUAA/0YLUD00000008Ll4AI'}`;
 
--- handlers/salesforce-event-bus/test/handlers/salesforceEventBusPlaceholder.test.ts
+++
@@ -1,3 +0,0 @@
-it('Placeholder test', () => {
-	expect(2).toBe(2);
-});
