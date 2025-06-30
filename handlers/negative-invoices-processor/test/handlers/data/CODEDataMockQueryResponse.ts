4e3a1e4af54a5f16f752d5f80da7689ce1c57f21
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-06-30 16:54:22 +0100

    Update CODEDataMockQueryResponse.ts

--- handlers/negative-invoices-processor/test/handlers/data/CODEDataMockQueryResponse.ts
+++ handlers/negative-invoices-processor/test/handlers/data/CODEDataMockQueryResponse.ts
@@ -2,10 +2,10 @@
 export const CODEDataMockQueryResponse = [
 	{
 		//actual records from the Zuora dev sandbox. Update as needed.
-		accountId: '8ad081c6863a873401863b8bacce51d8',
-		invoiceId: '8ad0880588dd84360188e17b4929743b',
-		invoiceNumber: 'INV00820612',
-		invoiceBalance: -6.33,
+		accountId: '8ad0823f83e3cf4a0183e5f23db449c3',
+		invoiceId: '8ad085298634dd3801863a578eab3443',
+		invoiceNumber: 'INV00340952',
+		invoiceBalance: -24.82,
 	},
 	{
 		//records that do not exist in Zuora dev sandbox. Used for error testing.
