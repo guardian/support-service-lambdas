e61b7a1788bfe803b11337d335a60b3064814373
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-07-01 17:12:37 +0100

    Add SrLambdaDomain construct for custom domain management in API Gateway

---
+++ cdk/lib/cdk/sr-lambda-domain.ts
@@ -0,0 +1,49 @@
+import { Construct } from 'constructs';
+import { CfnDomainName, CfnBasePathMapping, IRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { CfnRecordSet } from 'aws-cdk-lib/aws-route53';
+import type { GuStack } from '@guardian/cdk/lib/constructs/core';
+
+interface GuCustomDomainProps {
+    subdomain: string; // e.g. "metric-push-api", "mparticle-api"
+    stage: string;
+    restApi: IRestApi;
+}
+
+function generateSafeId(input: string): string {
+  return input
+    .split('-')
+    .map((word, index) => 
+      word.charAt(0).toUpperCase() + word.slice(1).toLocaleLowerCase()
+    )
+    .join('');
+}
+
+export class SrLambdaDomain extends Construct {
+    constructor(scope: GuStack, props: GuCustomDomainProps) {
+        const safeId = generateSafeId(props.subdomain);
+        super(scope, `${safeId}Domain`);
+        const domainNameString = `${props.subdomain}-${props.stage.toLowerCase()}.support.guardianapis.com`;
+
+        const domainName = new CfnDomainName(this, `${safeId}DomainName`, {
+            regionalCertificateArn: `arn:aws:acm:${scope.region}:${scope.account}:certificate/b384a6a0-2f54-4874-b99b-96eeff96c009`,
+            domainName: domainNameString,
+            endpointConfiguration: { types: ['REGIONAL'] },
+        });
+
+        const basePathMapping = new CfnBasePathMapping(this, `${safeId}BasePathMapping`, {
+            restApiId: props.restApi.restApiId,
+            domainName: domainName.ref,
+            stage: props.restApi.deploymentStage.stageName,
+        });
+
+        const dnsRecord = new CfnRecordSet(this, `${safeId}DnsRecord`, {
+            name: domainNameString,
+            type: 'CNAME',
+            comment: `CNAME for ${props.subdomain} API ${props.stage}`,
+            hostedZoneName: 'support.guardianapis.com',
+            ttl: '120',
+            resourceRecords: [domainName.attrRegionalDomainName],
+        });
+        dnsRecord.overrideLogicalId(`${safeId}DnsRecord`);
+    }
+}
