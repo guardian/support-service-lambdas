cbd0627bcfa9282cc25458e949c8d264e5bc0957
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-07-02 23:38:46 +0100

    Remove unused imports from mParticleApi

--- cdk/lib/mparticle-api.ts
+++ cdk/lib/mparticle-api.ts
@@ -5,11 +5,8 @@ import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import { type App, CfnOutput, Duration } from 'aws-cdk-lib';
 import { ComparisonOperator, Metric } from 'aws-cdk-lib/aws-cloudwatch';
 import {
-	ManagedPolicy,
 	Policy,
 	PolicyStatement,
-	Role,
-	ServicePrincipal,
 } from 'aws-cdk-lib/aws-iam';
 import { SrLambdaAlarm } from './cdk/sr-lambda-alarm';
 import { SrLambdaDomain } from './cdk/sr-lambda-domain';
