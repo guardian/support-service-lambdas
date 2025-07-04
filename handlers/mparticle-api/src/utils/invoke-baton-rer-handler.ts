513a6f34212e13f102232d62aa4babb805a5a4be
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-07-04 14:05:27 +0100

    Reapply "Refactor code for improved readability and consistency across multiple files"
    This reverts commit 48306bd05f13909cf573e2a8981373090e6c2b66.

--- handlers/mparticle-api/src/utils/invoke-baton-rer-handler.ts
+++ handlers/mparticle-api/src/utils/invoke-baton-rer-handler.ts
@@ -1,11 +1,17 @@
-import type {
-    Callback,
-    Context
-} from 'aws-lambda';
+import type { Callback, Context } from 'aws-lambda';
 import { handler } from '..';
-import type { BatonRerEventRequest, BatonRerEventResponse } from '../routers/baton';
+import type {
+	BatonRerEventRequest,
+	BatonRerEventResponse,
+} from '../routers/baton';
 
-export const invokeBatonRerHandler = async (event: BatonRerEventRequest): Promise<BatonRerEventResponse> => {
-    const result: unknown = await handler(event, {} as Context, (() => { }) as Callback<unknown>);
-    return result as BatonRerEventResponse;
-}
+export const invokeBatonRerHandler = async (
+	event: BatonRerEventRequest,
+): Promise<BatonRerEventResponse> => {
+	const result: unknown = await handler(
+		event,
+		{} as Context,
+		(() => {}) as Callback<unknown>,
+	);
+	return result as BatonRerEventResponse;
+};
