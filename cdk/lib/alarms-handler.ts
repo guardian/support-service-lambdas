90311d0c94697ad08bfc2e8d0b9233eead6a632d
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-08 12:28:21 +0100

    test alarm in CODE

--- cdk/lib/alarms-handler.ts
+++ cdk/lib/alarms-handler.ts
@@ -167,7 +167,7 @@ export class AlarmsHandler extends GuStack {
 			comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
 			threshold: 0,
 			evaluationPeriods: 24,
-			actionsEnabled: this.stage === 'PROD',
+			actionsEnabled: this.stage === 'PROD' || this.stage === 'CODE',
 		});
 
 		const scheduledLambda = new GuScheduledLambda(
