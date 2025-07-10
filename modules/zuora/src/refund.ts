3dbc5249ce52fa5e9c3321e0b8625a64c8b80c35
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-10 18:42:32 +0100

    Update refund.ts

--- modules/zuora/src/refund.ts
+++ modules/zuora/src/refund.ts
@@ -6,6 +6,6 @@ export const doRefund = async (
 	zuoraClient: ZuoraClient,
 	body: string,
 ): Promise<ZuoraUpperCaseSuccessResponse> => {
-	const path = `/v1/object/refund`;
+	const path = `/v1/object/refund1`;
 	return zuoraClient.post(path, body, zuoraUpperCaseSuccessResponseSchema);
 };
