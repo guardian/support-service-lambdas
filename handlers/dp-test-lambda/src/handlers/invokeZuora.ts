e58c2090eaaa33e94b66d5cf9b7f7a2f0c8aadef
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-17 15:29:57 +0100

    add lambda

---
+++ handlers/dp-test-lambda/src/handlers/invokeZuora.ts
@@ -0,0 +1,23 @@
+import { stageFromEnvironment } from '@modules/stage';
+import { applyCreditToAccountBalance } from '@modules/zuora/applyCreditToAccountBalance';
+import { ZuoraClient } from '@modules/zuora/zuoraClient';
+
+export const handler = async () => {
+	try {
+		const zuoraClient = await ZuoraClient.create(stageFromEnvironment());
+		const body = JSON.stringify({
+			Amount: 1,
+			SourceTransactionNumber: 'INVXXX',
+			Type: 'Increase',
+		});
+		const applyCreditToAccountBalanceAttempt =
+			await applyCreditToAccountBalance(zuoraClient, body);
+		console.log(
+			'applyCreditToAccountBalanceAttempt',
+			JSON.stringify(applyCreditToAccountBalanceAttempt, null, 2),
+		);
+		return true;
+	} catch (error) {
+		return false;
+	}
+};
---
+++ handlers/dp-test-lambda/test/handlers/invokeZuora.test.ts
@@ -0,0 +1,5 @@
+describe('basic', () => {
+	it('basic test', async () => {
+		expect(1).toEqual(1);
+	});
+});
