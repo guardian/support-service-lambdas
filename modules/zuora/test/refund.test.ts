a5b2a70590c89607c2524aa76fec5a51857c725b
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-21 14:18:48 +0100

    Fix import path for zuoraResponseSchema in refund test
    Updated the import path for zuoraResponseSchema in refund.test.ts to reference the correct relative location.

--- modules/zuora/test/refund.test.ts
+++ modules/zuora/test/refund.test.ts
@@ -1,5 +1,5 @@
 import { doRefund } from '@modules/zuora/refund';
-import { zuoraResponseSchema } from '@modules/zuora/types/zuoraClient';
+import { zuoraResponseSchema } from '../src/types/zuoraClient';
 import { mockZuoraClient } from '../test/mocks/mockZuoraClient';
 
 jest.mock('@modules/zuora/zuoraClient');
