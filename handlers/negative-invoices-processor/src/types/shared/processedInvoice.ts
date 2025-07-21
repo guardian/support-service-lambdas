6c30b160a2ac2d0c454a59c9c16c9a4254125f6f
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-21 18:15:22 +0100

    Update processedInvoice.ts

--- handlers/negative-invoices-processor/src/types/shared/processedInvoice.ts
+++ handlers/negative-invoices-processor/src/types/shared/processedInvoice.ts
@@ -3,13 +3,12 @@ import { InvoiceSchema } from './invoiceSchemas';
 import { RefundResultSchema } from './refund';
 import { ApplyCreditToAccountBalanceAttemptSchema } from '../handlers/ApplyCreditToAccountBalance';
 import { CheckForActiveSubAttemptSchema } from '../handlers/CheckForActiveSub';
-import { CheckForActivePaymentMethodAttemptSchema } from './paymentMethod';
+import { PaymentMethodResultSchema } from './paymentMethod';
 
 export const ProcessedInvoiceSchema = InvoiceSchema.extend({
 	applyCreditToAccountBalanceAttempt: ApplyCreditToAccountBalanceAttemptSchema,
 	checkForActiveSubAttempt: CheckForActiveSubAttemptSchema.optional(),
-	checkForActivePaymentMethodAttempt:
-		CheckForActivePaymentMethodAttemptSchema.optional(),
+	checkForActivePaymentMethodAttempt: PaymentMethodResultSchema.optional(),
 	refundResult: RefundResultSchema.optional(),
 });
 
