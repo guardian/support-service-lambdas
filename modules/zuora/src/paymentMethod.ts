19aa8faf4505b3e410bf9d1b78d3bdc526bef2d1
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-23 13:25:54 +0100

    debug

--- modules/zuora/src/paymentMethod.ts
+++ modules/zuora/src/paymentMethod.ts
@@ -2,7 +2,6 @@ import z from 'zod';
 import type { ZuoraClient } from './zuoraClient';
 import { DefaultPaymentMethodResponse } from './types/paymentMethod';
 
-//need to figure out how to use schema for this and its tests. Possibly a default schema for each object
 export const getPaymentMethods = async <T = DefaultPaymentMethodResponse>(
 	zuoraClient: ZuoraClient,
 	accountId: string,
