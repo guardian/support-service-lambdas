a6b9592855307816bf92ed0d4f1d20749906413b
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-24 15:09:56 +0100

    Refactor payment method filtering and schema usage
    Moved filterActivePaymentMethods to getPaymentMethods handler and updated it to work with PaymentMethodResponse. Refactored getPaymentMethods to accept an optional schema parameter for response validation, improving flexibility and type safety.

--- handlers/negative-invoices-processor/src/handlers/getPaymentMethods.ts
+++ handlers/negative-invoices-processor/src/handlers/getPaymentMethods.ts
@@ -1,11 +1,16 @@
 import { stageFromEnvironment } from '@modules/stage';
-import {
-	filterActivePaymentMethods,
-	getPaymentMethods,
-} from '@modules/zuora/paymentMethod';
+import { getPaymentMethods } from '@modules/zuora/paymentMethod';
 import { ZuoraClient } from '@modules/zuora/zuoraClient';
-import { GetPaymentMethodsInputSchema } from '../types';
-import type { GetPaymentMethodsInput, GetPaymentMethodsOutput } from '../types';
+import {
+	GetPaymentMethodsInputSchema,
+	PaymentMethodResponseSchema,
+} from '../types';
+import type {
+	GetPaymentMethodsInput,
+	GetPaymentMethodsOutput,
+	PaymentMethod,
+	PaymentMethodResponse,
+} from '../types';
 
 export const handler = async (
 	event: GetPaymentMethodsInput,
@@ -16,6 +21,7 @@ export const handler = async (
 		const paymentMethods = await getPaymentMethods(
 			zuoraClient,
 			parsedEvent.accountId,
+			PaymentMethodResponseSchema,
 		);
 		const activePaymentMethods = filterActivePaymentMethods(paymentMethods);
 		const hasActivePaymentMethod = activePaymentMethods.length > 0;
@@ -45,4 +51,20 @@ export const handler = async (
 			},
 		};
 	}
+};
+const filterActivePaymentMethods = (
+	paymentMethods: PaymentMethodResponse,
+): PaymentMethod[] => {
+	const { creditcard, creditcardreferencetransaction, banktransfer, paypal } =
+		paymentMethods;
+
+	const flattenedPaymentMethods = [
+		...(creditcard ?? []),
+		...(creditcardreferencetransaction ?? []),
+		...(banktransfer ?? []),
+		...(paypal ?? []),
+	];
+	return flattenedPaymentMethods.filter(
+		(pm) => pm.status.toLowerCase() === 'active',
+	);
 };
