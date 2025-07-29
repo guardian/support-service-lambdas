680b1bdad0bc3a27adaf58496293df773a8e84f7
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-29 19:05:22 +0100

    update import order

--- modules/zuora/src/auth/bearerTokenProvider.ts
+++ modules/zuora/src/auth/bearerTokenProvider.ts
@@ -1,6 +1,6 @@
-import { zuoraServerUrl } from '../utils/common';
 import type { OAuthClientCredentials, ZuoraBearerToken } from '../types/auth';
 import { zuoraBearerTokenSchema } from '../types/auth';
+import { zuoraServerUrl } from '../utils/common';
 
 export class BearerTokenProvider {
 	private bearerToken: ZuoraBearerToken | null = null;
