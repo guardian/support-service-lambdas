0cf1b7c1fe0755f4b0e5e9b2bda50e056f18d237
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-29 09:42:03 +0100

    Move updateAccount to account.ts and update imports

--- modules/zuora/src/account.ts
+++ modules/zuora/src/account.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod';
 import type { ZuoraClient } from './zuoraClient';
 import {
 	zuoraAccountSchema,
@@ -20,4 +21,26 @@ export const deleteAccount = async (
 ): Promise<ZuoraSuccessResponse> => {
 	const path = `/v1/accounts/${accountNumber}`;
 	return zuoraClient.delete(path, zuoraSuccessResponseSchema);
+};
+
+const responseSchema = z.object({
+	success: z.boolean(),
+	reasons: z.optional(
+		z.array(z.object({ code: z.number(), message: z.string() })),
+	),
+});
+
+export type UpdateAccountResponse = z.infer<typeof responseSchema>;
+
+export const updateAccount = async (
+	zuoraClient: ZuoraClient,
+	accountNumber: string,
+	payload: {
+		crmId?: string;
+		sfContactId__c?: string;
+	},
+): Promise<UpdateAccountResponse> => {
+	const path = `/v1/accounts/${accountNumber}`;
+	const body = JSON.stringify(payload);
+	return zuoraClient.put(path, body, responseSchema);
 };
