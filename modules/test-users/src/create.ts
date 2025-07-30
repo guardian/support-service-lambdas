9aa95f356021730546db9cc1bf171386a1f24107
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-30 09:20:00 +0100

    Update ZuoraSubscribeResponse import paths
    Changed import paths for ZuoraSubscribeResponse and zuoraSubscribeResponseSchema to reference the new location in modules/zuora/src/types/actions/subscribe. This ensures consistency and resolves potential import issues after file restructuring.

--- modules/test-users/src/create.ts
+++ modules/test-users/src/create.ts
@@ -1,7 +1,7 @@
 import { zuoraDateFormat } from '@modules/zuora/utils/common';
 import type { ZuoraClient } from '@modules/zuora/zuoraClient';
-import type { ZuoraSubscribeResponse } from '@modules/zuora/zuoraSchemas';
-import { zuoraSubscribeResponseSchema } from '@modules/zuora/zuoraSchemas';
+import type { ZuoraSubscribeResponse } from '../../../modules/zuora/src/types/actions/subscribe';
+import { zuoraSubscribeResponseSchema } from '../../../modules/zuora/src/types/actions/subscribe';
 import type { Dayjs } from 'dayjs';
 
 type SubscribeItem = {
