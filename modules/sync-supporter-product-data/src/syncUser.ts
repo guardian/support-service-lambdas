585be7dd8623a1db91ad31170bc6648724ce7a9a
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-30 13:23:42 +0100

    Update syncUser.ts

--- modules/sync-supporter-product-data/src/syncUser.ts
+++ modules/sync-supporter-product-data/src/syncUser.ts
@@ -8,12 +8,12 @@ import { getSubscriptionsByAccountNumber } from '@modules/zuora/subscription';
 import { createQueryResponseSchema } from '@modules/zuora/types';
 import { zuoraDateFormat } from '@modules/zuora/utils/common';
 import { ZuoraClient } from '@modules/zuora/zuoraClient';
-import dayjs from 'dayjs';
-import { z } from 'zod';
 import type {
 	RatePlan,
 	ZuoraSubscription,
 } from '../../../modules/zuora/src/types/objects/subscription';
+import dayjs from 'dayjs';
+import { z } from 'zod';
 
 export const getActiveAccountNumbersForIdentityId = async (
 	zuoraClient: ZuoraClient,
