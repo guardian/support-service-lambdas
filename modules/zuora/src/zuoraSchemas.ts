bfbc9749c88b834215515cf37f8929909314ad9f
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-07-31 12:11:44 +0100

    remove zuoraSuccessResponseSchema from zuoraSchemas

--- modules/zuora/src/zuoraSchemas.ts
+++ modules/zuora/src/zuoraSchemas.ts
@@ -1,19 +1,1 @@
-import { z } from 'zod';
 
-// --------------- Basic success response ---------------
-export const zuoraSuccessResponseSchema = z.object({
-	success: z.boolean(),
-	reasons: z.optional(
-		z.array(z.object({ code: z.number(), message: z.string() })),
-	),
-});
-
-export const zuoraUpperCaseSuccessResponseSchema = z.object({
-	Success: z.boolean(), //to do add reasons in case of failure
-});
-
-export type ZuoraSuccessResponse = z.infer<typeof zuoraSuccessResponseSchema>;
-
-export type ZuoraUpperCaseSuccessResponse = z.infer<
-	typeof zuoraUpperCaseSuccessResponseSchema
->;
