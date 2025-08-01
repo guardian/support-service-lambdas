1258110177890d8430e62aa17d33536aaf9f7480
ecguardian <elliott.cooper@guardian.co.uk>
2025-08-01 12:07:47 +0100

    fix: format code in sqs.ts and sqs.test.ts

--- modules/aws/src/sqs.ts
+++ modules/aws/src/sqs.ts
@@ -1,35 +1,35 @@
 import type { SendMessageCommandInput } from '@aws-sdk/client-sqs';
 import {
-    SQSClient,
-    GetQueueUrlCommand,
-    SendMessageCommand,
+	SQSClient,
+	GetQueueUrlCommand,
+	SendMessageCommand,
 } from '@aws-sdk/client-sqs';
 import { awsConfig } from '@modules/aws/config';
 
 const client = new SQSClient(awsConfig);
 
 export const sendMessageToQueue = async ({
-    queueName,
-    messageBody,
+	queueName,
+	messageBody,
 }: {
-    queueName: string;
-    messageBody: SendMessageCommandInput['MessageBody'];
+	queueName: string;
+	messageBody: SendMessageCommandInput['MessageBody'];
 }) => {
-    try {
-        const { QueueUrl } = await client.send(
-            new GetQueueUrlCommand({ QueueName: queueName })
-        );
+	try {
+		const { QueueUrl } = await client.send(
+			new GetQueueUrlCommand({ QueueName: queueName }),
+		);
 
-        if (!QueueUrl) throw new Error('Queue URL not found');
+		if (!QueueUrl) throw new Error('Queue URL not found');
 
-        const command = new SendMessageCommand({
-            QueueUrl,
-            MessageBody: messageBody,
-        });
-        const response = await client.send(command);
-        return response;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+		const command = new SendMessageCommand({
+			QueueUrl,
+			MessageBody: messageBody,
+		});
+		const response = await client.send(command);
+		return response;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
 };
