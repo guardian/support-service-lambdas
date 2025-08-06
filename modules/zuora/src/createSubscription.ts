9452d3902382a08d1805d4351479c5a5dae05bdf
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-08-06 15:21:46 +0100

    Use builder functions in createSubscription.ts

--- modules/zuora/src/createSubscription.ts
+++ modules/zuora/src/createSubscription.ts
@@ -1,24 +1,110 @@
 import type { ZuoraClient } from '@modules/zuora/zuoraClient';
-import { OrderRequest } from '@modules/zuora/orders/orders';
+import {
+	AnyOrderRequest,
+	executeOrderRequest,
+} from '@modules/zuora/orders/orderRequests';
 import { z } from 'zod';
+import { Currency } from '@modules/internationalisation/currency';
+import {
+	buildNewAccountObject,
+	Contact,
+	PaymentGateway,
+	PaymentMethod,
+} from '@modules/zuora/orders/newAccount';
+import dayjs, { Dayjs } from 'dayjs';
+import { buildCreateSubscriptionOrderAction } from '@modules/zuora/orders/orderActions';
+import { zuoraDateFormat } from '@modules/zuora/utils';
 
 const createSubscriptionResponseSchema = z.object({
 	orderNumber: z.string(),
 	accountNumber: z.string(),
 	subscriptionNumbers: z.array(z.string()),
+	invoiceNumbers: z.array(z.string()).optional(),
+	paymentNumber: z.string().optional(),
+	paidAmount: z.number().optional(),
 });
 
-type CreateSubscriptionResponse = z.infer<
+export type CreateSubscriptionResponse = z.infer<
 	typeof createSubscriptionResponseSchema
 >;
 
-export const createSubscription = async (
-	zuoraClient: ZuoraClient,
-	createSubscriptionRequest: OrderRequest,
-): Promise<CreateSubscriptionResponse> => {
-	const path = `/v1/orders`;
-	const body = JSON.stringify(createSubscriptionRequest);
-	return zuoraClient.post(path, body, createSubscriptionResponseSchema, {
-		'zuora-version': '211.0',
+type CreateSubscriptionInputFields<T extends PaymentMethod> = {
+	accountName: string;
+	createdRequestId: string;
+	salesforceAccountId: string;
+	salesforceContactId: string;
+	identityId: string;
+	currency: Currency;
+	paymentGateway: PaymentGateway<T>;
+	paymentMethod: T;
+	billToContact: Contact;
+	soldToContact?: Contact;
+	productRatePlanId: string;
+	contractEffectiveDate: Dayjs;
+	customerAcceptanceDate?: Dayjs;
+	chargeOverride?: { productRatePlanChargeId: string; overrideAmount: number };
+	runBilling?: boolean;
+	collectPayment?: boolean;
+};
+
+const buildCreateSubscriptionRequest = <T extends PaymentMethod>({
+	accountName,
+	createdRequestId,
+	salesforceAccountId,
+	salesforceContactId,
+	identityId,
+	currency,
+	paymentGateway,
+	paymentMethod,
+	billToContact,
+	soldToContact,
+	productRatePlanId,
+	contractEffectiveDate,
+	customerAcceptanceDate,
+	chargeOverride,
+	runBilling,
+	collectPayment,
+}: CreateSubscriptionInputFields<T>): AnyOrderRequest => {
+	const newAccount = buildNewAccountObject({
+		accountName: accountName,
+		createdRequestId: createdRequestId,
+		salesforceAccountId: salesforceAccountId,
+		salesforceContactId: salesforceContactId,
+		identityId: identityId,
+		currency: currency,
+		paymentGateway: paymentGateway,
+		paymentMethod: paymentMethod,
+		billToContact: billToContact,
+		soldToContact: soldToContact,
 	});
+	const createSubscriptionOrderAction = buildCreateSubscriptionOrderAction({
+		productRatePlanId: productRatePlanId,
+		contractEffectiveDate: contractEffectiveDate,
+		customerAcceptanceDate: customerAcceptanceDate,
+		chargeOverride: chargeOverride,
+	});
+	return {
+		newAccount: newAccount,
+		orderDate: zuoraDateFormat(dayjs()),
+		description: `Created by createSubscription.ts in support-service-lambdas`,
+		subscriptions: [
+			{
+				orderActions: [createSubscriptionOrderAction],
+			},
+		],
+		processingOptions: {
+			runBilling: runBilling ?? false,
+			collectPayment: collectPayment ?? false,
+		},
+	};
+};
+export const createSubscription = async <T extends PaymentMethod>(
+	zuoraClient: ZuoraClient,
+	inputFields: CreateSubscriptionInputFields<T>,
+): Promise<CreateSubscriptionResponse> => {
+	return executeOrderRequest(
+		zuoraClient,
+		buildCreateSubscriptionRequest(inputFields),
+		createSubscriptionResponseSchema,
+	);
 };
