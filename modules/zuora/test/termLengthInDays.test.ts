5c2c8e1fca2b82d70809b47484bf2d6083f39b0a
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-09-08 11:43:23 +0100

    PR feedback

--- modules/zuora/test/termLengthInDays.test.ts
+++ modules/zuora/test/termLengthInDays.test.ts
@@ -1,9 +1,19 @@
 import dayjs from 'dayjs';
 import { initialTermInDays } from '@modules/zuora/orders/orderActions';
 
-test('InitialTermLength', () => {
-	const termStart = dayjs('2019-2-3');
-	const firstPaperDate = termStart.add(3, 'day');
-	const termLength = initialTermInDays(termStart, firstPaperDate, 3);
-	expect(termLength).toBe(92);
+describe('initialTermInDays', () => {
+	test(
+		'returns the total number of days from the contract effective date ' +
+			'to [termLength] days after the customer acceptance date',
+		() => {
+			const contractEffectiveDate = dayjs('2019-2-3');
+			const firstPaperDate = contractEffectiveDate.add(3, 'day');
+			const termLengthInDays = initialTermInDays(
+				contractEffectiveDate,
+				firstPaperDate,
+				3,
+			);
+			expect(termLengthInDays).toBe(92);
+		},
+	);
 });
