18379ebba0d17b847a28e3b397b2eb66fdf332d5
John Duffell <john.duffell@guardian.co.uk>
2025-09-18 10:27:03 +0100

    only run buildcheck on handlers that have been converted

--- buildcheck/data/snippets/notices.ts
+++ buildcheck/data/snippets/notices.ts
@@ -2,10 +2,9 @@ export function relativePath(filename: string) {
 	// not watertight logic, but it isn't security critical
 	const moduleName = 'buildcheck';
 	const repoRootIndex = filename.indexOf('/' + moduleName + '/');
-	const relativePath =
-		repoRootIndex !== -1 ? filename.substring(repoRootIndex + 1) : filename;
-	console.log(`relativePath for ${filename}`, relativePath);
-	return relativePath;
+	return repoRootIndex !== -1
+		? filename.substring(repoRootIndex + 1)
+		: filename;
 }
 
 export const notice = (filename: string) =>
