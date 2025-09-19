dde6c3ee402f0fd5ea6a1a7af26da16530f03e5d
Olivier Andrade <olivier.andrade.mindera@guardian.co.uk>
2025-09-19 16:18:59 +0100

    feat(stripe-disputes): stripe webhook signature verification

--- handlers/stripe-disputes/src/producer.ts
+++ handlers/stripe-disputes/src/producer.ts
@@ -2,6 +2,10 @@ import { Logger } from '@modules/routing/logger';
 import { Router } from '@modules/routing/router';
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { handleStripeWebhook } from './services';
+import { getSecretValue } from '@modules/secrets-manager/getSecret';
+import { StripeCredentials } from './types';
+import { stageFromEnvironment } from '@modules/stage';
+import Stripe from 'stripe';
 
 const logger = new Logger();
 
@@ -23,9 +27,42 @@ export const handler = async (
 ): Promise<APIGatewayProxyResult | void> => {
 	logger.log(`Input: ${JSON.stringify(event)}`);
 
+	const stripeSignature: string | undefined = event.headers['stripe-signature'];
+
+	if (!stripeSignature) {
+		logger.error('Missing Stripe-Signature header');
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ message: 'Missing Stripe-Signature header' }),
+		};
+	}
+
+	const endpointSecretObject: StripeCredentials =
+		await getSecretValue<StripeCredentials>(
+			`${stageFromEnvironment()}/Stripe/ConnectedApp/StripeDisputeWebhooks`,
+		);
+
+	// const stripeInstance = new Stripe(endpointSecretObject.secret_key);
+	// let stripeEvent: Stripe.Event | null = null;
+
+	try {
+		new Stripe(endpointSecretObject.secret_key).webhooks.constructEvent(
+			JSON.stringify(event.body),
+			stripeSignature,
+			endpointSecretObject.secret_key,
+		);
+	} catch (err) {
+		const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+		logger.error(`Error verifying Stripe webhook signature: ${errorMessage}`);
+		return {
+			statusCode: 403,
+			body: JSON.stringify({ message: `Webhook Error: ${errorMessage}` }),
+		};
+	}
+
 	if (isApiGatewayEvent(event)) {
 		logger.log('Processing API Gateway webhook event');
-		const response = await router(event);
+		const response: APIGatewayProxyResult = await router(event);
 		logger.log(`Webhook response: ${JSON.stringify(response)}`);
 		return response;
 	} else {
