7d5272cc3e81248f5fbbe2c2c5ad605502e20327
John Duffell <john.duffell@guardian.co.uk>
2025-09-22 12:22:54 +0100

    remove accidental jest ignore line

--- handlers/alarms-handler/lambda/jest.config.js
+++ handlers/alarms-handler/lambda/jest.config.js
@@ -7,5 +7,4 @@ module.exports = {
 		'@modules/(.*)/(.*)$': '<rootDir>/../../../modules/$1/src/$2',
 		'@modules/(.*)$': '<rootDir>/../../../modules/$1',
 	},
-	testPathIgnorePatterns: ['<rootDir>/cdk/'],
 };
