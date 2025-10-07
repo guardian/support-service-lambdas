6b047ca95a2e7bf554f8b37c514d0a97eb3ddef2
John Duffell <john.duffell@guardian.co.uk>
2025-10-07 18:04:42 +0100

    wrap more essentials into SrApiLambda

--- cdk/lib/cdk/sr-api-lambda.ts
+++ cdk/lib/cdk/sr-api-lambda.ts
@@ -1,9 +1,15 @@
 import { GuApiLambda, type GuApiLambdaProps } from '@guardian/cdk';
 import type { Identity } from '@guardian/cdk/lib/constructs/core';
+import type { GuPolicy } from '@guardian/cdk/lib/constructs/iam/policies/base-policy';
 import { Duration } from 'aws-cdk-lib';
 import { ApiKeySourceType } from 'aws-cdk-lib/aws-apigateway';
+import { ComparisonOperator, Metric } from 'aws-cdk-lib/aws-cloudwatch';
 import type { SrLambdaProps } from './sr-lambda';
 import { getLambdaDefaultProps, getNameWithStage } from './sr-lambda';
+import type { SrLambdaAlarmProps } from './sr-lambda-alarm';
+import { SrLambdaAlarm } from './sr-lambda-alarm';
+import type { SrRestDomainProps } from './sr-rest-domain';
+import { SrRestDomain } from './sr-rest-domain';
 import type { SrStack } from './sr-stack';
 
 function getApiLambdaDefaultProps(scope: Identity, props: SrApiLambdaProps) {
@@ -37,17 +43,17 @@ type ApiDefaultProps = ReturnType<typeof getApiLambdaDefaultProps>;
 type SrApiLambdaOverrides = Omit<GuApiLambdaProps, keyof ApiDefaultProps> &
 	Partial<ApiDefaultProps>;
 type SrApiLambdaProps = SrLambdaProps & {
+	lambdaOverrides: SrApiLambdaOverrides;
 	apiDescriptionOverride?: string;
 	isPublic?: boolean;
+	errorImpact: string;
+	alarmOverrides?: Partial<SrLambdaAlarmProps>;
+	srRestDomainOverrides?: SrRestDomainProps;
 };
 
 export class SrApiLambda extends GuApiLambda {
-	constructor(
-		scope: SrStack,
-		id: string,
-		lambdaOverrides: SrApiLambdaOverrides,
-		props: SrApiLambdaProps,
-	) {
+	readonly domain: SrRestDomain;
+	constructor(scope: SrStack, props: SrApiLambdaProps) {
 		const defaultProps = getApiLambdaDefaultProps(scope, props);
 		const deprecatedVars = {
 			// for some reason we often use these instead of the upper case STACK/STAGE/APP added by GuCDK
@@ -58,14 +64,74 @@ export class SrApiLambda extends GuApiLambda {
 		};
 		const finalProps = {
 			...defaultProps,
-			...lambdaOverrides,
+			...props.lambdaOverrides,
 			environment: {
 				...defaultProps.environment,
 				...deprecatedVars,
-				...lambdaOverrides.environment,
+				...props.lambdaOverrides.environment,
 			},
 		};
 
-		super(scope, id, finalProps);
+		super(scope, `${scope.app}-lambda`, finalProps);
+
+		if (!props.isPublic) {
+			const usagePlan = this.api.addUsagePlan('UsagePlan', {
+				name: getNameWithStage(scope, props.nameSuffix),
+				description: 'REST endpoints for ' + scope.app,
+				apiStages: [
+					{
+						stage: this.api.deploymentStage,
+						api: this.api,
+					},
+				],
+			});
+
+			// create api key
+			const apiKey = this.api.addApiKey(`${scope.app}-key-${scope.stage}`, {
+				apiKeyName: `${scope.app}-key-${scope.stage}`,
+			});
+
+			// associate api key to plan
+			usagePlan.addApiKey(apiKey);
+		}
+
+		this.domain = new SrRestDomain(
+			scope,
+			this.api,
+			props.srRestDomainOverrides,
+		);
+
+		if (
+			scope.stage === 'PROD' &&
+			finalProps.monitoringConfiguration.noMonitoring
+		) {
+			new SrLambdaAlarm(scope, 'ApiGateway5XXAlarmCDK', {
+				app: scope.app,
+				alarmName: getNameWithStage(scope, props.nameSuffix) + ' 5XX errors',
+				alarmDescription:
+					scope.app +
+					' returned a 5XX response search the logs below for "error" for more information. Impact: ' +
+					props.errorImpact,
+				evaluationPeriods: 1,
+				threshold: 1,
+				lambdaFunctionNames: this.functionName,
+				comparisonOperator:
+					ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+				metric: new Metric({
+					metricName: '5XXError',
+					namespace: 'AWS/ApiGateway',
+					statistic: 'Sum',
+					period: Duration.seconds(60),
+					dimensionsMap: {
+						ApiName: getNameWithStage(scope, props.nameSuffix),
+					},
+				}),
+				...props.alarmOverrides,
+			});
+		}
+	}
+
+	addPolicies(...policies: GuPolicy[]) {
+		policies.forEach((p) => this.role!.attachInlinePolicy(p));
 	}
 }
