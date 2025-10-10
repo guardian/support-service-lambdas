01cf3f906b92e2de67a632b971341dbdfac5b47e
Michael McNamara <michael.mcnamara@theguardian.com>
2025-10-10 14:23:07 +0100

    Fix mm 202510101 write ophan raw support case to new bucket (#3138)
    * add extra case to ExportS3Path to direct ObjectName: Case to a new bucket
    
    * add unit test to track special situation for the salesforce Case export s3 bucket

--- handlers/sf-datalake-export/src/main/scala/com/gu/sf_datalake_export/util/ExportS3Path.scala
+++ handlers/sf-datalake-export/src/main/scala/com/gu/sf_datalake_export/util/ExportS3Path.scala
@@ -13,6 +13,9 @@ object ExportS3Path {
   }
 
   def apply(stage: Stage)(objectName: ObjectName, uploadToDataLake: ShouldUploadToDataLake): S3Path = stage match {
+// A separate case is needed for the salesforce-case data due to problems in S3 requiring a new destination bucket
+    case Stage("PROD") if uploadToDataLake.value && objectName.value == "Case" =>
+      S3Path(BucketName(s"ophan-raw-salesforce-${camelToHyphenCase(objectName.value)}v2"), None)
     case Stage("PROD") if uploadToDataLake.value =>
       S3Path(BucketName(s"ophan-raw-salesforce-${camelToHyphenCase(objectName.value)}"), None)
     case Stage(stageName) => S3Path(BucketName(s"gu-salesforce-export-${stageName.toLowerCase}"), None)
