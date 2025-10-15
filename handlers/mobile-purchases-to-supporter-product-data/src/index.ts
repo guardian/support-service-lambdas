24593136db0d463b5fd432af507f1977f97a49c2
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-10-15 13:31:18 +0100

    Correct input type

--- handlers/mobile-purchases-to-supporter-product-data/src/index.ts
+++ handlers/mobile-purchases-to-supporter-product-data/src/index.ts
@@ -1,20 +1,21 @@
 import { logger } from '@modules/routing/logger';
 import type { Stage } from '@modules/stage';
 import { stageFromEnvironment } from '@modules/stage';
-import type { DynamoDBStreamEvent, Handler } from 'aws-lambda';
+import type { DynamoDBRecord, Handler } from 'aws-lambda';
 
-const updateSupporterProductData = (
-	stage: Stage,
-	event: DynamoDBStreamEvent,
-) => {
+type InputEvent = {
+	detail: DynamoDBRecord;
+};
+const updateSupporterProductData = (stage: Stage, event: InputEvent) => {
 	logger.log(
 		'info',
 		`Updated supporter product data for stage ${stage} with event ${JSON.stringify(event)}`,
 	);
 	return Promise.resolve();
 };
-export const handler: Handler = async (event: DynamoDBStreamEvent) => {
+export const handler: Handler = async (event: InputEvent) => {
 	console.log(`Input is ${JSON.stringify(event, null, 2)}`);
 	const stage = stageFromEnvironment();
+	console.log(`Event type is ${event.detail.eventName}`);
 	await updateSupporterProductData(stage, event);
 };
