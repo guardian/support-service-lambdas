e363385a34cda4b50fbd743ba287b2d6afac6131
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-10-17 14:03:07 +0100

    Give event bus rule a name

--- handlers/mobile-purchases-to-supporter-product-data/src/index.ts
+++ handlers/mobile-purchases-to-supporter-product-data/src/index.ts
@@ -5,6 +5,7 @@ import { logger } from '@modules/routing/logger';
 import type { Stage } from '@modules/stage';
 import { stageFromEnvironment } from '@modules/stage';
 import type { SupporterRatePlanItem } from '@modules/supporter-product-data/supporterProductData';
+import { putSupporterProductData } from '@modules/supporter-product-data/supporterProductData';
 import type { DynamoDBRecord, Handler } from 'aws-lambda';
 import dayjs from 'dayjs';
 import type { Config } from './config';
@@ -67,7 +68,7 @@ export const fetchSubscriptionAndDoUpdate = async (
 		termEndDate: subscription.to.toISOString(),
 		contractEffectiveDate: subscription.from.toISOString(),
 	};
-	// await putSupporterProductData(stage, [supporterProductDataItem]);
+	await putSupporterProductData(stage, [supporterProductDataItem]);
 	logger.log(
 		'info',
 		`Successfully updated supporter product data with item ${prettyPrint(supporterProductDataItem)}`,
