fffe69896e1f04bb12ff2852f86073a8ce00ad95
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-10-21 13:43:53 +0100

    Add SQS queue

--- cdk/lib/mobile-purchases-to-supporter-product-data.ts
+++ cdk/lib/mobile-purchases-to-supporter-product-data.ts
@@ -1,36 +1,32 @@
-import type { App } from 'aws-cdk-lib';
+import { App, Duration } from 'aws-cdk-lib';
 import { EventBus, Rule } from 'aws-cdk-lib/aws-events';
-import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+import { SqsQueue } from 'aws-cdk-lib/aws-events-targets';
 import { AllowSupporterProductDataPutItemPolicy } from './cdk/policies';
-import { SrLambda } from './cdk/SrLambda';
-import { SrLambdaErrorAlarm } from './cdk/SrLambdaErrorAlarm';
+import { SrSqsLambda } from './cdk/SrSqsLambda';
 import { SrStack, type SrStageNames } from './cdk/SrStack';
 
 export class MobilePurchasesToSupporterProductData extends SrStack {
 	constructor(scope: App, stage: SrStageNames) {
 		super(scope, { stage, app: 'mobile-purchases-to-supporter-product-data' });
 
-		const lambda = new SrLambda(
+		const errorImpact =
+			'Supporter Product Data table is not being updated with mobile purchase data, which could result in users not receiving the correct benefits associated with their in app purchase.';
+		const lambda = new SrSqsLambda(
 			this,
 			'mobile-purchases-to-supporter-product-data-lambda',
 			{
+				monitoring: { errorImpact },
 				lambdaOverrides: {
 					description:
 						'A lambda that writes mobile purchase products into the supporter product data table',
+					timeout: Duration.minutes(5),
 				},
+				maxReceiveCount: 3,
+				// This must be >= the lambda timeout
+				visibilityTimeout: Duration.minutes(5),
 			},
 		);
 		lambda.addPolicies(new AllowSupporterProductDataPutItemPolicy(this));
-
-		new SrLambdaErrorAlarm(
-			this,
-			`FailedMobilePurchasesToSupporterProductDataLambdaAlarm`,
-			{
-				errorImpact:
-					'This means the the Supporter Product Data table is not being updated with mobile purchase data, which could result in users not receiving the correct benefits associated with their purchase.',
-				lambdaFunctionName: lambda.functionName,
-			},
-		);
 
 		const mobilePurchasesBus = EventBus.fromEventBusName(
 			this,
@@ -42,7 +38,7 @@ export class MobilePurchasesToSupporterProductData extends SrStack {
 			this,
 			'ForwardMobilePurchasesToSupporterProductData',
 			{
-				ruleName: `forward-mobile-purchases-to-lambda-${this.stage}`,
+				ruleName: `forward-mobile-purchases-to-sqs-${this.stage}`,
 				description:
 					'Forward DynamoDB stream purchase events to mobile-purchases-to-supporter-product-data-lambda',
 				eventBus: mobilePurchasesBus,
@@ -54,6 +50,6 @@ export class MobilePurchasesToSupporterProductData extends SrStack {
 			},
 		);
 
-		forwardMobilePurchasesRule.addTarget(new LambdaFunction(lambda));
+		forwardMobilePurchasesRule.addTarget(new SqsQueue(lambda.inputQueue));
 	}
 }
