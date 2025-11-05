6206c67f5fecae174b6a7a9bc43eb083f8c68b38
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-11-05 15:37:46 +0000

    First commit
    # Conflicts:
    #	handlers/observer-digital-benefits/src/index.ts

--- handlers/observer-digital-benefits/src/index.ts
+++ handlers/observer-digital-benefits/src/index.ts
@@ -1,15 +1,24 @@
+import { Router } from '@modules/routing/router';
 import type {
 	APIGatewayProxyEvent,
 	APIGatewayProxyResult,
 	Handler,
 } from 'aws-lambda';
 
-export const handler: Handler = async (
+export const handler: Handler = Router([
+	{
+		httpMethod: 'GET',
+		path: '/',
+		handler: handleRequest,
+	},
+]);
+
+async function handleRequest(
 	event: APIGatewayProxyEvent,
-): Promise<APIGatewayProxyResult> => {
+): Promise<APIGatewayProxyResult> {
 	console.log(`Input is ${JSON.stringify(event)}`);
 	return await Promise.resolve({
 		body: 'Hello World',
 		statusCode: 200,
 	});
-};
+}
