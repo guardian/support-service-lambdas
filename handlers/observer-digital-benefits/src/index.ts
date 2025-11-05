f3a82fd2dc6ce3637ce176156e7f0d6b38b50a4a
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-11-05 17:10:04 +0000

    Test

--- handlers/observer-digital-benefits/src/index.ts
+++ handlers/observer-digital-benefits/src/index.ts
@@ -1,24 +1,42 @@
-import { Router } from '@modules/routing/router';
+import { createRoute, Router } from '@modules/routing/router';
 import type {
 	APIGatewayProxyEvent,
 	APIGatewayProxyResult,
 	Handler,
 } from 'aws-lambda';
+import z from 'zod';
+
+const pathSchema = z.object({
+	subscriptionNumber: z
+		.string()
+		.regex(
+			/^A-S\d+$/,
+			'Subscription number must start with A-S and be followed by digits',
+		),
+});
+type ParsedPath = z.infer<typeof pathSchema>;
 
 export const handler: Handler = Router([
-	{
+	createRoute({
 		httpMethod: 'GET',
-		path: '/',
+		path: '/status/{subscriptionNumber}',
 		handler: handleRequest,
-	},
+		parser: {
+			path: pathSchema,
+		},
+	}),
 ]);
 
 async function handleRequest(
 	event: APIGatewayProxyEvent,
+	parsed: {
+		path: ParsedPath;
+	},
 ): Promise<APIGatewayProxyResult> {
-	console.log(`Input is ${JSON.stringify(event)}`);
+	console.log(`Checking status for ${parsed.path.subscriptionNumber}`);
+
 	return await Promise.resolve({
-		body: 'Hello World',
+		body: `Hello ${parsed.path.subscriptionNumber}`,
 		statusCode: 200,
 	});
 }
