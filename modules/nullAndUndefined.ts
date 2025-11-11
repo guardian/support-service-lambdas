812d5ef23261ca44b63b6219ec2e10dcd58b189b
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-11-11 17:13:50 +0000

    Add `isNonEmpty` function

--- modules/nullAndUndefined.ts
+++ modules/nullAndUndefined.ts
@@ -8,3 +8,9 @@ export const getIfDefined = <T>(
 	}
 	return value;
 };
+
+export function isNonEmpty<T>(arr: T[]): arr is [T, ...T[]] {
+	// This check ensures that the array has at least one element and narrows the type accordingly
+	// this way, TypeScript knows that arr[0] is of type T and not undefined
+	return arr.length > 0;
+}
