7b05012d913863f68ba01b6c903488f4104c0b85
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-11-20 13:51:21 +0000

    Add eslint.config.mjs

---
+++ modules/product-catalog/eslint.config.mjs
@@ -0,0 +1,15 @@
+import guardian from '@guardian/eslint-config';
+import sortKeysFix from 'eslint-plugin-sort-keys-fix';
+
+export default [
+	...guardian.configs.recommended,
+	{
+		files: ['src/productCatalogSchema.ts', 'src/productBillingPeriods.ts'],
+		plugins: {
+			'sort-keys-fix': sortKeysFix,
+		},
+		rules: {
+			'sort-keys-fix/sort-keys-fix': 'warn',
+		},
+	},
+];
