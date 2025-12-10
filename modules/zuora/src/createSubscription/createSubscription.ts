87fe6fcbbeeb8a29ba45d9c72f96bb28061debba
Tom Forbes <tom.forbes@theguardian.com>
2025-12-10 15:08:09 +0000

    format zuora

--- modules/zuora/src/createSubscription/createSubscription.ts
+++ modules/zuora/src/createSubscription/createSubscription.ts
@@ -6,7 +6,10 @@ import type {
 } from '@modules/product-catalog/productCatalog';
 import type { ProductPurchase } from '@modules/product-catalog/productPurchaseSchema';
 import { getDiscountRatePlanFromCatalog } from '@modules/promotions/v1/getPromotions';
-import type { AppliedPromotion, Promotion } from '@modules/promotions/v1/schema';
+import type {
+	AppliedPromotion,
+	Promotion,
+} from '@modules/promotions/v1/schema';
 import type { ValidatedPromotion } from '@modules/promotions/v1/validatePromotion';
 import { validatePromotion } from '@modules/promotions/v1/validatePromotion';
 import dayjs from 'dayjs';
