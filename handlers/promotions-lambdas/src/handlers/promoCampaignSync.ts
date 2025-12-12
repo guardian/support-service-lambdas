77ff59b2239789d85bd7f3904fc4eaf907cb2e79
Tom Forbes <tom.forbes@theguardian.com>
2025-12-12 10:39:44 +0000

    lint

--- handlers/promotions-lambdas/src/handlers/promoCampaignSync.ts
+++ handlers/promotions-lambdas/src/handlers/promoCampaignSync.ts
@@ -1,9 +1,8 @@
-import type {
-	DynamoDBStreamEvent,
-	DynamoDBStreamHandler,
-} from 'aws-lambda';
+import type { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda';
 
 export const handler: DynamoDBStreamHandler = (event: DynamoDBStreamEvent) => {
 	console.log('Running');
-	event.Records.forEach(record => console.log(JSON.stringify(record.dynamodb)));
-}
+	event.Records.forEach((record) =>
+		console.log(JSON.stringify(record.dynamodb)),
+	);
+};
