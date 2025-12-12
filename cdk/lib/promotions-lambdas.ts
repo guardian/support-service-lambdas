db17b5bbcab1ea891765fb9191e0bd099a070f11
Tom Forbes <tom.forbes@theguardian.com>
2025-12-12 10:01:45 +0000

    lint

--- cdk/lib/promotions-lambdas.ts
+++ cdk/lib/promotions-lambdas.ts
@@ -1,15 +1,12 @@
 import type { App } from 'aws-cdk-lib';
-import { SrApiLambda } from './cdk/SrApiLambda';
+import { SrLambda } from './cdk/SrLambda';
 import type { SrStageNames } from './cdk/SrStack';
 import { SrStack } from './cdk/SrStack';
-import {SrLambda} from "./cdk/SrLambda";
 
 export class PromotionsLambdas extends SrStack {
 	constructor(scope: App, stage: SrStageNames) {
 		super(scope, { stage, app: 'promotions-lambdas' });
 
-		new SrLambda(this, 'PromoCampaignSync', {
-
-		});
+		new SrLambda(this, 'PromoCampaignSync', {});
 	}
 }
