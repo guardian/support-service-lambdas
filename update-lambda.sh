819b57a06ce8c51a6e07e90a4dec8d378f75fcda
John Duffell <john.duffell@guardian.co.uk>
2025-12-12 14:18:33 +0000

    fix update-lambda script for multi lambda projects

--- update-lambda.sh
+++ update-lambda.sh
@@ -2,8 +2,9 @@
 
 # This script will build the provided project, copy the zip file to S3 and then call
 # `update-function-code` with the AWS Cli to update the lambda.
-# Usage: ./update-lambda.sh [project name]
+# Usage: ./update-lambda.sh [project name] [--quick] [function names...]
 # eg. ./update-lambda.sh discount-api
+# eg. ./update-lambda.sh --quick mparticle-api "mparticle-api-http-" "mparticle-api-baton-"
 
 # Exit if any of these commands fail
 set -e
@@ -11,14 +12,37 @@ set -e
 cd "$(dirname "$0")"
 
 if [ $# -lt 1 ]; then
-  echo "please provide the project name as an argument, eg. ./update-lambda.sh discount-api [--quick]"
+  echo "please provide the project name as an argument, eg. ./update-lambda.sh [--quick] discount-api [function names...]"
   exit 2
 fi
 
-PROJECT_NAME="$1"
-echo "Updating lambda $PROJECT_NAME"
+QUICK_MODE=""
+PROJECT_NAME=""
+function_names=""
 
-if [ "$2" = "--quick" ]; then
+while [ $# -gt 0 ]; do
+  if [ "$1" = "--quick" ]; then
+    QUICK_MODE="true"
+  elif [ -z "$PROJECT_NAME" ]; then
+    PROJECT_NAME="$1"
+  else
+    function_names="$function_names $1"
+  fi
+  shift
+done
+
+if [ -z "$PROJECT_NAME" ]; then
+  echo "please provide the project name as an argument, eg. ./update-lambda.sh discount-api [--quick] [function names...]"
+  exit 2
+fi
+
+if [ -z "$function_names" ]; then
+  function_names="$PROJECT_NAME-"
+fi
+
+echo "Updating handler $PROJECT_NAME"
+
+if [ -n "$QUICK_MODE" ]; then
   pushd "handlers/$PROJECT_NAME"
   pnpm build
   cd target && zip -qr "$PROJECT_NAME.zip" ./*.js.map ./*.js
@@ -32,12 +56,16 @@ s3Path="support/CODE/$PROJECT_NAME/$PROJECT_NAME.zip"
 zipFile="./handlers/$PROJECT_NAME/target/$PROJECT_NAME.zip"
 
 aws s3 cp $zipFile s3://$s3Bucket/$s3Path --profile membership --region eu-west-1
-aws lambda update-function-code \
-  --function-name $PROJECT_NAME-CODE \
-  --s3-bucket $s3Bucket \
-  --s3-key $s3Path \
-  --profile membership \
-  --region eu-west-1 \
-  > /dev/null
+
+for fn in $function_names; do
+  echo "Updating lambda $fn"
+  aws lambda update-function-code \
+    --function-name "${fn}CODE" \
+    --s3-bucket $s3Bucket \
+    --s3-key $s3Path \
+    --profile membership \
+    --region eu-west-1 \
+    > /dev/null
+done
 
 echo "Update complete"
