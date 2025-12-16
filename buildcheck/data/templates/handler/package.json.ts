6789b06b992ae06042bfe53a82c25ff27653d619
John Duffell <john.duffell@guardian.co.uk>
2025-12-16 16:39:32 +0000

    group package.json scripts according to use (#3291)

--- buildcheck/data/templates/handler/package.json.ts
+++ buildcheck/data/templates/handler/package.json.ts
@@ -1,33 +1,44 @@
 import { HandlerDefinition } from '../../build';
 import { notice } from '../../snippets/notices';
 
-export default (pkg: HandlerDefinition) => {
-	const entryPoints = pkg.entryPoints
-		? pkg.entryPoints?.join(' ')
-		: 'src/index.ts';
+export function buildPackageJson(
+	pkg: HandlerDefinition,
+	extraScripts: Record<string, string>,
+	filename: string,
+) {
 	return {
 		name: `${pkg.name}`,
 		scripts: {
-			'cdk:test': 'pnpm --filter cdk test ' + pkg.name,
-			'cdk:test-update': 'pnpm --filter cdk test-update ' + pkg.name,
 			test: 'jest --group=-integration',
 			'it-test': 'jest --group=integration',
 			'type-check': 'tsc --noEmit',
-			build:
-				'esbuild --bundle --platform=node --target=node20 --outdir=target/ ' +
-				entryPoints +
-				' --sourcemap',
 			lint: "eslint --cache --cache-location /tmp/eslintcache/ 'src/**/*.ts' 'test/**/*.ts'",
-			package: `pnpm type-check && pnpm lint && pnpm check-formatting && pnpm test && pnpm build && cd target && zip -qr ${pkg.name}.zip ./*.js.map ./*.js`,
 			'check-formatting': 'prettier --check "**/*.ts"',
 			'fix-formatting': 'prettier --write "**/*.ts"',
-			'update-lambda': `../../update-lambda.sh "${pkg.name}"${pkg.functionNames ? ` ${pkg.functionNames.join(' ')}` : ''}`,
-			'update-stack': `../../update-stack.sh "${pkg.name}"`,
+			...extraScripts,
 			...pkg.extraScripts,
 		},
-		NOTICE1: notice(__filename),
+		NOTICE1: notice(filename),
 		NOTICE2: 'all dependencies are defined in buildcheck/data/build.ts',
 		dependencies: pkg.dependencies,
 		devDependencies: pkg.devDependencies,
 	};
+}
+
+export default (pkg: HandlerDefinition) => {
+	const entryPoints = pkg.entryPoints
+		? pkg.entryPoints?.join(' ')
+		: 'src/index.ts';
+	const handlerScripts = {
+		build:
+			'esbuild --bundle --platform=node --target=node20 --outdir=target/ ' +
+			entryPoints +
+			' --sourcemap',
+		package: `pnpm type-check && pnpm lint && pnpm check-formatting && pnpm test && pnpm build && cd target && zip -qr ${pkg.name}.zip ./*.js.map ./*.js`,
+		'cdk:test': 'pnpm --filter cdk test ' + pkg.name,
+		'cdk:test-update': 'pnpm --filter cdk test-update ' + pkg.name,
+		'update-lambda': `../../update-lambda.sh "${pkg.name}"${pkg.functionNames ? ` ${pkg.functionNames.join(' ')}` : ''}`,
+		'update-stack': `../../update-stack.sh "${pkg.name}"`,
+	};
+	return buildPackageJson(pkg, handlerScripts, __filename);
 };
