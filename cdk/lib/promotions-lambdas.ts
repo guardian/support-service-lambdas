b7bcac11e260d5f85f378b12169751c8fdbc22f0
Tom Forbes <tom.forbes@theguardian.com>
2025-12-17 16:20:23 +0000

    Add alarm for promo-campaign-sync (#3299)

--- cdk/lib/promotions-lambdas.ts
+++ cdk/lib/promotions-lambdas.ts
@@ -1,11 +1,19 @@
 import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
+import {
+	ComparisonOperator,
+	Metric,
+	TreatMissingData,
+} from 'aws-cdk-lib/aws-cloudwatch';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { StartingPosition } from 'aws-cdk-lib/aws-lambda';
 import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { SrLambda } from './cdk/SrLambda';
+import { SrLambdaAlarm } from './cdk/SrLambdaAlarm';
 import type { SrStageNames } from './cdk/SrStack';
 import { SrStack } from './cdk/SrStack';
+
+const app = 'promotions-lambdas';
 
 interface Props {
 	oldPromoCampaignStreamLabel: string;
@@ -13,7 +21,7 @@ interface Props {
 
 export class PromotionsLambdas extends SrStack {
 	constructor(scope: App, stage: SrStageNames, props: Props) {
-		super(scope, { stage, app: 'promotions-lambdas' });
+		super(scope, { stage, app });
 
 		const promoCampaignSyncLambda = new SrLambda(this, 'PromoCampaignSync', {
 			nameSuffix: 'promo-campaign-sync',
@@ -54,5 +62,26 @@ export class PromotionsLambdas extends SrStack {
 
 		oldPromoCampaignTable.grantStreamRead(promoCampaignSyncLambda);
 		newPromoCampaignTable.grantWriteData(promoCampaignSyncLambda);
+
+		new SrLambdaAlarm(this, 'PromoCampaignSyncLambdaErrorAlarm', {
+			app: app,
+			alarmName: `${this.stage} ${app} - promo-campaign-sync lambda error`,
+			alarmDescription:
+				'The promo-campaign-sync lambda failed to process an event.',
+			lambdaFunctionNames: promoCampaignSyncLambda.functionName,
+			metric: new Metric({
+				metricName: 'Errors',
+				namespace: 'AWS/Lambda',
+				statistic: 'Sum',
+				period: Duration.minutes(5),
+				dimensionsMap: {
+					FunctionName: promoCampaignSyncLambda.functionName,
+				},
+			}),
+			threshold: 1,
+			evaluationPeriods: 1,
+			comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+			treatMissingData: TreatMissingData.NOT_BREACHING,
+		});
 	}
 }
