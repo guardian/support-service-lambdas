2030e36ee8c57fb021fab5655ba23ff623b0533c
Tom Forbes <tom.forbes@theguardian.com>
2025-12-17 11:50:04 +0000

    cdk update

--- cdk/lib/promotions-lambdas.ts
+++ cdk/lib/promotions-lambdas.ts
@@ -1,4 +1,8 @@
 import type { App } from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { StartingPosition } from 'aws-cdk-lib/aws-lambda';
+import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { SrLambda } from './cdk/SrLambda';
 import type { SrStageNames } from './cdk/SrStack';
 import { SrStack } from './cdk/SrStack';
@@ -7,11 +11,35 @@ export class PromotionsLambdas extends SrStack {
 	constructor(scope: App, stage: SrStageNames) {
 		super(scope, { stage, app: 'promotions-lambdas' });
 
-		new SrLambda(this, 'PromoCampaignSync', {
+		const promoCampaignSyncLambda = new SrLambda(this, 'PromoCampaignSync', {
 			nameSuffix: 'promo-campaign-sync',
 			lambdaOverrides: {
 				handler: 'handlers/promoCampaignSync.handler',
+				environment: {
+					STAGE: stage,
+				},
 			},
 		});
+
+		const oldPromoCampaignTable = dynamodb.Table.fromTableAttributes(
+			this,
+			'OldPromoCampaignTable',
+			{
+				tableArn: `arn:aws:dynamodb:${this.region}:${this.account}:table/MembershipSub-Campaigns-${stage}`,
+				tableStreamArn: `arn:aws:dynamodb:${this.region}:${this.account}:table/MembershipSub-Campaigns-${stage}/stream/*`,
+			},
+		);
+
+		promoCampaignSyncLambda.addEventSource(
+			new DynamoEventSource(oldPromoCampaignTable, {
+				startingPosition: StartingPosition.TRIM_HORIZON,
+				batchSize: 5,
+				maxBatchingWindow: Duration.seconds(10),
+				bisectBatchOnError: true,
+				retryAttempts: 3,
+				reportBatchItemFailures: true,
+				parallelizationFactor: 1,
+			}),
+		);
 	}
 }
