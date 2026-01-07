b9277a2c7de60112201fb2ac76da6d9317aa8339
AnastasiiaBalenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-01-07 10:50:51 +0000

    promoCodeView lambda: created handler and tests (#3311)
    * promoCodeView lambda: created handler and tests
    
    * formatting fix
    
    * Refactoring
    
    * Grant read/write permissions to PromoCodeView Lambda
    
    * streamLabels updated
    
    * Removed batching complexity
    
    * formatting fix
    
    * show promo codes in logs
    
    * test fix
    
    * refactor: use promoCampaignSchema and set promotion_type to 'percent_discount'
    
    * snapshots updated to reflect node version change

--- cdk/lib/promotions-lambdas.test.ts
+++ cdk/lib/promotions-lambdas.test.ts
@@ -8,10 +8,12 @@ describe('The Promotions lambdas stack', () => {
 		const codeStack = new PromotionsLambdas(app, 'CODE', {
 			oldPromoCampaignStreamLabel: '2025-12-17T11:57:50.933',
 			oldPromoStreamLabel: '2023-04-28T14:57:20.201',
+			newPromoStreamLabel: '2026-01-05T11:33:36.603',
 		});
 		const prodStack = new PromotionsLambdas(app, 'PROD', {
 			oldPromoCampaignStreamLabel: '2025-12-17T11:57:59.560',
 			oldPromoStreamLabel: '2016-06-01T13:26:09.654',
+			newPromoStreamLabel: '2026-01-05T11:50:46.239',
 		});
 
 		expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
