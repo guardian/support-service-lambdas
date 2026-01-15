5be72986a4986c79948b719c2187dfb73a6eb00a
Tom Forbes <tom.forbes@theguardian.com>
2026-01-15 15:04:56 +0000

    better comment

--- modules/schemaUtils.ts
+++ modules/schemaUtils.ts
@@ -5,6 +5,6 @@ export const dateFromStringSchema = z.preprocess(
 	z.date(),
 );
 
-// Defines an optional field, and transforms null to undefined
+// Defines an optional field, and transforms null to undefined. This is preferable to `nullish` as it does not pollute the code with `null | undefined`
 export const optionalDropNulls = <T extends z.ZodTypeAny>(schema: T) =>
 	z.preprocess((val) => (val === null ? undefined : val), schema.optional());
