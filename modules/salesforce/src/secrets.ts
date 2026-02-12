852dc2bc16562a54c474b48f91471e7cd462ea59
Tom Forbes <tom.forbes@theguardian.com>
2026-02-12 13:34:49 +0000

    Make salesforce client secret names configurable (#3402)
    * Make salesforce client secret names configurable
    
    * format

--- modules/salesforce/src/secrets.ts
+++ modules/salesforce/src/secrets.ts
@@ -1,27 +1,3 @@
-import type { Stage } from '@modules/stage';
-
-export function getSalesforceSecretNames(stage: Stage): SecretNames {
-	switch (stage) {
-		case 'CODE':
-			return {
-				apiUserSecretName: 'DEV/Salesforce/User/integrationapiuser',
-				connectedAppSecretName:
-					'DEV/Salesforce/ConnectedApp/AwsConnectorSandbox',
-			};
-		case 'PROD':
-			return {
-				apiUserSecretName: 'PROD/Salesforce/User/BillingAccountRemoverAPIUser',
-				connectedAppSecretName:
-					'PROD/Salesforce/ConnectedApp/BillingAccountRemover',
-			};
-		default:
-			return {
-				apiUserSecretName: '',
-				connectedAppSecretName: '',
-			};
-	}
-}
-
 export type SecretNames = {
 	apiUserSecretName: string;
 	connectedAppSecretName: string;
--- modules/salesforce/src/sfClient.ts
+++ modules/salesforce/src/sfClient.ts
@@ -1,11 +1,11 @@
-import type { Stage } from '@modules/stage';
 import { RestClient } from '@modules/zuora/restClient';
 import { SfBearerTokenProvider } from '@modules/salesforce/auth/sfBearerTokenProvider';
 import { getSfOauthCredentials } from '@modules/salesforce/auth/sfOAuthCredentials';
+import type { SecretNames } from '@modules/salesforce/secrets';
 
 export class SfClient extends RestClient {
-	static async create(stage: Stage) {
-		const credentials = await getSfOauthCredentials(stage);
+	static async create(secretNames: SecretNames) {
+		const credentials = await getSfOauthCredentials(secretNames);
 		const tokenProvider = new SfBearerTokenProvider(credentials);
 		return new SfClient(tokenProvider);
 	}
