ea8656591cc8c01e736e9f0992e0fa934414c210
Olivier Andrade <olivier.andrade.mindera@guardian.co.uk>
2026-03-11 12:11:17 +0000

    add newsletter-acquisition SQS queue for Identity integration
    # Conflicts:
    #	.github/workflows/ci-typescript.yml

---
+++ cdk/lib/newsletter-acquisition.test.ts
@@ -0,0 +1,31 @@
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { NewsletterAcquisition } from './newsletter-acquisition';
+
+describe('The NewsletterAcquisition stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const codeStack = new NewsletterAcquisition(
+			app,
+			'newsletter-acquisition-CODE',
+			{
+				stack: 'support',
+				stage: 'CODE',
+				identitySnsTopicArn:
+					'arn:aws:sns:eu-west-1:942464564246:newsletter-acquisition-CODE',
+			},
+		);
+		const prodStack = new NewsletterAcquisition(
+			app,
+			'newsletter-acquisition-PROD',
+			{
+				stack: 'support',
+				stage: 'PROD',
+				identitySnsTopicArn:
+					'arn:aws:sns:eu-west-1:942464564246:newsletter-acquisition-PROD',
+			},
+		);
+		expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
+		expect(Template.fromStack(prodStack).toJSON()).toMatchSnapshot();
+	});
+});
