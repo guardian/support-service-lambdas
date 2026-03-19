dca7e4820758a2f06283965ee69ce0bba43b59e3
Rupert Bates <Rupert.Bates@guardian.co.uk>
2026-03-19 10:46:24 +0000

    Bank Transfer works

--- modules/zuora/src/paymentMethod.ts
+++ modules/zuora/src/paymentMethod.ts
@@ -1,4 +1,5 @@
-import type z from 'zod';
+import { z } from 'zod';
+import type { BankTransferPaymentMethod } from './types';
 import { DefaultPaymentMethodResponseSchema } from './types';
 import type { ZuoraClient } from './zuoraClient';
 
@@ -12,4 +13,37 @@ export const getPaymentMethods = async <
 	const path = `/v1/accounts/${accountId}/payment-methods`;
 	const finalSchema = schema ?? DefaultPaymentMethodResponseSchema;
 	return zuoraClient.get(path, finalSchema);
+};
+
+const createPaymentMethodResponseSchema = z.object({
+	id: z.string(),
+});
+
+export const createBankTransferPaymentMethod = async (
+	zuoraClient: ZuoraClient,
+	accountKey: string,
+	bankTransfer: BankTransferPaymentMethod,
+): Promise<string> => {
+	const body = JSON.stringify({
+		accountKey,
+		type: bankTransfer.type,
+		bankCode: bankTransfer.bankCode,
+		accountNumber: bankTransfer.accountNumber,
+		skipValidation: true,
+		accountHolderInfo: {
+			accountHolderName:
+				bankTransfer.accountHolderInfo.accountHolderName ?? undefined,
+		},
+		mandateInfo: {
+			mandateId: bankTransfer.mandateInfo.mandateId,
+			mandateReason: bankTransfer.mandateInfo.mandateReason ?? undefined,
+			mandateStatus: bankTransfer.mandateInfo.mandateStatus ?? undefined,
+		},
+	});
+	const response: { id: string } = await zuoraClient.post(
+		'/v1/payment-methods',
+		body,
+		createPaymentMethodResponseSchema,
+	);
+	return response.id;
 };
