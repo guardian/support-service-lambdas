ea05aa907440dde27efed38d570901a5cb4174db
Olivier Andrade <olivier.andrade.mindera@guardian.co.uk>
2026-04-08 15:34:05 +0100

    Remove full address check in DigipackAddressValidator for Digital Plus acquisitions

--- handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/email/digipack/DigipackAddressValidator.scala
+++ handlers/new-product-api/src/main/scala/com/gu/newproduct/api/addsubscription/email/digipack/DigipackAddressValidator.scala
@@ -6,37 +6,25 @@ import com.gu.newproduct.api.addsubscription.validation.ValidationResult
 import com.gu.newproduct.api.addsubscription.zuora.GetContacts._
 
 case class ValidatedAddress(
-    address1: Address1,
+    address1: Option[Address1],
     address2: Option[Address2],
-    city: City,
+    city: Option[City],
     state: Option[State],
     country: Country,
-    postcode: Postcode,
+    postcode: Option[Postcode],
 )
 
 object DigipackAddressValidator {
   def apply(address: BillToAddress): ValidationResult[ValidatedAddress] = {
     for {
-      validatedAddress1 <- validateNotEmpty("address1", address.address1)
-      validatedCity <- validateNotEmpty("city", address.city)
-      validatedPostcode <- validateNotEmpty("postcode", address.postcode)
       validatedCountry <- address.country getOrFailWith "Billing country must be populated in Zuora"
     } yield ValidatedAddress(
-      validatedAddress1,
+      address.address1,
       address.address2,
-      validatedCity,
+      address.city,
       address.state,
       validatedCountry,
-      validatedPostcode,
+      address.postcode,
     )
   }
-
-  def validateNotEmpty[Field <: AddressField](
-      fieldName: String,
-      maybeAddressField: Option[Field],
-  ): ValidationResult[Field] = for {
-    addressField <- maybeAddressField getOrFailWith (s"Billing $fieldName must be populated in Zuora")
-    _ <- !addressField.value.trim.isEmpty orFailWith s"Billing $fieldName must be populated in Zuora"
-  } yield addressField
-
 }
