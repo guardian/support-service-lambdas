ab6b3f64951acfd863c1d24a769c5d2ac917d315
Rupert Bates <Rupert.Bates@guardian.co.uk>
2026-05-05 14:35:39 +0100

    Code generation
    # Conflicts:
    #	cdk/lib/__snapshots__/multiple-account-api.test.ts.snap
    #	cdk/lib/multiple-account-api.ts

--- cdk/lib/multiple-account-api.ts
+++ cdk/lib/multiple-account-api.ts
@@ -1,12 +1,4 @@
 import type { App } from 'aws-cdk-lib';
-import { RemovalPolicy } from 'aws-cdk-lib';
-import {
-	AttributeType,
-	BillingMode,
-	StreamViewType,
-	Table,
-	TableEncryption,
-} from 'aws-cdk-lib/aws-dynamodb';
 import { SrApiLambda } from './cdk/SrApiLambda';
 import type { SrStageNames } from './cdk/SrStack';
 import { SrStack } from './cdk/SrStack';
@@ -15,54 +7,15 @@ export class MultipleAccountApi extends SrStack {
 	constructor(scope: App, stage: SrStageNames) {
 		super(scope, { stage, app: 'multiple-account-api' });
 
-		const app = 'multiple-account';
-
-		const lambda = new SrApiLambda(this, 'Lambda', {
+		new SrApiLambda(this, 'Lambda', {
 			lambdaOverrides: {
 				description:
-					'A lambda that provides an API to manage multiple accounts',
+					'A lambda that enables the addition of discounts to existing subscriptions',
 			},
 			monitoring: {
-				errorImpact: 'The multiple account system is experiencing errors',
+				errorImpact:
+					'an eligible user may not have been offered a discount during the cancellation flow',
 			},
 		});
-
-		const invitationTable = new Table(this, 'InvitationTable', {
-			tableName: `${app}-invitation-${this.stage}`,
-			billingMode: BillingMode.PAY_PER_REQUEST,
-			partitionKey: { name: 'subscriptionName', type: AttributeType.STRING },
-			sortKey: { name: 'invitationCode', type: AttributeType.STRING },
-			timeToLiveAttribute: 'expiryDate',
-			encryption: TableEncryption.AWS_MANAGED,
-			stream: StreamViewType.NEW_AND_OLD_IMAGES,
-			removalPolicy:
-				this.stage === 'PROD' ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
-		});
-
-		invitationTable.addGlobalSecondaryIndex({
-			indexName: 'invitationCode-index',
-			partitionKey: { name: 'invitationCode', type: AttributeType.STRING },
-		});
-
-		invitationTable.grantFullAccess(lambda);
-
-		const secondaryUserTable = new Table(this, 'SecondaryUserTable', {
-			tableName: `${app}-secondary-user-${this.stage}`,
-			billingMode: BillingMode.PAY_PER_REQUEST,
-			partitionKey: { name: 'subscriptionName', type: AttributeType.STRING },
-			sortKey: { name: 'secondaryIdentityId', type: AttributeType.STRING },
-			timeToLiveAttribute: 'expiryDate',
-			encryption: TableEncryption.AWS_MANAGED,
-			stream: StreamViewType.NEW_AND_OLD_IMAGES,
-			removalPolicy:
-				this.stage === 'PROD' ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
-		});
-
-		secondaryUserTable.addGlobalSecondaryIndex({
-			indexName: 'secondaryIdentityId-index',
-			partitionKey: { name: 'secondaryIdentityId', type: AttributeType.STRING },
-		});
-
-		secondaryUserTable.grantFullAccess(lambda);
 	}
 }
