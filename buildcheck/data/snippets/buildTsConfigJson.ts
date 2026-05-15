4ad3611fbde4dcc6dc96f0ca81787229787df3ff
John Duffell <john.duffell@guardian.co.uk>
2026-05-15 13:07:30 +0100

    don't allow code to depend on things outside of their declared dependencies

--- buildcheck/data/snippets/buildTsConfigJson.ts
+++ buildcheck/data/snippets/buildTsConfigJson.ts
@@ -1,8 +1,38 @@
-import type { ModuleDefinition } from '../build';
+import { recordFromEntries } from '../../src/util/dependencyMapper';
+import type { HandlerDefinition, ModuleDefinition } from '../build';
+
+export const collectAllDeps = (
+	pkg: ModuleDefinition,
+	visited: Set<string> = new Set(),
+): string[] =>
+	pkg.moduleDeps.flatMap((dep) => {
+		if (visited.has(dep.name)) {
+			return [];
+		}
+		visited.add(dep.name);
+		return [dep.name, ...collectAllDeps(dep, visited)];
+	});
 
 export function buildTsConfigJson(pkg: ModuleDefinition) {
+
+	const transitiveDeps = [...collectAllDeps(pkg), pkg.name];
+
 	return {
 		extends: '../../tsconfig.json',
+		compilerOptions: {
+			paths: {
+				'@modules/*': ['../../modules/*'],
+				...recordFromEntries(
+					transitiveDeps.map(
+						(moduleName) =>
+							[
+								`@modules/${moduleName}/*`,
+								[`../../modules/${moduleName}/src/*`],
+							] as const,
+					),
+				),
+			},
+		},
 		...pkg.tsConfigExtra,
 	};
 }
