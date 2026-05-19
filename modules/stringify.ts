24391caebc1fdeb725b86d734f9a2fb4afaa70cb
Rupert Bates <Rupert.Bates@guardian.co.uk>
2026-05-19 09:54:31 +0100

    Use type safe ok in discount api

--- modules/stringify.ts
+++ modules/stringify.ts
@@ -1,7 +1,9 @@
 import type { z } from 'zod';
 
 /**
- * This is a type safe version of stringify
+ * This is a type safe version of stringify, it ensures that we only include
+ * values which are specified in the schema in the resulting JSON, helping to
+ * avoid accidental leakage of sensitive data.
  * @param t
  * @param type
  */
