07c364453be0291ae2b49f694b56ed21b187093b
John Duffell <john.duffell@guardian.co.uk>
2026-05-27 18:20:06 +0100

    call the endpoint /me and add a static example response

--- handlers/user-subscriptions-api/test/hello.test.ts
+++ handlers/user-subscriptions-api/test/hello.test.ts
@@ -2,6 +2,10 @@
  * This is a unit test, it can be run by the `pnpm test` command, and will be run by the CI/CD pipeline
  *
  */
-test('my app', () => {
-	expect(1 + 1).toEqual(2);
+import { handleMeEndpoint } from '../src/meEndpoint';
+
+test('me static endpoint', async () => {
+	await expect(handleMeEndpoint().then((r) => r.statusCode)).resolves.toEqual(
+		200,
+	);
 });
