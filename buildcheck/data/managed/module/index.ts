4fc05fbc60f745648329d78056f22a54018e1b55
John Duffell <john.duffell@guardian.co.uk>
2026-06-03 23:02:05 +0100

    improve comments

--- buildcheck/data/managed/module/index.ts
+++ buildcheck/data/managed/module/index.ts
@@ -1,2 +1,15 @@
-// this is so the generated index knows what parameter the templates should take
+/*
+
+There are various committed ts files under `data/managed/module/templates/`
+
+They have a default export, if it's a function, it must take the correct type as
+its parameter.
+
+The script src/dynamic/generate-template-list.sh generates an index of the above
+files and writes it to _generated_tsIndex.ts in this folder.
+
+In order to get compile time safety, the generated file expects the correct
+type to be exported from this file as `TemplateParams`.
+
+*/
 export type { ModuleDefinition as TemplateParams } from '../../build';
