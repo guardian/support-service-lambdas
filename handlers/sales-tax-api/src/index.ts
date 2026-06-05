f151e9e822dec20b690bc1e4bc0d1f8c8c62668c
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-06-05 18:46:40 +0100

    feat: removed Handler

--- handlers/sales-tax-api/src/index.ts
+++ handlers/sales-tax-api/src/index.ts
@@ -1,14 +1,13 @@
 import { logger } from '@modules/logger/logger';
 import { Router } from '@modules/routing/router';
 import { withBodyParser } from '@modules/routing/withParsers';
-import type { Handler } from 'aws-lambda';
 import { helloRequestEndpoint, helloRequestSchema } from './helloEndpoint';
 import {
 	salesTaxRequestEndpoint,
 	salesTaxRequestSchema,
 } from './salesTaxEndpoint';
 
-export const handler: Handler = Router([
+export const handler = Router([
 	{
 		httpMethod: 'POST',
 		path: '/hello',
