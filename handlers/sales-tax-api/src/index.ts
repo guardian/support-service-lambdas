ff1fa5af4765f4472b947f7402d480927db397e2
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-06-05 12:19:14 +0100

    feat: add initial endpoint

--- handlers/sales-tax-api/src/index.ts
+++ handlers/sales-tax-api/src/index.ts
@@ -1,7 +1,12 @@
+import { logger } from '@modules/logger/logger';
 import { Router } from '@modules/routing/router';
 import { withBodyParser } from '@modules/routing/withParsers';
 import type { Handler } from 'aws-lambda';
 import { helloRequestEndpoint, helloRequestSchema } from './helloEndpoint';
+import {
+	salesTaxRequestEndpoint,
+	salesTaxRequestSchema,
+} from './salesTaxEndpoint';
 
 export const handler: Handler = Router([
 	{
@@ -9,6 +14,17 @@ export const handler: Handler = Router([
 		path: '/hello',
 		handler: withBodyParser(helloRequestSchema, async (event, path, body) =>
 			helloRequestEndpoint(body),
+		),
+	},
+	{
+		httpMethod: 'POST',
+		path: '/tax-rate',
+		handler: withBodyParser(
+			salesTaxRequestSchema,
+			async (event, path, body) => {
+				logger.log('Received POST /tax-rate request', body);
+				return salesTaxRequestEndpoint(body);
+			},
 		),
 	},
 ]);
---
+++ handlers/sales-tax-api/src/salesTaxEndpoint.ts
@@ -0,0 +1,33 @@
+import { logger } from '@modules/logger/logger';
+import { buildErrorResponse, ok } from '@modules/routing/apiGatewayResponses';
+import type { APIGatewayProxyResult } from 'aws-lambda';
+import { z } from 'zod';
+
+export const salesTaxRequestSchema = z.object({
+	productKey: z.string(),
+	country: z.string(),
+	state: z.string(),
+});
+export type SalesTaxRequest = z.infer<typeof salesTaxRequestSchema>;
+
+const salesTaxResponseSchema = z.object({
+	taxRate: z.number(),
+});
+
+export function salesTaxRequestEndpoint({
+	productKey,
+	country,
+	state,
+}: SalesTaxRequest): Promise<APIGatewayProxyResult> {
+	try {
+		logger.log('Checking sales tax for', {
+			productKey,
+			country,
+			state,
+		});
+		return Promise.resolve(ok({ taxRate: 0 }, salesTaxResponseSchema));
+	} catch (error) {
+		logger.error('Error fetching sales tax', error);
+		return Promise.resolve(buildErrorResponse(error));
+	}
+}
