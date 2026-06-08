a925b2ce897b12e3a663bd62d8cd544d0ece7a3e
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-06-08 09:46:53 +0100

    feat: hello endpoint removal

--- handlers/sales-tax-api/src/index.ts
+++ handlers/sales-tax-api/src/index.ts
@@ -1,20 +1,12 @@
 import { logger } from '@modules/logger/logger';
 import { Router } from '@modules/routing/router';
 import { withBodyParser } from '@modules/routing/withParsers';
-import { helloRequestEndpoint, helloRequestSchema } from './helloEndpoint';
 import {
 	salesTaxRequestEndpoint,
 	salesTaxRequestSchema,
 } from './salesTaxEndpoint';
 
 export const handler = Router([
-	{
-		httpMethod: 'POST',
-		path: '/hello',
-		handler: withBodyParser(helloRequestSchema, async (event, path, body) =>
-			helloRequestEndpoint(body),
-		),
-	},
 	{
 		httpMethod: 'POST',
 		path: '/tax-rate',
