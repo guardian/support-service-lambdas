5c70a99bb34a11c78ab7b0c4d6ebf8364f5b4c68
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-06-10 19:28:35 +0100

    fix: index test correction

--- handlers/sales-tax-api/test/index.test.ts
+++ handlers/sales-tax-api/test/index.test.ts
@@ -1,7 +1,25 @@
+import type { IsoCountry } from '@modules/internationalisation/country';
 import type { APIGatewayProxyEvent } from 'aws-lambda';
 import { handler } from '../src/index';
-import { countryStates } from '../src/salesTaxEndpoint';
 import type { SalesTaxResponse } from '../src/schemas';
+
+const countryStates: Partial<Record<IsoCountry, Record<string, number>>> = {
+	CA: {
+		AB: 0.05,
+		BC: 0.12,
+		MB: 0.12,
+		NB: 0.15,
+		NL: 0.15,
+		NT: 0.15,
+		NS: 0.15,
+		NU: 0.05,
+		ON: 0.13,
+		PE: 0.15,
+		QC: 0.1498,
+		SK: 0.11,
+		YT: 0.05,
+	},
+};
 
 describe('SalesTax API', () => {
 	const baseEvent: Partial<APIGatewayProxyEvent> = {
