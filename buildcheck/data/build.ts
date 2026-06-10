27a536ce2e572fc5b74188f4e29c582faffb84fb
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2026-06-10 11:55:14 +0100

    Sales Tax Api : Setup (#3599)
    * feat: initial build
    
    * feat: sales tax description updated
    
    * fix: sales tax tests updated
    
    * fix: sales tax api tsconfig appended for routing
    
    * fix: sales tax api tsconfig tab format correction
    
    * fix: routing added to files tsconfig.json, package.json and build.ts
    
    * fix: install rerun, pnpm-lock.yaml updated
    
    * feat: 404 error added to openapi.yaml
    
    * fix: merge correction
    
    * fix: minimal ci-typescript corrections
    
    * fix: handler name configuration, add sales-tax-api
    
    * fix: format reset
    
    * fix: handler name configuration updated (pnpm snapshot:update) and space re-added to file end
    
    * Update cdk/lib/sales-tax-api.ts
    
    Co-authored-by: Rupert Bates <Rupert.Bates@guardian.co.uk>
    
    * fix: test snapshot updated
    
    ---------
    
    Co-authored-by: Rupert Bates <Rupert.Bates@guardian.co.uk>

--- buildcheck/data/build.ts
+++ buildcheck/data/build.ts
@@ -804,6 +804,23 @@ const userSubscriptionsApi: HandlerDefinition = {
 	moduleDependencies: [moduleRouting],
 };
 
+const salesTaxApi: HandlerDefinition = {
+	name: 'sales-tax-api',
+	dependencies: {
+		...dep.zod,
+	},
+	devDependencies: {
+		...devDeps['@types/aws-lambda'],
+
+		...devDeps['@redocly/cli'],
+	},
+	moduleDependencies: [moduleLogger, moduleRouting],
+	extraScripts: {
+		...openApiScripts,
+		package: `pnpm type-check && pnpm lint && pnpm openapi:lint && pnpm check-formatting && pnpm test && pnpm build && cd target && zip -qr sales-tax-api.zip ./*.js.map ./*.js`,
+	},
+};
+
 // MARKER new-lambda: buildcheck-const
 
 export const build: BuildDefinition = {
@@ -835,6 +852,7 @@ export const build: BuildDefinition = {
 		observerBenefitsApi,
 		contributionsOnlyCountriesApi,
 		userSubscriptionsApi,
+		salesTaxApi,
 		// MARKER new-lambda: buildcheck-reference
 	],
 
