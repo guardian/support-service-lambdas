68ef7a09153ffd0b10b2a037a1361e877f2dbea2
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-06-12 10:16:53 +0100

    feat: remove unused salesTaxEndpoint

--- handlers/sales-tax-api/src/index.ts
+++ handlers/sales-tax-api/src/index.ts
@@ -1,22 +1,10 @@
 import { logger } from '@modules/logger/logger';
 import { Router } from '@modules/routing/router';
 import { withBodyParser } from '@modules/routing/withParsers';
-import { salesTaxRequestEndpoint } from './salesTaxEndpoint';
-import { salesTaxRequestSchema, taxRatesRequestSchema } from './schemas';
+import { taxRatesRequestSchema } from './schemas';
 import { taxRatesRequestEndpoint } from './taxRatesEndpoint';
 
 export const handler = Router([
-	{
-		httpMethod: 'POST',
-		path: '/tax-rate',
-		handler: withBodyParser(
-			salesTaxRequestSchema,
-			async (event, path, body) => {
-				logger.log('Received POST /tax-rate request', body);
-				return salesTaxRequestEndpoint(body);
-			},
-		),
-	},
 	{
 		httpMethod: 'POST',
 		path: '/tax-rates',
--- handlers/sales-tax-api/test/index.test.ts
+++ handlers/sales-tax-api/test/index.test.ts
@@ -1,106 +1,14 @@
 import type { APIGatewayProxyEvent } from 'aws-lambda';
 import { handler } from '../src/index';
-import { countryStates } from '../src/salesTaxEndpoint';
-import type { SalesTaxResponse, TaxRatesResponse } from '../src/schemas';
+import type { TaxRatesResponse } from '../src/schemas';
 import { cadStates } from '../src/taxRatesEndpoint';
 
 describe('SalesTax API', () => {
-	const baseSalesTaxEvent: Partial<APIGatewayProxyEvent> = {
-		path: '/tax-rate',
-		httpMethod: 'POST',
-		headers: {},
-	};
 	const baseTaxRatesEvent: Partial<APIGatewayProxyEvent> = {
 		path: '/tax-rates',
 		httpMethod: 'POST',
 		headers: {},
 	};
-
-	describe('routing and body parsing salesTaxEndpoint', () => {
-		it('returns 400 for an empty body', async () => {
-			const response = await handler(
-				baseSalesTaxEvent as unknown as APIGatewayProxyEvent,
-			);
-
-			expect(response.statusCode).toEqual(400);
-		});
-		it('returns 400 when body not valid JSON', async () => {
-			const requestEvent = {
-				...baseSalesTaxEvent,
-				body: 'inValidJSON',
-			} as unknown as APIGatewayProxyEvent;
-
-			const response = await handler(requestEvent);
-			expect(response.statusCode).toEqual(400);
-		});
-		it('returns 400 when body is missing required fields', async () => {
-			const requestEvent = {
-				...baseSalesTaxEvent,
-				body: JSON.stringify({
-					invalidKey: 'X',
-				}),
-			} as unknown as APIGatewayProxyEvent;
-
-			const response = await handler(requestEvent);
-			expect(response.statusCode).toEqual(400);
-		});
-		it('returns 400 for an invalid productKey', async () => {
-			const requestEvent = {
-				...baseSalesTaxEvent,
-				body: JSON.stringify({
-					productKey: 'InvalidProductKey',
-				}),
-			} as unknown as APIGatewayProxyEvent;
-
-			const response = await handler(requestEvent);
-			expect(response.statusCode).toEqual(400);
-		});
-		it('returns 400 for an invalid country', async () => {
-			const requestEvent = {
-				...baseSalesTaxEvent,
-				body: JSON.stringify({
-					country: 'XX',
-				}),
-			} as unknown as APIGatewayProxyEvent;
-
-			const response = await handler(requestEvent);
-			expect(response.statusCode).toEqual(400);
-		});
-		it('returns 400 for an invalid state', async () => {
-			const requestEvent = {
-				...baseSalesTaxEvent,
-				body: JSON.stringify({
-					state: 'XX',
-				}),
-			} as unknown as APIGatewayProxyEvent;
-
-			const response = await handler(requestEvent);
-			expect(response.statusCode).toEqual(400);
-		});
-	});
-
-	describe('salesTaxEndpoint', () => {
-		it('returns 200 for a valid country, state, product', async () => {
-			const country = 'CA';
-			const province = 'ON';
-			const requestEvent = {
-				...baseSalesTaxEvent,
-				body: JSON.stringify({
-					productKey: 'SupporterPlus',
-					country: country,
-					state: province,
-				}),
-			} as unknown as APIGatewayProxyEvent;
-
-			const response = await handler(requestEvent);
-			expect(response.statusCode).toEqual(200);
-
-			const salesTaxResponse = JSON.parse(response.body) as SalesTaxResponse;
-			expect(salesTaxResponse.taxRate).toEqual(
-				countryStates[country]?.[province],
-			);
-		});
-	});
 
 	describe('routing and body parsing taxRatesEndpoint', () => {
 		it('returns 400 for an empty body', async () => {
