a3e09f95b06cd9f943d36bc8d4b9cc773f08233e
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-06-14 18:11:49 +0100

    wip: integration test zuoraTaxCodesEndpoint added

--- handlers/sales-tax-api/test/integration.test.ts
+++ handlers/sales-tax-api/test/integration.test.ts
@@ -7,9 +7,16 @@
  */
 
 import type { IsoCountry } from '@modules/internationalisation/country';
+import {
+	type ZuoraTaxCode,
+	zuoraTaxCodeSchema,
+} from '@modules/zuora/types/objects/tax';
 import { ZuoraClient } from '@modules/zuora/zuoraClient';
 import { taxRatesResponseSchema } from '../src/schemas';
-import { taxRatesEndpoint } from '../src/taxRatesEndpoint';
+import {
+	taxRatesEndpoint,
+	zuoraTaxCodesEndpoint,
+} from '../src/taxRatesEndpoint';
 
 const countryStates: Partial<Record<IsoCountry, Record<string, number>>> = {
 	CA: {
@@ -28,6 +35,13 @@ const countryStates: Partial<Record<IsoCountry, Record<string, number>>> = {
 		YT: 0.05,
 	},
 };
+const zuoraTaxCodeSupporterPlus: ZuoraTaxCode = {
+	id: '8ad0887181de06d70181de659fb63b57',
+	taxEngineId: '2c92c0f94568f996014570f746f75c52',
+	active: true,
+	name: 'Supporter Plus',
+	description: '',
+};
 
 describe('SalesTax API Integration', () => {
 	const country = 'CA';
@@ -45,4 +59,22 @@ describe('SalesTax API Integration', () => {
 		const body = taxRatesResponseSchema.parse(JSON.parse(result.body));
 		expect(body).toEqual(countryStates[country]);
 	});
+	test('we can retreive tax codes from Zuora', async () => {
+		const zuoraClient = await ZuoraClient.create(stage);
+
+		const result = await zuoraTaxCodesEndpoint(zuoraClient);
+
+		expect(result.statusCode).toEqual(200);
+		const body = zuoraTaxCodeSchema.parse(JSON.parse(result.body));
+		expect(body.taxCodes).toContainEqual(zuoraTaxCodeSupporterPlus);
+	});
+	// test('we can retreive SupporterPlus tax rates from Zuora', async () => {
+	// 	const zuoraClient = await ZuoraClient.create(stage);
+
+	// 	const result = await zuoraTaxRatesEndpoint(zuoraClient, {
+	// 		id: '8ad0887181de06d70181de659fb63b57',
+	// 	});
+
+	// 	expect(result.statusCode).toEqual(200);
+	// });
 });
