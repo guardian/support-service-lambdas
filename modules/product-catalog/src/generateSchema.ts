46b0629835402e994d9416156cb48630c7dd3089
Tom Wey <tom.wey@theguardian.com>
2026-06-15 13:45:37 +0100

    Make this optional for now to avoid deployment race condition

--- modules/product-catalog/src/generateSchema.ts
+++ modules/product-catalog/src/generateSchema.ts
@@ -119,7 +119,7 @@ const generateProductRatePlanSchema = (
 		${getBillingPeriodForRatePlan(productRatePlan)}
 		termType: termTypeSchema,
 		termLengthInMonths: z.number(),
-		taxMode: z.string().nullable(),
+		taxMode: z.string().nullable().optional(),
 	})`;
 };
 
