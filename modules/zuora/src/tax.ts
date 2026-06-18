9dd5e25e433ae73d7b212b6b50c1514b4fc37df0
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-06-18 15:05:40 +0100

    review: removed generic calls to zuora tax api's

--- modules/zuora/src/tax.ts
+++ modules/zuora/src/tax.ts
@@ -1,4 +1,3 @@
-import type { z } from 'zod';
 import {
 	zuoraTaxCodeSchema,
 	zuoraTaxPeriodsSchema,
@@ -6,30 +5,20 @@ import {
 } from './types/objects/tax';
 import type { ZuoraClient } from './zuoraClient';
 
-export const getZuoraTaxCodes = async <
-	T extends z.ZodType = typeof zuoraTaxCodeSchema,
->(
-	zuoraClient: ZuoraClient,
-): Promise<z.infer<T>> => {
+export const getZuoraTaxCodes = async (zuoraClient: ZuoraClient) => {
 	const path = `settings/tax-codes`;
 	return zuoraClient.get(path, zuoraTaxCodeSchema);
 };
 
-export const getZuoraTaxPeriods = async <
-	T extends z.ZodType = typeof zuoraTaxPeriodsSchema,
->(
-	zuoraClient: ZuoraClient,
-): Promise<z.infer<T>> => {
+export const getZuoraTaxPeriods = async (zuoraClient: ZuoraClient) => {
 	const path = `settings/tax-rate-periods`;
 	return zuoraClient.get(path, zuoraTaxPeriodsSchema);
 };
 
-export const getZuoraTaxRates = async <
-	T extends z.ZodType = typeof zuoraTaxRateSchema,
->(
+export const getZuoraTaxRates = async (
 	zuoraClient: ZuoraClient,
 	id: string,
-): Promise<z.infer<T>> => {
+) => {
 	const path = `settings/tax-rate-periods/${id}/tax-rates?page=1&size=500`;
 	return zuoraClient.get(path, zuoraTaxRateSchema);
 };
