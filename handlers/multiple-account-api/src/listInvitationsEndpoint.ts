3e5789770c0bba5ff83d5bde232a8788f0713299
jorgeCosta <jorge.costa@theguardian.com>
2026-06-18 11:51:01 +0100

    List invitations and Secondary users (#3622)
    * new endpoint for listing invitations and secondary users associated with a subscription
    
    * pluralize and use dash case naming convention
    
    * get subscription name from subscription object
    
    * update routes paths to embrace more restfull standards
    
    * update readme with new endpoints patterns
    
    ---------
    
    Co-authored-by: Jorge Costa <jorge.costa@maloky.com>

---
+++ handlers/multiple-account-api/src/listInvitationsEndpoint.ts
@@ -0,0 +1,22 @@
+import { logger } from '@modules/logger/logger';
+import { buildErrorResponse, ok } from '@modules/routing/apiGatewayResponses';
+import { z } from 'zod';
+import type { InvitationRepository } from './invitationRepository';
+
+export const listInvitationsPathSchema = z.object({
+	subscriptionName: z.string(),
+});
+
+export type ListInvitationsPath = z.infer<typeof listInvitationsPathSchema>;
+
+export const listInvitationsEndpoint =
+	(invitationRepository: InvitationRepository) =>
+	async ({ subscriptionName }: ListInvitationsPath) => {
+		try {
+			logger.mutableAddContext(subscriptionName);
+			const invitations = await invitationRepository.list(subscriptionName);
+			return ok({ invitations });
+		} catch (error) {
+			return buildErrorResponse(error);
+		}
+	};
