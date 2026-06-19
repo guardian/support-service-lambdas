15878e8ec787b390b88790c733bf173139393234
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-06-19 08:48:19 +0100

    review: refactored with missing states validation

--- handlers/sales-tax-api/src/taxRatesEndpoint.ts
+++ handlers/sales-tax-api/src/taxRatesEndpoint.ts
@@ -134,6 +134,13 @@ function extractCadZuoraTaxRates(
 function createCadStateTaxRates(
 	cadZuoraTaxRates: ZuoraTaxRate[],
 ): TaxRatesResponse {
+	const cadStatesMissing = checkForMissingCadStates(cadZuoraTaxRates);
+	if (cadStatesMissing.length > 0) {
+		throw new ValidationError(
+			`missing the following CA provinces: ${cadStatesMissing.join(', ')}`,
+		);
+	}
+
 	const cadStateTaxRates: TaxRatesResponse = { ...caStatesDefault };
 	Object.keys(cadStateTaxRates).forEach((key) => {
 		if (isTaxRateKey(key)) {
@@ -144,6 +151,12 @@ function createCadStateTaxRates(
 		}
 	});
 	return cadStateTaxRates;
+}
+function checkForMissingCadStates(cadZuoraTaxRates: ZuoraTaxRate[]): string[] {
+	const keys = taxRatesResponseSchema.keyof().options;
+	return keys.filter(
+		(key) => !cadZuoraTaxRates.some((rate) => caStates[key] === rate.state),
+	);
 }
 function isTaxRateKey(key: string): key is keyof TaxRatesResponse {
 	return key in caStatesDefault;
