c5c5fe566c4ff58978464f5ce4f90ef90e6d6d19
Tom Wey <tom.wey@theguardian.com>
2026-06-24 08:52:27 +0100

    Remove unused consts

--- handlers/sales-tax-api/src/schemas.ts
+++ handlers/sales-tax-api/src/schemas.ts
@@ -2,11 +2,6 @@ import { isoCountrySchema } from '@modules/internationalisation/schemas';
 import { productKeySchema } from '@modules/product-catalog/productCatalogSchema';
 import { z } from 'zod';
 
-export const zuoraGetRatesPathSchema = z.object({
-	id: z.string(),
-});
-export type ZuoraGetRatesPath = z.infer<typeof zuoraGetRatesPathSchema>;
-
 export const taxRatesRequestSchema = z.object({
 	productKey: productKeySchema,
 	country: isoCountrySchema,
