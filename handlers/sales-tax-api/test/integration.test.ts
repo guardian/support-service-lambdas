553f253a61b889347e71273fbf113b49ef701428
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2026-06-30 10:46:06 +0100

    Sales Tax Api: Retrieve CAD tax rates from Zuora (#3616)
    Modify the existing /tax-rates endpoint to return tax rates fetched from Zuora
    instead of hard coded values.
    
    As before, the only country supported is Canada and the only products are
    SupporterPlus and DigitalSubscription.
    
    Co-authored-by: Tom Wey <tom.wey@theguardian.com>

---
+++ handlers/sales-tax-api/test/integration.test.ts
@@ -0,0 +1,28 @@
+/**
+ * This is an integration test, the `@group integration` tag ensures that it will only be run by the `pnpm it-test`
+ * command and will not be run during continuous integration.
+ * This makes it useful for testing things that require credentials which are available locally but not on the CI server.
+ *
+ * @group integration
+ */
+
+import { ZuoraClient } from '@modules/zuora/zuoraClient';
+import { taxRatesResponseSchema } from '../src/schemas';
+import { taxRatesEndpoint } from '../src/taxRatesEndpoint';
+import { countryStates } from './fixtures';
+
+describe('SalesTax API Integration', () => {
+	const country = 'CA';
+	test('we can retrieve SupporterPlus Canadian tax rates', async () => {
+		const zuoraClient = await ZuoraClient.create('CODE');
+
+		const result = await taxRatesEndpoint(zuoraClient, {
+			productKey: 'SupporterPlus',
+			country,
+		});
+
+		expect(result.statusCode).toEqual(200);
+		const body = taxRatesResponseSchema.parse(JSON.parse(result.body));
+		expect(body).toEqual(countryStates[country]);
+	});
+});
