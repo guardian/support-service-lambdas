731ee2fa43686bcf0ff8fe12ecef74247c7b55be
Rupert Bates <Rupert.Bates@guardian.co.uk>
2026-07-08 16:08:34 +0100

    Rename schemas

--- modules/internationalisation/src/schemas.ts
+++ modules/internationalisation/src/schemas.ts
@@ -4,5 +4,5 @@ import { SupportRegionId } from '@modules/internationalisation/countryGroup';
 import { CurrencyValues } from '@modules/internationalisation/currency';
 
 export const isoCurrencySchema = z.enum(CurrencyValues);
-export const isoCountrySchema = z.enum(countryCodes);
+export const countryCodeSchema = z.enum(countryCodes);
 export const supportRegionSchema = z.nativeEnum(SupportRegionId);
