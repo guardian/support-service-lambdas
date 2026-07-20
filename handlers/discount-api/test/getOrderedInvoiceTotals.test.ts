677e9d1abdd3e229bb099d1dc4a4be5003dc2d00
Tom Wey <tom.wey@theguardian.com>
2026-07-20 10:13:42 +0100

    Fix some tests

--- handlers/discount-api/test/getOrderedInvoiceTotals.test.ts
+++ handlers/discount-api/test/getOrderedInvoiceTotals.test.ts
@@ -2,50 +2,150 @@ import { getOrderedInvoiceTotals } from '@modules/zuora/billingPreview';
 
 test('getOrderedInvoiceTotals doesnt affect if theyre already in order', () => {
 	const items = [
-		{ date: new Date('2020-01-01'), amount: 0 },
-		{ date: new Date('2020-02-01'), amount: 10 },
-		{ date: new Date('2020-03-01'), amount: 10 },
+		{
+			date: new Date('2020-01-01'),
+			amount: 0,
+			amountWithoutTax: 0,
+			taxAmount: 0,
+		},
+		{
+			date: new Date('2020-02-01'),
+			amount: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
+		{
+			date: new Date('2020-03-01'),
+			amount: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
 	];
 
 	const actual = getOrderedInvoiceTotals(items);
 
 	expect(actual).toEqual([
-		{ date: new Date('2020-01-01'), total: 0 },
-		{ date: new Date('2020-02-01'), total: 10 },
-		{ date: new Date('2020-03-01'), total: 10 },
+		{
+			date: new Date('2020-01-01'),
+			total: 0,
+			amountWithoutTax: 0,
+			taxAmount: 0,
+		},
+		{
+			date: new Date('2020-02-01'),
+			total: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
+		{
+			date: new Date('2020-03-01'),
+			total: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
 	]);
 });
 
 test('getOrderedInvoiceTotals reorders them if theyre NOT in order', () => {
 	const items = [
-		{ date: new Date('2020-02-01'), amount: 10 },
-		{ date: new Date('2020-01-01'), amount: 0 },
-		{ date: new Date('2020-03-01'), amount: 10 },
+		{
+			date: new Date('2020-02-01'),
+			amount: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
+		{
+			date: new Date('2020-01-01'),
+			amount: 0,
+			amountWithoutTax: 0,
+			taxAmount: 0,
+		},
+		{
+			date: new Date('2020-03-01'),
+			amount: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
 	];
 
 	const actual = getOrderedInvoiceTotals(items);
 
 	expect(actual).toEqual([
-		{ date: new Date('2020-01-01'), total: 0 },
-		{ date: new Date('2020-02-01'), total: 10 },
-		{ date: new Date('2020-03-01'), total: 10 },
+		{
+			date: new Date('2020-01-01'),
+			total: 0,
+			amountWithoutTax: 0,
+			taxAmount: 0,
+		},
+		{
+			date: new Date('2020-02-01'),
+			total: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
+		{
+			date: new Date('2020-03-01'),
+			total: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
 	]);
 });
 
 test('getOrderedInvoiceTotals adds them up and reorders them', () => {
 	const items = [
-		{ date: new Date('2020-01-01'), amount: 1 },
-		{ date: new Date('2020-02-01'), amount: 10 },
-		{ date: new Date('2020-03-01'), amount: 100 },
-		{ date: new Date('2020-02-01'), amount: 20 },
-		{ date: new Date('2020-01-01'), amount: 2 },
+		{
+			date: new Date('2020-01-01'),
+			amount: 1,
+			amountWithoutTax: 1,
+			taxAmount: 0,
+		},
+		{
+			date: new Date('2020-02-01'),
+			amount: 10,
+			amountWithoutTax: 8,
+			taxAmount: 2,
+		},
+		{
+			date: new Date('2020-03-01'),
+			amount: 100,
+			amountWithoutTax: 80,
+			taxAmount: 20,
+		},
+		{
+			date: new Date('2020-02-01'),
+			amount: 20,
+			amountWithoutTax: 16,
+			taxAmount: 4,
+		},
+		{
+			date: new Date('2020-01-01'),
+			amount: 2,
+			amountWithoutTax: 2,
+			taxAmount: 0,
+		},
 	];
 
 	const actual = getOrderedInvoiceTotals(items);
 
 	expect(actual).toEqual([
-		{ date: new Date('2020-01-01'), total: 3 },
-		{ date: new Date('2020-02-01'), total: 30 },
-		{ date: new Date('2020-03-01'), total: 100 },
+		{
+			date: new Date('2020-01-01'),
+			total: 3,
+			amountWithoutTax: 3,
+			taxAmount: 0,
+		},
+		{
+			date: new Date('2020-02-01'),
+			total: 30,
+			amountWithoutTax: 24,
+			taxAmount: 6,
+		},
+		{
+			date: new Date('2020-03-01'),
+			total: 100,
+			amountWithoutTax: 80,
+			taxAmount: 20,
+		},
 	]);
 });
