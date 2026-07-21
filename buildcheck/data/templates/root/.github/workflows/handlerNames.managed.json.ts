2e80c432d529ee1e077f51b57a0179e7d74a6192
John Duffell <john.duffell@guardian.co.uk>
2026-07-21 15:56:02 +0100

    add build for iam-policies

--- buildcheck/data/templates/root/.github/workflows/handlerNames.managed.json.ts
+++ buildcheck/data/templates/root/.github/workflows/handlerNames.managed.json.ts
@@ -2,9 +2,12 @@ import type { BuildDefinition } from '../../../../build';
 import { notice } from '../../../../snippets/notices';
 
 // not in buildcheck but we do want to build it
-const cdkOnlyProject = 'salesforce-event-bus';
+const cdkOnlyProjects = ['iam-policies', 'salesforce-event-bus'];
 
 export default (build: BuildDefinition) => ({
 	NOTICE: notice(__filename),
-	subproject: [...build.handlers.map((def) => def.name), cdkOnlyProject].sort(),
+	subproject: [
+		...build.handlers.map((def) => def.name),
+		...cdkOnlyProjects,
+	].sort(),
 });
