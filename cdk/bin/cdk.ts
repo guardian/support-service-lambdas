eff30c15775319d9234b6434d950ebc6d425bc7f
Enes Aydogan <enes.aydogan@guardian.co.uk>
2026-08-19 10:11:08 +0100

    DeliveryProblemCreditProcessor migration from Cloudformation to CDK (#3753)

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -12,6 +12,7 @@ import {
 	membershipHostedZoneId,
 } from '../lib/constants';
 import { ContributionsOnlyCountriesApi } from '../lib/contributions-only-countries-api';
+import { DeliveryProblemCreditProcessor } from '../lib/delivery-problem-credit-processor';
 import { DiscountApi } from '../lib/discount-api';
 import { DiscountExpiryNotifier } from '../lib/discount-expiry-notifier';
 import { GenerateProductCatalog } from '../lib/generate-product-catalog';
@@ -171,6 +172,7 @@ const stacks: Array<new (app: App, stage: SrStageNames) => unknown> = [
 	BrazeAcquisitionEventsSync,
 	SfMoveSubscriptionsApi,
 	SfEmailsToS3Exporter,
+	DeliveryProblemCreditProcessor,
 	IamPolicies,
 	ScrubNonTokenisedPaymentMethods,
 	// MARKER new-lambda: cdk-bin
---
+++ cdk/lib/delivery-problem-credit-processor.ts
@@ -0,0 +1,160 @@
+import type { App } from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import {
+	ComparisonOperator,
+	TreatMissingData,
+} from 'aws-cdk-lib/aws-cloudwatch';
+import { RuleTargetInput, Schedule } from 'aws-cdk-lib/aws-events';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Architecture, CfnPermission, Runtime } from 'aws-cdk-lib/aws-lambda';
+import type { IConstruct } from 'constructs';
+import { SrLambdaErrorAlarm } from './cdk/SrLambdaErrorAlarm';
+import { SrScheduledLambda } from './cdk/SrScheduledLambda';
+import type { SrStageNames } from './cdk/SrStack';
+import { SrStack } from './cdk/SrStack';
+
+export class DeliveryProblemCreditProcessor extends SrStack {
+	constructor(scope: App, stage: SrStageNames) {
+		super(scope, {
+			stack: 'membership',
+			stage,
+			app: 'delivery-problem-credit-processor',
+		});
+		const isProd = stage === 'PROD';
+		const scheduleName = `delivery-problem-credit-processor-schedule-${stage.toLowerCase()}`;
+		const scheduleRules = isProd
+			? [
+					{
+						schedule: Schedule.cron({ minute: '0/20' }),
+						input: RuleTargetInput.fromObject(null),
+						name: scheduleName,
+						description:
+							'Trigger processing of delivery-problem credits every 20 mins',
+					},
+				]
+			: [];
+
+		const lambda = new SrScheduledLambda(this, 'Lambda', {
+			rules: scheduleRules,
+			monitoring: {
+				noMonitoring: true, // Custom alarm
+			},
+			lambdaOverrides: {
+				functionName: `delivery-problem-credit-processor-${stage}`,
+				runtime: Runtime.JAVA_21,
+				architecture: Architecture.ARM_64,
+				fileName: 'delivery-problem-credit-processor.jar',
+				handler: 'com.gu.deliveryproblemcreditprocessor.Handler::handle',
+				memorySize: 1024,
+				timeout: Duration.minutes(15),
+				retryAttempts: 0,
+				description:
+					'Applies credit amendments for delivery problems. Source - https://github.com/guardian/support-service-lambdas/tree/main/handlers/delivery-problem-credit-processor',
+				environment: {
+					Stage: stage,
+				},
+			},
+		});
+
+		const zuoraRestS3Statement = new PolicyStatement({
+			effect: Effect.ALLOW,
+			actions: ['s3:GetObject'],
+			resources: [
+				`arn:aws:s3:::gu-reader-revenue-private/membership/support-service-lambdas/${stage}/zuoraRest-${stage}*.json`,
+			],
+		});
+		const sfAuthS3Statement = new PolicyStatement({
+			effect: Effect.ALLOW,
+			actions: ['s3:GetObject'],
+			resources: [
+				`arn:aws:s3:::gu-reader-revenue-private/membership/support-service-lambdas/${stage}/sfAuth-${stage}*.json`,
+			],
+		});
+
+		lambda.addToRolePolicy(zuoraRestS3Statement);
+		lambda.addToRolePolicy(sfAuthS3Statement);
+
+		let failureAlarm: SrLambdaErrorAlarm | undefined;
+		if (isProd) {
+			failureAlarm = new SrLambdaErrorAlarm(this, 'FailureAlarm', {
+				lambdaFunctionName: lambda.functionName,
+				alarmName:
+					'URGENT 9-5 - PROD: Failed to process delivery-problem credits',
+				errorImpact:
+					'IMPACT: If this goes unaddressed at least one subscription that was supposed to be suspended will be fulfilled. Until we document how to deal with likely problems please alert the SX team. For general advice, see https://docs.google.com/document/d/1_3El3cly9d7u_jPgTcRjLxmdG2e919zCLvmcFCLOYAk',
+				evaluationPeriods: 65,
+				datapointsToAlarm: 3,
+				threshold: 1,
+				comparisonOperator:
+					ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+				treatMissingData: TreatMissingData.NOT_BREACHING,
+			});
+		}
+
+		const resourcesKeepingExistingLogicalIds: Array<{
+			construct: IConstruct;
+			forcedLogicalId: string;
+			reason: string;
+		}> = [
+			{
+				construct: lambda,
+				forcedLogicalId: 'DeliveryProblemCreditProcessor',
+				reason:
+					'Keep resource names consistent with the original cfn template.',
+			},
+			{
+				construct: lambda.node.findChild('EventInvokeConfig'),
+				forcedLogicalId: 'DeliveryProblemCreditProcessorLambdaInvokeConfig',
+				reason:
+					'Keep resource names consistent with the original cfn template.',
+			},
+			{
+				construct: lambda.node.findChild('ServiceRole'),
+				forcedLogicalId: 'DeliveryProblemCreditProcessorRole',
+				reason:
+					'Keep resource names consistent with the original cfn template.',
+			},
+			{
+				construct: lambda.node
+					.findChild('ServiceRole')
+					.node.findChild('DefaultPolicy'),
+				forcedLogicalId: 'DeliveryProblemCreditProcessorPolicy',
+				reason:
+					'Keep resource names consistent with the original cfn template.',
+			},
+			...(failureAlarm
+				? [
+						{
+							construct: failureAlarm,
+							forcedLogicalId: 'DeliveryProblemCreditProcessorFailureAlarm',
+							reason:
+								'Keep resource names consistent with the original cfn template.',
+						},
+						{
+							construct: lambda.node.findChild('Rule0'),
+							forcedLogicalId: 'DeliveryProblemCreditProcessorScheduleRule',
+							reason:
+								'Keep resource names consistent with the original cfn template.',
+						},
+					]
+				: []),
+		];
+
+		resourcesKeepingExistingLogicalIds.forEach(
+			({ construct, forcedLogicalId, reason }) => {
+				this.overrideLogicalId(construct, {
+					logicalId: forcedLogicalId,
+					reason,
+				});
+			},
+		);
+
+		lambda.node.findAll().forEach((child) => {
+			if (child instanceof CfnPermission) {
+				child.overrideLogicalId(
+					'DeliveryProblemCreditProcessorLambdaInvokePermission',
+				);
+			}
+		});
+	}
+}
