071061d4803c896be146da79dcc201a2780f14c7
John Duffell <john.duffell@guardian.co.uk>
2026-09-18 10:43:50 +0100

    use corrected supporter plus bands and add extra check

--- handlers/update-supporter-plus-amount/src/supporterPlusAmountBands.ts
+++ handlers/update-supporter-plus-amount/src/supporterPlusAmountBands.ts
@@ -6,29 +6,29 @@ export const supporterPlusAmountBands: Record<
 	Record<ProductBillingPeriod<'SupporterPlus'>, { min: number; max: number }>
 > = {
 	GBP: {
-		Month: { min: 10, max: 166 },
-		Annual: { min: 95, max: 2000 },
+		Month: { min: 14, max: 166 },
+		Annual: { min: 140, max: 2000 },
 	},
 
 	AUD: {
-		Month: { min: 17, max: 200 },
-		Annual: { min: 160, max: 2400 },
+		Month: { min: 25, max: 200 },
+		Annual: { min: 250, max: 2000 },
 	},
 
 	USD: {
-		Month: { min: 13, max: 800 },
-		Annual: { min: 120, max: 10000 },
+		Month: { min: 18, max: 800 },
+		Annual: { min: 180, max: 10000 },
 	},
 	NZD: {
-		Month: { min: 17, max: 200 },
-		Annual: { min: 160, max: 2400 },
+		Month: { min: 25, max: 200 },
+		Annual: { min: 250, max: 2000 },
 	},
 	CAD: {
-		Month: { min: 13, max: 166 },
-		Annual: { min: 120, max: 2000 },
+		Month: { min: 18, max: 166 },
+		Annual: { min: 180, max: 2000 },
 	},
 	EUR: {
-		Month: { min: 10, max: 166 },
-		Annual: { min: 95, max: 2000 },
+		Month: { min: 14, max: 166 },
+		Annual: { min: 140, max: 2000 },
 	},
 } as const;
